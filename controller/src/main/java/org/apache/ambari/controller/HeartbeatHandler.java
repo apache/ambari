e419ef96ff73dda9edf0b804c4e9ca910d917a86
Devaraj Das <ddas@apache.org>
2011-10-14 01:35:37 +0000

    AMBARI-70. Fixed a bug and renamed a method
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/trunk@1183193 13f79535-47bb-0310-9956-ffa450edef68

--- controller/src/main/java/org/apache/ambari/controller/HeartbeatHandler.java
+++ controller/src/main/java/org/apache/ambari/controller/HeartbeatHandler.java
@@ -100,11 +100,13 @@ public class HeartbeatHandler {
           .getStateMachineClusterInstance(desiredClusterId);
       
       //get the list of uninstall actions
-      //create a map from component/role to 'started' for easy lookup later
+      //also, create a map from component/role to 'started' & 'installed' 
+      //for easy lookup later
       allActions = 
           inspectAgentState(heartbeat, cluster, componentStates, clusterFsm);
 
-      checkActionResults(cluster, clusterFsm, heartbeat, allActions);
+      //check/create the special actions (like safemode check)
+      checkAndCreateActions(cluster, clusterFsm, heartbeat, allActions);
       
       //Install software for roles like gateways (note that client role 
       //start/stops are not tracked through FSM).
@@ -246,12 +248,9 @@ public class HeartbeatHandler {
     return id;
   }
   
-  private void checkActionResults(Cluster cluster,
+  private void checkAndCreateActions(Cluster cluster,
       ClusterFSM clusterFsm, HeartBeat heartbeat, List<Action> allActions)
           throws IOException {
-    //this method should check things like number of retries (based on 
-    //action-IDs) etc. this method should note issues like too many failures 
-    //starting a role, etc.
     for (ServiceFSM service : clusterFsm.getServices()) {
       //see whether the service is in the STARTED state, and if so,
       //check whether there is any action-result that indicates success
@@ -307,7 +306,7 @@ public class HeartbeatHandler {
         for (String service : services) {
           ComponentPlugin plugin = 
               cluster.getComponentDefinition(service);
-          if (components.isInstalled(service, "CLIENT")) {
+          if (!components.isInstalled(service, "CLIENT")) {
             Action action = plugin.install(cluster.getName(), "CLIENT"); //TODO:this needs to be resolved
             addAction(action,actions);
             action = plugin.configure(cluster.getName(), "CLIENT"); //TODO:this needs to be resolved
