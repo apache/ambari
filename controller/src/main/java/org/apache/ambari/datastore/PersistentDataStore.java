7f8f881fa27a0e973d349b204d2b88badc85ce57
Suhas <vgogate@apache.org>
2011-10-28 00:02:00 +0000

    minor update to persistent data store interface
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/trunk@1190098 13f79535-47bb-0310-9956-ffa450edef68

--- controller/src/main/java/org/apache/ambari/datastore/PersistentDataStore.java
+++ controller/src/main/java/org/apache/ambari/datastore/PersistentDataStore.java
@@ -1,6 +1,7 @@
 package org.apache.ambari.datastore;
 
-import java.util.LinkedHashMap;
+import java.io.IOException;
+import java.util.List;
 
 import org.apache.ambari.common.rest.entities.Stack;
 import org.apache.ambari.common.rest.entities.ClusterDefinition;
@@ -12,7 +13,7 @@ public interface PersistentDataStore {
     /*
      * Shutdown the data store. It will stop the data store service
      */
-    public void close () throws Exception;
+    public void close () throws IOException;
     
     /**
      * Persist the cluster definition.
@@ -20,31 +21,39 @@ public interface PersistentDataStore {
      * Create new cluster entry, if one does not exist already else add new revision to existing cluster
      * Return the revision number for each newly added cluster definition
      */
-    public long storeClusterDefinition (ClusterDefinition clusterDef) throws Exception;
+    public int storeClusterDefinition (ClusterDefinition clusterDef) throws IOException;
     
     /**
      * Retrieve the cluster definition given the cluster name and revision number
      * If revision number is less than zero, then return latest cluster definition
      */
-    public ClusterDefinition retrieveClusterDefinition (String clusterName, long revision) throws Exception;
+    public ClusterDefinition retrieveClusterDefinition (String clusterName, int revision) throws IOException;
     
+    public static class NameRevisionPair {
+        public String name;
+        public int maxRevision;
+        public NameRevisionPair(String name, int maxRevision) {
+            this.name = name;
+            this.maxRevision = maxRevision;
+        }
+    }
+  
     /**
-     * Retrieve all cluster definitions in a specified order of their revision numbers 
-     * Return LinkedHashMap of <Revision, ClusterDefinition>. LinkedHashMap should preserve insertion order
+     * Retrieve all cluster definitions with their latest revisions
      * 
      */
-    public LinkedHashMap<String, ClusterDefinition> retrieveClusterDefinitions (boolean ascending) throws Exception;
+    public List<NameRevisionPair> retrieveClusterList () throws IOException;
     
     
     /**
      * Delete cluster entry
      */
-    public void deleteCluster (String clusterName) throws Exception;
+    public void deleteCluster (String clusterName) throws IOException;
     
     /**
      * Delete cluster revision(s) less than specified version number 
      */
-    public void deleteClusterDefinitionRevisions(String clusterName, long lessThanRevision) throws Exception;
+    public void purgeClusterDefinitionRevisions(String clusterName, int lessThanRevision) throws IOException;
     
     /**
      * Store/update the cluster state
@@ -52,7 +61,7 @@ public interface PersistentDataStore {
      * Cluster state is updated in place i.e. only laster cluster state is preserved
      * Fields not to be updated should be initialized to null in the ClusterState object
      */
-    public void updateClusterState (String clusterName, ClusterState newstate) throws Exception;
+    public void updateClusterState (String clusterName, ClusterState newstate) throws IOException;
     
     
     /**
@@ -60,64 +69,63 @@ public interface PersistentDataStore {
      * If stack does not exist, create new one else create new revision
      * Return the new stack revision 
      */
-    public long storeStack (String stackName, Stack stack) throws Exception;
+    public int storeStack (String stackName, Stack stack) throws IOException;
     
     /**
      * Retrieve stack with specified revision number
      * If revision number is less than zero, then return latest cluster definition
      */
-    public Stack retrieveStack (String stackName, long revision) throws Exception;
+    public Stack retrieveStack (String stackName, int revision) throws IOException;
     
     /**
-     * Retrieve all stacks in a specified order of their revision numbers 
-     * Return LinkedHashMap of <Revision, Stack>. LinkedHashMap should preserve the insertion order
+     * Retrieve all cluster definitions with their latest revisions
      * 
      */
-    public LinkedHashMap<String, Stack> retrieveStackRevisions (boolean ascending) throws Exception;
+    public List<NameRevisionPair> retrieveStackList () throws IOException;
     
     /**
      * Delete stack
      */
-    public long deleteStack(String stackName) throws Exception;
+    public int deleteStack(String stackName) throws IOException;
     
     /**
      * Delete stack revision(s) less than specified version number 
      */
-    public void deleteStackRevisions(String stackName, long lessThanRevision) throws Exception;
+    public void deleteStackRevisions(String stackName, int lessThanRevision) throws IOException;
     
     /**
      * Update the component state.
      * If component entry does not exist for given cluster it will be created 
      */
-    public void updateComponentState (String clusterName, String componentName, String state) throws Exception;
+    public void updateComponentState (String clusterName, String componentName, String state) throws IOException;
     
     /**
      * Get the component state.
      * Returns null, if specific component is not associated with the cluster
      */
-    public String getComponentState (String clusterName, String componentName) throws Exception;
+    public String getComponentState (String clusterName, String componentName) throws IOException;
 
     
     /**
      * Delete the component state for specified component 
      */
-    public void deleteComponentState (String clusterName, String componentName) throws Exception;
+    public void deleteComponentState (String clusterName, String componentName) throws IOException;
  
     /**
      * Update the component role state.
      * If role entry does not exist for given cluster component it will be created 
      */
-    public void updateRoleState (String clusterName, String componentName, String roleName, String state) throws Exception;
+    public void updateRoleState (String clusterName, String componentName, String roleName, String state) throws IOException;
     
     /**
      * Get the role state.
      * Returns null, if specific component/role is not associated with the cluster
      */
-    public String getRoleState (String clusterName, String componentName, String RoleName) throws Exception;
+    public String getRoleState (String clusterName, String componentName, String RoleName) throws IOException;
 
     
     /**
      * Delete the role state for specified component 
      */
-    public void deleteRoleState (String clusterName, String componentName, String roleName) throws Exception;
+    public void deleteRoleState (String clusterName, String componentName, String roleName) throws IOException;
 }
