9a076d5d386f5f8864716d1aa5f2a77c193b8eaf
Ramya Sunil <ramya@apache.org>
2012-05-17 18:13:51 +0000

    AMBARI-254. Parameterize zookeeper configs
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/branches/ambari-186@1339767 13f79535-47bb-0310-9956-ffa450edef68

--- hmc/puppet/modules/hdp-zookeeper/manifests/params.pp
+++ hmc/puppet/modules/hdp-zookeeper/manifests/params.pp
@@ -8,4 +8,11 @@ class hdp-zookeeper::params() inherits hdp::params
   $zk_data_dir = hdp_default("zk_data_dir","/var/lib/zookeeper/data")
   $zk_pid_dir = hdp_default("zk_pid_dir","/var/run/zookeeper")
   $zk_pid_file = "${zk_pid_dir}/zookeeper_server.pid"
+
+
+  $tickTime = hdp_default("tickTime","2000")
+  $initLimit = hdp_default("initLimit","10")
+  $syncLimit = hdp_default("syncLimit","5")
+  $clientPort = hdp_default("clientPort","2181")
+
 }
