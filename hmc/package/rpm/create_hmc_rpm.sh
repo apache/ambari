ca04361f7c4fd43fb673c894ad660b57f621d259
Ramya Sunil <ramya@apache.org>
2012-05-17 17:44:22 +0000

    AMBARI-248. Add yuiCombinator.php to rpm
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/branches/ambari-186@1339739 13f79535-47bb-0310-9956-ffa450edef68

--- hmc/package/rpm/create_hmc_rpm.sh
+++ hmc/package/rpm/create_hmc_rpm.sh
@@ -54,6 +54,7 @@ cp -rf ${BASEDIR}/../../puppet ${HMC_DIR}
 cp -rf ${BASEDIR}/../../php ${HMC_DIR}
 cp -rf ${BASEDIR}/../../yui-3.4.1 ${HMC_DIR}
 cp -rf ${BASEDIR}/../../conf ${HMC_DIR}
+cp -f ${BASEDIR}/../../yuiCombinator.php ${HMC_DIR}
 TAR_DEST="${BUILD_DIR}/${PKG_NAME}-$VERSION.tar.gz"
 
 cd ${BUILD_DIR};
