9fff917731abfce00b4b510cb79c35c8fed736d0
Vikram Dixit K <vikram@apache.org>
2012-06-07 03:49:25 +0000

    AMBARI-511. Fix for previous patch. (Contributed by Jitendra)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/branches/ambari-186@1347368 13f79535-47bb-0310-9956-ffa450edef68

--- hmc/puppet/modules/hdp/manifests/params.pp
+++ hmc/puppet/modules/hdp/manifests/params.pp
@@ -216,7 +216,7 @@ class hdp::params()
       32 =>  ['mysql','mysql-server']
   },
     mysql-connector => {
-      64 =>  ['mysql-connector-java-5.0.8-4.jpp5']
+      64 =>  ['mysql-connector-java-5.0.8-1']
   },
     extjs => {
       64 =>  ['extjs-2.2-1']
