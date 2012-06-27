a4d35dd71bf26d3128a6155043f29e4f781567c5
Mahadev Konar <mahadev@apache.org>
2012-06-27 01:24:52 +0000

    AMBARI-544. Templeton configs for pig archive not correct in HMC
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/trunk@1354305 13f79535-47bb-0310-9956-ffa450edef68

--- hmc/puppet/modules/hdp-templeton/manifests/params.pp
+++ hmc/puppet/modules/hdp-templeton/manifests/params.pp
@@ -5,8 +5,8 @@ class hdp-templeton::params() inherits hdp::params
   ###pig and hive tar url connector
   $download_url = $hdp::params::apache_artifacts_download_url
 
-  $dest_pig_tar_name = hdp_default("pig_tar_name","pig.tar.gz")
-  $dest_hive_tar_name = hdp_default("hive_tar_name","hive.tar.gz")
+  $dest_pig_tar_name = hdp_default("dest_pig_tar_name","pig.tar.gz")
+  $dest_hive_tar_name = hdp_default("dest_hive_tar_name","hive.tar.gz")
   $src_pig_tar_name = hdp_default("src_pig_tar_name","pig-0.9.2.tar.gz")
   $src_hive_tar_name = hdp_default("src_hive_tar_name","hive-0.9.0.tar.gz")
 
@@ -27,10 +27,10 @@ class hdp-templeton::params() inherits hdp::params
   $hadoop_conf_dir = hdp_default("hadoop/templeton-site/hadoop_conf_dir")
   $templeton_jar = hdp_default("hadoop/templeton-site/templeton_jar","/usr/share/templeton/templeton-0.1.4.jar")
   $zookeeper_jar = hdp_default("hadoop/templeton-site/zookeeper_jar","/usr/lib/zookeeper/zookeeper.jar")
-  $pig_tar_gz = hdp_default("hadoop/templeton-site/pig_tar_gz","$pig_tar_name")
+  $pig_tar_gz = hdp_default("hadoop/templeton-site/pig_tar_gz","$dest_pig_tar_name")
   $pig_tar_name_hdfs = hdp_default("hadoop/templeton-site/pig_tar_name_hdfs","pig-0.9.2")
 
-  $hive_tar_gz = hdp_default("hadoop/templeton-site/hive_tar_gz","hive.tar.gz")
+  $hive_tar_gz = hdp_default("hadoop/templeton-site/hive_tar_gz","$dest_hive_tar_name")
   $hive_tar_gz_name = hdp_default("hadoop/templeton-site/hive_tar_gz_name","hive-0.9.0")
   $hive_metastore_sasl_enabled = hdp_default("hadoop/templeton-site/hive_metastore_sasl_enabled","no")
 
