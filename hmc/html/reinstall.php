6a9f85b7ce6461039b093b4f7b720276a93cfcc0
Yusaku Sako <yusaku@apache.org>
2012-07-03 21:18:26 +0000

    AMBARI-591. License header for PHP files should use PHP comments, not HTML comments (Contributed by yusaku)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/branches/branch-0.9@1356953 13f79535-47bb-0310-9956-ffa450edef68

--- hmc/html/reinstall.php
+++ hmc/html/reinstall.php
@@ -1,6 +1,5 @@
-<?php require_once "./head.inc" ?>
-<!--
-
+<?php
+/*
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
  distributed with this work for additional information
@@ -17,9 +16,9 @@
  KIND, either express or implied.  See the License for the
  specific language governing permissions and limitations
  under the License.
-
--->
-
+*/
+?>
+<?php require_once "./head.inc" ?>
 <html>
   <head>
     <?php require "./head.htmli" ?>
