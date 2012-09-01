e654c60844caca7c1b627ce0584f0452c4e9500a
Jitendra Nath Pandey <jitendra@apache.org>
2012-09-01 06:46:52 +0000

    AMBARI-693. Classes for request objects.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/branches/AMBARI-666@1379695 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ambari-server/src/main/java/org/apache/ambari/server/controller/ServiceConfigurationRequest.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ambari.server.controller;
+
+import java.util.Map;
+
+/**
+ * This class encapsulates a configuration update request.
+ * The configuration properties are grouped at service level. It is assumed that
+ * different components of a service don't overload same property name.
+ */
+public class ServiceConfigurationRequest extends Request {
+  final private String clusterName;
+  final private String serviceName;
+  
+  //The complete set of desired configurations can be derived as
+  //properties corresponding to baseConfigVersion overridden with 
+  //desiredProperties.
+  final private String baseConfigVersion;
+  private Map<String, String> desiredProperties = null;
+  private Map<String, String> hostComponentMap = null;
+
+  public ServiceConfigurationRequest(long requestId, Request.Method m,
+      String clusterName, String serviceName, String baseConfigVersion,
+      Map<String, String> desiredProperties,
+      Map<String, String> hostComponentMap) {
+    super(requestId, m);
+    this.clusterName = clusterName;
+    this.serviceName = serviceName;
+    this.baseConfigVersion = baseConfigVersion;
+    this.desiredProperties = desiredProperties;
+    this.hostComponentMap = hostComponentMap;
+  }
+}
---
+++ ambari-server/src/main/java/org/apache/ambari/server/controller/ServiceRequest.java
@@ -0,0 +1,54 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ambari.server.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class encapsulates any request relating to a state change or action in
+ * a service or its components.
+ */
+public class ServiceRequest extends Request {
+  private String clusterName;
+  private String service;
+  private String configVersion;
+  private String hostComponentMapVersion;
+  
+  public class ComponentRequest {
+    private String componentName;
+    private Map<String, String> params = new HashMap<String, String>();
+  }
+  
+  public void addComponentRequest(ComponentRequest c) {
+    components.add(c);
+  }
+  
+  public ServiceRequest(long requestId, Request.Method m, String clusterName,
+      String service, String configVersion, String hostComponentMapVersion) {
+    super(requestId, m);
+    this.clusterName = clusterName;
+    this.service = service;
+    this.configVersion = configVersion;
+    this.hostComponentMapVersion = hostComponentMapVersion;
+  }
+  
+  private List<ComponentRequest> components = new ArrayList<ComponentRequest>();
+}
