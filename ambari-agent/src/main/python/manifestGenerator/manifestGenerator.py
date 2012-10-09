a0fdd3603012d2a46fcf216e64932afc51279866
Mahadev Konar <mahadev@apache.org>
2012-10-09 01:13:06 +0000

    AMBARI-828. Manifest generation for various actions from the server. (mahadev)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/branches/AMBARI-666@1395833 13f79535-47bb-0310-9956-ffa450edef68

--- ambari-agent/src/main/python/manifestGenerator/manifestGenerator.py
+++ ambari-agent/src/main/python/manifestGenerator/manifestGenerator.py
@@ -1,5 +1,14 @@
 import json
 
+  #read static imports from file and write them to manifest
+def writeImports(outputFile, inputFileName='imports.txt'):
+  inputFile = open(inputFileName, 'r')
+  
+  for line in inputFile:
+    outputFile.write(line)
+    
+  inputFile.close()
+
 def generateManifest(inputJsonStr):
 #reading json
   parsedJson = json.loads(inputJsonStr)
@@ -7,7 +16,7 @@ def generateManifest(inputJsonStr):
   clusterHostInfo = parsedJson['clusterHostInfo']
   params = parsedJson['params']
   configurations = parsedJson['configurations']
-  hostAttributes = parsedJson['hostAttributes']
+  #hostAttributes = parsedJson['hostAttributes']
   roles = parsedJson['roles']
   
 #writing manifest
@@ -25,8 +34,8 @@ def generateManifest(inputJsonStr):
   #writing config maps
   writeConfigurations(manifest, configurations)
 
-  #cycle here - writing host attributes
-  writeHostAttributes(manifest, hostAttributes)
+  #writing host attributes
+  #writeHostAttributes(manifest, hostAttributes)
 
   #writing task definitions 
   writeTasks(manifest, roles)
@@ -34,65 +43,115 @@ def generateManifest(inputJsonStr):
   manifest.close()
     
   
-  #read static imports from file and write them to manifest
-  def writeImports(outputFile, inputFileName='imports.txt'):
-    inputFile = open(inputFileName, 'r')
-    
-    for line in inputFile:
-      outputFile.write(line)
-      
-    inputFile.close()
+  #read dictionary
+def readDict(file, separator='='):
+  result = dict()
+  
+  for line in file :
+    dictTuple = line.partition(separator)
+    result[dictTuple[0].strip()] = dictTuple[2].strip()
+  
+  return result
+  
 
   #write nodes
-  def writeNodes(outputFile, clusterHostInfo):
-    for node in clusterHostInfo.iterkeys():
-      outputFile.write('$' + node + '= ['
-    
-      coma = ''
-      for host in node:
-        outputFile.write(coma + '\'' + host + '\'')
-        coma = ', '
-
-      outputFile.write(']\n'
-
-  #write params
-  def writeParams(outputFile, params):
-    for param in params.iterkeys():
-      outputFile.write('$' +  param + '="' + params[param] + '"\n')
-
-  #write host attributes
-  def writeHostAttributes(outputFile, hostAttributes):
-    outputFile.write('$hostAttributes={\n')
-  
+def writeNodes(outputFile, clusterHostInfo):
+  for node in clusterHostInfo.iterkeys():
+    outputFile.write('$' + node + '= [')
     coma = ''
-    for attribute in hostAttributes.iterkeys():
-      outputFile.write(coma + '"' +  attribute + '" => "{' + hostAttributes[attribute] + '"}')
+    
+    for host in node:
+      outputFile.write(coma + '\'' + host + '\'')
+      coma = ', '
+
+    outputFile.write(']\n')
+
+#write params
+def writeParams(outputFile, params):
+  for param in params.iterkeys():
+    outputFile.write('$' +  param + '="' + params[param] + '"\n')
+
+#write host attributes
+def writeHostAttributes(outputFile, hostAttributes):
+  outputFile.write('$hostAttributes={\n')
+
+  coma = ''
+  for attribute in hostAttributes.iterkeys():
+    outputFile.write(coma + '"' +  attribute + '" => "{' + hostAttributes[attribute] + '"}')
+    coma = ',\n'
+
+  outputFile.write('}\n')
+
+#write configurations
+def writeConfigurations(outputFile, configs):
+  outputFile.write('$configuration =  {\n')
+
+  for configName in configs.iterkeys():
+    outputFile.write('$' + configName + '=> {\n')
+    config = configs[configName]
+    
+    coma = ''
+    for configParam in config.iterkeys():
+      outputFile.write(coma + '"' + configParam + '" => "' + config[configParam] + '"')
       coma = ',\n'
 
-    outputFile.write('}\n')
+    outputFile.write('\n}\n')
+    
+  outputFile.write('\n}\n')
 
-  #write configurations
-  def writeConfigurations(outputFile, configs):
-    outputFile.write('$configuration =  {\n'
+#write node tasks
+def writeTasks(outputFile, roles):
+  #reading dictionaries
+  rolesToClassFile = open('rolesToClass.dict', 'r')
+  rolesToClass = readDict(rolesToClassFile)
+  rolesToClassFile.close()
+
+  serviceStatesFile =  open('serviceStates.dict', 'r')
+  serviceStates = readDict(serviceStatesFile)
+  serviceStatesFile.close()
+
+  outputFile.write('node /<toBeDefined>/ {\n ')
+  writeStages(outputFile, len(roles))
+  stageNum = 1
+
+  for role in roles :
+    rolename = role['role']
+    command = role['roleCommand']
+    taskParams = role['params']
+    taskParamsNormalized = normalizeTaskParams(taskParams)
+    taskParamsPostfix = ''
+    
+    if len(taskParamsNormalized) > 0 :
+      taskParamsPostfix = ', ' + taskParamsNormalized
+    
+    className = rolesToClass[rolename]
+    serviceState = serviceStates[command]
+    
+    outputFile.write('class {\'' + className + '\':' + ' stage => ' + str(stageNum) + ', service_state => ' + serviceState + taskParamsPostfix + '}\n')
+    stageNum = stageNum + 1
+    
+def normalizeTaskParams(taskParams):
+  result = ''
+  coma = ''
   
-    for configName in configs.iterkeys():
-      outputFile.write('$' + configName + '=> {\n')
-      config = configs[configName]
-      
-      coma = ''
-      for configParam in config.iterkeys():
-        outputFile.write(coma + '"' + configParam + '" => "' + config[configParam] + '"')
-        coma = ',\n'
-
-      outputFile.write('\n}\n')
-      
-    outputFile.write('\n}\n'
-
-  #write node tasks
-  def writeTasks(outputFile, tasks):
-    for task in tasks :
-      nodename = task['role']
-      command = task['roleCommand']
-      taskParams = task['params']
-    #TODO: write node task to file
-      
+  for paramName in taskParams.iterkeys():
+    result = coma + result + paramName + ' => ' + taskParams[paramName]
+    coma = ','
+    
+  return result
+  
+def writeStages(outputFile, numStages):
+  arrow = ''
+  
+  for i in range(numStages):
+    outputFile.write(arrow + 'stage{' + str(i) + ' :}')
+    arrow = ' -> '
+  
+  outputFile.write('\n')
+    
+    
+#test code
+jsonFile = open('test.json', 'r')
+jsonStr = jsonFile.read() 
+print '>>>JSON STRING: ' + jsonStr
+generateManifest(jsonStr)
