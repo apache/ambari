80f29f4f457ebfb3e45b92040252ebdc6eaabb5f
Yusaku Sako <yusaku@apache.org>
2013-01-11 02:19:13 +0000

    AMBARI-1119. Service Summary pages no longer show service-specific info. (Srimanth Gunturi via yusaku)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/trunk@1431812 13f79535-47bb-0310-9956-ffa450edef68

--- ambari-web/app/views/main/service/info/summary.js
+++ ambari-web/app/views/main/service/info/summary.js
@@ -259,7 +259,7 @@ App.MainServiceInfoSummaryView = Em.View.extend({
       }
     }
     return svc;
-  }.property().volatile(),
+  }.property('controller.content.serviceName').volatile(),
 
   isHide:true,
   moreStatsView:Em.View.extend({
