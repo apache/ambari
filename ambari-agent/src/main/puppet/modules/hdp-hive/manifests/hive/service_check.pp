8266f981dad538c56106a88237a2d116f6b8c842
Jaimin Jetly <jaimin@apache.org>
2013-03-28 00:20:54 +0000

    AMBARI-1730. Hive Service check fails in non secure cluster (jaimin)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/trunk@1461908 13f79535-47bb-0310-9956-ffa450edef68

--- ambari-agent/src/main/puppet/modules/hdp-hive/manifests/hive/service_check.pp
+++ ambari-agent/src/main/puppet/modules/hdp-hive/manifests/hive/service_check.pp
@@ -30,13 +30,11 @@ class hdp-hive::hive::service_check() inherits hdp-hive::params
     $kinit_cmd = "${hdp::params::kinit_path_local} -kt ${smoke_user_keytab} ${smoke_test_user};"
     $hive_principal_ext = "principal=${hive_user}/_HOST@${hdp::params::kerberos_domain}"
     $hive_url_ext = "${hive_url}\;${hive_principal_ext}"
+    $smoke_cmd = "${kinit_cmd} env JAVA_HOME=${hdp::params::java64_home} ${smoke_test_path} ${hive_url_ext} ${smoke_test_sql}"
   } else {
-    $kinit_cmd = ""
-    $hive_principal_ext = ""
-    $hive_url_ext = $hive_url
+    $smoke_cmd = "env JAVA_HOME=$hdp::params::java64_home $smoke_test_path $hive_url $smoke_test_sql"
   }
 
-  $smoke_cmd = "${kinit_cmd} env JAVA_HOME=${hdp::params::java64_home} ${smoke_test_path} ${hive_url_ext} ${smoke_test_sql}"
 
   file { $smoke_test_path:
     ensure => present,
