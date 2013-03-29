a32d1c012e4b336c83e3496fd72f222dbf7a0c96
Siddharth Wagle <swagle@apache.org>
2013-03-29 18:27:24 +0000

    AMBARI-1749. set default heap size for zookeeper. (swagle)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ambari/trunk@1462577 13f79535-47bb-0310-9956-ffa450edef68

--- ambari-agent/src/main/puppet/modules/hdp-zookeeper/manifests/params.pp
+++ ambari-agent/src/main/puppet/modules/hdp-zookeeper/manifests/params.pp
@@ -28,7 +28,7 @@ class hdp-zookeeper::params() inherits hdp::params
   $zk_data_dir = hdp_default("zk_data_dir","/var/lib/zookeeper/data")
   $zk_pid_dir = hdp_default("zk_pid_dir","/var/run/zookeeper")
   $zk_pid_file = "${zk_pid_dir}/zookeeper_server.pid"
-
+  $zk_server_heapsize = hdp_default("zk_server_heapsize","-Xmx1024m")
 
   $tickTime = hdp_default("tickTime","2000")
   $initLimit = hdp_default("initLimit","10")
