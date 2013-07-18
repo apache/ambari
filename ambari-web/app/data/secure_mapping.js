5b2e8f4cab00d0ad37c5f6f03378fa0e0e95b846
Jaimin Jetly <jaimin@hortonworks.com>
2013-07-18 10:29:10 -0700

    AMBARI-2680: Webhcat property templeton.hive.properties value in webhcat-site.xml is defined incorrect. (jaimin)

--- ambari-web/app/data/secure_mapping.js
+++ ambari-web/app/data/secure_mapping.js
@@ -375,8 +375,8 @@ module.exports = [
     "name": "templeton.hive.properties",
     "templateName": ["hivemetastore_host","hive_metastore_principal_name","kerberos_domain"],
     "foreignKey": null,
-    "value": "hive.metastore.local=false, hive.metastore.uris=thrift://<templateName[0]>:9083, hive." +
-      "metastore.sasl.enabled=true,hive.metastore.execute.setugi=true, hive.exec.mode.local.auto=false, hive.metastore.kerberos.principal=<templateName[1]>@<templateName[2]>",
+    "value": "hive.metastore.local=false,hive.metastore.uris=thrift://<templateName[0]>:9083,hive." +
+      "metastore.sasl.enabled=true,hive.metastore.execute.setugi=true,hive.exec.mode.local.auto=false,hive.metastore.kerberos.principal=<templateName[1]>@<templateName[2]>",
     "filename": "webhcat-site.xml",
     "serviceName": "WEBHCAT"
   },
