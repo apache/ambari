becdc592448f0935a445b0776b66990191caef4b
Yusaku Sako <yusaku@hortonworks.com>
2013-09-12 17:57:15 -0700

    AMBARI-3219. NameNode HA Wizard: refreshing does not reload the page and the user is stuck. (yusaku)

--- ambari-web/app/mappers/service_mapper.js
+++ ambari-web/app/mappers/service_mapper.js
@@ -340,10 +340,10 @@ App.servicesMapper = App.QuickDataMapper.create({
         if ( component.host_components.length == 2) {
           var haState1;
           var haState2;
-          if (component.host_components[1].metrics.dfs) {
+          if (component.host_components[1].metrics && component.host_components[1].metrics.dfs) {
             haState2 = component.host_components[1].metrics.dfs.FSNamesystem.HAState;
           }
-          if (component.host_components[0].metrics.dfs) {
+          if (component.host_components[0].metrics && component.host_components[0].metrics.dfs) {
             haState1 = component.host_components[0].metrics.dfs.FSNamesystem.HAState;
           }
           var active_name_node = [];
