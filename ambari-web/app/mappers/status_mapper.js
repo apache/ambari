1f4ab2a7e063843425485d797c4a4dd657793197
atkach <andrey91.07@gmail.com>
2013-09-25 13:50:57 +0300

    AMBARI-3335 UI optimization: move update of statuses into status mapper. (atkach)

--- ambari-web/app/mappers/status_mapper.js
+++ ambari-web/app/mappers/status_mapper.js
@@ -36,6 +36,24 @@ App.statusMapper = App.QuickDataMapper.create({
 
     if (json.items) {
       var result = {};
+
+      //host_components
+      result = this.parse_host_components(json);
+
+      var hostComponents = App.HostComponent.find();
+      var servicesMap = {};
+      var hostsMap = {};
+
+      hostComponents.forEach(function(hostComponent) {
+        var item = result[hostComponent.get('id')];
+        if (item) {
+          hostComponent.set('workStatus', item.work_status);
+          hostComponent.set('haStatus', item.ha_status);
+          this.countHostComponents(hostComponent, hostsMap, hostsMap[hostComponent.get('host.id')]);
+          this.countServiceComponents(hostComponent, servicesMap, servicesMap[hostComponent.get('service.id')]);
+        }
+      }, this);
+
       json.items.forEach(function (item) {
         item = this.parseIt(item, this.config);
         result[item.id] = item;
@@ -49,24 +67,165 @@ App.statusMapper = App.QuickDataMapper.create({
         }
       });
 
-      //host_components
-      result = this.parse_host_components(json);
-
-      // console.profile("App.statusMapper.map() profile");
-
-      var hostComponents = App.HostComponent.find();
-
-      hostComponents.forEach(function(hostComponent) {
-        var item = result[hostComponent.get('id')];
-        if (item) {
-         hostComponent.set('workStatus', item.work_status);
-        }
-      });
-
-      // console.profileEnd();
+      this.updateHostsStatus(App.Host.find(), hostsMap);
+      this.updateServicesStatus(App.Service.find(), servicesMap);
 
       console.log('out status mapper.  Took ' + (new Date().getTime() - start) + 'ms');
     }
+  },
+
+  /**
+   * fill serviceMap with aggregated data of hostComponents for each service
+   * @param hostComponent
+   * @param servicesMap
+   * @param service
+   */
+  countServiceComponents: function(hostComponent, servicesMap, service){
+    if (!service) {
+      service = {
+        everyStarted: true,
+        everyStartedOrMaintenance: true,
+        masterComponents: [],
+        isStarted: false,
+        isUnknown: false,
+        isStarting: false,
+        isStopped: false,
+        isHbaseActive: false,
+        serviceName: this.get('serviceName'),
+        isRunning: true,
+        runningHCs: [],
+        unknownHCs: [],
+        hdfsHealthStatus: '',
+        toolTipContent: ''
+      };
+      servicesMap[hostComponent.get('service.id')] = service;
+    }
+    if (hostComponent.get('isMaster')) {
+      service.everyStartedOrMaintenance = (service.everyStartedOrMaintenance)
+        ? ([App.HostComponentStatus.started, App.HostComponentStatus.maintenance].contains(hostComponent.get('workStatus')))
+        : false;
+      service.everyStarted = (service.everyStarted)
+        ? (hostComponent.get('workStatus') === App.HostComponentStatus.started)
+        : false;
+      service.isStarted = (!service.isStarted)
+        ? (hostComponent.get('workStatus') === App.HostComponentStatus.started)
+        : true;
+      service.isUnknown = (!service.isUnknown)
+        ? (hostComponent.get('workStatus') === App.HostComponentStatus.unknown)
+        : true;
+      service.isStarting = (!service.isStarting)
+        ? (hostComponent.get('workStatus') === App.HostComponentStatus.starting)
+        : true;
+      service.isStopped = (!service.isStopped)
+        ? (hostComponent.get('workStatus') === App.HostComponentStatus.stopped)
+        : true;
+      service.isHbaseActive = (!service.isHbaseActive)
+        ? (hostComponent.get('haStatus') === 'active')
+        : true;
+
+      service.masterComponents.push(hostComponent);
+
+      service.toolTipContent += hostComponent.get("displayName") + " " + hostComponent.get("componentTextStatus") + "<br/>";
+    }
+
+    if (hostComponent.get('workStatus') !== App.HostComponentStatus.stopped &&
+      hostComponent.get('workStatus') !== App.HostComponentStatus.install_failed &&
+      hostComponent.get('workStatus') !== App.HostComponentStatus.unknown &&
+      hostComponent.get('workStatus') !== App.HostComponentStatus.maintenance) {
+      service.isRunning = false;
+      service.runningHCs.addObject(hostComponent);
+    } else if (hostComponent.get('workStatus') == App.HostComponentStatus.unknown) {
+      service.unknownHCs.addObject(hostComponent);
+    }
+  },
+
+  /**
+   * compute service status and properties by servicesMap of hostComponents
+   * @param services
+   * @param servicesMap
+   */
+  updateServicesStatus: function(services, servicesMap){
+    services.forEach(function(_service){
+      var service = servicesMap[_service.get('id')];
+      var serviceName = _service.get('serviceName');
+      //computation of service health status
+      var isGreen = serviceName === 'HBASE' && App.supports.multipleHBaseMasters ? service.isStarted : service.everyStartedOrMaintenance;
+      if (isGreen) {
+        _service.set('healthStatus', 'green');
+      } else if (service.isUnknown) {
+        _service.set('healthStatus', 'yellow');
+      } else if (service.isStarting) {
+        _service.set('healthStatus', 'green-blinking');
+      } else if (service.isStopped) {
+        _service.set('healthStatus', 'red');
+      } else {
+        _service.set('healthStatus', 'red-blinking');
+      }
+
+      if (serviceName === 'HBASE' && App.supports.multipleHBaseMasters) {
+        if (!service.isHbaseActive) {
+          _service.set('healthStatus', 'red');
+        }
+      }
+
+      _service.set('isStarted', service.everyStarted);
+      _service.set('runningHostComponents', service.runningHCs);
+      _service.set('unknownHostComponents', service.unknownHCs);
+      _service.set('isStopped', service.isRunning);
+      _service.set('toolTipContent', service.toolTipContent);
+    }, this);
+  },
+
+  /**
+   * fill hostsMap with aggregated data of hostComponents for each host
+   * @param hostComponent
+   * @param hostsMap
+   * @param host
+   */
+  countHostComponents: function(hostComponent, hostsMap, host){
+    var isMasterRunning = (hostComponent.get('isMaster') && hostComponent.get('workStatus') === App.HostComponentStatus.started);
+    var isSlaveRunning = (hostComponent.get('isSlave') && hostComponent.get('workStatus') === App.HostComponentStatus.started);
+    if (host) {
+      host.mastersRunning = host.mastersRunning + ~~isMasterRunning;
+      host.slavesRunning = host.slavesRunning + ~~isSlaveRunning;
+      host.totalMasters = host.totalMasters + ~~hostComponent.get('isMaster');
+      host.totalSlaves = host.totalSlaves + ~~hostComponent.get('isSlave');
+    } else {
+      hostsMap[hostComponent.get('host.id')] = {
+        mastersRunning: ~~isMasterRunning,
+        slavesRunning: ~~isSlaveRunning,
+        totalMasters: ~~hostComponent.get('isMaster'),
+        totalSlaves: ~~hostComponent.get('isSlave')
+      }
+    }
+  },
+
+  /**
+   * compute host status by hostsMap of hostComponents
+   * @param hosts
+   * @param hostsMap
+   */
+  updateHostsStatus: function(hosts, hostsMap){
+    hosts.forEach(function(_host){
+      var healthStatus = _host.get('healthStatus');
+      var host = hostsMap[_host.get('id')];
+      var status;
+      var masterComponentsRunning = (host.mastersRunning === host.totalMasters);
+      var slaveComponentsRunning = (host.slavesRunning === host.totalSlaves);
+      if (_host.get('isNotHeartBeating') || healthStatus == 'UNKNOWN') {
+        status = 'DEAD-YELLOW';
+      } else if (masterComponentsRunning && slaveComponentsRunning) {
+        status = 'LIVE';
+      } else if (host.totalMasters > 0 && !masterComponentsRunning) {
+        status = 'DEAD-RED';
+      } else {
+        status = 'DEAD-ORANGE';
+      }
+      if (status) {
+        healthStatus = status;
+      }
+      _host.set('healthClass', 'health-status-' + healthStatus);
+    }, this);
   },
 
   parse_host_components: function(json) {
@@ -75,7 +234,10 @@ App.statusMapper = App.QuickDataMapper.create({
       item.components.forEach(function (component) {
         component.host_components.forEach(function (host_component) {
           host_component.id = host_component.HostRoles.component_name + "_" + host_component.HostRoles.host_name;
-          result[host_component.id] = this.parseIt(host_component, this.config3);
+          result[host_component.id] = {
+            work_status: host_component.HostRoles.state,
+            ha_status: host_component.HostRoles.ha_status
+          };
         }, this)
       }, this)
     }, this);
