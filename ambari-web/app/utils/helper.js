c13df4d7dc3d65dfd580fccadb278913f47f843a
Jaimin Jetly <jaimin@hortonworks.com>
2013-11-04 01:19:01 -0800

    AMBARI-3675: Default value of 'Default virtual memory for a job's map-task' is not valid. (jaimin)

--- ambari-web/app/utils/helper.js
+++ ambari-web/app/utils/helper.js
@@ -23,6 +23,9 @@ String.prototype.trim = function () {
 String.prototype.endsWith = function(suffix) {
   return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
+String.prototype.startsWith = function (prefix){
+  return this.indexOf(prefix) == 0;
+};
 
 /**
  * convert ip address string to long int
