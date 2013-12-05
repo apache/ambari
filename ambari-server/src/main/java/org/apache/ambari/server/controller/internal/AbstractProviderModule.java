e353a1f5d0a4cb53026bdeca746e56cbfd4fdafc
tbeerbower <tbeerbower@hortonworks.com>
2013-12-05 10:20:06 -0500

    AMBARI-3987 - Resource providers are set with wrong stack version.

--- ambari-server/src/main/java/org/apache/ambari/server/controller/internal/AbstractProviderModule.java
+++ ambari-server/src/main/java/org/apache/ambari/server/controller/internal/AbstractProviderModule.java
@@ -43,11 +43,12 @@ import org.apache.ambari.server.state.DesiredConfig;
 import org.apache.ambari.server.state.HostState;
 import org.apache.ambari.server.state.Service;
 import org.apache.ambari.server.state.State;
-import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * An abstract provider module implementation.
@@ -309,6 +310,18 @@ public abstract class AbstractProviderModule implements ProviderModule, Resource
 
   // ----- utility methods ---------------------------------------------------
 
+  /**
+   * Get the metrics version for the given cluster name.
+   *
+   * @param clusterName  the cluster name
+   *
+   * @return the metrics version for the given cluster name
+   */
+  protected PropertyHelper.MetricsVersion getMetricsVersion(String clusterName) {
+    updateClusterVersion();
+    return clusterVersionsMap.get(clusterName);
+  }
+
   protected abstract ResourceProvider createResourceProvider(Resource.Type type);
 
   protected void registerResourceProvider(Resource.Type type) {
@@ -465,9 +478,15 @@ public abstract class AbstractProviderModule implements ProviderModule, Resource
 
         for (Resource cluster : clusters) {
           String clusterVersion = (String) cluster.getPropertyValue(CLUSTER_VERSION_PROPERTY_ID);
+          PropertyHelper.MetricsVersion version = PropertyHelper.MetricsVersion.HDP2;
 
-          PropertyHelper.MetricsVersion version =  clusterVersion.startsWith("HDP-1") ?
-              PropertyHelper.MetricsVersion.HDP1 : PropertyHelper.MetricsVersion.HDP2;
+          Matcher m = Pattern.compile("[0-9]+(\\.[0-9]+)+").matcher(clusterVersion);
+          if (m.find()) {
+            clusterVersion = m.group(0);
+            if (clusterVersion.equals("1") || clusterVersion.startsWith("1.")) {
+              version = PropertyHelper.MetricsVersion.HDP1;
+            }
+          }
 
           clusterVersionsMap.put(
               (String) cluster.getPropertyValue(CLUSTER_NAME_PROPERTY_ID),
