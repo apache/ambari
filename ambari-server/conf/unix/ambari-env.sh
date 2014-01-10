dbb345b6ac29a909eebd48896379ac54997d079e
Nate Cole <ncole@hortonworks.com>
2014-01-10 15:14:06 -0500

    AMBARI-4265. Provide way to set http proxy for Ambari Server (Jeff Sposetti via ncole)

--- ambari-server/conf/unix/ambari-env.sh
+++ ambari-server/conf/unix/ambari-env.sh
@@ -15,4 +15,4 @@
 
 
 AMBARI_PASSHPHRASE="DEV"
-export AMBARI_JVM_ARGS='-Xms512m -Xmx2048m -Djava.security.auth.login.config=/etc/ambari-server/conf/krb5JAASLogin.conf -Djava.security.krb5.conf=/etc/krb5.conf -Djavax.security.auth.useSubjectCredsOnly=false'
+export AMBARI_JVM_ARGS=$AMBARI_JVM_ARGS' -Xms512m -Xmx2048m -Djava.security.auth.login.config=/etc/ambari-server/conf/krb5JAASLogin.conf -Djava.security.krb5.conf=/etc/krb5.conf -Djavax.security.auth.useSubjectCredsOnly=false'
