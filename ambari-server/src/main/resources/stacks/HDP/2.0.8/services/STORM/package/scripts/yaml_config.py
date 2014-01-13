e3722e9b7ee59bc433b83951d297d34f80c73021
Andrew Onischuk <aonishuk@hortonworks.com>
2014-01-13 05:31:37 -0800

    AMBARI-4273. Update yaml parser/escaper for different type of values (aonishuk)

--- ambari-server/src/main/resources/stacks/HDP/2.0.8/services/STORM/package/scripts/yaml_config.py
+++ ambari-server/src/main/resources/stacks/HDP/2.0.8/services/STORM/package/scripts/yaml_config.py
@@ -1,4 +1,35 @@
+import re
 from resource_management import *
+
+def escape_yaml_propetry(value):
+  unquouted = False
+  unquouted_values = ["null","Null","NULL","true","True","TRUE","false","False","FALSE","YES","Yes","yes","NO","No","no","ON","On","on","OFF","Off","off"]
+  
+  if value in unquouted_values:
+    unquouted = True
+
+  # if is list [a,b,c]
+  if re.match('^\w*\[.+\]\w*$', value):
+    unquouted = True
+    
+  try:
+    int(value)
+    unquouted = True
+  except ValueError:
+    pass
+  
+  try:
+    float(value)
+    unquouted = True
+  except ValueError:
+    pass
+
+  value = value.replace("'","''")
+  
+  if not unquouted:
+    value = "'"+value+"'"
+    
+  return value
 
 def yaml_config(
   filename,
@@ -8,8 +39,8 @@ def yaml_config(
   owner = None,
   group = None
 ):
-    config_content = InlineTemplate('''{% for key, value in configurations_dict.items() %}{{ key }}: {{ value }}
-{% endfor %}''', configurations_dict=configurations)
+    config_content = InlineTemplate('''{% for key, value in configurations_dict.items() %}{{ key }}: {{ escape_yaml_propetry(value) }}
+{% endfor %}''', configurations_dict=configurations, extra_imports=[escape_yaml_propetry])
 
     File (format("{conf_dir}/{filename}"),
       content = config_content,
