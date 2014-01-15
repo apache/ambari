f4e0f6ca4050a36e3007928c0049672dc5263ac9
Srimanth Gunturi <sgunturi@hortonworks.com>
2014-01-14 19:06:11 -0800

    AMBARI-4292. Fix license issues reported by apache-rat:check. (srimanth)

--- ambari-agent/src/main/python/resource_management/libraries/functions/get_unique_id_and_date.py
+++ ambari-agent/src/main/python/resource_management/libraries/functions/get_unique_id_and_date.py
@@ -27,7 +27,7 @@ from resource_management.core.shell import checked_call
 def get_unique_id_and_date():
     code, out = checked_call("hostid")
     id = out.strip()
-    
+
     now = datetime.datetime.now()
     date = now.strftime("%M%d%y")
 
