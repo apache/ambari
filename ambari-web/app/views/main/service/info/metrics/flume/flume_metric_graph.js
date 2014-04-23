bb21aae27d916899f6b134de294bd014cc7d0ea3
Srimanth Gunturi <sgunturi@hortonworks.com>
2014-04-23 12:00:03 -0700

    AMBARI-5548. Flume host graphs not showing lines even when data is available. (srimanth)

--- ambari-web/app/views/main/service/info/metrics/flume/flume_metric_graph.js
+++ ambari-web/app/views/main/service/info/metrics/flume/flume_metric_graph.js
@@ -35,7 +35,7 @@ App.ChartServiceFlumeMetricGraph = App.ChartLinearTimeView.extend({
   hostName: null,
 
   id: function(){
-    return "service-metrics-flume-metric-graph-" + this.get('metricType') + '-' + this.get('metricType');
+    return "service-metrics-flume-metric-graph-" + this.get('metricType') + '-' + this.get('metricName');
   }.property('metricType', 'metricName'),
 
   title: function(){
@@ -55,23 +55,16 @@ App.ChartServiceFlumeMetricGraph = App.ChartLinearTimeView.extend({
   transformToSeries: function (jsonData) {
     var seriesArray = [];
     var self = this;
-    if (jsonData && jsonData.host_components) {
-      jsonData.host_components.forEach(function (hc) {
-        var hostName = hc.HostRoles.host_name;
-        var host = App.Host.find(hostName);
-        if (host && host.get('publicHostName')) {
-          hostName = host.get('publicHostName');
+    if (jsonData && jsonData.metrics && jsonData.metrics.flume && jsonData.metrics.flume.flume &&
+        jsonData.metrics.flume.flume[this.get('metricType')]) {
+      var metricTypeData = jsonData.metrics.flume.flume[this.get('metricType')];
+      for ( var componentName in metricTypeData) {
+        var seriesName = componentName;
+        var seriesData = metricTypeData[componentName][this.get('metricName')];
+        if (seriesData) {
+          seriesArray.push(self.transformData(seriesData, seriesName));
         }
-        if (hc.metrics && hc.metrics.flume && hc.metrics.flume.flume && hc.metrics.flume.flume.SINK) {
-          for ( var cname in hc.metrics.flume.flume.SINK) {
-            var seriesName = Em.I18n.t('services.service.info.metrics.flume.sinkName').format(cname + " (" + hostName + ")");
-            var seriesData = hc.metrics.flume.flume.SINK[cname]['ConnectionFailedCount'];
-            if (seriesData) {
-              seriesArray.push(self.transformData(seriesData, seriesName));
-            }
-          }
-        }
-      });
+      }
     }
     return seriesArray;
   }
