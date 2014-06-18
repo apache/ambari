8c73f08cf937fea3cf842703b45d619742d2e3d1
atkach <atkach@hortonworks.com>
2014-06-18 18:42:44 +0300

    AMBARI-6188 HBase Master components links on 'HBase summary' tab contains inaccurate text first few seconds after refresh. (Max Shepel via atkach)

--- ambari-web/app/routes/main.js
+++ ambari-web/app/routes/main.js
@@ -684,6 +684,7 @@ module.exports = Em.Route.extend({
         route: '/summary',
         connectOutlets: function (router, context) {
           var item = router.get('mainServiceItemController.content');
+          router.get('updateController').updateServiceMetric(Em.K);
           //if service is not existed then route to default service
           if (item.get('isLoaded')) {
             router.get('mainServiceItemController').connectOutlet('mainServiceInfoSummary', item);
