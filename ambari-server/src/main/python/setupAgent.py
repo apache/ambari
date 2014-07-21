afb7fea3bb6c9927141e34ccb8ccc75c0fb40dfd
Andrew Onishuk <aonishuk@hortonworks.com>
2014-07-21 20:43:06 +0300

    AMBARI-6552. In Ubuntu host, failed to install and register Ambari agents (aonishuk)

--- ambari-server/src/main/python/setupAgent.py
+++ ambari-server/src/main/python/setupAgent.py
@@ -108,7 +108,7 @@ def findNearestAgentPackageVersion(projectVersion):
       Command = ["bash", "-c", "apt-cache -q show ambari-agent |grep 'Version\:'|cut -d ' ' -f 2|tr -d '\\n'|sed -s 's/[-|~][A-Za-z0-9]*//'"]
     else:
       Command = ["bash", "-c", "apt-cache -q show ambari-agent |grep 'Version\:'|cut -d ' ' -f 2|grep '" +
-               projectVersion + "'|tr -d '\\n'|sed -s 's/[-|~][A-Za-z\d]*//'"]
+               projectVersion + "'|tr -d '\\n'|sed -s 's/[-|~][A-Za-z0-9]*//'"]
   else:
     Command = ["bash", "-c", "yum -q list all ambari-agent | grep '" + projectVersion +
                               "' | sed -re 's/\s+/ /g' | cut -d ' ' -f 2 | head -n1 | sed -e 's/-\w[^:]*//1' "]
