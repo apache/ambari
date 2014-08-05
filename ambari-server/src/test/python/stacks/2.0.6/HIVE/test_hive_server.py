b240fa30767ce95638c3996c6826493d36229cd0
Mahadev Konar <mahadev@apache.org>
2014-08-05 16:31:31 -0700

    Revert "AMBARI-6709 After Namenode is moved via Ambari the hive metastore namenode hostname is not updated with the new namenode hostname (Dmytro Shkvyra via dsen)"
    This reverts commit 26d2c9f6e0bb0fdbb1138a3cf3bcdc73b27f1ef8.

--- ambari-server/src/test/python/stacks/2.0.6/HIVE/test_hive_server.py
+++ ambari-server/src/test/python/stacks/2.0.6/HIVE/test_hive_server.py
@@ -46,10 +46,9 @@ class TestHiveServer(RMFTestCase):
                               content='log4jproperties\nline2'
     )
     self.assertNoMoreResources()
-
-  @patch("hive_service.check_fs_root")
+  
   @patch("socket.socket")
-  def test_start_default(self, socket_mock, check_fs_root_mock):
+  def test_start_default(self, socket_mock):
     s = socket_mock.return_value
     
     self.executeScript("2.0.6/services/HIVE/package/scripts/hive_server.py",
@@ -130,7 +129,6 @@ class TestHiveServer(RMFTestCase):
     )
 
     self.assertNoMoreResources()
-    self.assertTrue(check_fs_root_mock.called)
     self.assertTrue(socket_mock.called)
     self.assertTrue(s.close.called)
 
@@ -173,11 +171,10 @@ class TestHiveServer(RMFTestCase):
     )
     self.assertNoMoreResources()
 
-  @patch("hive_service.check_fs_root")
   @patch("socket.socket")
-  def test_start_secured(self, socket_mock, check_fs_root_mock):
+  def test_start_secured(self, socket_mock):
     s = socket_mock.return_value
-
+    
     self.executeScript("2.0.6/services/HIVE/package/scripts/hive_server.py",
                        classname = "HiveServer",
                        command = "start",
@@ -209,7 +206,6 @@ class TestHiveServer(RMFTestCase):
     )
 
     self.assertNoMoreResources()
-    self.assertTrue(check_fs_root_mock.called)
     self.assertTrue(socket_mock.called)
     self.assertTrue(s.close.called)
 
@@ -418,11 +414,10 @@ class TestHiveServer(RMFTestCase):
       owner = 'hive',
       group = 'hadoop',
     )
-
-  @patch("hive_service.check_fs_root")
+    
   @patch("time.time")
   @patch("socket.socket")
-  def test_socket_timeout(self, socket_mock, time_mock, check_fs_root_mock):
+  def test_socket_timeout(self, socket_mock, time_mock):        
     s = socket_mock.return_value
     s.connect = MagicMock()    
     s.connect.side_effect = socket.error("")
