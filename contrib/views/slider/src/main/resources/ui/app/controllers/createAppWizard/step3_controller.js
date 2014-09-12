e10ec8b0943eda245c6ae6959b31430c45515f4a
atkach <atkach@hortonworks.com>
2014-09-12 14:48:05 +0300

    AMBARI-7277 Slider View: Creating app without metrics should not show Metrics section. (atkach)

--- contrib/views/slider/src/main/resources/ui/app/controllers/createAppWizard/step3_controller.js
+++ contrib/views/slider/src/main/resources/ui/app/controllers/createAppWizard/step3_controller.js
@@ -76,6 +76,7 @@ App.CreateAppWizardStep3Controller = Ember.ObjectController.extend({
    */
   configsSet: [
     {
+      name: 'ganglia_metrics',
       trigger: {value: false, label: Em.I18n.t('configs.enable.metrics'), viewType: 'checkbox'},
       isSet: true,
       section: 'global',
@@ -152,6 +153,9 @@ App.CreateAppWizardStep3Controller = Ember.ObjectController.extend({
     configsSet.forEach(function (configSet) {
       if (configSet.configs.length === configSet.configNames.length) {
         delete configSet.configNames;
+        if (configSet.name === 'ganglia_metrics') {
+          configSet.trigger.readOnly = (!App.get('gangliaClusters') || App.get('gangliaClusters').length === 0);
+        }
         configSet.trigger = App.ConfigProperty.create(configSet.trigger);
         this.initConfigSetDependecies(configSet);
         configs.unshift(configSet);
