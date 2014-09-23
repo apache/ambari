d29c34ae2e6156dfa21a7f3cbfca74f663b4bb57
Andrew Onishuk <aonishuk@hortonworks.com>
2014-09-23 20:04:56 +0300

    AMBARI-7453. Check HDFS failed on 1.3 stack cluster (aonishuk)

--- ambari-server/src/test/python/stacks/1.3.2/HDFS/test_service_check.py
+++ ambari-server/src/test/python/stacks/1.3.2/HDFS/test_service_check.py
@@ -25,7 +25,7 @@ from stacks.utils.RMFTestCase import *
 @patch.object(resource_management.libraries.functions, "get_unique_id_and_date", new = MagicMock(return_value=''))
 class TestServiceCheck(RMFTestCase):
   def test_service_check_default(self):
-    self.executeScript("2.0.6/services/HDFS/package/scripts/service_check.py",
+    self.executeScript("1.3.2/services/HDFS/package/scripts/service_check.py",
                         classname="HdfsServiceCheck",
                         command="service_check",
                         config_file="default.json"
@@ -35,54 +35,43 @@ class TestServiceCheck(RMFTestCase):
     self.assertNoMoreResources()
     
   def test_service_check_secured(self):
-    self.executeScript("2.0.6/services/HDFS/package/scripts/service_check.py",
+    self.executeScript("1.3.2/services/HDFS/package/scripts/service_check.py",
                         classname="HdfsServiceCheck",
                         command="service_check",
-                        config_file="default.json"
+                        config_file="secured.json"
     )
-    
+    self.assertResourceCalled('Execute', "su - ambari-qa -c '/usr/bin/kinit -kt /etc/security/keytabs/smokeuser.headless.keytab ambari-qa'",)
     self.assert_service_check()
     self.assertNoMoreResources()
         
   def assert_service_check(self):
     self.assertResourceCalled('ExecuteHadoop', 'dfsadmin -safemode get | grep OFF',
-        logoutput = True,
-        tries = 20,
-        conf_dir = '/etc/hadoop/conf',
-        try_sleep = 3,
-        bin_dir = '/usr/bin',
-        user = 'ambari-qa',
-    )
-    self.assertResourceCalled('ExecuteHadoop', 'fs -mkdir /tmp',
-        conf_dir = '/etc/hadoop/conf',
-        bin_dir = '/usr/bin',
-        logoutput = True,
-        not_if = 'su - ambari-qa -c \'/usr/bin/hadoop --config /etc/hadoop/conf fs -test -e /tmp\'',
-        try_sleep = 3,
-        tries = 5,
-        user = 'ambari-qa',
-    )
-    self.assertResourceCalled('ExecuteHadoop', 'fs -chmod 777 /tmp',
-        conf_dir = '/etc/hadoop/conf',
-        bin_dir = '/usr/bin',
-        logoutput = True,
-        try_sleep = 3,
-        tries = 5,
-        user = 'ambari-qa',
-    )
-    self.assertResourceCalled('ExecuteHadoop', 'fs -rm /tmp/; hadoop --config /etc/hadoop/conf fs -put /etc/passwd /tmp/',
-        logoutput = True,
-        bin_dir = '/usr/bin',
-        tries = 5,
-        conf_dir = '/etc/hadoop/conf',
-        try_sleep = 3,
-        user = 'ambari-qa',
-    )
+                              logoutput = True,
+                              tries = 20,
+                              conf_dir = '/etc/hadoop/conf',
+                              try_sleep = 15,
+                              user = 'ambari-qa',
+                              )
+    self.assertResourceCalled('ExecuteHadoop', 'fs -mkdir /tmp ; hadoop fs -chmod 777 /tmp',
+                              conf_dir = '/etc/hadoop/conf',
+                              logoutput = True,
+                              not_if = "su - ambari-qa -c 'hadoop fs -test -e /tmp'",
+                              try_sleep = 3,
+                              tries = 5,
+                              user = 'ambari-qa',
+                              )
+    self.assertResourceCalled('ExecuteHadoop', 'fs -rm /tmp/; hadoop fs -put /etc/passwd /tmp/',
+                              logoutput = True,
+                              tries = 5,
+                              conf_dir = '/etc/hadoop/conf',
+                              try_sleep = 3,
+                              user = 'ambari-qa',
+                              )
     self.assertResourceCalled('ExecuteHadoop', 'fs -test -e /tmp/',
-        logoutput = True,
-        tries = 5,
-        bin_dir = '/usr/bin',
-        conf_dir = '/etc/hadoop/conf',
-        try_sleep = 3,
-        user = 'ambari-qa',
-    )
+                              logoutput = True,
+                              tries = 5,
+                              conf_dir = '/etc/hadoop/conf',
+                              try_sleep = 3,
+                              user = 'ambari-qa',
+                              )
+    self.assertNoMoreResources()
