f10eb9182be99a1d47f2bba10d8253e06c92b75c
Oleg Nechiporenko <onechiporenko@apache.org>
2014-09-29 16:00:48 +0300

    AMBARI-7536. Create skeleton for UI unit tests. (onechiporenko)

--- contrib/views/slider/src/main/resources/ui/app/assets/javascripts/tests.js
+++ contrib/views/slider/src/main/resources/ui/app/assets/javascripts/tests.js
@@ -16,9 +16,4 @@
  * limitations under the License.
  */
 
-test("/", function() {
-  visit("/").then(function() {
-    equal(find("h2").text(), "Welcome to Ember.js", "Application header is rendered");
-    equal(find("li").length, 3, "There are three items in the list");
-  });
-});
+require('test/models/slider_app_test');
