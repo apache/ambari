a0284cc33fa9a22359953103c0516222acb3757e
Andrew Onishuk <aonishuk@hortonworks.com>
2015-02-02 13:32:45 +0200

    AMBARI-9422. Group Add In Any Hooks Fail Because of Invalid Group Name after Upgrade (aonishuk)

--- ambari-server/src/test/java/org/apache/ambari/server/upgrade/UpgradeCatalog200Test.java
+++ ambari-server/src/test/java/org/apache/ambari/server/upgrade/UpgradeCatalog200Test.java
@@ -307,18 +307,24 @@ public class UpgradeCatalog200Test {
     Method addNewConfigurationsFromXml = AbstractUpgradeCatalog.class.getDeclaredMethod
         ("addNewConfigurationsFromXml");
     Method setSecurityType = UpgradeCatalog200.class.getDeclaredMethod("setSecurityType");
+    Method updateDfsClusterAdmintistratorsProperty = UpgradeCatalog200.class.getDeclaredMethod("updateDfsClusterAdmintistratorsProperty");
 
     UpgradeCatalog200 upgradeCatalog = createMockBuilder(UpgradeCatalog200.class)
         .addMockedMethod(removeNagiosService)
         .addMockedMethod(updateHiveDatabaseType)
         .addMockedMethod(addNewConfigurationsFromXml)
         .addMockedMethod(setSecurityType)
+        .addMockedMethod(updateDfsClusterAdmintistratorsProperty)
         .createMock();
 
     upgradeCatalog.removeNagiosService();
     expectLastCall().once();
     upgradeCatalog.addNewConfigurationsFromXml();
     expectLastCall();
+
+    upgradeCatalog.updateDfsClusterAdmintistratorsProperty();
+    expectLastCall();
+    
     upgradeCatalog.updateHiveDatabaseType();
     expectLastCall().once();
     upgradeCatalog.setSecurityType();
