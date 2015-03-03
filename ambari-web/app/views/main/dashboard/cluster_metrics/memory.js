cb8672ef99955461d90292c93ca02750e780127d
Alex Antonenko <hiveww@gmail.com>
2015-03-03 19:41:24 +0200

    AMBARI-9898. Memory Usage widget shows wrong value (alexantonenko)

--- ambari-web/app/views/main/dashboard/cluster_metrics/memory.js
+++ ambari-web/app/views/main/dashboard/cluster_metrics/memory.js
@@ -38,11 +38,22 @@ App.ChartClusterMetricsMemory = App.ChartLinearTimeView.extend({
   transformToSeries: function (jsonData) {
     var seriesArray = [];
     if (jsonData && jsonData.metrics && jsonData.metrics.memory) {
+      var isAmbariMetricsAvailable = App.StackService.find().someProperty('serviceName', 'AMBARI_METRICS');
+      var isAmbariMetricsInstalled = App.Service.find().someProperty('serviceName', 'AMBARI_METRICS');
+      var isGangliaInstalled = App.Service.find().someProperty('serviceName', 'GANGLIA');
+      var shouldConvertToBytes = isAmbariMetricsInstalled || isAmbariMetricsAvailable && !isGangliaInstalled;
+      var KB = Math.pow(2, 10);
       for ( var name in jsonData.metrics.memory) {
         var displayName = name;
         var seriesData = jsonData.metrics.memory[name];
         if (seriesData) {
-          seriesArray.push(this.transformData(seriesData, displayName));
+          var s = this.transformData(seriesData, displayName);
+          if (shouldConvertToBytes) {
+            for (var i = 0; i < s.data.length; i++) {
+              s.data[i].y *= KB;
+            }
+          }
+          seriesArray.push(s);
         }
       }
     }
