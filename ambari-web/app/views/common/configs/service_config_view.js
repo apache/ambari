e813af2fe942fba96627f54744d9ee758d328830
aBabiichuk <ababiichuk@cybervisiontech.com>
2015-03-16 19:35:05 +0200

    AMBARI-10087 Refactor existing configs to support tabs of configs with Advanced tab. (ababiichuk)

--- ambari-web/app/views/common/configs/service_config_view.js
+++ ambari-web/app/views/common/configs/service_config_view.js
@@ -36,6 +36,18 @@ App.ServiceConfigView = Em.View.extend({
         return false;
     }
   }.property('controller.name', 'controller.selectedService'),
+
+  /**
+   * Check for layout config supports.
+   * @returns {Boolean}
+   */
+  supportsConfigLayout: function() {
+    var supportedControllers = ['wizardStep7Controller', 'mainServiceInfoConfigsController'];
+    var unSupportedServices = ['MISC'];
+
+    return supportedControllers.contains(this.get('controllerName')) || !unSupportedServices.contains(this.get('controller.selectedService.serviceName'));
+  }.property('controller.name', 'controller.selectedService'),
+
   showConfigHistoryFeature: false,
   toggleRestartMessageView: function () {
     this.$('.service-body').toggle('blind', 200);
@@ -71,5 +83,47 @@ App.ServiceConfigView = Em.View.extend({
   }.observes(
       'App.router.mainServiceInfoConfigsController.selectedConfigGroup.name',
       'App.router.wizardStep7Controller.selectedConfigGroup.name'
-    )
+  ),
+
+  // TODO remove workaround for `Advanced` tab after Tab model implementation
+  /**
+   * Create `Advanced` tab which is not related to fancy tab section.
+   * This tab contains properties without widgets.
+   *
+   * @returns {Ember.Object}
+   */
+  getAdvancedTab: function() {
+    var tabId = 'advanced-' + this.get('controller.selectedService.serviceName').toLowerCase() + '-tab';
+    return this.createTabObject(Em.Object.create({
+      id: tabId,
+      name: Em.I18n.t('common.advanced')
+    }));
+  },
+
+  /**
+   * @param {Ember.Object} model
+   * @returns {Ember.Object}
+   */
+  createTabObject: function(model) {
+    return Em.Object.create({
+      id: model.get('id'),
+      displayName: model.get('name'),
+      tabClassName: '.' + model.get('id'),
+      isAdvanced: model.get('name') == Em.I18n.t('common.advanced')
+    });
+  },
+
+  /**
+   * Object that used for Twitter Bootstrap tabs markup.
+   *
+   * @returns {Ember.A}
+   */
+  tabs: function() {
+    var tabs = Em.A([]);
+    // filter Tab models by service name and create object here
+    tabs.pushObject(this.getAdvancedTab());
+    // make first tab active
+    tabs.get('firstObject').set('isActive', true);
+    return tabs;
+  }.property('controller.selectedService.serviceName')
 });
