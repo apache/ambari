efbc665c783b40ee0c2a465597cd0d601ab248a8
Myroslav Papirkovskyy <mpapyrkovskyy@hortonworks.com>
2015-05-18 20:10:55 +0300

    AMBARI-11221. ORA-00942: table or view does not exist when executing Ambari-DDL-Oracle-CREATE.sql. (mpapirkovskyy)

--- ambari-server/src/main/resources/Ambari-DDL-Oracle-CREATE.sql
+++ ambari-server/src/main/resources/Ambari-DDL-Oracle-CREATE.sql
@@ -1042,31 +1042,6 @@ CREATE TABLE clusterEvent (
 
 -- Quartz tables
 
-delete from qrtz_fired_triggers;
-delete from qrtz_simple_triggers;
-delete from qrtz_simprop_triggers;
-delete from qrtz_cron_triggers;
-delete from qrtz_blob_triggers;
-delete from qrtz_triggers;
-delete from qrtz_job_details;
-delete from qrtz_calendars;
-delete from qrtz_paused_trigger_grps;
-delete from qrtz_locks;
-delete from qrtz_scheduler_state;
-
-drop table qrtz_calendars;
-drop table qrtz_fired_triggers;
-drop table qrtz_blob_triggers;
-drop table qrtz_cron_triggers;
-drop table qrtz_simple_triggers;
-drop table qrtz_simprop_triggers;
-drop table qrtz_triggers;
-drop table qrtz_job_details;
-drop table qrtz_paused_trigger_grps;
-drop table qrtz_locks;
-drop table qrtz_scheduler_state;
-
-
 CREATE TABLE qrtz_job_details
   (
     SCHED_NAME VARCHAR2(120) NOT NULL,
