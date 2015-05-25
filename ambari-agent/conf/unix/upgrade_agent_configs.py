7102a7765be17338d5d2d4d6ce4558dcfc30e93b
Andrew Onishuk <aonishuk@hortonworks.com>
2015-05-25 17:14:55 +0300

    AMBARI-11370. Agent configs upgrade fails on upgrade 1.6.0 -> 2.0.1 (aonishuk)

--- ambari-agent/conf/unix/upgrade_agent_configs.py
+++ ambari-agent/conf/unix/upgrade_agent_configs.py
@@ -23,6 +23,9 @@ import ConfigParser
 PROPERTIES_TO_REWRITE = [
   ('heartbeat', 'dirs')
 ]
+SECTIONS_TO_REMOVE = [
+  'stack', 'puppet', 'command', 'python'
+]
 
 CONFIG_FILE_BACKUP = '/etc/ambari-agent/conf/ambari-agent.ini.old'
 CONFIG_FILE = '/etc/ambari-agent/conf/ambari-agent.ini'
@@ -30,7 +33,7 @@ CONFIG_FILE = '/etc/ambari-agent/conf/ambari-agent.ini'
 if os.path.isfile(CONFIG_FILE_BACKUP):
   if os.path.isfile(CONFIG_FILE):
     print "Upgrading configs in {0}".format(CONFIG_FILE)
-    print "Values will be updated from {0} except the following list: {1}".format(CONFIG_FILE_BACKUP, PROPERTIES_TO_REWRITE)
+    print "Values will be updated from {0} except the following list: {1}, {2}".format(CONFIG_FILE_BACKUP, PROPERTIES_TO_REWRITE, SECTIONS_TO_REMOVE)
 
     agent_config_backup = ConfigParser.ConfigParser()
     agent_config_backup.read(CONFIG_FILE_BACKUP)
@@ -40,8 +43,12 @@ if os.path.isfile(CONFIG_FILE_BACKUP):
 
     for section in agent_config_backup.sections():
       for (property_name, property_val) in agent_config_backup.items(section):
-        if (section, property_name) not in PROPERTIES_TO_REWRITE:
-          agent_config.set(section, property_name, property_val)
+        if section not in SECTIONS_TO_REMOVE and (section, property_name) not in PROPERTIES_TO_REWRITE:
+          try:
+            agent_config.set(section, property_name, property_val)
+          except ConfigParser.NoSectionError:
+            agent_config.add_section(section)
+            agent_config.set(section, property_name, property_val)
 
     with (open(CONFIG_FILE, "wb")) as new_agent_config:
       agent_config.write(new_agent_config)
