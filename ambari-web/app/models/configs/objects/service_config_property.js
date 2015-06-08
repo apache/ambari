6fdb2ea763d4e46e4ae8825e4fd0c0ff572f8474
Oleg Nechiporenko <onechiporenko@apache.org>
2015-06-08 14:56:04 +0300

    AMBARI-11782. DataNode directories overrides values with "\n" symbol (onechiporenko)

--- ambari-web/app/models/configs/objects/service_config_property.js
+++ ambari-web/app/models/configs/objects/service_config_property.js
@@ -475,14 +475,14 @@ App.ServiceConfigProperty = Em.Object.extend({
   _validateOverrides: function () {
     var self = this;
     var isError = false;
-    var value = '' + this.get('value');
+    var value = this._getValueForCheck(this.get('value'));
     var isOriginalSCP = this.get('isOriginalSCP');
     var supportsFinal = this.get('supportsFinal');
     var isFinal = this.get('isFinal');
     var parentSCP = this.get('parentSCP');
     if (!isOriginalSCP) {
       if (!Em.isNone(parentSCP)) {
-        if (value === '' + parentSCP.get('value')) {
+        if (value === this._getValueForCheck(parentSCP.get('value'))) {
           if (supportsFinal) {
             if (isFinal === parentSCP.get('isFinal')) {
               this.set('errorMessage', Em.I18n.t('config.override.valueEqualToParentConfig'));
@@ -499,7 +499,7 @@ App.ServiceConfigProperty = Em.Object.extend({
           if (overrides) {
             overrides.forEach(function (override) {
               if (self == override) return;
-              if (value === '' + override.get('value')) {
+              if (value === self._getValueForCheck(override.get('value'))) {
                 if (supportsFinal) {
                   if (isFinal === parentSCP.get('isFinal')) {
                     self.set('errorMessage', Em.I18n.t('config.override.valueEqualToAnotherOverrideConfig'));
@@ -517,6 +517,24 @@ App.ServiceConfigProperty = Em.Object.extend({
       }
     }
     return isError;
+  },
+
+  /**
+   * Some values should be little bit changed before checking for overrides values
+   * `directories`-values should be "trimmed" for multiple mew-line symbols
+   * @param {string} value
+   * @returns {string}
+   * @private
+   */
+  _getValueForCheck: function (value) {
+    value = '' + value;
+    switch(this.get('displayType')) {
+      case 'directories':
+        return value.replace(/(\n\r?)+/g, '\n');
+        break;
+      default:
+        return value;
+    }
   }
 
 });
