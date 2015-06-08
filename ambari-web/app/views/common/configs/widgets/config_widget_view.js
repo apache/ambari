60f1b553bcfd361aa011f5ae9111ba1f7561c8c6
Yusaku Sako <yusaku@hortonworks.com>
2015-06-07 21:41:26 -0700

    AMBARI-11638. Configs: Different values in warnings tooltip when config is in raw mode. (Richard Zang via yusaku)

--- ambari-web/app/views/common/configs/widgets/config_widget_view.js
+++ ambari-web/app/views/common/configs/widgets/config_widget_view.js
@@ -432,8 +432,8 @@ App.ConfigWidgetView = Em.View.extend(App.SupportsDependentConfigs, App.WidgetPo
   updateWarningsForCompatibilityWithWidget: function (message) {
     this.setProperties({
       warnMessage: message,
+      'config.warnMessage': message,
       issueMessage: message,
-      iconClass: message ? 'warning' : '',
       configLabelClass: message ? 'text-warning' : ''
     });
   }
