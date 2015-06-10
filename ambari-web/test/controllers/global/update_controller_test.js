9b75f819e5acd7c46a029bde4f755abd440d501f
Jaimin Jetly <jaimin@hortonworks.com>
2015-06-10 14:49:03 -0700

    AMBARI-11821. With HBase master HA Ambari sometimes displays incorrect dashboard information. (jaimin)

--- ambari-web/test/controllers/global/update_controller_test.js
+++ ambari-web/test/controllers/global/update_controller_test.js
@@ -147,11 +147,11 @@ describe('App.UpdateController', function () {
           }
         ],
         result: ["host_components/metrics/yarn/Queue," +
-          "ServiceComponentInfo/rm_metrics/cluster/activeNMcount," +
-          "ServiceComponentInfo/rm_metrics/cluster/lostNMcount," +
-          "ServiceComponentInfo/rm_metrics/cluster/unhealthyNMcount," +
-          "ServiceComponentInfo/rm_metrics/cluster/rebootedNMcount," +
-          "ServiceComponentInfo/rm_metrics/cluster/decommissionedNMcount"]
+        "host_components/metrics/yarn/ClusterMetrics/NumActiveNMs," +
+        "host_components/metrics/yarn/ClusterMetrics/NumLostNMs," +
+        "host_components/metrics/yarn/ClusterMetrics/NumUnhealthyNMs," +
+        "host_components/metrics/yarn/ClusterMetrics/NumRebootedNMs," +
+        "host_components/metrics/yarn/ClusterMetrics/NumDecommissionedNMs"]
       },
       {
         title: 'HBASE service',
@@ -163,11 +163,10 @@ describe('App.UpdateController', function () {
           }
         ],
         result: ["host_components/metrics/hbase/master/IsActiveMaster," +
-          "ServiceComponentInfo/MasterStartTime," +
-          "ServiceComponentInfo/MasterActiveTime," +
-          "ServiceComponentInfo/AverageLoad," +
-          "ServiceComponentInfo/Revision," +
-          "ServiceComponentInfo/RegionsInTransition"]
+        "host_components/metrics/hbase/master/MasterStartTime," +
+        "host_components/metrics/hbase/master/MasterActiveTime," +
+        "host_components/metrics/hbase/master/AverageLoad," +
+        "host_components/metrics/master/AssignmentManger/ritCount"]
       },
       {
         title: 'STORM service',
