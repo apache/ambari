5b9d1a5245d8d98f9c7eb1afc6b3d26df938af85
Alex Antonenko <hiveww@gmail.com>
2015-06-11 18:58:31 +0300

    Revert "AMBARI-11821. With HBase master HA Ambari sometimes displays incorrect dashboard information. (jaimin)"
    This reverts commit 83ce96d760d653fd07eef286226ef1a387f7bfaa.

--- ambari-web/test/mappers/service_mapper_test.js
+++ ambari-web/test/mappers/service_mapper_test.js
@@ -32,112 +32,48 @@ describe('App.serviceMetricsMapper', function () {
         {
           components: [
             {
-              ServiceComponentInfo: {
-                component_name: "HBASE_MASTER",
-                RegionsInTransition : [ ]
-              },
-              host_components: [
-                {
-                  metrics:{
-                    hbase: {
-                      master: {
-                        IsActiveMaster: 'true',
-                        AverageLoad: 1.23456789
-                      }
-                    },
-                    master: {
-                      AssignmentManger: {
-                        ritCount: 0
-                      }
-                    }
-                  }
+                ServiceComponentInfo: {
+                  AverageLoad: 1.23456789,
+                  component_name: "HBASE_MASTER",
+                  RegionsInTransition : [ ]
                 }
-              ]
-            }
+              }
           ],
           e: '1.23'
         },
         {
           components: [
             {
-              ServiceComponentInfo: {
-                component_name: "HBASE_MASTER",
-                RegionsInTransition : [ ]
-              },
-              host_components: [
-                {
-                  metrics:{
-                    hbase: {
-                      master: {
-                        IsActiveMaster: 'true',
-                        AverageLoad: 1.00
-                      }
-                    },
-                    master: {
-                      AssignmentManger: {
-                        ritCount: 0
-                      }
-                    }
-                  }
+                ServiceComponentInfo: {
+                  AverageLoad: 1.00,
+                  component_name: "HBASE_MASTER",
+                  RegionsInTransition : [ ]
                 }
-              ]
-            }
+              }
           ],
           e: '1.00'
         },
         {
           components: [
             {
-              ServiceComponentInfo: {
-                component_name: "HBASE_MASTER",
-                RegionsInTransition : [ ]
-              },
-              host_components: [
-                {
-                  metrics:{
-                    hbase: {
-                      master: {
-                        IsActiveMaster: 'true',
-                        AverageLoad: 1
-                      }
-                    },
-                    master: {
-                      AssignmentManger: {
-                        ritCount: 0
-                      }
-                    }
-                  }
+                ServiceComponentInfo: {
+                  AverageLoad: 1,
+                  component_name: "HBASE_MASTER",
+                  RegionsInTransition : [ ]
                 }
-              ]
-            }
+              }
           ],
           e: '1.00'
         },
         {
           components: [
             {
-              ServiceComponentInfo: {
-                component_name: "HBASE_MASTER",
-                RegionsInTransition : [ ]
-              },
-              host_components: [
-                {
-                  metrics:{
-                    hbase: {
-                      master: {
-                        IsActiveMaster: 'true',
-                        AverageLoad: 1.2
-                      }
-                    },
-                    master: {
-                      AssignmentManger: {
-                        ritCount: 0
-                      }
-                    }
-                  }
+                ServiceComponentInfo: {
+                  AverageLoad: 1.2,
+                  component_name: "HBASE_MASTER",
+                  RegionsInTransition : [ ]
                 }
-              ]
-            }
+              }
           ],
           e: '1.20'
         }
