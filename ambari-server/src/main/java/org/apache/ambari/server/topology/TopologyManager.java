80c221bd407469cdc47de187aa1d65a43be5c3a4
John Speidel <jspeidel@hortonworks.com>
2015-06-12 10:35:04 -0400

    AMBARI-11886.  return 400 response and good error msg when user attempts                to use API to scale a UI provisioned cluster

--- ambari-server/src/main/java/org/apache/ambari/server/topology/TopologyManager.java
+++ ambari-server/src/main/java/org/apache/ambari/server/topology/TopologyManager.java
@@ -117,7 +117,11 @@ public class TopologyManager {
     String clusterName = request.getClusterName();
     ClusterTopology topology = clusterTopologyMap.get(clusterName);
     if (topology == null) {
-      throw new AmbariException("TopologyManager: Unable to retrieve cluster topology for cluster: " + clusterName);
+      throw new InvalidTopologyException("Unable to retrieve cluster topology for cluster. This is most likely a " +
+                                         "result of trying to scale a cluster via the API which was created using " +
+                                         "the Ambari UI. At this time only clusters created via the API using a " +
+                                         "blueprint can be scaled with this API.  If the cluster was originally created " +
+                                         "via the API as described above, please file a Jira for this matter.");
     }
 
     PersistedTopologyRequest persistedRequest = persistedState.persistTopologyRequest(request);
