832bfe880fb99f5355f457a15606438d84ad9834
Alejandro Fernandez <afernandez@hortonworks.com>
2015-06-19 13:28:54 -0700

    AMBARI-12013. Datanode failed to restart during RU because the shutdownDatanode -upgrade command can fail sometimes (alejandro)

--- ambari-server/src/test/python/stacks/2.0.6/HDFS/test_datanode.py
+++ ambari-server/src/test/python/stacks/2.0.6/HDFS/test_datanode.py
@@ -545,9 +545,12 @@ class TestDatanode(RMFTestCase):
       self.assertEqual(mocks_dict['call'].call_count,12)
 
 
+  @patch("resource_management.core.shell.call")
   @patch('time.sleep')
-  def test_stop_during_upgrade(self, time_mock):
+  def test_stop_during_upgrade(self, time_mock, call_mock):
     config_file = self.get_src_folder()+"/test/python/stacks/2.0.6/configs/default.json"
+    call_mock_side_effects = [(0, ""), ]
+    call_mock.side_effects = call_mock_side_effects
     with open(config_file, "r") as f:
       json_content = json.load(f)
 
@@ -561,15 +564,16 @@ class TestDatanode(RMFTestCase):
         config_dict = json_content,
         hdp_stack_version = self.STACK_VERSION,
         target = RMFTestCase.TARGET_COMMON_SERVICES,
+        call_mocks = call_mock_side_effects,
         command_args=[True])
 
       raise Fail("Expected a fail since datanode didn't report a shutdown")
-    except:
-      pass
+    except Exception, err:
+      expected_message = "DataNode has not shutdown."
+      if str(err.message) != expected_message:
+        self.fail("Expected this exception to be thrown. " + expected_message + ". Got this instead, " + str(err.message))
 
-    self.assertResourceCalled('Execute', 'hdfs dfsadmin -shutdownDatanode 0.0.0.0:8010 upgrade', user="hdfs", tries=1)
-    self.assertResourceCalled('Execute', 'hdfs dfsadmin -D ipc.client.connect.max.retries=5 -D ipc.client.connect.retry.interval=1000 -getDatanodeInfo 0.0.0.0:8010', user="hdfs", tries=1)
-
+    self.assertResourceCalled("Execute", "hdfs dfsadmin -D ipc.client.connect.max.retries=5 -D ipc.client.connect.retry.interval=1000 -getDatanodeInfo 0.0.0.0:8010", tries=1, user="hdfs")
 
   @patch("resource_management.libraries.functions.security_commons.build_expectations")
   @patch("resource_management.libraries.functions.security_commons.get_params_from_filesystem")
