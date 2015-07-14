5d894952e025928d155ec2da9e17c17919bc7a38
Andrew Onishuk <aonishuk@hortonworks.com>
2015-07-14 16:41:29 +0300

    AMBARI-12406. Can't download client configs with ambari-server installed using non root user  (aonishuk)

--- ambari-server/conf/unix/ambari-sudo.sh
+++ ambari-server/conf/unix/ambari-sudo.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -11,11 +12,30 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
 
+# since ambari-server doesn't have sudo permissions this file is just a mock file
+# which always runs commands without sudo.
+ENV=()
+SUDO_ARGS=()
 
-AMBARI_PASSHPHRASE="DEV"
-export AMBARI_JVM_ARGS=$AMBARI_JVM_ARGS' -Xms512m -Xmx2048m -Djava.security.auth.login.config=/etc/ambari-server/conf/krb5JAASLogin.conf -Djava.security.krb5.conf=/etc/krb5.conf -Djavax.security.auth.useSubjectCredsOnly=false'
-export PATH=$PATH:/var/lib/ambari-server
-# to add additional directory or jar to server classpath use SERVER_CLASSPATH variable
-# export SERVER_CLASSPATH=/etc/hadoop/conf/secure
+for i ; do
+  if [[ "$i" == *"="* ]] ; then
+    ENV+=("$i")
+    shift
+  elif [[ "$i" == "-"* ]] ; then
+    SUDO_ARGS+=("$i")
+    shift
+  else
+    break
+  fi
+done
+  
+#echo "sudo arguments: ${SUDO_ARGS[@]}"
+#echo "env: ${ENV[@]}"
+#echo "args: $@"
+
+if [ "$ENV" ] ; then
+  export "${ENV[@]}"
+fi
+
+"$@"
