f97945c9f441882094734b01a41726767249bb56
Andrew Onishuk <aonishuk@hortonworks.com>
2015-07-28 15:51:01 +0300

    AMBARI-12564. Logging of exceptions in python code in Ambari Agent (aonishuk)

--- ambari-agent/src/main/python/ambari_agent/main.py
+++ ambari-agent/src/main/python/ambari_agent/main.py
@@ -47,6 +47,7 @@ from HeartbeatHandlers import bind_signal_handlers
 from ambari_commons.constants import AMBARI_SUDO_BINARY
 logger = logging.getLogger(__name__)
 
+is_logger_setup = False
 formatstr = "%(levelname)s %(asctime)s %(filename)s:%(lineno)d - %(message)s"
 agentPid = os.getpid()
 config = AmbariConfig.AmbariConfig()
@@ -72,6 +73,9 @@ def setup_logging(verbose):
     logging.basicConfig(format=formatstr, level=logging.INFO, filename=AmbariConfig.AmbariConfig.getLogFile())
     logger.setLevel(logging.INFO)
     logger.info("loglevel=logging.INFO")
+    
+  global is_logger_setup
+  is_logger_setup = True
 
 def add_syslog_handler(logger):
     
@@ -301,6 +305,12 @@ def main(heartbeat_stop_callback=None):
   logger.info("finished")
 
 if __name__ == "__main__":
-  heartbeat_stop_callback = bind_signal_handlers(agentPid)
-
-  main(heartbeat_stop_callback)
+  try:
+    heartbeat_stop_callback = bind_signal_handlers(agentPid)
+  
+    main(heartbeat_stop_callback)
+  except:
+    global is_logger_setup
+    if is_logger_setup:
+      logger.exception("Fatal exception occurred:")
+    raise
