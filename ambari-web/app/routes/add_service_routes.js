49b47c354fae80b3c0f748d756f439d5553d8e7e
Richard Zang <rzang@apache.org>
2015-07-31 17:09:13 -0700

    AMBARI-12141. Kerberos configs aren't restored when moving back from Review to the Configure Identities step (rzang)

--- ambari-web/app/routes/add_service_routes.js
+++ ambari-web/app/routes/add_service_routes.js
@@ -242,6 +242,7 @@ module.exports = App.WizardRoute.extend({
       addServiceController.saveServiceConfigProperties(wizardStep7Controller);
       addServiceController.saveServiceConfigGroups(wizardStep7Controller, true);
       if (App.get('isKerberosEnabled')) {
+        addServiceController.clearCachedStepConfigValues(router.get('kerberosWizardStep4Controller'));
         router.transitionTo('step5');
         return;
       }
@@ -288,6 +289,7 @@ module.exports = App.WizardRoute.extend({
         } else {
           router.get('kerberosWizardStep2Controller').createKerberosAdminSession(router.get('kerberosWizardStep4Controller.stepConfigs')[0].get('configs'));
         }
+        router.get('addServiceController').cacheStepConfigValues(router.get('kerberosWizardStep4Controller'));
       }
       router.transitionTo('step6');
     }
--- ambari-web/app/utils/config.js
+++ ambari-web/app/utils/config.js
@@ -1412,6 +1412,23 @@ App.config = Em.Object.create({
     return newOverride;
   },
 
+
+  /**
+   * Merge values in "stored" to "base" if name matches, it's a value only merge.
+   * @param base {Array} Em.Object
+   * @param stored {Array} Object
+   */
+  mergeStoredValue: function(base, stored) {
+    if (stored) {
+      base.forEach(function (p) {
+        var sp = stored.findProperty("name", p.name);
+        if (sp) {
+          p.set("value", sp.value);
+        }
+      });
+    }
+  },
+
   /**
    * Update config property value based on its current value and list of zookeeper server hosts.
    * Used to prevent sort order issues.
