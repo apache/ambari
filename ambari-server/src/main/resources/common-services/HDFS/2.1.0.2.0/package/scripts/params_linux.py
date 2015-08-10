649703e17fdfe30c9a6810a0d14a8bb9985be2dd
Sumit Mohanty <smohanty@hortonworks.com>
2015-08-10 09:43:22 -0700

    AMBARI-12681. Namenode crashes with - No valid image files found (smohanty)

--- ambari-server/src/main/resources/common-services/HDFS/2.1.0.2.0/package/scripts/params_linux.py
+++ ambari-server/src/main/resources/common-services/HDFS/2.1.0.2.0/package/scripts/params_linux.py
@@ -133,6 +133,7 @@ falcon_user = config['configurations']['falcon-env']['falcon_user']
 hdfs_exclude_file = default("/clusterHostInfo/decom_dn_hosts", [])
 exclude_file_path = config['configurations']['hdfs-site']['dfs.hosts.exclude']
 update_exclude_file_only = default("/commandParams/update_exclude_file_only",False)
+command_phase = default("/commandParams/phase","")
 
 klist_path_local = get_klist_path(default('/configurations/kerberos-env/executable_search_paths', None))
 kinit_path_local = get_kinit_path(default('/configurations/kerberos-env/executable_search_paths', None))
