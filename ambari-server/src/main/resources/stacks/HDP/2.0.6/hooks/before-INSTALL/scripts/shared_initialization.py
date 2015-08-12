e12cba522e2bb1df2b15ead94b86e2133ea1e79b
Vitaly Brodetskyi <vbrodetskyi@hortonworks.com>
2015-08-12 12:15:23 +0300

    AMBARI-12282. Ambari JDK8 upgrade does not deploy JDK8 on DataNodes/NodeManager on rerun of setup.(vbrodetskyi)

--- ambari-server/src/main/resources/stacks/HDP/2.0.6/hooks/before-INSTALL/scripts/shared_initialization.py
+++ ambari-server/src/main/resources/stacks/HDP/2.0.6/hooks/before-INSTALL/scripts/shared_initialization.py
@@ -20,61 +20,6 @@ limitations under the License.
 import os
 
 from resource_management import *
-import getpass
-
-def setup_java():
-  """
-  Installs jdk using specific params, that comes from ambari-server
-  """
-  import params
-
-  jdk_curl_target = format("{tmp_dir}/{jdk_name}")
-  java_dir = os.path.dirname(params.java_home)
-  java_exec = format("{java_home}/bin/java")
-  tmp_java_dir = format("{tmp_dir}/jdk")
-
-  if not params.jdk_name:
-    return
-
-  Directory(params.artifact_dir,
-      recursive = True,
-  )
-
-  File(jdk_curl_target,
-       content = DownloadSource(format("{jdk_location}/{jdk_name}")),
-       not_if = format("test -f {jdk_curl_target}")
-  )
-
-  if params.jdk_name.endswith(".bin"):
-    chmod_cmd = ("chmod", "+x", jdk_curl_target)
-    install_cmd = format("mkdir -p {tmp_java_dir} && cd {tmp_java_dir} && echo A | {jdk_curl_target} -noregister && {sudo} cp -rp {tmp_java_dir}/* {java_dir}")
-  elif params.jdk_name.endswith(".gz"):
-    chmod_cmd = ("chmod","a+x", java_dir)
-    install_cmd = format("mkdir -p {tmp_java_dir} && cd {tmp_java_dir} && tar -xf {jdk_curl_target} && {sudo} cp -rp {tmp_java_dir}/* {java_dir}")
-
-  Directory(java_dir
-  )
-  
-  Execute(chmod_cmd,
-          not_if = format("test -e {java_exec}"),
-          sudo = True,
-  )
-
-  Execute(install_cmd,
-          not_if = format("test -e {java_exec}")
-  )
-  
-  File(format("{java_home}/bin/java"),
-          mode=0755,
-          cd_access="a",
-  )
-  
-  Execute(("chgrp","-R", params.user_group, params.java_home),
-          sudo = True,          
-  )
-  Execute(("chown","-R", getpass.getuser(), params.java_home),
-          sudo = True,          
-  )
 
 def install_packages():
   import params
