a7dfbfa45c274aec6d4bd24c676b8ec8b8c7a689
aBabiichuk <ababiichuk@cybervisiontech.com>
2015-09-01 16:36:25 +0300

    AMBARI-12958 SQLA: when "db name" for hive/oozie changed, "jdbc class" changes too so recommendations should be called. (ababiichuk)

--- ambari-web/test/views/common/controls_view_test.js
+++ ambari-web/test/views/common/controls_view_test.js
@@ -251,6 +251,7 @@ describe('App.ServiceConfigRadioButtons', function () {
 
     afterEach(function () {
       App.get.restore();
+      view.sendRequestRorDependentConfigs.restore();
     });
 
     after(function () {
@@ -261,6 +262,7 @@ describe('App.ServiceConfigRadioButtons', function () {
       it(item.title, function () {
         sinon.stub(App, 'get').withArgs('currentStackName').returns('HDP').withArgs('currentStackVersion').returns(item.currentStackVersion);
         view = App.ServiceConfigRadioButtons.create({parentView: item.parentView});
+        sinon.stub(view, 'sendRequestRorDependentConfigs', Em.K);
         view.setProperties({
           categoryConfigsAll: item.parentView.get('serviceConfigs'),
           serviceConfig: item.serviceConfig
