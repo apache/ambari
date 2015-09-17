3ce0bea5ae49eaad1dab2921cd59c9b133798efd
Myroslav Papirkovskyy <mpapyrkovskyy@hortonworks.com>
2015-09-17 21:39:12 +0300

    AMBARI-13027. AMS status should indicate failure in a dead RS scenario. (mpapirkovskyy)

--- ambari-server/src/main/resources/common-services/AMBARI_METRICS/0.1.0/package/scripts/hbase.py
+++ ambari-server/src/main/resources/common-services/AMBARI_METRICS/0.1.0/package/scripts/hbase.py
@@ -197,6 +197,9 @@ def hbase(name=None # 'master' or 'regionserver' or 'client'
 
         params.HdfsResource(None, action="execute")
 
+        #Workaround for status commands not aware of operating mode
+        File(format("{params.hbase_pid_dir}/distributed_mode"), action="create", mode=0644, owner=params.hbase_user)
+
       pass
 
     else:
@@ -212,6 +215,8 @@ def hbase(name=None # 'master' or 'regionserver' or 'client'
                 cd_access="a",
                 recursive = True
       )
+
+      File(format("{params.hbase_pid_dir}/distributed_mode"), action="delete", owner=params.hbase_user)
 
   if name != "client":
     Directory( params.hbase_pid_dir,
