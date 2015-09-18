6ea0cd9a0bb8e7dc6898a0016d9d229035cb3f56
Oleg Nechiporenko <onechiporenko@apache.org>
2015-09-18 14:40:26 +0300

    AMBARI-13143. Links from alerts-popup to host/service don't work (onechiporenko)

--- ambari-web/test/views/main/alerts/definition_details_view_test.js
+++ ambari-web/test/views/main/alerts/definition_details_view_test.js
@@ -30,4 +30,25 @@ describe('App.MainAlertDefinitionDetailsView', function () {
 
   });
 
+  describe("#goToHostAlerts()", function () {
+    beforeEach(function () {
+      sinon.stub(App.get('router'), 'transitionTo', Em.K);
+    });
+    afterEach(function () {
+      App.get('router').transitionTo.restore();
+    });
+    it("not route to host - no event", function () {
+      instanceTableRow.goToHostAlerts(null);
+      expect(App.get('router').transitionTo.notCalled).to.be.true;
+    });
+    it("not route to host - no event context", function () {
+      instanceTableRow.goToHostAlerts({});
+      expect(App.get('router').transitionTo.notCalled).to.be.true;
+    });
+    it("routes to host", function () {
+      instanceTableRow.goToHostAlerts({"context": "hostname"});
+      expect(App.get('router').transitionTo.calledOnce).to.be.true;
+    });
+  });
+
 });
