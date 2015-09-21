7e0965f557500a5951c638aa3f6ed5632464acc3
Dmytro Sen <dsen@apache.org>
2015-09-21 18:46:04 +0300

    AMBARI-13122 [Upgrade] ambari server schema upgrade failed for Path 1.6.1 -> 2.1.2 and 2.0.2->2.1.2 and 2.1.1->2.1.2 (dsen)

--- ambari-server/src/test/python/TestAmbariServer.py
+++ ambari-server/src/test/python/TestAmbariServer.py
@@ -3824,6 +3824,7 @@ MIIFHjCCAwYCCQDpHKOBI+Lt0zANBgkqhkiG9w0BAQUFADBRMQswCQYDVQQGEwJV
     pass
 
 
+  @patch("ambari_server.serverUpgrade.ensure_jdbc_driver_is_installed")
   @patch("ambari_server.serverUpgrade.get_jdbc_driver_path")
   @patch("ambari_server.serverUpgrade.ensure_can_start_under_current_user")
   @patch("ambari_server.serverUpgrade.generate_env")
@@ -3837,7 +3838,8 @@ MIIFHjCCAwYCCQDpHKOBI+Lt0zANBgkqhkiG9w0BAQUFADBRMQswCQYDVQQGEwJV
   def test_run_schema_upgrade(self, get_YN_input_mock, get_ambari_properties_mock, java_exe_path_mock, run_os_command_mock,
                               get_ambari_classpath_mock, get_conf_dir_mock,
                               read_ambari_user_mock, generate_env_mock,
-                              ensure_can_start_under_current_user_mock, get_jdbc_mock):
+                              ensure_can_start_under_current_user_mock, get_jdbc_mock,
+                              ensure_jdbc_driver_is_installed_mock):
     java_exe_path_mock.return_value = "/usr/lib/java/bin/java"
     run_os_command_mock.return_value = (0, None, None)
     get_ambari_classpath_mock.return_value = 'test:path12'
@@ -5062,7 +5064,7 @@ MIIFHjCCAwYCCQDpHKOBI+Lt0zANBgkqhkiG9w0BAQUFADBRMQswCQYDVQQGEwJV
     # Non-Silent option, no drivers at first ask, present drivers after that
     args = reset_mocks()
 
-    glob_mock.side_effect = [[], drivers_list]
+    glob_mock.side_effect = [[], drivers_list, drivers_list]
 
     dbms = factory.create(args, props)
     rcode = dbms.ensure_jdbc_driver_installed(props)
@@ -5089,7 +5091,7 @@ MIIFHjCCAwYCCQDpHKOBI+Lt0zANBgkqhkiG9w0BAQUFADBRMQswCQYDVQQGEwJV
 
     # Failed to copy_files
     args = reset_mocks()
-    glob_mock.side_effect = [drivers_list]
+    glob_mock.side_effect = [[], drivers_list, drivers_list]
 
     try:
       dbms = factory.create(args, props)
