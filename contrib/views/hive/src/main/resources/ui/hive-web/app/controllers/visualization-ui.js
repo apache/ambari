6d165213e25ff31ce64e0bd92f07ed986f8bac28
Yusaku Sako <yusaku@hortonworks.com>
2015-09-21 16:58:55 -0700

    AMBARI-13055. [Hive View Visualization] Add more options for max number of rows for reports/charts. (Pallav Kulshreshtha via yusaku)

--- contrib/views/hive/src/main/resources/ui/hive-web/app/controllers/visualization-ui.js
+++ contrib/views/hive/src/main/resources/ui/hive-web/app/controllers/visualization-ui.js
@@ -20,6 +20,7 @@ import Ember from 'ember';
 import constants from 'hive/utils/constants';
 
 export default Ember.Controller.extend({
+  selectedRowCount: constants.defaultVisualizationRowCount,
   needs: [ constants.namingConventions.index,
             constants.namingConventions.openQueries,
             constants.namingConventions.jobResults
@@ -27,11 +28,15 @@ export default Ember.Controller.extend({
   index         : Ember.computed.alias('controllers.' + constants.namingConventions.index),
   openQueries   : Ember.computed.alias('controllers.' + constants.namingConventions.openQueries),
   results   : Ember.computed.alias('controllers.' + constants.namingConventions.jobResults),
+  notifyService: Ember.inject.service(constants.namingConventions.notify),
 
   polestarUrl: '',
   voyagerUrl: '',
   polestarPath: 'polestar/#/',
   voyagerPath: 'voyager/#/',
+
+  showDataExplorer: true,
+  showAdvVisulization: false,
 
   visualizationTabs: function () {
     return [
@@ -48,6 +53,12 @@ export default Ember.Controller.extend({
     ]
   }.property('polestarUrl', 'voyagerUrl'),
 
+  activeTab: function () {
+    console.log("I am in activeTab function.");
+    this.get('visualizationTabs')[0].active = this.get("showDataExplorer");
+    this.get('visualizationTabs')[1].active = this.get("showAdvVisulization");
+  }.observes('polestarUrl', 'voyagerUrl'),
+
   alterIframe: function () {
     Ember.$("#visualization_frame").height(Ember.$("#visualization").height());
   },
@@ -60,7 +71,7 @@ export default Ember.Controller.extend({
         var existingJob = this.get('results').get('cachedResults').findBy('id', model.get('id'));
         var url = this.container.lookup('adapter:application').buildURL();
         url += '/' + constants.namingConventions.jobs + '/' + model.get('id') + '/results?&first=true';
-        url += '&count='+constants.visualizationRowCount+'&job_id='+model.get('id');
+        url += '&count='+self.get('selectedRowCount')+'&job_id='+model.get('id')
         if (existingJob) {
           if(existingJob.results[0].rows.length === 0){
             this.set("error", "Query has insufficient results to visualize the data.");
@@ -77,6 +88,47 @@ export default Ember.Controller.extend({
           this.set("error", "No visualization available. Please execute a query and wait for the results to visualize the data.");
         }
       }
-    }
+    },
+
+      changeRowCount: function () {
+        var self = this;
+        if(isNaN(self.get('selectedRowCount')) || !(self.get('selectedRowCount')%1 === 0) || (self.get('selectedRowCount') <= 0)){
+          self.get('notifyService').error("Please enter a posive integer number.");
+          return;
+        }
+        var model = this.get('index.model');
+        if (model) {
+          var existingJob = this.get('results').get('cachedResults').findBy('id', model.get('id'));
+          var url = this.container.lookup('adapter:application').buildURL();
+          url += '/' + constants.namingConventions.jobs + '/' + model.get('id') + '/results?&first=true';
+          url += '&count='+self.get('selectedRowCount')+'&job_id='+model.get('id');
+          if (existingJob) {
+            this.set("error", null);
+            var id = model.get('id');
+
+            $('.nav-tabs.visualization-tabs li.active').each(function( index ) {
+
+              if($(this)[index].innerText.indexOf("Data Explorer") > -1){
+                self.set("showDataExplorer",true);
+                self.set("showAdvVisulization",false);
+                self.set("voyagerUrl", self.get('voyagerPath') + "?url=" + url);
+                self.set("polestarUrl", self.get('polestarPath') + "?url=" + url);
+                document.getElementById("visualization_frame").src =  self.get("voyagerUrl");
+              }
+              if($(this)[index].innerText.indexOf("Advanced Visualization") > -1){
+                self.set("showAdvVisulization",true);
+                self.set("showDataExplorer",false);
+                self.set("voyagerUrl", self.get('voyagerPath') + "?url=" + url);
+                self.set("polestarUrl", self.get('polestarPath') + "?url=" + url);
+                document.getElementById("visualization_frame").src = self.get("polestarUrl");
+              }
+            })
+            document.getElementById("visualization_frame").contentWindow.location.reload();
+          } else {
+            this.set("error", "No visualization available. Please execute a query and wait for the results to visualize data.");
+          }
+        }
+
+      }
   }
 });
