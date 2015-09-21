e59f157447e2d69623ccf7d3558a118032041554
Dmytro Sen <dsen@apache.org>
2015-09-21 18:41:03 +0300

    AMBARI-13122 [Upgrade] ambari server schema upgrade failed for Path 1.6.1 -> 2.1.2 and 2.0.2->2.1.2 and 2.1.1->2.1.2 (dsen)

--- ambari-server/src/test/python/TestAmbariServer.py
+++ ambari-server/src/test/python/TestAmbariServer.py
@@ -4668,6 +4668,7 @@ class TestAmbariServer(TestCase):
     pass
 
 
+  @patch("ambari_server.serverUpgrade.ensure_jdbc_driver_is_installed")
   @patch("ambari_server.serverUpgrade.get_jdbc_driver_path")
   @patch("ambari_server.serverUpgrade.ensure_can_start_under_current_user")
   @patch("ambari_server.serverUpgrade.generate_env")
@@ -4681,7 +4682,8 @@ class TestAmbariServer(TestCase):
   def test_run_schema_upgrade(self, get_YN_input_mock, get_ambari_properties_mock, java_exe_path_mock, run_os_command_mock,
                               get_ambari_classpath_mock, get_conf_dir_mock,
                               read_ambari_user_mock, generate_env_mock,
-                              ensure_can_start_under_current_user_mock, get_jdbc_mock):
+                              ensure_can_start_under_current_user_mock, get_jdbc_mock,
+                              ensure_jdbc_driver_is_installed_mock):
     java_exe_path_mock.return_value = "/usr/lib/java/bin/java"
     run_os_command_mock.return_value = (0, None, None)
     get_ambari_classpath_mock.return_value = 'test' + os.pathsep + 'path12'
@@ -6235,7 +6237,7 @@ class TestAmbariServer(TestCase):
     # Non-Silent option, no drivers at first ask, present drivers after that
     args = reset_mocks()
 
-    glob_mock.side_effect = [[], drivers_list]
+    glob_mock.side_effect = [[], drivers_list, drivers_list]
 
     dbms = factory.create(args, props)
     rcode = dbms.ensure_jdbc_driver_installed(props)
@@ -6262,7 +6264,7 @@ class TestAmbariServer(TestCase):
 
     # Failed to copy_files
     args = reset_mocks()
-    glob_mock.side_effect = [drivers_list]
+    glob_mock.side_effect = [[], drivers_list, drivers_list]
 
     try:
       dbms = factory.create(args, props)
