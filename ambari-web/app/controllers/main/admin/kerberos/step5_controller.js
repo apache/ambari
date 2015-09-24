3ca19829908a9dc99cb7b7f0771665dc747dd621
Aleksandr Kovalenko <akovalenko@hortonworks.com>
2015-09-24 15:50:39 +0300

    AMBARI-13224. "Retry" button is not work properly on "Enable Kerberos Wizard" at "Kerberize Cluster" step (akovalenko)

--- ambari-web/app/controllers/main/admin/kerberos/step5_controller.js
+++ ambari-web/app/controllers/main/admin/kerberos/step5_controller.js
@@ -146,31 +146,30 @@ App.KerberosWizardStep5Controller = App.KerberosProgressPageController.extend({
   }.property('status'),
 
   confirmProperties: function () {
-    var kdc_type = App.router.kerberosWizardController.content.serviceConfigProperties.findProperty('name','kdc_type').value,
-      filterObject = [
-        {
-          key: Em.I18n.t('admin.kerberos.wizard.step1.option.kdc'),
-          properties: ['kdc_type','kdc_host','realm','executable_search_paths']
-        },
-        {
-          key: Em.I18n.t('admin.kerberos.wizard.step1.option.ad'),
-          properties: ['kdc_type','kdc_host','realm','ldap_url','container_dn','executable_search_paths']
-        },
-        {
-          key: Em.I18n.t('admin.kerberos.wizard.step1.option.manual'),
-          properties: ['kdc_type','realm','executable_search_paths']
-        }
-      ],
-      kdcTypeProperties = filterObject.filter(function(item) {
-        return item.key === kdc_type;
-      }),
-      filterBy = kdcTypeProperties.length ? kdcTypeProperties[0].properties : [],
-      returnArray = App.router.kerberosWizardController.content.serviceConfigProperties.filter(function(item) {
-        return filterBy.contains(item.name);
-      }).map(function(item) {
-        item['label'] = Em.I18n.t('admin.kerberos.wizard.step5.'+item['name']+'.label');
-        return item;
-      });
-    return returnArray;
+    var kdc_type = App.router.get('kerberosWizardController.content.kerberosOption'),
+        filterObject = [
+          {
+            key: Em.I18n.t('admin.kerberos.wizard.step1.option.kdc'),
+            properties: ['kdc_type', 'kdc_host', 'realm', 'executable_search_paths']
+          },
+          {
+            key: Em.I18n.t('admin.kerberos.wizard.step1.option.ad'),
+            properties: ['kdc_type', 'kdc_host', 'realm', 'ldap_url', 'container_dn', 'executable_search_paths']
+          },
+          {
+            key: Em.I18n.t('admin.kerberos.wizard.step1.option.manual'),
+            properties: ['kdc_type', 'realm', 'executable_search_paths']
+          }
+        ],
+        kdcTypeProperties = filterObject.filter(function (item) {
+          return item.key === kdc_type;
+        }),
+        filterBy = kdcTypeProperties.length ? kdcTypeProperties[0].properties : [];
+    return App.router.kerberosWizardController.content.serviceConfigProperties.filter(function (item) {
+      return filterBy.contains(item.name);
+    }).map(function (item) {
+      item['label'] = Em.I18n.t('admin.kerberos.wizard.step5.' + item['name'] + '.label');
+      return item;
+    });
   }.property('App.router.kerberosWizardController.content.@each.serviceConfigProperties')
 });
