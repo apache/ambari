fd237975500b9b4124b983ab21b1cc56a8e5ee5d
Yusaku Sako <yusaku@hortonworks.com>
2015-09-24 12:12:56 -0700

    AMBARI-12951. Prompt user to save checkpoint before shutdown if last checkpoint is too old. (xiwang via yusaku)

--- ambari-web/app/controllers/main/service/item.js
+++ ambari-web/app/controllers/main/service/item.js
@@ -261,7 +261,7 @@ App.MainServiceItemController = Em.Controller.extend({
     if (!lastCheckpointTime) {
       this.set("isNNCheckpointTooOld", null);
     } else {
-      var time_criteria = 12; // time in hours to define how many hours ago is too old
+      var time_criteria = App.nnCheckpointAgeAlertThreshold; // time in hours to define how many hours ago is too old
       var time_ago = (Math.round(App.dateTime() / 1000) - (time_criteria * 3600)) *1000;
       if (lastCheckpointTime <= time_ago) {
         // too old, set the effected hostName
