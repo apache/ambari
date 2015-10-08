d6645794e74b75705a4ef58c43614f7da78fced9
Gautam Borad <gautam@apache.com>
2015-10-08 18:28:20 +0530

    AMBARI-13273. Ambari stack changes to allow Ranger Usersync to be installed on separate host (gautam)

--- ambari-server/src/main/resources/common-services/RANGER/0.4.0/package/scripts/params.py
+++ ambari-server/src/main/resources/common-services/RANGER/0.4.0/package/scripts/params.py
@@ -22,6 +22,7 @@ from resource_management.libraries.script import Script
 from resource_management.libraries.functions.version import format_hdp_stack_version
 from resource_management.libraries.functions.format import format
 from resource_management.libraries.functions.default import default
+from resource_management.libraries.functions.is_empty import is_empty
 
 # a map of the Ambari role to the component name
 # for use with /usr/hdp/current/<component>
@@ -85,6 +86,10 @@ ambari_server_hostname = config['clusterHostInfo']['ambari_server_host'][0]
 db_flavor =  (config['configurations']['admin-properties']['DB_FLAVOR']).lower()
 usersync_exturl =  config['configurations']['admin-properties']['policymgr_external_url']
 ranger_host = config['clusterHostInfo']['ranger_admin_hosts'][0]
+ugsync_host = 'localhost'
+usersync_host_info = config['clusterHostInfo']['ranger_usersync_hosts']
+if not is_empty(usersync_host_info) and len(usersync_host_info) > 0:
+  ugsync_host = config['clusterHostInfo']['ranger_usersync_hosts'][0]
 ranger_external_url = config['configurations']['admin-properties']['policymgr_external_url']
 ranger_db_name = config['configurations']['admin-properties']['db_name']
 ranger_auditdb_name = config['configurations']['admin-properties']['audit_db_name']
