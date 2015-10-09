370077ea8268e2d4c06a1886391c9ed467530015
Alex Antonenko <hiveww@gmail.com>
2015-10-09 15:49:20 +0300

    AMBARI-13373. hdfs balancer via ambari fails to run once HA is enabled

--- ambari-web/app/controllers/main/admin/highAvailability/nameNode/step3_controller.js
+++ ambari-web/app/controllers/main/admin/highAvailability/nameNode/step3_controller.js
@@ -34,7 +34,7 @@ App.HighAvailabilityWizardStep3Controller = Em.Controller.extend({
    * @type Object
    */
   configsToRemove: {
-    'hdfs-site': ['dfs.namenode.secondary.http-address']
+    'hdfs-site': ['dfs.namenode.secondary.http-address', 'dfs.namenode.rpc-address']
   },
 
   clearStep: function () {
