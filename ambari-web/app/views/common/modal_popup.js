19db33f728a4985ea24c2660307c0b0db7b3c1b5
Alex Antonenko <hiveww@gmail.com>
2015-10-23 16:05:34 +0300

    AMBARI-13544. Kerberos: credential dialog ux edits

--- ambari-web/app/views/common/modal_popup.js
+++ ambari-web/app/views/common/modal_popup.js
@@ -36,6 +36,7 @@ App.ModalPopup = Ember.View.extend({
   disableThird: false,
   primaryClass: 'btn-success',
   secondaryClass: '',
+  thirdClass: '',
   onPrimary: function () {
     this.hide();
   },
