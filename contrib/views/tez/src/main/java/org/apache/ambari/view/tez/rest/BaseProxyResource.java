dd8bdd2748b6ae276752b3601b64a5f36dade466
Srimanth Gunturi <sgunturi@hortonworks.com>
2015-10-30 09:23:05 -0700

    AMBARI-13612. Add RM and ATS Proxy Urls to Ambari Tez (DIPAYAN BHOWMICK via srimanth)

---
+++ contrib/views/tez/src/main/java/org/apache/ambari/view/tez/rest/BaseProxyResource.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ambari.view.tez.rest;
+
+import com.google.inject.Inject;
+import org.apache.ambari.view.tez.utils.ProxyHelper;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.*;
+import java.util.HashMap;
+
+/**
+ * Base class for the proxy resources
+ */
+public abstract class BaseProxyResource {
+
+  private ProxyHelper proxyHelper;
+
+  @Inject
+  public BaseProxyResource(ProxyHelper proxyHelper) {
+    this.proxyHelper = proxyHelper;
+  }
+
+  @Path("/{endpoint:.+}")
+  @GET
+  @Produces({MediaType.APPLICATION_JSON})
+  public Response getData(@Context UriInfo uriInfo, @PathParam("endpoint") String endpoint) {
+    String url = getProxyUrl(endpoint, uriInfo.getQueryParameters());
+    String response = proxyHelper.getResponse(url, new HashMap<String, String>());
+
+    JSONObject jsonObject = (JSONObject) JSONValue.parse(response);
+    return Response.ok(jsonObject).type(MediaType.APPLICATION_JSON).build();
+  }
+
+  public abstract String getProxyUrl(String endpoint, MultivaluedMap<String, String> queryParams);
+}
---
+++ contrib/views/tez/src/main/java/org/apache/ambari/view/tez/utils/ProxyHelper.java
@@ -0,0 +1,97 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ambari.view.tez.utils;
+
+
+import com.google.inject.Inject;
+import org.apache.ambari.view.URLConnectionProvider;
+import org.apache.ambari.view.ViewContext;
+import org.apache.ambari.view.tez.exceptions.ProxyException;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.util.Map;
+import java.util.Set;
+
+public class ProxyHelper {
+
+  private ViewContext viewContext;
+
+  private static final Logger LOG = LoggerFactory.getLogger(ProxyHelper.class);
+
+  @Inject
+  public ProxyHelper(ViewContext viewContext) {
+    this.viewContext = viewContext;
+  }
+
+
+  public String getResponse(String url, Map<String, String> headers) {
+    LOG.debug("Fetching the result from the URL: {} using proxy", url);
+    InputStream inputStream = null;
+    try {
+      URLConnectionProvider provider = viewContext.getURLConnectionProvider();
+      HttpURLConnection connection = provider.getConnectionAsCurrent(url, "GET", (String) null, headers);
+
+      if(connection.getResponseCode() != Response.Status.OK.getStatusCode()) {
+        LOG.error("Failure in fetching results for the URL: {}. Status: {}", url, connection.getResponseCode());
+        inputStream = connection.getErrorStream();
+        String trace = IOUtils.toString(inputStream);
+        throw new ProxyException("Failed to fetch results by the proxy from url: " + url, connection.getResponseCode(), trace);
+      }
+
+      inputStream = connection.getInputStream();
+      return IOUtils.toString(inputStream);
+
+    } catch (IOException e) {
+      LOG.error("Cannot access the url: {}", url, e);
+      throw new ProxyException("Failed to fetch results by the proxy from url: " + url + ".Internal Error.",
+        Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), e.getMessage());
+    } finally {
+      if (inputStream != null) {
+        try {
+          inputStream.close();
+        } catch (IOException e) { /* Noting to do */ }
+      }
+    }
+  }
+
+  public String getQueryParamsString(MultivaluedMap<String, String> queryParameters) {
+    Set<String> keySet = queryParameters.keySet();
+    StringBuilder builder = new StringBuilder();
+    if(keySet.size() > 0)
+      builder.append("?");
+
+    int count = 0;
+    for(String key: keySet) {
+      builder.append(key);
+      builder.append("=");
+      builder.append(queryParameters.getFirst(key));
+      if(count < keySet.size() - 1) {
+        builder.append("&");
+      }
+    }
+    return builder.toString();
+  }
+}
