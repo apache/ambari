837710b436f7be3a8adb90767c28f353ffc40124
aBabiichuk <ababiichuk@cybervisiontech.com>
2015-10-30 12:24:51 +0200

    AMBARI-13654 Ranger DB host should be left empty by default. (ababiichuk)

--- ambari-web/app/utils/configs/config_property_helper.js
+++ ambari-web/app/utils/configs/config_property_helper.js
@@ -215,13 +215,6 @@ module.exports = {
           configProperty.set('isRequired', 'false');
         }
         break;
-      case 'db_host':
-        var masterComponent =  masterComponentHostsInDB.findProperty('component', 'RANGER_ADMIN');
-        if (masterComponent) {
-          configProperty.set('value', masterComponent.hostName);
-          configProperty.set('recommendedValue', masterComponent.hostName);
-        }
-        break;
     }
   },
 
