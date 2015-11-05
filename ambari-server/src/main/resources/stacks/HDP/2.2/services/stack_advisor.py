4db9a1eefe7ea847f93d7c7c19ee025d8ca638c6
Vitaly Brodetskyi <vbrodetskyi@hortonworks.com>
2015-11-05 18:19:49 +0200

    AMBARI-13746. Ranger Plugin Enabling not working in 2.2 because of a stack_advisor error.(vbrodetskyi)

--- ambari-server/src/main/resources/stacks/HDP/2.2/services/stack_advisor.py
+++ ambari-server/src/main/resources/stacks/HDP/2.2/services/stack_advisor.py
@@ -779,9 +779,8 @@ class HDP22StackAdvisor(HDP21StackAdvisor):
   def recommendStormConfigurations(self, configurations, clusterData, services, hosts):
     putStormSiteProperty = self.putProperty(configurations, "storm-site", services)
     putStormSiteAttributes = self.putPropertyAttribute(configurations, "storm-site")
-    if "core-site" in services["configurations"]:
-      core_site = services["configurations"]["core-site"]["properties"]
-    stackVersion = services["Versions"]["stack_version"]
+    storm_site = getServicesSiteProperties(services, "storm-site")
+    security_enabled = (storm_site is not None and "storm.zookeeper.superACL" in storm_site)
     if "ranger-env" in services["configurations"] and "ranger-storm-plugin-properties" in services["configurations"] and \
         "ranger-storm-plugin-enabled" in services["configurations"]["ranger-env"]["properties"]:
       putStormRangerPluginProperty = self.putProperty(configurations, "ranger-storm-plugin-properties", services)
@@ -805,8 +804,7 @@ class HDP22StackAdvisor(HDP21StackAdvisor):
     else:
       rangerClass = 'org.apache.ranger.authorization.storm.authorizer.RangerStormAuthorizer'
     # Cluster is kerberized
-    if "core-site" in services["configurations"] and \
-          ('hadoop.security.authentication' in core_site and core_site['hadoop.security.authentication'] == 'kerberos'):
+    if security_enabled:
       if rangerPluginEnabled and (rangerPluginEnabled.lower() == 'Yes'.lower()):
         putStormSiteProperty('nimbus.authorizer',rangerClass)
       elif (services["configurations"]["storm-site"]["properties"]["nimbus.authorizer"] == rangerClass):
