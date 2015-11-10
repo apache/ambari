9b8701c7934201e4593fced6128841031f7ef2b0
Oleg Nechiporenko <onechiporenko@apache.org>
2015-11-10 16:15:57 +0200

    AMBARI-13812. Refactor RM HA step3 controller (onechiporenko)

--- ambari-web/app/utils/configs/nn_ha_config_initializer.js
+++ ambari-web/app/utils/configs/nn_ha_config_initializer.js
@@ -17,7 +17,7 @@
  */
 
 var App = require('app');
-require('utils/configs/config_initializer_class');
+require('utils/configs/ha_config_initializer_class');
 
 /**
  * @typedef {topologyLocalDB} extendedTopologyLocalDB
@@ -37,89 +37,6 @@ function getRenameWithNamespaceConfig(toReplace) {
     type: 'rename',
     toReplace: toReplace
   };
-}
-
-/**
- * Settings for <code>host_with_port</code>-initializer
- * Used for configs with value equal to hostName where some component exists concatenated with port-value
- * Port-value is calculated according to <code>port</code> and <code>portFromDependencies</code> values
- * If <code>portFromDependencies</code> is <code>true</code>, <code>port</code>-value is used as key of the <code>dependencies</code> (where real port-value is)
- * Otherwise - <code>port</code>-value used as is
- * Value also may be customized with prefix and suffix
- *
- * @param {string} component needed component
- * @param {boolean} componentExists component already exists or just going to be installed
- * @param {string} prefix=''
- * @param {string} suffix=''
- * @param {string} port
- * @param {boolean} portFromDependencies=false
- * @returns {{type: string, component: string, componentExists: boolean, modifier: {prefix: (string), suffix: (string)}}}
- */
-function getHostWithPortConfig(component, componentExists, prefix, suffix, port, portFromDependencies) {
-  if (arguments.length < 6) {
-    portFromDependencies = false;
-  }
-  prefix = prefix || '';
-  suffix = suffix || '';
-  var ret = {
-    type: 'host_with_port',
-    component: component,
-    componentExists: componentExists,
-    modifier: {
-      prefix: prefix,
-      suffix: suffix
-    }
-  };
-  if (portFromDependencies) {
-    ret.portKey = port;
-  }
-  else {
-    ret.port = port;
-  }
-  return ret;
-}
-
-/**
- * Settings for <code>hosts_with_port</code>-initializer
- * Used for configs with value equal to the list of hostNames with port
- * Value also may be customized with prefix, suffix and delimiter between host:port elements
- * Port-value is calculated according to <code>port</code> and <code>portFromDependencies</code> values
- * If <code>portFromDependencies</code> is <code>true</code>, <code>port</code>-value is used as key of the <code>dependencies</code> (where real port-value is)
- * Otherwise - <code>port</code>-value used as is
- *
- * @param {string} component hosts where this component exists are used as config-value
- * @param {string} prefix='' substring added before hosts-list
- * @param {string} suffix='' substring added after hosts-list
- * @param {string} delimiter=',' delimiter between hosts in the value
- * @param {string} port if <code>portFromDependencies</code> is <code>false</code> this value is used as port for hosts
- * if <code>portFromDependencies</code> is <code>true</code> `port` is used as key in the <code>dependencies</code> to get real port-value
- * @param {boolean} portFromDependencies=false true - use <code>port</code> as key for <code>dependencies</code> to get real port-value,
- * false - use <code>port</code> as port-value
- * @returns {{type: string, component: string, modifier: {prefix: (string), suffix: (string), delimiter: (string)}}}
- */
-function getHostsWithPortConfig(component, prefix, suffix, delimiter, port, portFromDependencies) {
-  if (arguments.length < 6) {
-    portFromDependencies = false;
-  }
-  prefix = prefix || '';
-  suffix = suffix || '';
-  delimiter = delimiter || ',';
-  var ret = {
-    type: 'hosts_with_port',
-    component: component,
-    modifier: {
-      prefix: prefix,
-      suffix: suffix,
-      delimiter: delimiter
-    }
-  };
-  if (portFromDependencies) {
-    ret.portKey = port;
-  }
-  else {
-    ret.port = port;
-  }
-  return ret;
 }
 
 /**
@@ -162,42 +79,42 @@ function getReplaceNamespaceConfig(toReplace) {
  *
  * @class {NnHaConfigInitializer}
  */
-App.NnHaConfigInitializer = App.ConfigInitializerClass.create({
+App.NnHaConfigInitializer = App.HaConfigInitializerClass.create({
 
   initializers: {
     'dfs.ha.namenodes.${dfs.nameservices}': getRenameWithNamespaceConfig('${dfs.nameservices}'),
     'dfs.namenode.rpc-address.${dfs.nameservices}.nn1': [
-      getHostWithPortConfig('NAMENODE', true, '', '', 'nnRpcPort', true),
+      App.HaConfigInitializerClass.getHostWithPortConfig('NAMENODE', true, '', '', 'nnRpcPort', true),
       getRenameWithNamespaceConfig('${dfs.nameservices}')
     ],
     'dfs.namenode.rpc-address.${dfs.nameservices}.nn2': [
-      getHostWithPortConfig('NAMENODE', false, '', '', '8020', false),
+      App.HaConfigInitializerClass.getHostWithPortConfig('NAMENODE', false, '', '', '8020', false),
       getRenameWithNamespaceConfig('${dfs.nameservices}')
     ],
     'dfs.namenode.http-address.${dfs.nameservices}.nn1': [
-      getHostWithPortConfig('NAMENODE', true, '', '', 'nnHttpPort', true),
+      App.HaConfigInitializerClass.getHostWithPortConfig('NAMENODE', true, '', '', 'nnHttpPort', true),
       getRenameWithNamespaceConfig('${dfs.nameservices}')
     ],
     'dfs.namenode.http-address.${dfs.nameservices}.nn2': [
-      getHostWithPortConfig('NAMENODE', false, '', '', '50070', false),
+      App.HaConfigInitializerClass.getHostWithPortConfig('NAMENODE', false, '', '', '50070', false),
       getRenameWithNamespaceConfig('${dfs.nameservices}')
     ],
     'dfs.namenode.https-address.${dfs.nameservices}.nn1': [
-      getHostWithPortConfig('NAMENODE', true, '', '', 'nnHttpsPort', true),
+      App.HaConfigInitializerClass.getHostWithPortConfig('NAMENODE', true, '', '', 'nnHttpsPort', true),
       getRenameWithNamespaceConfig('${dfs.nameservices}')
     ],
     'dfs.namenode.https-address.${dfs.nameservices}.nn2': [
-      getHostWithPortConfig('NAMENODE', false, '', '', '50470', false),
+      App.HaConfigInitializerClass.getHostWithPortConfig('NAMENODE', false, '', '', '50470', false),
       getRenameWithNamespaceConfig('${dfs.nameservices}')
     ],
     'dfs.client.failover.proxy.provider.${dfs.nameservices}': getRenameWithNamespaceConfig('${dfs.nameservices}'),
     'dfs.nameservices': getNamespaceConfig(),
     'fs.defaultFS': getNamespaceConfig('hdfs://'),
     'dfs.namenode.shared.edits.dir': [
-      getHostsWithPortConfig('JOURNALNODE', 'qjournal://', '/${dfs.nameservices}', ';', '8485', false),
+      App.HaConfigInitializerClass.getHostsWithPortConfig('JOURNALNODE', 'qjournal://', '/${dfs.nameservices}', ';', '8485', false),
       getReplaceNamespaceConfig('${dfs.nameservices}')
     ],
-    'ha.zookeeper.quorum': getHostsWithPortConfig('ZOOKEEPER_SERVER', '', '', ',', 'zkClientPort', true)
+    'ha.zookeeper.quorum': App.HaConfigInitializerClass.getHostsWithPortConfig('ZOOKEEPER_SERVER', '', '', ',', 'zkClientPort', true)
   },
 
   uniqueInitializers: {
@@ -300,76 +217,6 @@ App.NnHaConfigInitializer = App.ConfigInitializerClass.create({
       recommendedValue: recommendedValue
     });
     return configProperty;
-  },
-
-  /**
-   * Initializer for configs with value equal to the hostName where some component exists
-   * Value may be customized with prefix and suffix (see <code>initializer.modifier</code>)
-   * Port-value is calculated according to <code>initializer.portKey</code> or <code>initializer.port</code> values
-   * Value-examples: 'SOME_COOL_PREFIXhost1:port1SOME_COOL_SUFFIX', 'host1:port2'
-   *
-   * @param {object} configProperty
-   * @param {extendedTopologyLocalDB} localDB
-   * @param {nnHaConfigDependencies} dependencies
-   * @param {object} initializer
-   * @returns {object}
-   * @private
-   * @method _initAsHostWithPort
-   */
-  _initAsHostWithPort: function (configProperty, localDB, dependencies, initializer) {
-    var hostName = localDB.masterComponentHosts.filterProperty('component', initializer.component).findProperty('isInstalled', initializer.componentExists).hostName;
-    var port = this.__getPort(dependencies, initializer);
-    var value = initializer.modifier.prefix + hostName + ':' + port + initializer.modifier.suffix;
-    Em.setProperties(configProperty, {
-      value: value,
-      recommendedValue: value
-    });
-    return configProperty;
-  },
-
-  /**
-   * Initializer for configs with value equal to the list of hosts where some component exists
-   * Value may be customized with prefix and suffix (see <code>initializer.modifier</code>)
-   * Delimiter between hostNames also may be customized in the <code>initializer.modifier</code>
-   * Port-value is calculated according to <code>initializer.portKey</code> or <code>initializer.port</code> values
-   * Value examples: 'SOME_COOL_PREFIXhost1:port,host2:port,host2:portSOME_COOL_SUFFIX', 'host1:port|||host2:port|||host2:port'
-   *
-   * @param {object} configProperty
-   * @param {topologyLocalDB} localDB
-   * @param {nnHaConfigDependencies} dependencies
-   * @param {object} initializer
-   * @returns {object}
-   * @private
-   * @method _initAsHostsWithPort
-   */
-  _initAsHostsWithPort: function (configProperty, localDB, dependencies, initializer) {
-    var hostNames = localDB.masterComponentHosts.filterProperty('component', initializer.component).mapProperty('hostName');
-    var port = this.__getPort(dependencies, initializer);
-    var value = initializer.modifier.prefix + hostNames.map(function (hostName) {
-      return hostName + ':' + port;
-    }).join(initializer.modifier.delimiter) + initializer.modifier.suffix;
-    Em.setProperties(configProperty, {
-      value: value,
-      recommendedValue: value
-    });
-    return configProperty;
-  },
-
-  /**
-   * Returns port-value from <code>dependencies</code> accorfing to <code>initializer.portKey</code> or <code>initializer.port</code> values
-   *
-   * @param {nnHaConfigDependencies} dependencies
-   * @param {object} initializer
-   * @returns {string|number}
-   * @private
-   * @method __getPort
-   */
-  __getPort: function (dependencies, initializer) {
-    var portKey = initializer.portKey;
-    if (portKey) {
-      return  dependencies[portKey];
-    }
-    return initializer.port;
   },
 
   /**
