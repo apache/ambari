0e990b9525f6d6b2f62b55cce340c1569b4120e4
aBabiichuk <ababiichuk@cybervisiontech.com>
2015-11-13 11:16:10 +0200

    AMBARI-13873 During ranger plugins enabling warning about dependent properties is placed incorrectly. (ababiichuk)

--- ambari-web/app/views/main/service/info/configs.js
+++ ambari-web/app/views/main/service/info/configs.js
@@ -91,7 +91,7 @@ App.MainServiceInfoConfigsView = Em.View.extend({
       Em.run.next(function () {
         $(".dependencies-info-bar-wrapper").stick_in_parent({parent: '#serviceConfig', offset_top: 60});
         Em.run.next(function () {
-          $(window).scrollTop(window.scrollY + 1); // '.dependencies-info-bar-wrapper' position should be recalculated
+          $(".dependencies-info-bar-wrapper").trigger("sticky_kit:recalc"); // '.dependencies-info-bar-wrapper' position should be recalculated
         });
       });
     }
