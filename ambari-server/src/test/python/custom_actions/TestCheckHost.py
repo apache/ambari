9403dfa93de99f89aadb3f9b157ab4ae29676791
Andrew Onishuk <aonishuk@hortonworks.com>
2015-12-01 13:21:05 +0200

    AMBARI-14125. DB Admin Test connection from ambari screen does not work for Oracle DB 'SYS' user (aonishuk)

--- ambari-server/src/test/python/custom_actions/TestCheckHost.py
+++ ambari-server/src/test/python/custom_actions/TestCheckHost.py
@@ -147,7 +147,7 @@ class TestCheckHost(TestCase):
                                                                                     'exit_code': 1}})
     self.assertEquals(format_mock.call_args[0][0],'{java_exec} -cp {check_db_connection_path}{class_path_delimiter}'
             '{jdbc_jar_path} -Djava.library.path={java_library_path} org.apache.ambari.server.DBConnectionVerification'
-            ' "{db_connection_url}" {user_name} {user_passwd!p} {jdbc_driver_class}')
+            ' "{db_connection_url}" "{user_name}" {user_passwd!p} {jdbc_driver_class}')
 
     # test, db connection success
     download_file_mock.reset_mock()
