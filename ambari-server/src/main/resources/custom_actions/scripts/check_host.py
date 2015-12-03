2f553c2675803062b383c1bce252bf29a91050da
Andrew Onishuk <aonishuk@hortonworks.com>
2015-12-03 19:47:01 +0200

    AMBARI-14181. "Check host" background operation was not failed for failed DB test connection (aonishuk)

--- ambari-server/src/main/resources/custom_actions/scripts/check_host.py
+++ ambari-server/src/main/resources/custom_actions/scripts/check_host.py
@@ -34,6 +34,7 @@ from resource_management import Script, Execute, format
 from ambari_agent.HostInfo import HostInfo
 from ambari_agent.HostCheckReportFileHandler import HostCheckReportFileHandler
 from resource_management.core.resources import Directory, File
+from resource_management.core.exceptions import Fail
 from ambari_commons.constants import AMBARI_SUDO_BINARY
 from resource_management.core import shell
 
@@ -48,6 +49,9 @@ CHECK_EXISTING_REPOS = "existing_repos"
 CHECK_TRANSPARENT_HUGE_PAGE = "transparentHugePage"
 
 BEFORE_CLEANUP_HOST_CHECKS = ','.join([CHECK_LAST_AGENT_ENV, CHECK_INSTALLED_PACKAGES, CHECK_EXISTING_REPOS, CHECK_TRANSPARENT_HUGE_PAGE])
+
+# If exit_code for some of these checks is not 0 task will be failed
+FALLIBLE_CHECKS = [CHECK_DB_CONNECTION]
 
 DB_MYSQL = "mysql"
 DB_ORACLE = "oracle"
@@ -193,6 +197,19 @@ class CheckHost(Script):
     self.reportFileHandler.writeHostChecksCustomActionsFile(structured_output)
     
     self.put_structured_out(structured_output)
+
+    error_message = ""
+    for check_name in FALLIBLE_CHECKS:
+      if check_name in structured_output and "exit_code" in structured_output[check_name] \
+          and structured_output[check_name]["exit_code"] != 0:
+        error_message += "Check {0} was unsuccessful. Exit code: {1}.".format(check_name, \
+                                                                             structured_output[check_name]["exit_code"])
+        if "message" in structured_output[check_name]:
+          error_message += " Message: {0}".format(structured_output[check_name]["message"])
+        error_message += "\n"
+
+    if error_message:
+      raise Fail(error_message)
 
   def execute_existing_repos_and_installed_packages_check(self, config):
       installedPackages = []
