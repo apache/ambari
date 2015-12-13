0ee5b323c77b416b9ffff7e55613e218b9b292b8
Yusaku Sako <yusaku@hortonworks.com>
2015-12-13 12:59:13 -0800

    Revert "AMBARI-14352. Should show error message when "Pause Upgrade" failed. (Xi Wang via Jaimin)"
    This reverts commit 38a92b7ff52a005467aecede087222f9e9df79c5.

--- ambari-web/app/controllers/main/admin/stack_and_upgrade_controller.js
+++ ambari-web/app/controllers/main/admin/stack_and_upgrade_controller.js
@@ -1542,8 +1542,6 @@ App.MainAdminStackAndUpgradeController = Em.Controller.extend(App.LocalStorage, 
         wizardControllerName: self.get('name'),
         localdb: App.db.data
       });
-    }).fail(function () {
-      return App.showAlertPopup(Em.I18n.t('admin.stackUpgrade.state.paused.fail.header'), Em.I18n.t('admin.stackUpgrade.state.paused.fail.body'));
     });
   },
 
