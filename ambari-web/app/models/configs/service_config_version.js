a55c81267c787b3ab980ba22375b47cf66bd6ec0
Andrii Tkach <atkach@hortonworks.com>
2015-12-17 16:00:51 +0200

    AMBARI-14415 Cover Config models with unit tests. (atkach)

--- ambari-web/app/models/configs/service_config_version.js
+++ ambari-web/app/models/configs/service_config_version.js
@@ -22,6 +22,8 @@ var dateUtil = require('utils/date/date');
 
 
 App.ServiceConfigVersion = DS.Model.extend({
+  MAX_AUTHOR_LENGTH: 20,
+  MAX_NOTES_LENGTH: 80,
   serviceName: DS.attr('string'),
   displayName: Em.computed.formatRole('serviceName'),
   groupName: DS.attr('string'),
@@ -41,36 +43,79 @@ App.ServiceConfigVersion = DS.Model.extend({
   canBeMadeCurrent: Em.computed.and('isCompatible', '!isCurrent'),
   isDefault: Em.computed.equal('groupName', 'default'),
   currentTooltip: Em.computed.i18nFormat('dashboard.configHistory.table.current.tooltip', 'displayName', 'configGroupName'),
+
+  /**
+   * @type {string}
+   */
   configGroupName: function () {
     return this.get('isDefault') ? Em.I18n.t('common.default') : this.get('groupName');
   }.property('groupName','isDefault'),
+
+  /**
+   * @type {string}
+   */
   authorFormatted: function () {
     var author = this.get('author');
     if (author) {
-      return author.length > 20 ? author.slice(0, 20) + '...' : author;
+      return author.length > this.get('MAX_AUTHOR_LENGTH') ? author.slice(0, this.get('MAX_AUTHOR_LENGTH')) + '...' : author;
     }
   }.property('author'),
+
+  /**
+   * @type {string}
+   */
   fullNotes: function () {
-    return (typeof this.get('notes') === 'string') ? this.get('notes') || Em.I18n.t('dashboard.configHistory.table.notes.no') : Em.I18n.t('dashboard.configHistory.table.notes.no');
+    return (typeof this.get('notes') === 'string') ?
+           (this.get('notes') || Em.I18n.t('dashboard.configHistory.table.notes.no')) :
+           Em.I18n.t('dashboard.configHistory.table.notes.no');
   }.property('notes'),
+
+  /**
+   * @type {string}
+   */
   briefNotes: function () {
-    return this.get('fullNotes').slice(0, 81);
+    return this.get('fullNotes').slice(0, (this.get('MAX_NOTES_LENGTH') + 1));
   }.property('fullNotes'),
+
+  /**
+   * @type {boolean}
+   */
   moreNotesExists: function () {
-    return (typeof this.get('notes') === 'string') ?  this.get('notes').length > 80 : false;
+    return (typeof this.get('notes') === 'string') && this.get('notes').length > this.get('MAX_NOTES_LENGTH');
   }.property('notes'),
+
+  /**
+   * @type {string}
+   */
   versionText: Em.computed.i18nFormat('dashboard.configHistory.table.version.versionText', 'version'),
+
+  /**
+   * @type {string}
+   */
   makeCurrentButtonText: Em.computed.i18nFormat('dashboard.configHistory.info-bar.revert.versionButton', 'versionText'),
+
+  /**
+   * @type {string}
+   */
   createdDate: function () {
     return dateUtil.dateFormat(this.get('createTime'));
   }.property('createTime'),
+
+  /**
+   * @type {string}
+   */
   timeSinceCreated: function () {
     return $.timeago(this.get('rawCreateTime'));
   }.property('rawCreateTime'),
+
   /**
    * determine whether ServiceConfigVersion is requested from server
    */
   isRequested: DS.attr('boolean'),
+
+  /**
+   * @type {boolean}
+   */
   isRestartRequired: function () {
     if (this.get('service.isRestartRequired') && this.get('isCurrent')) {
       var hostNames = this.get('hosts');
@@ -83,6 +128,10 @@ App.ServiceConfigVersion = DS.Model.extend({
     }
     return false;
   }.property('service.isRestartRequired','isDefault', 'isCurrent', 'hosts', 'service.restartRequiredHostsAndComponents', 'router.mainServiceInfoConfigsController.configGroups'),
+
+  /**
+   * {{view: string, compare: string, revert: string}} disabledActionMessages
+   */
   disabledActionMessages: function () {
     return {
       view: (this.get('isDisplayed')) ? Em.I18n.t('dashboard.configHistory.info-bar.view.button.disabled') : '',
@@ -90,6 +139,10 @@ App.ServiceConfigVersion = DS.Model.extend({
       revert: (this.get('isCurrent')) ? Em.I18n.t('dashboard.configHistory.info-bar.revert.button.disabled') : ''
     }
   }.property('isDisplayed', 'isCurrent'),
+
+  /**
+   * {{view: (string|boolean), compare: (string|boolean), revert: (string|boolean)}} disabledActionAttr
+   */
   disabledActionAttr: function () {
     return {
       view: (this.get('isDisplayed')) ? 'disabled' : false,
--- ambari-web/test/models/configs/service_config_version_test.js
+++ ambari-web/test/models/configs/service_config_version_test.js
@@ -31,6 +31,8 @@ describe('App.ServiceConfigVersion', function () {
     model = getModel();
   });
 
+  App.TestAliases.testAsComputedAnd(getModel(), 'canBeMadeCurrent', ['isCompatible', '!isCurrent']);
+
   describe('#authorFormatted', function () {
 
     var cases = [
@@ -55,6 +57,290 @@ describe('App.ServiceConfigVersion', function () {
 
   });
 
-  App.TestAliases.testAsComputedAnd(getModel(), 'canBeMadeCurrent', ['isCompatible', '!isCurrent']);
+  describe("#configGroupName", function() {
+
+    it("default group", function() {
+      model.reopen({
+        groupName: 'g1',
+        isDefault: false
+      });
+      expect(model.get('configGroupName')).to.equal('g1');
+    });
+
+    it("default group", function() {
+      model.reopen({
+        isDefault: true
+      });
+      expect(model.get('configGroupName')).to.equal(Em.I18n.t('common.default'));
+    });
+
+  });
+
+  describe("#fullNotes", function() {
+
+    it("notes is null", function() {
+      model.set('notes', null);
+      expect(model.get('fullNotes')).to.equal(Em.I18n.t('dashboard.configHistory.table.notes.no'));
+    });
+
+    it("notes is empty", function() {
+      model.set('notes', "");
+      expect(model.get('fullNotes')).to.equal(Em.I18n.t('dashboard.configHistory.table.notes.no'));
+    });
+
+    it("notes has value", function() {
+      model.set('notes', "notes-value");
+      expect(model.get('fullNotes')).to.equal('notes-value');
+    });
+
+  });
+
+  describe("#briefNotes", function() {
+
+    it("notes shorter than MAX_NOTES_LENGTH", function() {
+      model.reopen({
+        fullNotes: 'short-notes'
+      });
+      expect(model.get('briefNotes')).to.equal('short-notes');
+    });
+
+    it("notes longer than MAX_NOTES_LENGTH", function() {
+      model.reopen({
+        fullNotes: 'long-notes-long-notes-long-notes-long-notes-long-notes-long-notes-long-notes-long-notes' +
+        '-long-notes-long-notes-long-notes-long-notes-long-notes'
+      });
+      expect(model.get('briefNotes')).to.equal('long-notes-long-notes-long-notes-long-notes-long-notes-long-notes-long-notes-long');
+    });
+
+  });
+
+  describe("#moreNotesExists", function() {
+
+    it("notes is null", function() {
+      model.set('notes', null);
+      expect(model.get('moreNotesExists')).to.be.false;
+    });
+
+    it("notes is shorter than MAX_NOTES_LENGTH", function() {
+      model.set('notes', 'short-notes');
+      expect(model.get('moreNotesExists')).to.be.false;
+    });
+
+    it("notes is longer than MAX_NOTES_LENGTH", function() {
+      model.set('notes', 'long-notes-long-notes-long-notes-long-notes-long-notes-long-notes-long-notes-long-notes' +
+      '-long-notes-long-notes-long-notes-long-notes-long-notes');
+      expect(model.get('moreNotesExists')).to.be.true;
+    });
+
+  });
+
+  describe("#createdDate", function() {
+
+    it("should return created date", function() {
+      model.set('createTime', 1450267588961);
+      expect(model.get('createdDate')).to.equal('Wed, Dec 16, 2015 14:06');
+    });
+
+  });
+
+  describe("#timeSinceCreated", function () {
+
+    before(function () {
+      sinon.stub($, 'timeago').returns('timeago');
+    });
+    after(function () {
+      $.timeago.restore()
+    });
+
+    it("should return time since created", function () {
+      model.set('rawCreateTime', 1450267588961);
+      expect(model.get('timeSinceCreated')).to.equal('timeago');
+    });
+
+  });
+
+  describe("#isRestartRequired", function() {
+
+    it("service.isRestartRequired is false", function() {
+      model.set('service', Em.Object.create({
+        isRestartRequired: false
+      }));
+      expect(model.get('isRestartRequired')).to.be.false;
+    });
+
+    it("non-current version", function() {
+      model.set('service', Em.Object.create({
+        isRestartRequired: true
+      }));
+      model.set('isCurrent', false);
+      expect(model.get('isRestartRequired')).to.be.false;
+    });
+
+    it("version has no hosts", function() {
+      model.setProperties({
+        service: Em.Object.create({
+          isRestartRequired: true
+        }),
+        isCurrent: true,
+        hosts: []
+      });
+      expect(model.get('isRestartRequired')).to.be.false;
+    });
+
+    it("version hosts don't need restart", function() {
+      model.setProperties({
+        service: Em.Object.create({
+          isRestartRequired: true,
+          restartRequiredHostsAndComponents: {}
+        }),
+        isCurrent: true,
+        hosts: ['host1']
+      });
+      expect(model.get('isRestartRequired')).to.be.false;
+    });
+
+    it("version hosts need restart", function() {
+      model.setProperties({
+        service: Em.Object.create({
+          isRestartRequired: true,
+          restartRequiredHostsAndComponents: {'host1': {}}
+        }),
+        isCurrent: true,
+        hosts: ['host1']
+      });
+      expect(model.get('isRestartRequired')).to.be.true;
+    });
+
+  });
+
+  describe("#disabledActionMessages", function() {
+    var testCases = [
+      {
+        input: {
+          isDisplayed: false,
+          isCurrent: false
+        },
+        expected: {
+          view: '',
+          compare: '',
+          revert: ''
+        }
+      },
+      {
+        input: {
+          isDisplayed: true,
+          isCurrent: false
+        },
+        expected: {
+          view: Em.I18n.t('dashboard.configHistory.info-bar.view.button.disabled'),
+          compare: Em.I18n.t('dashboard.configHistory.info-bar.compare.button.disabled'),
+          revert: ''
+        }
+      },
+      {
+        input: {
+          isDisplayed: false,
+          isCurrent: true
+        },
+        expected: {
+          view: '',
+          compare: '',
+          revert: Em.I18n.t('dashboard.configHistory.info-bar.revert.button.disabled')
+        }
+      },
+      {
+        input: {
+          isDisplayed: true,
+          isCurrent: true
+        },
+        expected: {
+          view: Em.I18n.t('dashboard.configHistory.info-bar.view.button.disabled'),
+          compare: Em.I18n.t('dashboard.configHistory.info-bar.compare.button.disabled'),
+          revert: Em.I18n.t('dashboard.configHistory.info-bar.revert.button.disabled')
+        }
+      }
+    ];
+
+    testCases.forEach(function(test) {
+      it("isDisplayed = " + test.input.isDisplayed + ", isCurrent = " + test.input.isCurrent, function() {
+        model.setProperties(test.input);
+        expect(model.get('disabledActionMessages')).to.eql(test.expected);
+      });
+    });
+
+  });
+
+  describe("#disabledActionAttr", function() {
+    var testCases = [
+      {
+        input: {
+          isDisplayed: false,
+          isCurrent: false,
+          isDisabled: false
+        },
+        expected: {
+          view: false,
+          compare: false,
+          revert: false
+        }
+      },
+      {
+        input: {
+          isDisplayed: true,
+          isCurrent: false,
+          isDisabled: false
+        },
+        expected: {
+          view: 'disabled',
+          compare: 'disabled',
+          revert: false
+        }
+      },
+      {
+        input: {
+          isDisplayed: false,
+          isCurrent: false,
+          isDisabled: true
+        },
+        expected: {
+          view: false,
+          compare: 'disabled',
+          revert: 'disabled'
+        }
+      },
+      {
+        input: {
+          isDisplayed: false,
+          isCurrent: true,
+          isDisabled: false
+        },
+        expected: {
+          view: false,
+          compare: false,
+          revert: 'disabled'
+        }
+      },
+      {
+        input: {
+          isDisplayed: true,
+          isCurrent: true,
+          isDisabled: true
+        },
+        expected: {
+          view: 'disabled',
+          compare: 'disabled',
+          revert: 'disabled'
+        }
+      }
+    ];
+
+    testCases.forEach(function(test) {
+      it("isDisplayed = " + test.input.isDisplayed + ", isCurrent = " + test.input.isCurrent + ", isDisabled = " + test.input.isDisabled, function() {
+        model.setProperties(test.input);
+        expect(model.get('disabledActionAttr')).to.eql(test.expected);
+      });
+    });
+
+  });
 
 });
