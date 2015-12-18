1c88d47c0fb7cdba2d5626f7714dca467355d9ed
Jonathan Hurley <jhurley@hortonworks.com>
2015-12-18 14:35:46 -0500

    AMBARI-14437 - Unable To Restart HCat Client When Not Colocated With WebHCat Server (jonathanhurley)

--- ambari-server/src/test/python/stacks/2.0.6/HIVE/test_hcat_client.py
+++ ambari-server/src/test/python/stacks/2.0.6/HIVE/test_hcat_client.py
@@ -17,6 +17,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
+import json
 from mock.mock import MagicMock, call, patch
 from stacks.utils.RMFTestCase import *
 
@@ -99,4 +100,27 @@ class TestHcatClient(RMFTestCase):
                               group = 'hadoop',
                               )
 
+    self.assertNoMoreResources()
+
+
+  @patch("resource_management.core.shell.call")
+  def test_pre_upgrade_restart(self, call_mock):
+    config_file = self.get_src_folder()+"/test/python/stacks/2.0.6/configs/default.json"
+    with open(config_file, "r") as f:
+      json_content = json.load(f)
+
+    version = '2.3.0.0-1234'
+    json_content['commandParams']['version'] = version
+
+    mocks_dict = {}
+    self.executeScript(self.COMMON_SERVICES_PACKAGE_DIR + "/scripts/hcat_client.py",
+      classname = "HCatClient",
+      command = "pre_upgrade_restart",
+      config_dict = json_content,
+      hdp_stack_version = self.STACK_VERSION,
+      target = RMFTestCase.TARGET_COMMON_SERVICES,
+      call_mocks = [(0, None, ''), (0, None, ''), (0, None, ''), (0, None, '')],
+      mocks_dict = mocks_dict)
+
+    self.assertResourceCalled('Execute',('ambari-python-wrap', '/usr/bin/hdp-select', 'set', 'hive-webhcat', version), sudo=True,)
     self.assertNoMoreResources()
