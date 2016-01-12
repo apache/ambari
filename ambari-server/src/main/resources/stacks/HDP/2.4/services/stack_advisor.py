53c4b6b2fc0c71412a812bc44a59de0d8f9294b1
Lisnichenko Dmitro <dlysnichenko@hortonworks.com>
2016-01-12 11:42:52 +0200

    AMBARI-14616. Add yarn-site properties for Spark Shuffle Aux services (including RU/EU and Fresh install) (dgrinenko via dlysnichenko)

--- ambari-server/src/main/resources/stacks/HDP/2.4/services/stack_advisor.py
+++ ambari-server/src/main/resources/stacks/HDP/2.4/services/stack_advisor.py
@@ -17,5 +17,81 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+
 class HDP24StackAdvisor(HDP23StackAdvisor):
-  pass
+  def getServiceConfigurationRecommenderDict(self):
+    parent_recommend_conf_dict = super(HDP24StackAdvisor, self).getServiceConfigurationRecommenderDict()
+    child_recommend_conf_dict = {
+      "YARN": self.recommendYARNConfigurations
+    }
+    parent_recommend_conf_dict.update(child_recommend_conf_dict)
+    return parent_recommend_conf_dict
+
+  def getServiceConfigurationValidators(self):
+    parentValidators = super(HDP24StackAdvisor, self).getServiceConfigurationValidators()
+    childValidators = {
+      "YARN": {"yarn-site": self.validateYARNConfigurations}
+    }
+    self.mergeValidators(parentValidators, childValidators)
+    return parentValidators
+
+  def recommendYARNConfigurations(self, configurations, clusterData, services, hosts):
+    super(HDP24StackAdvisor, self).recommendYARNConfigurations(configurations, clusterData, services, hosts)
+
+    yarn_site_config = "yarn-site"
+    properties = services["configurations"] if yarn_site_config in services["configurations"] else []
+    yarn_site_properties = properties[yarn_site_config]["properties"] if yarn_site_config in properties and \
+                                                                         "properties" in properties[yarn_site_config] else []
+    put_yarn_site_property = self.putProperty(configurations, yarn_site_config, services)
+    put_yarn_site_property_attributes = self.putPropertyAttribute(configurations, yarn_site_config)
+    services_list = [service["StackServices"]["service_name"] for service in services["services"]]
+
+    if 'SPARK' in services_list:
+      if "yarn.nodemanager.aux-services" in yarn_site_properties:
+        aux_services = yarn_site_properties["yarn.nodemanager.aux-services"].split(",")
+        aux_services.append("spark_shuffle")
+        put_yarn_site_property("yarn.nodemanager.aux-services", ",".join(aux_services))
+      else:
+        put_yarn_site_property("yarn.nodemanager.aux-services", "spark_shuffle")
+
+      put_yarn_site_property("yarn.nodemanager.aux-services.spark_shuffle.class",
+                             "org.apache.spark.network.yarn.YarnShuffleService")
+    else:
+      put_yarn_site_property_attributes("yarn.nodemanager.aux-services.spark_shuffle.class", "delete", "true")
+
+  def validateYARNConfigurations(self, properties, recommendedDefaults, configurations, services, hosts):
+    yarn_site = properties
+    validationItems = []
+    services_list = [service["StackServices"]["service_name"] for service in services["services"]]
+
+    if "SPARK" in services_list and 'YARN' in services_list:
+      # yarn.nodemanager.aux-services = ...,spark_shuffle,....
+      # yarn.nodemanager.aux-services.spark_shuffle.class = <not set>
+      if "yarn.nodemanager.aux-services" in yarn_site \
+        and "spark_shuffle" in yarn_site["yarn.nodemanager.aux-services"].lower() \
+        and "yarn.nodemanager.aux-services.spark_shuffle.class" not in yarn_site:
+        validationItems.append({
+          "config-name": "yarn.nodemanager.aux-services.spark_shuffle.class",
+          "item": self.getErrorItem("If spark_shuffle is listed in the aux-services, property value for " +
+                                    "yarn.nodemanager.aux-services.spark_shuffle.class need to be set")
+        })
+
+      # yarn.nodemanager.aux-services = <not set>
+      # yarn.nodemanager.aux-services.spark_shuffle.class = is set
+      spark_aux_service_warning = False
+      if "yarn.nodemanager.aux-services" in yarn_site and "spark_shuffle" not in yarn_site[
+        "yarn.nodemanager.aux-services"].lower() \
+        and "yarn.nodemanager.aux-services.spark_shuffle.class" in yarn_site:
+        spark_aux_service_warning = True
+
+      if "yarn.nodemanager.aux-services" not in yarn_site and "yarn.nodemanager.aux-services.spark_shuffle.class" in yarn_site:
+        spark_aux_service_warning = True
+
+      if spark_aux_service_warning:
+        validationItems.append({
+          "config-name": "yarn.nodemanager.aux-services",
+          "item": self.getWarnItem("If yarn.nodemanager.aux-services.spark_shuffle.class is set, probably " +
+                                   "aux-services property need to be updated to enable spark_shuffle")
+        })
+
+    return self.toConfigurationValidationProblems(validationItems, "yarn-site")
