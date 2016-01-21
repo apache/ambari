b6d2996515283cf9709fb681fccae0a2ac95515e
Dmytro Sen <dsen@apache.org>
2016-01-21 15:28:25 +0200

    AMBARI-14694 Unnecessary multiple TreeMap creation (dsen)

--- ambari-metrics/ambari-metrics-timelineservice/src/main/java/org/apache/hadoop/yarn/server/applicationhistoryservice/metrics/timeline/PhoenixHBaseAccessor.java
+++ ambari-metrics/ambari-metrics-timelineservice/src/main/java/org/apache/hadoop/yarn/server/applicationhistoryservice/metrics/timeline/PhoenixHBaseAccessor.java
@@ -205,7 +205,7 @@ public class PhoenixHBaseAccessor {
 
   @SuppressWarnings("unchecked")
   public static TreeMap<Long, Double>  readMetricFromJSON(String json) throws IOException {
-    return (TreeMap<Long, Double>) mapper.readValue(json, metricValuesTypeRef);
+    return mapper.readValue(json, metricValuesTypeRef);
   }
 
   private Connection getConnectionRetryingOnException()
