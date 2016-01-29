cb220fab1e559e5158d4c662462959ca3001eb61
Jonathan Hurley <jhurley@hortonworks.com>
2016-01-29 11:26:44 -0500

    Revert "AMBARI-14839 - DEA is not enabled due to configuration conflict and history log directory can not be changed (Jeff Zhang via jonathanhurley)"
    This reverts commit 981ede5455c02ae1bd4aac7495c2c667bc889dbc.

--- ambari-server/src/main/resources/common-services/SPARK/1.2.0.2.2/package/scripts/params.py
+++ ambari-server/src/main/resources/common-services/SPARK/1.2.0.2.2/package/scripts/params.py
@@ -79,9 +79,7 @@ hive_user = status_params.hive_user
 spark_group = status_params.spark_group
 user_group = status_params.user_group
 spark_hdfs_user_dir = format("/user/{spark_user}")
-spark_history_dir = "hdfs:///spark-history"
-if 'spark-defaults' in config['configurations'] and 'spark.history.fs.logDirectory' in config['configurations']['spark-defaults']:
-  spark_history_dir = config['configurations']['spark-defaults']['spark.history.fs.logDirectory']
+spark_history_dir = 'hdfs:///spark-history'
 
 spark_history_server_pid_file = status_params.spark_history_server_pid_file
 spark_thrift_server_pid_file = status_params.spark_thrift_server_pid_file
