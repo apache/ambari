ed3ca611b98a6a68c12a735ca934a83755913b39
Myroslav Papirkovskyy <mpapyrkovskyy@hortonworks.com>
2016-02-04 13:14:06 +0200

    AMBARI-14915. Ambari Server should not use /tmp for extraction. (mpapirkovskyy)

--- ambari-server/src/main/java/org/apache/ambari/server/controller/AmbariServer.java
+++ ambari-server/src/main/java/org/apache/ambari/server/controller/AmbariServer.java
@@ -253,6 +253,14 @@ public class AmbariServer {
 
   private static AmbariManagementController clusterController = null;
 
+  /**
+   * Alters system variables on base of Ambari configuration
+   */
+  static void setSystemProperties(Configuration configs) {
+    // modify location of temporary dir to avoid using default /tmp dir
+    System.setProperty("java.io.tmpdir", configs.getServerTempDir());
+  }
+
   public static AmbariManagementController getController() {
     return clusterController;
   }
@@ -264,6 +272,8 @@ public class AmbariServer {
     server = new Server();
     server.setSessionIdManager(sessionIdManager);
     Server serverForAgent = new Server();
+
+    setSystemProperties(configs);
 
     if (System.getProperty("skipDatabaseConsistencyValidation") == null) {
       DatabaseChecker.checkDBVersion();
