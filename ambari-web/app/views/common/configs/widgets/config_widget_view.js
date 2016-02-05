94932c99bb8ff5ad309c15c6a0ecf382f428c73b
ababiichuk <ababiichuk@hortonworks.com>
2016-02-05 09:35:47 +0200

    AMBARI-14933 ranger audit db password is required even when audit to db is off.(ababiichuk)

--- ambari-web/app/views/common/configs/widgets/config_widget_view.js
+++ ambari-web/app/views/common/configs/widgets/config_widget_view.js
@@ -434,6 +434,9 @@ App.ConfigWidgetView = Em.View.extend(App.SupportsDependentConfigs, App.WidgetPo
         var conditionalConfig = serviceConfigs.filterProperty('filename',conditionalConfigFileName).findProperty('name', conditionalConfigName);
         if (conditionalConfig) {
           conditionalConfig.set(valueAttribute, valueAttributes[key]);
+          if (valueAttribute === 'isVisible') {
+            conditionalConfig.set('hiddenBySection', !valueAttributes[key]);
+          }
         }
       }
     }
