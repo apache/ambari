d1cec98c9a98d8e183e13ceeb06e11ebd02a0701
Dmytro Sen <dsen@apache.org>
2016-02-26 09:25:43 +0200

    AMBARI-15100 OOM on TimelineMetricCache in Nodemanager (dsen)

--- ambari-metrics/ambari-metrics-common/src/test/java/org/apache/hadoop/metrics2/sink/timeline/cache/TimelineMetricsCacheTest.java
+++ ambari-metrics/ambari-metrics-common/src/test/java/org/apache/hadoop/metrics2/sink/timeline/cache/TimelineMetricsCacheTest.java
@@ -24,6 +24,8 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 public class TimelineMetricsCacheTest {
 
@@ -31,9 +33,9 @@ public class TimelineMetricsCacheTest {
   private static final String METRIC_NAME = "Test name";
   private static final double delta = 0.00001;
 
-  private final TimelineMetricsCache timelineMetricsCache = new TimelineMetricsCache(
-      TimelineMetricsCache.MAX_RECS_PER_NAME_DEFAULT,
-      TimelineMetricsCache.MAX_EVICTION_TIME_MILLIS);
+  private final TimelineMetricsCache timelineMetricsCache =
+    new TimelineMetricsCache(TimelineMetricsCache.MAX_RECS_PER_NAME_DEFAULT,
+                             TimelineMetricsCache.MAX_EVICTION_TIME_MILLIS);
 
   @Test
   public void testPutGetCounterTimelineMetric() throws Exception {
@@ -72,6 +74,62 @@ public class TimelineMetricsCacheTest {
     assertEquals(70, cachedMetric.getMetricValues().get(8L), delta);
   }
 
+  @Test
+  public void testMaxRecsPerName() throws Exception {
+    int maxRecsPerName = 2;
+    int maxEvictionTime = TimelineMetricsCache.MAX_EVICTION_TIME_MILLIS ;
+    TimelineMetricsCache timelineMetricsCache =
+      new TimelineMetricsCache(maxRecsPerName, maxEvictionTime);
+
+    // put 2 metrics , no cache overflow
+    timelineMetricsCache.putTimelineMetric(
+      createTimelineMetricSingleValue(DEFAULT_START_TIME ));
+    timelineMetricsCache.putTimelineMetric(
+      createTimelineMetricSingleValue(DEFAULT_START_TIME + maxEvictionTime * 2));
+    TimelineMetric cachedMetric = timelineMetricsCache.getTimelineMetric(METRIC_NAME);
+    assertNotNull(cachedMetric);
+    assertFalse(cachedMetric.getMetricValues().isEmpty());
+    assertEquals("2 values added.", 2, cachedMetric.getMetricValues().size());
+    assertEquals(DEFAULT_START_TIME, cachedMetric.getStartTime());
+
+    // put 3 metrics, no cache overflow. check is performed before put operation
+    timelineMetricsCache.putTimelineMetric(
+      createTimelineMetricSingleValue(DEFAULT_START_TIME ));
+    timelineMetricsCache.putTimelineMetric(
+      createTimelineMetricSingleValue(DEFAULT_START_TIME + maxEvictionTime * 2));
+    timelineMetricsCache.putTimelineMetric(
+      createTimelineMetricSingleValue(DEFAULT_START_TIME + maxEvictionTime * 3));
+    cachedMetric = timelineMetricsCache.getTimelineMetric(METRIC_NAME);
+    assertNotNull(cachedMetric);
+    assertFalse(cachedMetric.getMetricValues().isEmpty());
+    assertEquals("3 values added.", 3, cachedMetric.getMetricValues().size());
+    assertEquals(DEFAULT_START_TIME, cachedMetric.getStartTime());
+
+    // put 4 metric values, cache cleaned.
+    timelineMetricsCache.putTimelineMetric(
+      createTimelineMetricSingleValue(DEFAULT_START_TIME ));
+    timelineMetricsCache.putTimelineMetric(
+      createTimelineMetricSingleValue(DEFAULT_START_TIME + maxEvictionTime * 2));
+    timelineMetricsCache.putTimelineMetric(
+      createTimelineMetricSingleValue(DEFAULT_START_TIME + maxEvictionTime * 3));
+    timelineMetricsCache.putTimelineMetric(
+      createTimelineMetricSingleValue(DEFAULT_START_TIME + maxEvictionTime * 4));
+    cachedMetric = timelineMetricsCache.getTimelineMetric(METRIC_NAME);
+    assertNotNull(cachedMetric);
+    assertFalse(cachedMetric.getMetricValues().isEmpty());
+    // check is performed before put operation. while putting 4th metric value,
+    // the first value deleted
+    assertEquals("1 metric value should have been removed", 3, cachedMetric.getMetricValues().size());
+    // first metric value was removed, starttime == second metric value starttime
+    assertEquals(DEFAULT_START_TIME + maxEvictionTime * 2, cachedMetric.getStartTime());
+  }
+
+  private TimelineMetric createTimelineMetricSingleValue(final long startTime) {
+    TreeMap<Long, Double> values = new TreeMap<Long, Double>();
+    values.put(startTime, 0.0);
+    return createTimelineMetric(values, startTime);
+
+  }
   private TimelineMetric createTimelineMetric(Map<Long, Double> metricValues,
                                               long startTime) {
     TimelineMetric timelineMetric = new TimelineMetric();
