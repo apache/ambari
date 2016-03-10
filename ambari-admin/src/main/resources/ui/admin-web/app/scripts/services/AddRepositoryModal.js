d0b999e4261c72a8db8b6fa234ea9cb3db527552
Matt <mmathew@pivotal.io>
2016-03-10 04:00:37 -0800

    AMBARI-13216: Add a "Add" button to Repo management UI. (mithmatt)

--- ambari-admin/src/main/resources/ui/admin-web/app/scripts/services/AddRepositoryModal.js
+++ ambari-admin/src/main/resources/ui/admin-web/app/scripts/services/AddRepositoryModal.js
@@ -19,77 +19,80 @@
 
 angular.module('ambariAdminConsole')
   .factory('AddRepositoryModal', ['$modal', '$q', function($modal, $q) {
-    return {
-      show: function(osList, stackName, stackVersion, repositoryVersionId) {
-        var deferred = $q.defer();
-        var modalInstance = $modal.open({
-          templateUrl: 'views/modals/AddRepositoryModal.html',
-          controller: ['$scope', '$modalInstance', function($scope, $modalInstance) {
-            $scope.osTypes = osList.map(function(os) {
-              return os.OperatingSystems.os_type;
-            });
-            $scope.repo = {
-              selectedOS: $scope.osTypes[0]
-            };
+    var modalObject = {};
 
-            // Check if repository with the same repoId exists among repositories for the selectedOS
-            var repoExists = function(existingRepos, repoId) {
-              for(var i = existingRepos.length - 1; i >= 0; --i) {
-                if (existingRepos[i].Repositories.repo_id === repoId) {
-                  return true;
-                }
-              }
-              return false;
-            };
-
-            $scope.add = function(newRepo) {
-              var repositoriesForOS;
-
-              // Get existing list of repositories for selectedOS
-              for(var i = osList.length - 1; i >= 0; --i) {
-                if (osList[i].OperatingSystems.os_type === newRepo.selectedOS) {
-                  osList[i].repositories = osList[i].repositories || [];
-                  repositoriesForOS = osList[i].repositories;
-                  break;
-                }
-              }
-
-              // If repo with the same id exists for the selectedOS, show an alert and do not take any action
-              $scope.showAlert = repoExists(repositoriesForOS, newRepo.id);
-              if($scope.showAlert) {
-                return;
-              }
-
-              // If no duplicate repository is found on the selectedOS, add the new repository
-              repositoriesForOS.push({
-                Repositories: {
-                  repo_id: newRepo.id,
-                  repo_name: newRepo.name,
-                  os_type: newRepo.selectedOS,
-                  base_url: newRepo.baseUrl,
-                  stack_name: stackName,
-                  stack_version: stackVersion,
-                  repository_version_id: repositoryVersionId
-                }
-              });
-
-              $modalInstance.close();
-              deferred.resolve();
-            };
-
-            $scope.cancel = function() {
-              $modalInstance.dismiss();
-              deferred.reject();
-            };
-          }]
-        });
-        modalInstance.result.then(function() {
-          // Gets triggered on close
-        }, function() {
-          // Gets triggered on dismiss
-          deferred.reject();
-        });
-        return deferred.promise;
+    modalObject.repoExists = function(existingRepos, repoId) {
+      for(var i = existingRepos.length - 1; i >= 0; --i) {
+        if (existingRepos[i].Repositories.repo_id === repoId) {
+          return true;
+        }
       }
+      return false;
     };
+
+    modalObject.getRepositoriesForOS = function (osList, selectedOS) {
+      // Get existing list of repositories for selectedOS
+      for (var i = osList.length - 1; i >= 0; --i) {
+        if (osList[i].OperatingSystems.os_type === selectedOS) {
+          osList[i].repositories = osList[i].repositories || [];
+          return osList[i].repositories;
+        }
+      }
+      return null;
+    };
+
+    modalObject.show = function (osList, stackName, stackVersion, repositoryVersionId) {
+      var deferred = $q.defer();
+      var modalInstance = $modal.open({
+        templateUrl: 'views/modals/AddRepositoryModal.html',
+        controller: ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+          $scope.osTypes = osList.map(function (os) {
+            return os.OperatingSystems.os_type;
+          });
+          $scope.repo = {
+            selectedOS: $scope.osTypes[0]
+          };
+
+          $scope.add = function (newRepo) {
+            var repositoriesForOS = modalObject.getRepositoriesForOS(osList, newRepo.selectedOS);
+
+            // If repo with the same id exists for the selectedOS, show an alert and do not take any action
+            $scope.showAlert = modalObject.repoExists(repositoriesForOS, newRepo.id);
+            if ($scope.showAlert) {
+              return;
+            }
+
+            // If no duplicate repository is found on the selectedOS, add the new repository
+            repositoriesForOS.push({
+              Repositories: {
+                repo_id: newRepo.id,
+                repo_name: newRepo.name,
+                os_type: newRepo.selectedOS,
+                base_url: newRepo.baseUrl,
+                stack_name: stackName,
+                stack_version: stackVersion,
+                repository_version_id: repositoryVersionId
+              }
+            });
+
+            $modalInstance.close();
+            deferred.resolve();
+          };
+
+          $scope.cancel = function () {
+            $modalInstance.dismiss();
+            deferred.reject();
+          };
+        }]
+      });
+      modalInstance.result.then(function () {
+        // Gets triggered on close
+      }, function () {
+        // Gets triggered on dismiss
+        deferred.reject();
+      });
+      return deferred.promise;
+    };
+
+    return modalObject;
   }]);
