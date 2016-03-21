c78296809ef24e516a7d42ef4d7ae606c29ff8d8
Dipayan Bhowmick <dipayan.bhowmick@gmail.com>
2016-03-21 21:03:56 +0530

    AMBARI_14717. Fixed the visual issues in Hive History view. (Rajendra Patil via dipayanb)

--- contrib/views/hive/src/main/resources/ui/hive-web/app/controllers/history.js
+++ contrib/views/hive/src/main/resources/ui/hive-web/app/controllers/history.js
@@ -82,7 +82,7 @@ export default Ember.ArrayController.extend(FilterableMixin, {
       });
 
       minDuration = items.length ? Math.min.apply(Math, items) : 0;
-      maxDuration = items.length ? Math.max.apply(Math, items) : 0;
+      maxDuration = items.length ? Math.max.apply(Math, items) : 60; //Default 1 min
 
       durationColumn.set('numberRange.min', minDuration);
       durationColumn.set('numberRange.max', maxDuration);
@@ -160,6 +160,9 @@ export default Ember.ArrayController.extend(FilterableMixin, {
 
               rangeFilter.set('from', rangeFilter.get('min'));
               rangeFilter.set('to', rangeFilter.get('max'));
+
+              rangeFilter.set('fromDuration', rangeFilter.get('min'));
+              rangeFilter.set('toDuration', rangeFilter.get('max'));
             }
           }
         });
