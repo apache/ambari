548ed9fdabf8c711d507abb080bab24ec78b501a
Sumit Mohanty <smohanty@hortonworks.com>
2016-04-06 15:57:07 -0700

    Revert "Ambari-15747. Kafka jaas files are not generated on a Kafa upgrade if security.inter.broker.protocol is set to SASL_PLAINTEXT (Sriharsha Chintalapani via smohanty)"
    This reverts commit fd3692ecf23bfcccaeb4b83aa95735e084ca8f2b.

--- ambari-server/src/main/resources/common-services/KAFKA/0.8.1.2.2/package/scripts/params.py
+++ ambari-server/src/main/resources/common-services/KAFKA/0.8.1.2.2/package/scripts/params.py
@@ -140,9 +140,7 @@ if has_metric_collector:
 # Security-related params
 security_enabled = config['configurations']['cluster-env']['security_enabled']
 kafka_kerberos_enabled = ('security.inter.broker.protocol' in config['configurations']['kafka-broker'] and
-                         (config['configurations']['kafka-broker']['security.inter.broker.protocol'] == "PLAINTEXTSASL" or
-                          config['configurations']['kafka-broker']['security.inter.broker.protocol'] == "SASL_PLAINTEXT")
-
+                          config['configurations']['kafka-broker']['security.inter.broker.protocol'] == "PLAINTEXTSASL")
 
 if security_enabled and stack_version_formatted != "" and 'kafka_principal_name' in config['configurations']['kafka-env'] \
   and check_stack_feature(StackFeature.KAFKA_KERBEROS, stack_version_formatted):
@@ -182,7 +180,7 @@ if has_ranger_admin and is_supported_kafka_ranger:
 
   ranger_env = config['configurations']['ranger-env']
   ranger_plugin_properties = config['configurations']['ranger-kafka-plugin-properties']
-
+  
   ranger_kafka_audit = config['configurations']['ranger-kafka-audit']
   ranger_kafka_audit_attrs = config['configuration_attributes']['ranger-kafka-audit']
   ranger_kafka_security = config['configurations']['ranger-kafka-security']
@@ -191,7 +189,7 @@ if has_ranger_admin and is_supported_kafka_ranger:
   ranger_kafka_policymgr_ssl_attrs = config['configuration_attributes']['ranger-kafka-policymgr-ssl']
 
   policy_user = config['configurations']['ranger-kafka-plugin-properties']['policy_user']
-
+  
   ranger_plugin_config = {
     'username' : config['configurations']['ranger-kafka-plugin-properties']['REPOSITORY_CONFIG_USERNAME'],
     'password' : unicode(config['configurations']['ranger-kafka-plugin-properties']['REPOSITORY_CONFIG_PASSWORD']),
