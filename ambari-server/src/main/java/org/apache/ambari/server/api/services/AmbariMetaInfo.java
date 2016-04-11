e91f2a656dd4bb037bd0a29b0cf207e6993d4836
Aravindan Vijayan <avijayan@hortonworks.com>
2016-04-11 15:18:57 -0700

    AMBARI-15698 : Metrics is not showing data (avijayan)

--- ambari-server/src/main/java/org/apache/ambari/server/api/services/AmbariMetaInfo.java
+++ ambari-server/src/main/java/org/apache/ambari/server/api/services/AmbariMetaInfo.java
@@ -19,6 +19,7 @@
 package org.apache.ambari.server.api.services;
 
 import static org.apache.ambari.server.controller.spi.Resource.InternalType.Component;
+import static org.apache.ambari.server.controller.spi.Resource.InternalType.HostComponent;
 import static org.apache.ambari.server.controller.utilities.PropertyHelper.AGGREGATE_FUNCTION_IDENTIFIERS;
 
 import java.io.File;
@@ -931,7 +932,8 @@ public class AmbariMetaInfo {
 
                 // NOTE: Only Component aggregates for AMS supported for now.
                 if (metricDefinition.getType().equals("ganglia") &&
-                  metricDefEntry.getKey().equals(Component.name())) {
+                  (metricDefEntry.getKey().equals(Component.name()) ||
+                    metricDefEntry.getKey().equals(HostComponent.name()))) {
                   for (Map.Entry<String, Metric> processedMetric : processedMetrics.entrySet()) {
                     newMetricsToAdd.putAll(getAggregateFunctionMetrics(processedMetric.getKey(),
                       processedMetric.getValue()));
