0780149ec762e3762049d5d75c3633e9d6dda26b
Matt <mmathew@pivotal.io>
2016-04-29 17:50:02 -0700

    AMBARI-16106: Use additional flags for HAWQ Check (mithmatt)

--- ambari-server/src/main/resources/common-services/HAWQ/2.0.0/package/scripts/params.py
+++ ambari-server/src/main/resources/common-services/HAWQ/2.0.0/package/scripts/params.py
@@ -66,6 +66,8 @@ kinit_path_local = get_kinit_path(default('/configurations/kerberos-env/executab
 hdfs_principal_name = config['configurations']['hadoop-env']['hdfs_principal_name']
 dfs_nameservice = default('/configurations/hdfs-site/dfs.nameservices', None)
 
+hawq_global_rm_type = default('/configurations/hawq-site/hawq_global_rm_type', None)
+
 # HDFSResource partial function
 HdfsResource = functools.partial(HdfsResource,
                                  user=hdfs_superuser,
@@ -118,7 +120,7 @@ table_definition = {
 # YARN
 # Note: YARN is not mandatory for HAWQ. It is required only when the users set HAWQ to use YARN as resource manager
 rm_host = __get_component_host('rm_host')
-yarn_ha_enabled = default('/configurations/yarn-site/yarn.resourcemanager.ha.enabled', False)
+is_yarn_ha_enabled = True if str(default('/configurations/yarn-site/yarn.resourcemanager.ha.enabled', False)).lower() == "true" else False
 
 # Config files
 hawq_check_content = config['configurations']['hawq-check-env']['content']
