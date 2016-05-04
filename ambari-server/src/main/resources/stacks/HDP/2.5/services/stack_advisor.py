04b6f14be5fa8e23d784469fdc73d6df41c2ca4d
Sumit Mohanty <smohanty@hortonworks.com>
2016-05-03 20:55:07 -0700

    AMBARI-16241. While adding Hive Server Interactive, ATLAS config recommendation throws error - add check only (smohanty)

--- ambari-server/src/main/resources/stacks/HDP/2.5/services/stack_advisor.py
+++ ambari-server/src/main/resources/stacks/HDP/2.5/services/stack_advisor.py
@@ -133,7 +133,7 @@ class HDP25StackAdvisor(HDP24StackAdvisor):
     servicesList = [service["StackServices"]["service_name"] for service in services["services"]]
 
     include_logsearch = "LOGSEARCH" in servicesList
-    if include_logsearch:
+    if include_logsearch and "logsearch-solr-env" in services["configurations"]:
       logsearch_solr_znode = services["configurations"]["logsearch-solr-env"]["properties"]['logsearch_solr_znode']
       putAtlasApplicationProperty('atlas.graph.index.search.solr.zookeeper-url', '{{zookeeper_quorum}}' + logsearch_solr_znode)
 
