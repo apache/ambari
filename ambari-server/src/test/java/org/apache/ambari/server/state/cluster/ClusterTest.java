30899abc069d6e9afcd92b2630d71d1f3fe5a4a3
Myroslav Papirkovskyi <mpapyrkovskyy@hortonworks.com>
2016-05-26 16:55:15 +0300

    AMBARI-16897. Sorting works incorrectly for config groups in Config History tab. (mpapirkovskyy)

--- ambari-server/src/test/java/org/apache/ambari/server/state/cluster/ClusterTest.java
+++ ambari-server/src/test/java/org/apache/ambari/server/state/cluster/ClusterTest.java
@@ -1370,7 +1370,7 @@ public class ClusterTest {
     Assert.assertEquals("HDFS", hdfsResponse.getServiceName());
     Assert.assertEquals("c1", hdfsResponse.getClusterName());
     Assert.assertEquals("admin", hdfsResponse.getUserName());
-    Assert.assertEquals("default", hdfsResponse.getGroupName());
+    Assert.assertEquals("Default", hdfsResponse.getGroupName());
     Assert.assertEquals(Long.valueOf(-1), hdfsResponse.getGroupId());
     Assert.assertEquals(Long.valueOf(1), hdfsResponse.getVersion());
 
