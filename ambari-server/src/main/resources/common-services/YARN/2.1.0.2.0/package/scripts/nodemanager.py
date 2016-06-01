29f9fa341ff795503ee5b782181179b50180850e
Jayush Luniya <jluniya@hortonworks.com>
2016-06-01 13:53:43 -0700

    AMBARI-16849: clean up import * for YARN service - add missing import ( Juanjo Marron via jluniya )

--- ambari-server/src/main/resources/common-services/YARN/2.1.0.2.0/package/scripts/nodemanager.py
+++ ambari-server/src/main/resources/common-services/YARN/2.1.0.2.0/package/scripts/nodemanager.py
@@ -25,6 +25,7 @@ from resource_management.libraries.script.script import Script
 from resource_management.libraries.functions import conf_select, stack_select
 from resource_management.libraries.functions.constants import StackFeature
 from resource_management.libraries.functions.stack_features import check_stack_feature
+from resource_management.libraries.functions.check_process_status import check_process_status
 from resource_management.libraries.functions.format import format
 from resource_management.libraries.functions.security_commons import build_expectations, \
   cached_kinit_executor, get_params_from_filesystem, validate_security_config_properties, \
