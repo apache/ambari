b7c95413b19037b99e3d7ddf9f58f1c902ad914e
Srimanth Gunturi <sgunturi@hortonworks.com>
2016-06-01 22:36:21 -0700

    Revert "AMBARI-16272. Ambari Upgrade shouldn't automatically add stack configs. Fix default upgrade policy and script defaults (dlysnichenko)" - faling tests
    This reverts commit 8e7103a863b8649d7373537eb3bfbdffdfb18bf2.

--- ambari-server/src/main/resources/scripts/configurations-set-default-update-policy.sh
+++ ambari-server/src/main/resources/scripts/configurations-set-default-update-policy.sh
@@ -3,17 +3,13 @@
 for i in `find ../../../ -regex '.*/configuration.*/.*.xml'` ;
 do
   echo $i;
-
-  xmlstarlet ed --inplace -d "//on-ambari-upgrade" $i
-  xmlstarlet ed --inplace -d "//on-stack-upgrade" $i
-
   `xmlstarlet ed --inplace --subnode "/configuration/property" -t 'elem' -n "on-ambari-upgrade" $i`;
-  `xmlstarlet ed --inplace --subnode "/configuration/property/on-ambari-upgrade" -t 'attr' -n "add" -v "false" $i`;
-  `xmlstarlet ed --inplace --subnode "/configuration/property/on-ambari-upgrade" -t 'attr' -n "change" -v "true" $i`;
-  `xmlstarlet ed --inplace --subnode "/configuration/property/on-ambari-upgrade" -t 'attr' -n "delete" -v "true" $i`;
+  `xmlstarlet ed --inplace --subnode "/configuration/property/on-ambari-upgrade" -t 'attr' -n "add" -v "true" $i`;
+  `xmlstarlet ed --inplace --subnode "/configuration/property/on-ambari-upgrade" -t 'attr' -n "change" -v "false" $i`;
+  `xmlstarlet ed --inplace --subnode "/configuration/property/on-ambari-upgrade" -t 'attr' -n "delete" -v "false" $i`;
 
   `xmlstarlet ed --inplace --subnode "/configuration/property" -t 'elem' -n "on-stack-upgrade" $i`;
   `xmlstarlet ed --inplace --subnode "/configuration/property/on-stack-upgrade" -t 'attr' -n "add" -v "true" $i`;
-  `xmlstarlet ed --inplace --subnode "/configuration/property/on-stack-upgrade" -t 'attr' -n "change" -v "true" $i`;
+  `xmlstarlet ed --inplace --subnode "/configuration/property/on-stack-upgrade" -t 'attr' -n "change" -v "false" $i`;
   `xmlstarlet ed --inplace --subnode "/configuration/property/on-stack-upgrade" -t 'attr' -n "delete" -v "false" $i`;
 done
