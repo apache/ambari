9933d1784cebca2a7df1b8f61a5819dee28576b7
Aravindan Vijayan <avijayan@hortonworks.com>
2016-06-14 21:57:49 -0700

    AMBARI-17238 : AMS extrapolation should be done only for Counter metrics. (avijayan)

--- ambari-metrics/ambari-metrics-timelineservice/src/main/java/org/apache/hadoop/yarn/server/applicationhistoryservice/metrics/timeline/aggregators/TimelineMetricClusterAggregatorSecond.java
+++ ambari-metrics/ambari-metrics-timelineservice/src/main/java/org/apache/hadoop/yarn/server/applicationhistoryservice/metrics/timeline/aggregators/TimelineMetricClusterAggregatorSecond.java
@@ -18,6 +18,7 @@
 package org.apache.hadoop.yarn.server.applicationhistoryservice.metrics.timeline.aggregators;
 
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.metrics2.sink.timeline.PostProcessingUtil;
 import org.apache.hadoop.metrics2.sink.timeline.TimelineMetric;
@@ -268,30 +269,77 @@ public class TimelineMetricClusterAggregatorSecond extends AbstractTimelineAggre
                                          Map<Long, Double> timeSliceValueMap) {
 
 
-    List<Long> requiredTimestamps = new ArrayList<>();
-    for (Long[] timeSlice : timeSlices) {
-      if (!timeSliceValueMap.containsKey(timeSlice[1])) {
-        requiredTimestamps.add(timeSlice[1]);
+    if (StringUtils.isNotEmpty(timelineMetric.getType()) && "COUNTER".equalsIgnoreCase(timelineMetric.getType())) {
+      //For Counter Based metrics, ok to do interpolation and extrapolation
+
+      List<Long> requiredTimestamps = new ArrayList<>();
+      for (Long[] timeSlice : timeSlices) {
+        if (!timeSliceValueMap.containsKey(timeSlice[1])) {
+          requiredTimestamps.add(timeSlice[1]);
+        }
       }
-    }
+      Map<Long, Double> interpolatedValuesMap = PostProcessingUtil.interpolate(timelineMetric.getMetricValues(), requiredTimestamps);
 
-    Map<Long, Double> interpolatedValuesMap = PostProcessingUtil.interpolate(timelineMetric.getMetricValues(), requiredTimestamps);
+      if (interpolatedValuesMap != null) {
+        for (Map.Entry<Long, Double> entry : interpolatedValuesMap.entrySet()) {
+          Double interpolatedValue = entry.getValue();
 
-    if (interpolatedValuesMap != null) {
-      for (Map.Entry<Long, Double> entry : interpolatedValuesMap.entrySet()) {
-        Double interpolatedValue = entry.getValue();
+          if (interpolatedValue != null) {
+            TimelineClusterMetric clusterMetric = new TimelineClusterMetric(
+              timelineMetric.getMetricName(),
+              timelineMetric.getAppId(),
+              timelineMetric.getInstanceId(),
+              entry.getKey(),
+              timelineMetric.getType());
 
-        if (interpolatedValue != null) {
-          TimelineClusterMetric clusterMetric = new TimelineClusterMetric(
-            timelineMetric.getMetricName(),
-            timelineMetric.getAppId(),
-            timelineMetric.getInstanceId(),
-            entry.getKey(),
-            timelineMetric.getType());
+            timelineClusterMetricMap.put(clusterMetric, interpolatedValue);
+          } else {
+            LOG.debug("Cannot compute interpolated value, hence skipping.");
+          }
+        }
+      }
+    } else {
+      //For other metrics, ok to do only interpolation
 
-          timelineClusterMetricMap.put(clusterMetric, interpolatedValue);
-        } else {
-          LOG.debug("Cannot compute interpolated value, hence skipping.");
+      for (int sliceNum = 0; sliceNum < timeSlices.size(); sliceNum++) {
+        Long[] timeSlice = timeSlices.get(sliceNum);
+
+        if (!timeSliceValueMap.containsKey(timeSlice[1])) {
+          LOG.debug("Found an empty slice : " + new Date(timeSlice[0]) + ", " + new Date(timeSlice[1]));
+
+          Double lastSeenValue = null;
+          int index = sliceNum - 1;
+          Long[] prevTimeSlice = null;
+          while (lastSeenValue == null && index >= 0) {
+            prevTimeSlice = timeSlices.get(index--);
+            lastSeenValue = timeSliceValueMap.get(prevTimeSlice[1]);
+          }
+
+          Double nextSeenValue = null;
+          index = sliceNum + 1;
+          Long[] nextTimeSlice = null;
+          while (nextSeenValue == null && index < timeSlices.size()) {
+            nextTimeSlice = timeSlices.get(index++);
+            nextSeenValue = timeSliceValueMap.get(nextTimeSlice[1]);
+          }
+
+          Double interpolatedValue = PostProcessingUtil.interpolate(timeSlice[1],
+            (prevTimeSlice != null ? prevTimeSlice[1] : null), lastSeenValue,
+            (nextTimeSlice != null ? nextTimeSlice[1] : null), nextSeenValue);
+
+          if (interpolatedValue != null) {
+            TimelineClusterMetric clusterMetric = new TimelineClusterMetric(
+              timelineMetric.getMetricName(),
+              timelineMetric.getAppId(),
+              timelineMetric.getInstanceId(),
+              timeSlice[1],
+              timelineMetric.getType());
+
+            LOG.debug("Interpolated value : " + interpolatedValue);
+            timelineClusterMetricMap.put(clusterMetric, interpolatedValue);
+          } else {
+            LOG.debug("Cannot compute interpolated value, hence skipping.");
+          }
         }
       }
     }
