f613d3914a119ae3da82a8b022132bd3856027c3
oleewere <oleewere@gmail.com>
2016-06-21 15:12:44 +0200

    AMBARI-17309. Removing Event history module (Dharmesh Makwana via oleewere)

--- ambari-logsearch/ambari-logsearch-portal/src/main/webapp/libs/other/dashboard/dashboard.js
+++ ambari-logsearch/ambari-logsearch-portal/src/main/webapp/libs/other/dashboard/dashboard.js
@@ -1,10 +1,20 @@
 /**
- * @version: 1.0.0
- * @author: Keval Bhatt 
- * @copyright: Copyright (c) 2015 Keval Bhatt. All rights reserved.
- * @license: Licensed under the MIT license. See http://www.opensource.org/licenses/mit-license.php
- * @website: http://kevalbhatt.github.io/dashboard/
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 'use strict';
 (function(root, factory) {
 
