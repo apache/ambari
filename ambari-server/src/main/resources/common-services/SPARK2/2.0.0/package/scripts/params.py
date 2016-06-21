706946e84cf8c1b6e9121da3ed08516c9d1329c8
Srimanth Gunturi <sgunturi@hortonworks.com>
2016-06-21 13:35:01 -0700

    AMBARI-17074. Expose Spark daemon memory in Spark2 (Weiqing Yang via srimanth)

--- ambari-server/src/main/resources/common-services/SPARK2/2.0.0/package/scripts/params.py
+++ ambari-server/src/main/resources/common-services/SPARK2/2.0.0/package/scripts/params.py
@@ -64,6 +64,7 @@ if stack_version_formatted and check_stack_feature(StackFeature.ROLLING_UPGRADE,
   spark_pid_dir = status_params.spark_pid_dir
   spark_home = format("{stack_root}/current/{component_directory}")
 
+spark_daemon_memory = config['configurations']['spark2-env']['spark_daemon_memory']
 spark_thrift_server_conf_file = spark_conf + "/spark-thrift-sparkconf.conf"
 java_home = config['hostLevelParams']['java_home']
 
