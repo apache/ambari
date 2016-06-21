8dbbd4fadf64b827943e89d8483542d3657fec93
Dipayan Bhowmick <dipayan.bhowmick@gmail.com>
2016-06-22 01:20:59 +0530

    AMBARI-17317. 'tez.tez-ui.history-url.base' did not get updated after ambari upgrade. (dipayanb)

--- ambari-server/src/main/java/org/apache/ambari/server/upgrade/AbstractUpgradeCatalog.java
+++ ambari-server/src/main/java/org/apache/ambari/server/upgrade/AbstractUpgradeCatalog.java
@@ -17,6 +17,14 @@
  */
 package org.apache.ambari.server.upgrade;
 
+import javax.persistence.EntityManager;
+import javax.xml.bind.JAXBException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.io.StringReader;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -33,6 +41,8 @@ import java.util.Set;
 import java.util.Stack;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.persistence.EntityManager;
 import javax.xml.parsers.DocumentBuilder;
@@ -70,6 +80,8 @@ import org.apache.ambari.server.state.kerberos.KerberosDescriptorFactory;
 import org.apache.ambari.server.state.kerberos.KerberosIdentityDescriptor;
 import org.apache.ambari.server.state.kerberos.KerberosServiceDescriptor;
 import org.apache.ambari.server.utils.VersionUtils;
+import org.apache.ambari.server.view.ViewArchiveUtility;
+import org.apache.ambari.server.view.configuration.ViewConfig;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -91,6 +103,8 @@ public abstract class AbstractUpgradeCatalog implements UpgradeCatalog {
   protected Configuration configuration;
   @Inject
   protected StackUpgradeUtil stackUpgradeUtil;
+  @Inject
+  protected ViewArchiveUtility archiveUtility;
 
   protected Injector injector;
 
@@ -883,6 +897,80 @@ public abstract class AbstractUpgradeCatalog implements UpgradeCatalog {
   @Override
   public void upgradeData() throws AmbariException, SQLException {
     executeDMLUpdates();
+    updateTezHistoryUrlBase();
+  }
+
+  /**
+   * Version of the Tez view changes with every new version on Ambari. Hence the 'tez.tez-ui.history-url.base' in tez-site.xml
+   * has to be changed every time ambari update happens. This will read the latest tez-view jar file and find out the
+   * view version by reading the view.xml file inside it and update the 'tez.tez-ui.history-url.base' property in tez-site.xml
+   * with the proper value of the updated tez view version.
+   */
+  private void updateTezHistoryUrlBase() throws AmbariException {
+    AmbariManagementController ambariManagementController = injector.getInstance(AmbariManagementController.class);
+    Clusters clusters = ambariManagementController.getClusters();
+
+    if (clusters != null) {
+      Map<String, Cluster> clusterMap = clusters.getClusters();
+      if (clusterMap != null && !clusterMap.isEmpty()) {
+        for (final Cluster cluster : clusterMap.values()) {
+          Set<String> installedServices = cluster.getServices().keySet();
+          if (installedServices.contains("TEZ")) {
+            Config tezSite = cluster.getDesiredConfigByType("tez-site");
+            if (tezSite != null) {
+              String currentTezHistoryUrlBase = tezSite.getProperties().get("tez.tez-ui.history-url.base");
+              if(currentTezHistoryUrlBase != null && !currentTezHistoryUrlBase.isEmpty()) {
+                String newTezHistoryUrlBase = getUpdatedTezHistoryUrlBase(currentTezHistoryUrlBase);
+                updateConfigurationProperties("tez-site", Collections.singletonMap("tez.tez-ui.history-url.base", newTezHistoryUrlBase), true, false);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  protected String getUpdatedTezHistoryUrlBase(String currentTezHistoryUrlBase) throws AmbariException{
+    String pattern = "(.*\\/TEZ\\/)(.*)(\\/TEZ_CLUSTER_INSTANCE)";
+    Pattern regex = Pattern.compile(pattern);
+    Matcher matcher = regex.matcher(currentTezHistoryUrlBase);
+    String prefix;
+    String suffix;
+    String oldVersion;
+    if (matcher.find()) {
+      prefix = matcher.group(1);
+      oldVersion = matcher.group(2);
+      suffix = matcher.group(3);
+    } else {
+      throw new AmbariException("Cannot prepare the new value for property: 'tez.tez-ui.history-url.base' using the old value: '" + currentTezHistoryUrlBase + "'");
+    }
+
+    String latestTezViewVersion = getLatestTezViewVersion(oldVersion);
+
+    return prefix + latestTezViewVersion + suffix;
+  }
+
+  protected String getLatestTezViewVersion(String oldVersion) {
+    File viewsDirectory = configuration.getViewsDir();
+    File[] files = viewsDirectory.listFiles(new FilenameFilter() {
+      @Override
+      public boolean accept(File dir, String name) {
+        return name.startsWith("tez-view");
+      }
+    });
+
+    if(files == null || files.length == 0) {
+      LOG.error("Could not file tez-view jar file in '{}'. Returning the old version", viewsDirectory.getAbsolutePath());
+      return oldVersion;
+    }
+    File tezViewFile = files[0];
+    try {
+      ViewConfig viewConfigFromArchive = archiveUtility.getViewConfigFromArchive(tezViewFile);
+      return viewConfigFromArchive.getVersion();
+    } catch (JAXBException | IOException e) {
+      LOG.error("Failed to read the tez view version from: {}. Returning the old version", tezViewFile);
+      return oldVersion;
+    }
   }
 
   @Override
