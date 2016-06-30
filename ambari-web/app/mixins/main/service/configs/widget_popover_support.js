52d7fe4478fdd13deb4654b3d47ef326b7a15689
Oleg Nechiporenko <onechiporenko@apache.org>
2016-06-30 15:00:52 +0300

    AMBARI-17500. UI: a popup with a property descriprtion doesn't fade away (onechiporenko)

--- ambari-web/app/mixins/main/service/configs/widget_popover_support.js
+++ ambari-web/app/mixins/main/service/configs/widget_popover_support.js
@@ -39,24 +39,26 @@ App.WidgetPopoverSupport = Em.Mixin.create({
 
   /**
    * Where popover should be displayed - top|left|right|bottom
+   * popover to left if config is located at the right most sub-section of the right most section.
+   *
    * @type {string}
    */
   popoverPlacement: function () {
-    // popover to left if config is located at the right most sub-section of the right most section.
     return this.get('section.isLastColumn') && this.get('subSection.isLastColumn')? 'left' : 'right';
-  }.property(),
+  }.property('section.isLastColumn', 'subSection.isLastColumn'),
 
   initPopover: function () {
     if (this.get('isPopoverEnabled') !== false) {
+      this.destroyPopover();
       var leftPopoverTemplate = '<div class="popover config-widget-left-popover"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>',
         isWidget = !Em.isEmpty(this.$('.original-widget')),
         popoverSelector = isWidget ? this.$('.original-widget') : this.$('.input-append');
 
       App.popover(popoverSelector, {
-        template: this.get('popoverPlacement') == 'left'? leftPopoverTemplate : undefined,
+        template: this.get('popoverPlacement') === 'left'? leftPopoverTemplate : undefined,
         title: Em.I18n.t('installer.controls.serviceConfigPopover.title').format(
           this.get('configLabel'),
-          (this.get('configLabel') == this.get('config.name')) ? '' : this.get('config.name')
+          this.get('configLabel') === this.get('config.name') ? '' : this.get('config.name')
         ),
         content: this.get('config.description'),
         placement: this.get('popoverPlacement'),
@@ -66,11 +68,27 @@ App.WidgetPopoverSupport = Em.Mixin.create({
           hide: 0
         }
       });
+      this.on('willDestroyElement', this, this.destroyPopover);
     }
   },
 
-  willDestroyElement: function() {
-    this.$().popover('destroy');
+  /**
+   * Destroy popover after config becomes hidden
+   */
+  destroyPopover: function () {
+    this.movePopover('destroy');
+  },
+
+  /**
+   * Hide popover on config state changing (from widget-view to raw-mode and from raw-mode to widget-view)
+   */
+  hidePopover: function () {
+    this.movePopover('hide');
+  }.observes('config.showAsTextBox'),
+
+  movePopover: function (action) {
+    var popoverSelector = Em.isEmpty(this.$('.original-widget')) ? this.$('.input-append') : this.$('.original-widget');
+    this.$(popoverSelector).popover(action);
   }
 
 });
