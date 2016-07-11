18f0a8f61b86ce4fa021333f36b3bdabf85b2a9f
Lisnichenko Dmitro <dlysnichenko@hortonworks.com>
2016-07-11 18:53:12 +0300

    AMBARI-17651. Restart indicator is not shown after modifying the configs (dlysnichenko)

--- ambari-server/src/main/java/org/apache/ambari/server/state/svccomphost/ServiceComponentHostImpl.java
+++ ambari-server/src/main/java/org/apache/ambari/server/state/svccomphost/ServiceComponentHostImpl.java
@@ -1797,7 +1797,7 @@ public class ServiceComponentHostImpl implements ServiceComponentHost {
       if (desiredStateEntity != null) {
         desiredStateEntity.setRestartRequired(restartRequired);
         saveComponentDesiredStateEntityIfPersisted();
-        helper.invalidateStaleConfigsCache(this);
+        helper.invalidateStaleConfigsCache(this, null);
       } else {
         LOG.warn("Setting a member on an entity object that may have been " +
           "previously deleted, serviceName = " + getServiceName() + ", " +
