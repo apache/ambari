13ab83a0309608940bdf12cc9bc3ede136471d8a
oleewere <oleewere@gmail.com>
2016-08-01 14:43:41 +0200

    AMBARI-17974. Improvements/fixes for AMBARI_INFRA initial service definition (oleewere)

--- ambari-server/src/main/resources/common-services/AMBARI_INFRA/0.1.0/package/scripts/service_check.py
+++ ambari-server/src/main/resources/common-services/AMBARI_INFRA/0.1.0/package/scripts/service_check.py
@@ -17,14 +17,31 @@ limitations under the License.
 
 """
 
-from resource_management.libraries.script.script import Script
 from resource_management.core.logger import Logger
+from resource_management.core.resources.system import Execute
+from resource_management.libraries.functions.format import format
+from resource_management.libraries.script.script import Script
 
 class InfraServiceCheck(Script):
   def service_check(self, env):
     import params
     env.set_params(params)
-    Logger.info('Infra Service Check')
+
+    Logger.info('Infra Service Check ...')
+    if "infra-solr-env" in params.config['configurations'] \
+      and params.infra_solr_hosts is not None \
+      and len(params.infra_solr_hosts) > 0:
+      solr_protocol = "https" if params.infra_solr_ssl_enabled else "http"
+      solr_host = params.infra_solr_hosts[0] # choose the first solr host
+      solr_port = params.infra_solr_port
+      solr_url = format("{solr_protocol}://{solr_host}:{solr_port}/solr/#/")
+
+      smokeuser_kinit_cmd = format("{kinit_path_local} -kt {smoke_user_keytab} {smokeuser_principal};") if params.security_enabled else ""
+      smoke_infra_solr_cmd = format("{smokeuser_kinit_cmd} curl -s -o /dev/null -w'%{{http_code}}' --negotiate -u: -k {solr_url} | grep 200")
+      Execute(smoke_infra_solr_cmd,
+              tries = 40,
+              try_sleep=3,
+              logoutput=True)
 
 if __name__ == "__main__":
   InfraServiceCheck().execute()
