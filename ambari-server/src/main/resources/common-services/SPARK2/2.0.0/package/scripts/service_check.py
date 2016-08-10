4e41ec399fdf15da6879feda9cc124eec24f76ef
Jayush Luniya <jluniya@hortonworks.com>
2016-08-10 09:39:31 -0700

    AMBARI-18091: Fix Spark2 service check failure when WE is enabled (Saisai Shao via jluniya)

--- ambari-server/src/main/resources/common-services/SPARK2/2.0.0/package/scripts/service_check.py
+++ ambari-server/src/main/resources/common-services/SPARK2/2.0.0/package/scripts/service_check.py
@@ -32,7 +32,8 @@ class SparkServiceCheck(Script):
       spark_kinit_cmd = format("{kinit_path_local} -kt {spark_kerberos_keytab} {spark_principal}; ")
       Execute(spark_kinit_cmd, user=params.spark_user)
 
-    Execute(format("curl -s -o /dev/null -w'%{{http_code}}' --negotiate -u: -k http://{spark_history_server_host}:{spark_history_ui_port} | grep 200"),
+    scheme = "https" if params.ui_ssl_enabled else "http"
+    Execute(format("curl -s -o /dev/null -w'%{{http_code}}' --negotiate -u: -k {scheme}://{spark_history_server_host}:{spark_history_ui_port} | grep 200"),
       tries = 10,
       try_sleep=3,
       logoutput=True
