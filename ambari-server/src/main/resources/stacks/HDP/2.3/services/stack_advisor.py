2d6ade5a21f2c055ef73a5b8d321a4265c41c6f9
Jayush Luniya <jluniya@hortonworks.com>
2016-08-18 16:13:33 -0700

    Revert "AMBARI-18188. KAFKA is spelled as 'KAKFA' in stack_advisor.py because of which the function validate (Anita Jebaraj via rlevas)"
    This reverts commit 3c51317e62efbdc213dab92f076c95126465387f.

--- ambari-server/src/main/resources/stacks/HDP/2.3/services/stack_advisor.py
+++ ambari-server/src/main/resources/stacks/HDP/2.3/services/stack_advisor.py
@@ -749,7 +749,7 @@ class HDP23StackAdvisor(HDP22StackAdvisor):
       "HIVE": {"hiveserver2-site": self.validateHiveServer2Configurations,
                "hive-site": self.validateHiveConfigurations},
       "HBASE": {"hbase-site": self.validateHBASEConfigurations},
-      "KAFKA": {"kafka-broker": self.validateKAFKAConfigurations},
+      "KAKFA": {"kafka-broker": self.validateKAFKAConfigurations},
       "YARN": {"yarn-site": self.validateYARNConfigurations},
       "RANGER": {"admin-properties": self.validateRangerAdminConfigurations,
                  "ranger-env": self.validateRangerConfigurationsEnv}
