a9bbf4644890690c45e9879f7bda9ff9d0ec7000
ababiichuk <ababiichuk@hortonworks.com>
2016-08-25 16:15:22 +0300

    AMBARI-18180 Add ES6 support to Ambari Web. (ababiichuk)

--- ambari-web/brunch-config.js
+++ ambari-web/brunch-config.js
@@ -1,36 +1,40 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 
-fs   = require 'fs'
-path = require 'path'
-
-# See docs at http://brunch.readthedocs.org/en/latest/config.html.
-
-exports.config =
-
-  files:
-
-    javascripts:
-      joinTo:
-        'javascripts/app.js': /^app/
-        'javascripts/vendor.js': /^vendor/
-        'test/javascripts/test.js': /^test(\/|\\)(?!vendor)/
+module.exports.config = {
+  plugins: {
+    babel: {
+      ignore: [
+        /^(vendor|app\/data|app\/assets|test)/
+      ],
+      pattern: /\.(js)$/
+    }
+  },
+  files: {
+    javascripts: {
+      joinTo: {
+        'javascripts/app.js': /^app/,
+        'javascripts/vendor.js': /^vendor/,
+        'test/javascripts/test.js': /^test(\/|\\)(?!vendor)/,
         'test/javascripts/test-vendor.js': /^test(\/|\\)(?=vendor)/
-      order:
+      },
+      order: {
         before: [
           'vendor/scripts/console-helper.js',
           'vendor/scripts/jquery-1.7.2.min.js',
@@ -71,15 +75,16 @@ exports.config =
           'vendor/scripts/jquery.flexibleArea.js',
           'vendor/scripts/FileSaver.js',
           'vendor/scripts/Blob.js'
-
-          ]
-
-    stylesheets:
-      defaultExtension: 'css'
-      joinTo:
-        'stylesheets/app.css': /^app/
+        ]
+      }
+    },
+    stylesheets: {
+      defaultExtension: 'css',
+      joinTo: {
+        'stylesheets/app.css': /^app/,
         'stylesheets/vendor.css': /^vendor/
-      order:
+      },
+      order: {
         before: [
           'vendor/styles/bootstrap.css',
           'vendor/styles/font-awesome.css',
@@ -94,19 +99,27 @@ exports.config =
           'vendor/styles/visualsearch-datauri.css'
         ],
         after: ['app/styles/custom-ui.css']
+      }
+    },
 
-    templates:
-      precompile: true
-      defaultExtension: 'hbs'
-      joinTo: 'javascripts/app.js' : /^app/
-      paths:
-        jquery: 'vendor/scripts/jquery-1.7.2.min.js'
-        handlebars: 'vendor/scripts/handlebars-1.0.0.beta.6.js'
+
+    templates: {
+      precompile: true,
+      defaultExtensions: ['hbs'],
+      joinTo: {'javascripts/app.js': /^app/},
+      paths: {
+        jquery: 'vendor/scripts/jquery-1.7.2.min.js',
+        handlebars: 'vendor/scripts/handlebars-1.0.0.beta.6.js',
         ember: 'vendor/scripts/ember-latest.js'
+      }
+    }
+  },
 
-  server:
-    port: 3333
-    base: '/'
-    run: no
+  server: {
+    port: 3333,
+    base: '/',
+    run: 'no'
+  },
 
   sourceMaps: false
+};
