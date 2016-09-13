532b689f3158f8c90b9bc69e1d62d3816f3e81b1
Mugdha Varadkar <mugdha@apache.org>
2016-09-13 16:53:35 +0530

    AMBARI-18375 Ranger Plugin configs are not generated for Hive interactive (mugdha)

--- ambari-server/src/main/resources/common-services/HIVE/0.12.0.2.0/package/scripts/setup_ranger_hive_interactive.py
+++ ambari-server/src/main/resources/common-services/HIVE/0.12.0.2.0/package/scripts/setup_ranger_hive_interactive.py
@@ -53,8 +53,8 @@ def setup_ranger_hive_interactive(upgrade_type = None):
       )
       params.HdfsResource(None, action="execute")
 
-      from resource_management.libraries.functions.setup_ranger_plugin_xml import setup_ranger_plugin
-      setup_ranger_plugin('hive-server2-hive2', 'hive', params.ranger_previous_jdbc_jar,
+    from resource_management.libraries.functions.setup_ranger_plugin_xml import setup_ranger_plugin
+    setup_ranger_plugin('hive-server2-hive2', 'hive', params.ranger_previous_jdbc_jar,
                           params.ranger_downloaded_custom_connector, params.ranger_driver_curl_source,
                           params.ranger_driver_curl_target, params.java64_home,
                           params.repo_name, params.hive_ranger_plugin_repo,
@@ -73,6 +73,5 @@ def setup_ranger_hive_interactive(upgrade_type = None):
                           is_stack_supports_ranger_kerberos = params.stack_supports_ranger_kerberos,
                           component_user_principal=params.hive_principal if params.security_enabled else None,
                           component_user_keytab=params.hive_server2_keytab if params.security_enabled else None)
-
   else:
     Logger.info('Ranger admin not installed')
