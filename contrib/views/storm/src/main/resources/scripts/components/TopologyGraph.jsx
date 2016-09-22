1f7d0f11e4fa00cf39af9e968372db524072145a
Sanket Shah <sanket.shah@freestoneinfotech.com>
2016-09-22 15:50:01 +0530

    AMBARI-18431: Storm Ambari view - Fixes to DAG, kafka offset info , Misc fixes.

--- contrib/views/storm/src/main/resources/scripts/components/TopologyGraph.jsx
+++ contrib/views/storm/src/main/resources/scripts/components/TopologyGraph.jsx
@@ -16,7 +16,7 @@
  limitations under the License.
 */
 
-define(['react', 'react-dom', 'd3', 'd3.tip'], function(React, ReactDOM, d3) {
+define(['react', 'react-dom', 'd3', 'dagreD3', 'd3.tip'], function(React, ReactDOM, d3, dagreD3) {
 	'use strict';
 	return React.createClass({
 		displayName: 'TopologyGraph',
@@ -26,39 +26,24 @@ define(['react', 'react-dom', 'd3', 'd3.tip'], function(React, ReactDOM, d3) {
             height: React.PropTypes.string
         },
 		getInitialState: function(){
-			this.width = this.props.width || '1100';
-			this.height = this.props.height || '260';
 			this.syncData(this.props.data);
 			return null;
 		},
-        componentWillUpdate: function(){
-
-        },
 		componentDidUpdate: function(){
 			this.syncData(this.props.data);
             this.updateGraph();
-            this.force.start();
-            for (var i = 300; i > 0; --i) this.force.tick();
-            this.force.stop();
 		},
 		componentDidMount: function(){
-			var width = this.width,
-                height = this.height,
-                nodes = this.nodeArray,
-                links = this.linkArray,
-                radius = this.radius = 20.75;
-
-            var svg = this.svg = d3.select(ReactDOM.findDOMNode(this))
-                .attr('width', width)
-                .attr('height', height);
-
+            var that = this;
+            this.svg = d3.select(ReactDOM.findDOMNode(this))
             //Set up tooltip
-            this.tip = d3.tip()
+            this.tooltip = d3.tip()
                 .attr('class', function() {
-                    return 'd3-tip';
+                    return 'd3-tip testing';
                 })
                 .offset([-10, 0])
-                .html(function(d) {
+                .html(function(data) {
+                    var d = that.g.node(data);
                     var tip = "<ul>";
                     if (d[":capacity"] !== null) tip += "<li>Capacity: " + d[":capacity"].toFixed(2) + "</li>";
                     if (d[":latency"] !== null) tip += "<li>Latency: " + d[":latency"].toFixed(2) + "</li>";
@@ -66,171 +51,105 @@ define(['react', 'react-dom', 'd3', 'd3.tip'], function(React, ReactDOM, d3) {
                     tip += "</ul>";
                     return tip;
                 });
-            svg.call(this.tip);
-
-            // define arrow markers for graph links
-            svg.append('svg:defs').append('svg:marker')
-                .attr('id', 'end-arrow')
-                .attr('viewBox', '0 -5 10 10')
-                .attr('refX', 6)
-                .attr('markerWidth', 6)
-                .attr('markerHeight', 6.5)
-                .attr('orient', 'auto')
-                .append('svg:path')
-                .attr('d', 'M0,-5L10,0L0,5');
-
-            svg.append('svg:defs').append('svg:marker')
-                .attr('id', 'start-arrow')
-                .attr('viewBox', '0 -5 10 10')
-                .attr('refX', 4)
-                .attr('markerWidth', 3)
-                .attr('markerHeight', 3)
-                .attr('orient', 'auto')
-                .append('svg:path')
-                .attr('d', 'M10,-5L0,0L10,5');
-
-            // handles to link and node element groups
-            this.path = svg.append('svg:g').selectAll('path');
-            this.image = svg.append('svg:g').selectAll('g');
-
-            this.selected_node = null;
-			
-			// only respond once per keydown
-            this.lastKeyDown = -1;
-            d3.select(window)
-                .on('keydown', this.keydown)
-                .on('keyup', this.keyup);
-            this.updateGraph();
-            this.force.start();
-            this.force.tick();
-            this.force.stop();
-		},
+            //Set up zoom
+            this.zoom = d3.behavior.zoom()
+                .scaleExtent([0, 8])
+                .on("zoom", this.zoomed);
+        },
+        zoomed: function(){
+            this.inner.attr("transform",
+                "translate(" + this.zoom.translate() + ")" +
+                "scale(" + this.zoom.scale() + ")"
+            );
+        },
 		// update graph (called when needed)
 		updateGraph: function(){
-			// init D3 force layout
-            this.force = d3.layout.force()
-                .nodes(this.nodeArray)
-                .links(this.linkArray)
-                .size([this.width, this.height])
-                .linkDistance(150)
-                .charge(-500)
-                .on('tick', this.tick);
-
-			// path (link) group
-            this.path = this.path.data(this.linkArray);
-
-            // update existing links
-            this.path.style('marker-start', function(d) {
-                    return ''; })
-                .style('marker-end', function(d) {
-                    return 'url(#end-arrow)'; });
-
-
-            // add new links
-            this.path.enter().append('svg:path')
-                .attr('class', 'link')
-                .style('marker-start', function(d) {
-                    return ''; })
-                .attr("stroke-dasharray", "5, 5")
-                .attr('stroke-width', '2')
-                .style('marker-end', function(d) {
-                    return 'url(#end-arrow)'; });
-
-            // remove old links
-            this.path.exit().remove();
-
-
-            // image (node) group
-            // NB: the function arg is crucial here! nodes are known by id, not by index!
-            this.image = this.image.data(this.nodeArray, function(d) {
-                return d.id; 
-            });
-
-            //update old nodes
-            this.image
-                .on('mouseover', function(d) {
-                    this.tip.show(d);
-                }.bind(this))
-                .on('mouseout', function(d) {
-                    this.tip.hide();
-                }.bind(this));
-
-            // add new nodes
-            var g = this.image.enter().append('svg:g');
-
-            g.append('svg:image')
-            	.attr("xlink:href", function(d){
-					if(d.type === 'spout'){
-						return "images/icon-spout.png";
-					} else if(d.type === 'bolt'){
-						return "images/icon-bolt.png";
-					}
-				})
-				.attr("width", "30px")
-				.attr("height", "30px")
-                .on('mouseover', function(d) {
-                    this.tip.show(d);
-                }.bind(this))
-                .on('mouseout', function(d) {
-                    this.tip.hide();
-                }.bind(this));
-
-            g.append("svg:text")
-                .attr("dx", 18)
-                .attr("dy", 38)
-                .text(function(d) {
-                    return d.id; });
-
-            // remove old nodes
-            this.image.exit().remove();
-		},
-		// update force layout (called automatically each iteration)
-		tick: function(){
-			// draw directed edges with proper padding from node centers
-            this.path.attr('d', function(d) {
-                var deltaX = d.target.x - d.source.x,
-                    deltaY = d.target.y - d.source.y,
-                    dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
-                    normX = deltaX / dist,
-                    normY = deltaY / dist,
-                    sourcePadding = 30,
-                    targetPadding = 5,
-                    sourceX = d.source.x + (sourcePadding * normX),
-                    sourceY = d.source.y + (sourcePadding * normY) + 15,
-                    targetX = d.target.x - (targetPadding * normX),
-                    targetY = d.target.y - (targetPadding * normY) + 15;
-                return 'M' + sourceX + ',' + sourceY + 'L' + targetX + ',' + targetY;
-            });
+            var that = this;
+            var g = ReactDOM.findDOMNode(this).children[0];
+            if(g){
+                g.remove();
+            }
+            var inner = this.inner = this.svg.append("g");
+            // Create the renderer
+            var render = new dagreD3.render();
             
-            this.image.attr('transform', function(d) {
-                return 'translate(' + Math.max(this.radius, Math.min(this.width - this.radius, d.x)) + ',' + Math.max(this.radius, Math.min(this.height - this.radius, d.y)) + ')';
-            }.bind(this));
-		},
-		keydown: function(){
-			if (this.lastKeyDown !== -1) return;
-            this.lastKeyDown = d3.event.keyCode;
+            render.arrows().arrowPoint = function normal(parent, id, edge, type) {
+                var marker = parent.append("marker")
+                    .attr("id", id)
+                    .attr("viewBox", "0 0 10 10")
+                    .attr("refX", 5)
+                    .attr("refY", 5)
+                    .attr("markerUnits", "strokeWidth")
+                    .attr("markerWidth", 6)
+                    .attr("markerHeight", 6.5)
+                    .attr("orient", "auto");
 
-            // ctrl
-            if (d3.event.keyCode === 17) {
-                d3.event.preventDefault();
-                this.tip.hide();
-                this.image.call(this.force.drag);
-                this.svg.classed('ctrl', true);
+                var path = marker.append("path")
+                    .attr("d", "M 0 0 L 10 5 L 0 10 z")
+                    .style("stroke-width", 1)
+                    .style("stroke-dasharray", "1,0")
+                    .style("fill", "grey")
+                    .style("stroke", "grey");
+                dagreD3.util.applyStyle(path, edge[type + "Style"]);
+            };
+
+            render.shapes().img = function circle(parent, bbox, node) {
+                var shapeSvg = parent.insert("image")
+                    .attr("class", "nodeImage")
+                    .attr("xlink:href", function(d) {
+                        if (node) {
+                            if(node.type === 'spout'){
+                                return "images/icon-spout.png";
+                            } else if(node.type === 'bolt'){
+                                return "images/icon-bolt.png";
+                            }
+                        }
+                    }).attr("x", "-12px")
+                    .attr("y", "-12px")
+                    .attr("width", "30px")
+                    .attr("height", "30px");
+                node.intersect = function(point) {
+                    return dagreD3.intersect.circle(node, 20, point);
+                };
+                return shapeSvg;
             }
-		},
-		keyup: function(){
-			this.lastKeyDown = -1;
-            // ctrl
-            if (d3.event.keyCode === 17) {
-                this.image
-                    .on('mousedown.drag', null)
-                    .on('touchstart.drag', null);
-                this.svg.classed('ctrl', false);
+            this.svg.call(this.zoom).call(this.tooltip);
+            // Run the renderer. This is what draws the final graph.
+            render(inner, this.g);
+
+            inner.selectAll("g.nodes image")
+                .on('mouseover', function(d) {
+                    that.tooltip.show(d);
+                })
+                .on('mouseout', function(d) {
+                    that.tooltip.hide();
+                });
+
+            inner.selectAll("g.nodes g.label")
+                    .attr("transform", "translate(2,-30)");
+            // Center the graph
+            var initialScale = 1;
+            var svgWidth = this.svg[0][0].parentNode.clientWidth;
+            var svgHeight = this.svg[0][0].parentNode.clientHeight;
+            if(this.linkArray.length > 0){
+                this.zoom.translate([(svgWidth - this.g.graph().width * initialScale) / 2, (svgHeight - this.g.graph().height * initialScale) / 2])
+                    .scale(initialScale)
+                    .event(this.svg);
             }
-		},
+        },
 		syncData: function(data){
 			this.nodeArray = [];
             this.linkArray = [];
+            this.g = new dagreD3.graphlib.Graph().setGraph({
+                nodesep: 50,
+                ranksep: 190,
+                rankdir: "LR",
+                marginx: 20,
+                marginy: 20,
+                // transition: function transition(selection) {
+                //     return selection.transition().duration(500);
+                // }
+            });
             if(data){
             	var keys = _.keys(data);
             	keys.map(function(key){
@@ -243,10 +162,11 @@ define(['react', 'react-dom', 'd3', 'd3.tip'], function(React, ReactDOM, d3) {
 
             	var spoutObjArr = _.where(this.nodeArray, { "type": "spout" });
 	            if (spoutObjArr.length > 1) {
-	            	var index = this.nodeArray.length - 1;
-	                this.nodeArray[index].x = 50;
-	                this.nodeArray[index].y = 100;
-	                this.nodeArray[index].fixed = true;
+                    for(var i = 0; i < spoutObjArr.length; i++){
+                        spoutObjArr[i].x = 50;
+                        spoutObjArr[i].y = parseInt(i+'10', 10);
+                        spoutObjArr[i].fixed = true;
+                    }
 	            } else if (spoutObjArr.length == 1) {
 	                spoutObjArr[0].x = 50;
 	                spoutObjArr[0].y = 100;
@@ -257,10 +177,14 @@ define(['react', 'react-dom', 'd3', 'd3.tip'], function(React, ReactDOM, d3) {
 	            	var inputArr = node[":inputs"] || [];
 	            	inputArr.map(function(input){
 	            		if(!input[":component"].startsWith("__")){
-	            			this.linkArray.push({
-	            				source: _.findWhere(this.nodeArray, {id: input[":component"]}),
+	            			var sourceNode = _.findWhere(this.nodeArray, {id: input[":component"]});
+                            this.linkArray.push({
+	            				source: sourceNode,
 	            				target: node
 	            			});
+                            this.g.setNode(sourceNode.id, _.extend(sourceNode, {label: sourceNode.id, shape: 'img'}));
+                            this.g.setNode(node.id, _.extend(node, {label: node.id, shape: 'img'}));
+                            this.g.setEdge(sourceNode.id, node.id, {"arrowhead": 'arrowPoint'});
 	            		}
 	            	}.bind(this));
 	            }.bind(this));
@@ -268,7 +192,7 @@ define(['react', 'react-dom', 'd3', 'd3.tip'], function(React, ReactDOM, d3) {
 		},
 		render: function() {
 			return (
-				<svg className="topology-graph"></svg>
+				<svg className="topology-graph" width="100%" height="300"></svg>
 			);
 		},
 	});
