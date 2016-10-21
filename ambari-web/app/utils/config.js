7b1a0041629387295ae35b78a47a285abcd24208
Alex Antonenko <hiveww@gmail.com>
2016-10-21 22:09:38 +0300

    AMBARI-18648. There were no labels on fields on step "Configure Kerberos" after clicking back	 (alexantonenko)

--- ambari-web/app/utils/config.js
+++ ambari-web/app/utils/config.js
@@ -1154,16 +1154,18 @@ App.config = Em.Object.create({
    * Merge values in "stored" to "base" if name matches, it's a value only merge.
    * @param base {Array} Em.Object
    * @param stored {Array} Object
+   * @returns {Object[]|Em.Object[]} base
    */
   mergeStoredValue: function(base, stored) {
     if (stored) {
       base.forEach(function (p) {
-        var sp = stored.filterProperty("filename", p.filename).findProperty("name", p.name);
+        var sp = stored.filterProperty('filename', p.filename).findProperty('name', p.name);
         if (sp) {
-          p.set("value", sp.value);
+          Em.set(p, 'value', Em.get(sp, 'value'));
         }
       });
     }
+    return base;
   },
 
 
