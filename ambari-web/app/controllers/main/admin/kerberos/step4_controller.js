dd7b3b9323f2c4b2bc2dcad60b1329bedb35cbc9
ababiichuk <ababiichuk@hortonworks.com>
2016-10-27 17:04:18 +0300

    AMBARI-18719 Add service wizard getting stuck at Configure Identities page. (ababiichuk)

--- ambari-web/app/controllers/main/admin/kerberos/step4_controller.js
+++ ambari-web/app/controllers/main/admin/kerberos/step4_controller.js
@@ -439,7 +439,7 @@ App.KerberosWizardStep4Controller = App.WizardStep7Controller.extend(App.AddSecu
     var self = this;
     var configs, servicesConfigurations;
     if (this.get('isWithinAddService')) {
-      configs = this.get('wizardController').getDBProperty('serviceConfigProperties');
+      configs = this.get('content.serviceConfigProperties');
       servicesConfigurations = configs.reduce(function(configTags, property) {
         var fileName = App.config.getConfigTagFromFileName(property.filename),
             configType;
