c503f7c9cabd00f760366536e1d1c1c513186eca
Matt <mmathew@pivotal.io>
2016-11-09 13:01:38 -0800

    AMBARI-18837: HAWQ Master fails to start when webhdfs is disabled (mithmatt)

--- ambari-server/src/main/resources/common-services/HAWQ/2.0.0/package/scripts/params.py
+++ ambari-server/src/main/resources/common-services/HAWQ/2.0.0/package/scripts/params.py
@@ -20,6 +20,8 @@ import functools
 import hawq_constants
 from resource_management import Script
 from resource_management.core.resources.system import File
+from resource_management.libraries.functions import conf_select
+from resource_management.libraries.functions import stack_select
 from resource_management.libraries.functions.default import default
 from resource_management.libraries.resources.hdfs_resource import HdfsResource
 from resource_management.libraries.resources.xml_config import XmlConfig
@@ -60,6 +62,8 @@ hawq_all_hosts = sorted(set(hawq_master_hosts + hawqsegment_hosts))
 # HDFS
 hdfs_site = config['configurations']['hdfs-site']
 default_fs = config['configurations']['core-site']['fs.defaultFS']
+hadoop_bin_dir = stack_select.get_hadoop_dir("bin")
+hadoop_conf_dir = conf_select.get_hadoop_conf_dir()
 
 security_enabled = config['configurations']['cluster-env']['security_enabled']
 hdfs_user_keytab = config['configurations']['hadoop-env']['hdfs_user_keytab']
@@ -78,6 +82,8 @@ HdfsResource = functools.partial(HdfsResource,
                                  keytab=hdfs_user_keytab,
                                  kinit_path_local=kinit_path_local,
                                  principal_name=hdfs_principal_name,
+                                 hadoop_bin_dir = hadoop_bin_dir,
+                                 hadoop_conf_dir = hadoop_conf_dir,
                                  hdfs_site=hdfs_site,
                                  default_fs=default_fs,
                                  immutable_paths = get_not_managed_resources())
