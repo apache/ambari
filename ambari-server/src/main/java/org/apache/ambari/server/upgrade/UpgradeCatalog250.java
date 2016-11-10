c1b3262219acd9da336e26bad1e633686817a452
Vitaly Brodetskyi <vbrodetskyi@hortonworks.com>
2016-11-10 03:16:57 +0200

    AMBARI-18640. Part2.Ambari upgrade failed while running "Alter Table blueprint" - blueprint_name column.(vbrodetskyi)

--- ambari-server/src/main/java/org/apache/ambari/server/upgrade/UpgradeCatalog250.java
+++ ambari-server/src/main/java/org/apache/ambari/server/upgrade/UpgradeCatalog250.java
@@ -19,6 +19,7 @@ package org.apache.ambari.server.upgrade;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
