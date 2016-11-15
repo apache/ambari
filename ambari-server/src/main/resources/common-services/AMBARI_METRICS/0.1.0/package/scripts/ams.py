191a62833afb871f70ff86d656e373c4061dc968
Dmytro Sen <dsen@apache.org>
2016-11-15 18:17:27 +0200

    Revert "AMBARI-17898 Add Kerberos HTTP SPNEGO authentication support to Ambari Metrics Monitor (qinliu via dsen)"
    This reverts commit 02b3da7b71204298079e863806169cdaba78fc80.

--- ambari-server/src/main/resources/common-services/AMBARI_METRICS/0.1.0/package/scripts/ams.py
+++ ambari-server/src/main/resources/common-services/AMBARI_METRICS/0.1.0/package/scripts/ams.py
@@ -315,9 +315,18 @@ def ams(name=None, action=None):
             mode=0644
       )
 
+      # Remove spnego configs from core-site, since AMS does not support spnego (AMBARI-14384)
+      truncated_core_site = {}
+      truncated_core_site.update(params.config['configurations']['core-site'])
+      if 'core-site' in params.config['configurations']:
+        if 'hadoop.http.authentication.type' in params.config['configurations']['core-site']:
+          truncated_core_site.pop('hadoop.http.authentication.type')
+        if 'hadoop.http.filter.initializers' in params.config['configurations']['core-site']:
+          truncated_core_site.pop('hadoop.http.filter.initializers')
+
       XmlConfig("core-site.xml",
                 conf_dir=params.ams_collector_conf_dir,
-                configurations=params.config['configurations']['core-site'],
+                configurations=truncated_core_site,
                 configuration_attributes=params.config['configuration_attributes']['core-site'],
                 owner=params.ams_user,
                 group=params.user_group,
@@ -326,7 +335,7 @@ def ams(name=None, action=None):
 
       XmlConfig("core-site.xml",
                 conf_dir=params.hbase_conf_dir,
-                configurations=params.config['configurations']['core-site'],
+                configurations=truncated_core_site,
                 configuration_attributes=params.config['configuration_attributes']['core-site'],
                 owner=params.ams_user,
                 group=params.user_group,
