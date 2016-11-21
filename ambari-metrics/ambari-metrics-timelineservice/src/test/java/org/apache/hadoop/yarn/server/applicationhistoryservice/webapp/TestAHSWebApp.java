a5594ee46001e68bf5511325016be7897156fc68
Aravindan Vijayan <avijayan@hortonworks.com>
2016-11-21 13:21:32 -0800

    AMBARI-18915 : Update AMS pom to use Apache hbase,hadoop,phoenix tarballs. (avijayan)

--- ambari-metrics/ambari-metrics-timelineservice/src/test/java/org/apache/hadoop/yarn/server/applicationhistoryservice/webapp/TestAHSWebApp.java
+++ ambari-metrics/ambari-metrics-timelineservice/src/test/java/org/apache/hadoop/yarn/server/applicationhistoryservice/webapp/TestAHSWebApp.java
@@ -21,6 +21,7 @@ package org.apache.hadoop.yarn.server.applicationhistoryservice.webapp;
 import static org.apache.hadoop.yarn.webapp.Params.TITLE;
 import static org.mockito.Mockito.mock;
 
+import org.apache.hadoop.yarn.server.resourcemanager.webapp.AboutPage;
 import org.junit.Assert;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.yarn.api.ApplicationBaseProtocol;
