90967bcd0df7a5ab089611ee5502bb90d0084a0a
Dmytro Sen <dsen@apache.org>
2016-11-23 18:47:20 +0200

    Revert "AMBARI-18713 use exclude list of mount device types on docker containers (dsen)"

--- ambari-server/src/main/resources/scripts/stack_advisor.py
+++ ambari-server/src/main/resources/scripts/stack_advisor.py
@@ -70,11 +70,13 @@ def main(argv=None):
   if len(args) < 3:
     sys.stderr.write(USAGE)
     sys.exit(2)
+    pass
 
   action = args[0]
   if action not in ALL_ACTIONS:
     sys.stderr.write(USAGE)
     sys.exit(2)
+    pass
 
   hostsFile = args[1]
   servicesFile = args[2]
@@ -87,7 +89,6 @@ def main(argv=None):
   stackName = services["Versions"]["stack_name"]
   stackVersion = services["Versions"]["stack_version"]
   parentVersions = []
-
   if "stack_hierarchy" in services["Versions"]:
     parentVersions = services["Versions"]["stack_hierarchy"]["stack_versions"]
 
@@ -95,9 +96,8 @@ def main(argv=None):
 
   # Perform action
   actionDir = os.path.realpath(os.path.dirname(args[1]))
-
-  # filter
-  hosts = stackAdvisor.filterHostMounts(hosts, services)
+  result = {}
+  result_file = "non_valid_result_file.json"
 
   if action == RECOMMEND_COMPONENT_LAYOUT_ACTION:
     result = stackAdvisor.recommendComponentLayout(services, hosts)
@@ -111,11 +111,12 @@ def main(argv=None):
   elif action == RECOMMEND_CONFIGURATION_DEPENDENCIES:
     result = stackAdvisor.recommendConfigurationDependencies(services, hosts)
     result_file = os.path.join(actionDir, "configurations.json")
-  else:  # action == VALIDATE_CONFIGURATIONS
+  else: # action == VALIDATE_CONFIGURATIONS
     result = stackAdvisor.validateConfigurations(services, hosts)
     result_file = os.path.join(actionDir, "configurations-validation.json")
 
   dumpJson(result, result_file)
+  pass
 
 
 def instantiateStackAdvisor(stackName, stackVersion, parentVersions):
