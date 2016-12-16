7df6bba4411a304f5750de4a0947f5d6d53833d6
Alejandro Fernandez <afernandez@hortonworks.com>
2016-12-16 11:01:20 -0800

    AMBARI-19220. Fix version of HDFS and YARN used by HDP 3.0 (alejandro)

--- ambari-server/src/main/resources/common-services/HDFS/3.0.0.3.0/package/scripts/utils.py
+++ ambari-server/src/main/resources/common-services/HDFS/3.0.0.3.0/package/scripts/utils.py
@@ -34,11 +34,10 @@ from resource_management.libraries.functions.curl_krb_request import curl_krb_re
 from resource_management.core.exceptions import Fail
 from resource_management.libraries.functions.namenode_ha_utils import get_namenode_states
 from resource_management.libraries.functions.show_logs import show_logs
-from resource_management.libraries.script.script import Script
-from ambari_commons.inet_utils import ensure_ssl_using_protocol
+from ambari_commons.inet_utils import ensure_ssl_using_tls_v1
 from zkfc_slave import ZkfcSlaveDefault
 
-ensure_ssl_using_protocol(Script.get_force_https_protocol())
+ensure_ssl_using_tls_v1()
 
 def safe_zkfc_op(action, env):
   """
