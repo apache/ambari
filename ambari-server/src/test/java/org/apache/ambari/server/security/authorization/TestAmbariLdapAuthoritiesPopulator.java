585c2b87775c868f0d3df60bf3aac26649f8fe05
Lisnichenko Dmitro <dlysnichenko@hortonworks.com>
2016-12-21 18:05:18 +0200

    AMBARI-19235. 'Cluster User' role issue after Ambari 2.4.2.0 upgrade (echekanskiy via dlysnichenko)

--- ambari-server/src/test/java/org/apache/ambari/server/security/authorization/TestAmbariLdapAuthoritiesPopulator.java
+++ ambari-server/src/test/java/org/apache/ambari/server/security/authorization/TestAmbariLdapAuthoritiesPopulator.java
@@ -17,15 +17,15 @@
  */
 package org.apache.ambari.server.security.authorization;
 
+import static org.easymock.EasyMock.expect;
+
+import java.util.Collections;
+
 import org.apache.ambari.server.orm.dao.MemberDAO;
 import org.apache.ambari.server.orm.dao.PrivilegeDAO;
 import org.apache.ambari.server.orm.dao.UserDAO;
-import org.apache.ambari.server.orm.entities.GroupEntity;
-import org.apache.ambari.server.orm.entities.MemberEntity;
-import org.apache.ambari.server.orm.entities.PrincipalEntity;
 import org.apache.ambari.server.orm.entities.PrivilegeEntity;
 import org.apache.ambari.server.orm.entities.UserEntity;
-import org.easymock.EasyMock;
 import org.easymock.EasyMockSupport;
 import org.junit.Before;
 import org.junit.Test;
@@ -35,25 +35,17 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.ldap.core.DirContextOperations;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import static org.easymock.EasyMock.*;
-
 @RunWith(PowerMockRunner.class)               // Allow mocking static methods
 @PrepareForTest(AuthorizationHelper.class)    // This class has a static method that will be mocked
 public class TestAmbariLdapAuthoritiesPopulator extends EasyMockSupport {
 
   AuthorizationHelper helper = new AuthorizationHelper();
   UserDAO userDAO = createMock(UserDAO.class);
+  Users users = createMock(Users.class);
   MemberDAO memberDAO = createMock(MemberDAO.class);
   PrivilegeDAO privilegeDAO = createMock(PrivilegeDAO.class);
   DirContextOperations userData = createMock(DirContextOperations.class);
   UserEntity userEntity = createMock(UserEntity.class);
-  PrincipalEntity principalEntity = createMock(PrincipalEntity.class);
-  PrincipalEntity groupPrincipalEntity = createMock(PrincipalEntity.class);
-  MemberEntity memberEntity = createMock(MemberEntity.class);
-  GroupEntity groupEntity = createMock(GroupEntity.class);
   PrivilegeEntity privilegeEntity = createMock(PrivilegeEntity.class);
 
   @Before
@@ -63,21 +55,14 @@ public class TestAmbariLdapAuthoritiesPopulator extends EasyMockSupport {
   }
 
   @Test
-  public void testGetGrantedAuthorities_mappingDisabled() throws Exception {
+  public void testGetGrantedAuthorities() throws Exception {
     String username = "user";
 
     AmbariLdapAuthoritiesPopulator populator = createMockBuilder(AmbariLdapAuthoritiesPopulator.class)
-        .withConstructor(helper, userDAO, memberDAO, privilegeDAO).createMock();
+        .withConstructor(helper, userDAO, memberDAO, privilegeDAO, users).createMock();
 
-    expect(userEntity.getPrincipal()).andReturn(principalEntity);
     expect(userEntity.getActive()).andReturn(true);
-    expect(memberDAO.findAllMembersByUser(userEntity)).andReturn(Collections.singletonList(memberEntity));
-    expect(memberEntity.getGroup()).andReturn(groupEntity);
-    expect(groupEntity.getPrincipal()).andReturn(groupPrincipalEntity);
-    List<PrincipalEntity> principalEntityList = new LinkedList<PrincipalEntity>();
-    principalEntityList.add(principalEntity);
-    principalEntityList.add(groupPrincipalEntity);
-    expect(privilegeDAO.findAllByPrincipal(principalEntityList)).andReturn(Collections.singletonList(privilegeEntity));
+    expect(users.getUserPrivileges(userEntity)).andReturn(Collections.singletonList(privilegeEntity));
 
     expect(userDAO.findLdapUserByName(username)).andReturn(userEntity);
     replayAll();
@@ -86,34 +71,6 @@ public class TestAmbariLdapAuthoritiesPopulator extends EasyMockSupport {
 
     verifyAll();
 
-  }
-
-  @Test
-  public void testGetGrantedAuthorities_mappingEnabled() throws Exception {
-
-    AmbariLdapAuthoritiesPopulator populator = createMockBuilder(AmbariLdapAuthoritiesPopulator.class)
-        .withConstructor(helper, userDAO, memberDAO, privilegeDAO).createMock();
-
-    expect(userEntity.getPrincipal()).andReturn(principalEntity).anyTimes();
-    expect(userEntity.getActive()).andReturn(true);
-    expect(memberDAO.findAllMembersByUser(userEntity)).andReturn(Collections.singletonList(memberEntity)).anyTimes();
-    expect(memberEntity.getGroup()).andReturn(groupEntity).anyTimes();
-    expect(groupEntity.getPrincipal()).andReturn(groupPrincipalEntity).anyTimes();
-    List<PrincipalEntity> principalEntityList = new LinkedList<PrincipalEntity>();
-    principalEntityList.add(principalEntity);
-    principalEntityList.add(groupPrincipalEntity);
-    expect(privilegeDAO.findAllByPrincipal(principalEntityList)).andReturn(Collections.singletonList(privilegeEntity)).anyTimes();
-
-    expect(userDAO.findLdapUserByName(EasyMock.<String> anyObject())).andReturn(null).andReturn(userEntity).once();
-
-    replayAll();
-
-    //test with admin user
-    populator.getGrantedAuthorities(userData, "admin");
-    //test with non-admin
-    populator.getGrantedAuthorities(userData, "user");
-
-    verifyAll();
   }
 
   @Test
@@ -128,17 +85,10 @@ public class TestAmbariLdapAuthoritiesPopulator extends EasyMockSupport {
     PowerMock.replay(AuthorizationHelper.class);
 
     AmbariLdapAuthoritiesPopulator populator = createMockBuilder(AmbariLdapAuthoritiesPopulator.class)
-      .withConstructor(helper, userDAO, memberDAO, privilegeDAO).createMock();
+      .withConstructor(helper, userDAO, memberDAO, privilegeDAO, users).createMock();
 
-    expect(userEntity.getPrincipal()).andReturn(principalEntity);
     expect(userEntity.getActive()).andReturn(true);
-    expect(memberDAO.findAllMembersByUser(userEntity)).andReturn(Collections.singletonList(memberEntity));
-    expect(memberEntity.getGroup()).andReturn(groupEntity);
-    expect(groupEntity.getPrincipal()).andReturn(groupPrincipalEntity);
-    List<PrincipalEntity> principalEntityList = new LinkedList<PrincipalEntity>();
-    principalEntityList.add(principalEntity);
-    principalEntityList.add(groupPrincipalEntity);
-    expect(privilegeDAO.findAllByPrincipal(principalEntityList)).andReturn(Collections.singletonList(privilegeEntity));
+    expect(users.getUserPrivileges(userEntity)).andReturn(Collections.singletonList(privilegeEntity));
 
     expect(userDAO.findLdapUserByName(ambariUserName)).andReturn(userEntity); // user should be looked up by user name instead of login alias
 
