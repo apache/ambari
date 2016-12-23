c4d8f2dc8d28fe6eb42b3bfa102383e089a502f3
Dmytro Sen <dsen@apache.org>
2016-12-23 18:55:17 +0200

    AMBARI-19257 BE changes needed for SNMP Ambari MIB support (dsen)

--- ambari-server/src/main/java/org/apache/ambari/server/state/AlertState.java
+++ ambari-server/src/main/java/org/apache/ambari/server/state/AlertState.java
@@ -24,27 +24,37 @@ public enum AlertState {
   /**
    * Alert does not need to be distributed.  Normal Operation.
    */
-  OK,
+  OK(0),
 
   /**
    * Alert indicates there may be an issue.  The component may be operating
    * normally but may be in danger of becoming <code>CRITICAL</code>.
    */
-  WARNING,
+  WARNING(2),
 
   /**
    * Indicates there is a critical situation that needs to be addressed.
    */
-  CRITICAL,
+  CRITICAL(3),
 
   /**
    * The state of the alert is not known.
    */
-  UNKNOWN,
+  UNKNOWN(1),
 
   /**
    * Indicates that the state of the alert should be discarded, but the alert
    * timestamps should be updated so that it is not considered stale.
    */
-  SKIPPED;
+  SKIPPED(4);
+
+  private final int intValue;
+
+  public int getIntValue() {
+    return intValue;
+  }
+
+  AlertState(int i) {
+    this.intValue = i;
+  }
 }
