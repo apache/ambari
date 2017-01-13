4e1fabb3acb4c4e8f5b326f7918ba2192d410a82
Nate Cole <ncole@hortonworks.com>
2017-01-13 09:36:14 -0500

    AMBARI-19527. Downgrade executes <pre-upgrade> section if <pre-downgrade> is not defined (ncole)

--- ambari-server/src/test/java/org/apache/ambari/server/controller/AmbariManagementControllerTest.java
+++ ambari-server/src/test/java/org/apache/ambari/server/controller/AmbariManagementControllerTest.java
@@ -7282,7 +7282,7 @@ public class AmbariManagementControllerTest {
     Assert.assertEquals(1, responsesWithParams.size());
     StackVersionResponse resp = responsesWithParams.iterator().next();
     assertNotNull(resp.getUpgradePacks());
-    assertEquals(11, resp.getUpgradePacks().size());
+    assertEquals(12, resp.getUpgradePacks().size());
     assertTrue(resp.getUpgradePacks().contains("upgrade_test"));
   }
 
