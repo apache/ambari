d8ab5f55e694cfd2e354c63e5a86c279844d1301
Vitaly Brodetskyi <vbrodetskyi@hortonworks.com>
2017-01-27 17:36:28 +0200

    AMBARI-19748. Upgrade default JDK installed by Ambari to be >8u100.(vbrodetskyi)

--- ambari-server/src/main/python/ambari_server/serverSetup.py
+++ ambari-server/src/main/python/ambari_server/serverSetup.py
@@ -741,9 +741,9 @@ class JDKSetupLinux(JDKSetup):
     super(JDKSetupLinux, self).__init__()
     self.JDK_DEFAULT_CONFIGS = [
       JDKRelease("jdk1.8", "Oracle JDK 1.8 + Java Cryptography Extension (JCE) Policy Files 8",
-                 "http://public-repo-1.hortonworks.com/ARTIFACTS/jdk-8u77-linux-x64.tar.gz", "jdk-8u77-linux-x64.tar.gz",
+                 "http://public-repo-1.hortonworks.com/ARTIFACTS/jdk-8u112-linux-x64.tar.gz", "jdk-8u112-linux-x64.tar.gz",
                  "http://public-repo-1.hortonworks.com/ARTIFACTS/jce_policy-8.zip", "jce_policy-8.zip",
-                 AmbariPath.get("/usr/jdk64/jdk1.8.0_77"),
+                 AmbariPath.get("/usr/jdk64/jdk1.8.0_112"),
                  "(jdk.*)/jre")
     ]
 
