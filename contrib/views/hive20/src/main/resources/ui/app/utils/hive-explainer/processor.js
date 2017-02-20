361f225d85a869c78fa0bfe954f852e8951b566c
pallavkul <pallav.kul@gmail.com>
2017-02-20 15:56:41 +0530

    AMBARI-20048. Hive View 2.0: Visual Explain-The operators are being shown out of order. (Abhishek Kumar via pallavkul)

---
+++ contrib/views/hive20/src/main/resources/ui/app/utils/hive-explainer/processor.js
@@ -0,0 +1,240 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export function getProcessedVertices(vertices, edges) {
+  const edgedVertices = processEdges(vertices, edges);
+  return processSource(edgedVertices);
+}
+
+function processEdges(vertices, edges) {
+
+  return vertices
+    .map(cVertex => {
+      const isVertexPartOfSimpleEdge = edges.some(cEdge => cEdge.type === 'SIMPLE_EDGE' && cEdge.parent === cVertex._vertex);
+      const isVertexPartOfBroadcastEdge = edges.some(cEdge => cEdge.type === 'BROADCAST_EDGE' && cEdge.parent === cVertex._vertex);
+      const isVertexPartOfCustomSimpleEdge = edges.some(cEdge => cEdge.type === 'CUSTOM_SIMPLE_EDGE' && cEdge.parent === cVertex._vertex);
+      const isVertexPartOfCustomEdge = edges.some(cEdge => cEdge.type === 'CUSTOM_EDGE' && cEdge.parent === cVertex._vertex);
+      const isVertexPartOfXProdEdge = edges.some(cEdge => cEdge.type === 'XPROD_EDGE' && cEdge.parent === cVertex._vertex);
+
+      let tVertex = cVertex;
+
+      if(isVertexPartOfSimpleEdge) {
+        tVertex = appendIfTerminusOfOperator(tVertex, {
+          _operator: 'Partition/Sort Pseudo-Edge'
+        });
+      }
+      if(isVertexPartOfBroadcastEdge) {
+        tVertex = appendIfTerminusOfOperator(tVertex, {
+          _operator: 'Broadcast Pseudo-Edge'
+        });
+      }
+      if(isVertexPartOfCustomSimpleEdge) {
+        tVertex = appendIfTerminusOfOperator(tVertex, {
+          _operator: 'Partition Pseudo-Edge'
+        });
+      }
+      if(isVertexPartOfCustomEdge) {
+        tVertex = appendIfTerminusOfOperator(tVertex, {
+          _operator: 'Co-partition Pseudo-Edge'
+        });
+      }
+      if(isVertexPartOfXProdEdge) {
+        tVertex = appendIfTerminusOfOperator(tVertex, {
+          _operator: 'Cross-product Distribute Pseudo-Edge'
+        });
+      }
+
+      return tVertex;
+    });
+}
+
+function appendIfTerminusOfOperator(node, pseudoNode) {
+  if(Array.isArray(node._children) === false || node._children.length === 0) {
+    // is terminus
+    switch(node._operator) {
+      case 'Reduce Output Operator':
+        return Object.assign({}, node, pseudoNode);
+      default:
+        return node;
+    }
+  }
+
+  return Object.assign({}, node, {
+    _children: node._children.map(cChild => appendIfTerminusOfOperator(cChild, pseudoNode))
+  });
+}
+
+function processSource(vertices) {
+  return vertices.map(cVertex => Object.assign({}, cVertex, {
+    _children: cVertex._children.map(cChild => getProcessedSequenceViaStack(cChild))
+  }));
+}
+
+// DANGER: impure function
+function getProcessedSequenceViaStack(root) {
+  const stack = [];
+
+  let cNode = root;
+  stack.push(cNode);
+  doCompaction(stack);
+  while(cNode._children.length === 1) {
+    cNode = cNode._children[0];
+
+    stack.push(cNode);
+    doCompaction(stack);
+  }
+
+  const lNode = stack[stack.length - 1];
+  if(lNode._children.length > 1) {
+    // begin processing new subtree
+    lNode._children = lNode._children.map(cChild => getProcessedSequenceViaStack(cChild));
+  }
+
+  return stack[0];
+}
+
+function doCompaction(stack) {
+  let index = stack.length;
+
+  while(index > 0) {
+    const cNode = stack[index - 0 - 1];
+    const cNodeMinus1 = stack[index - 1 - 1];
+    const cNodeMinus2 = stack[index - 2 - 1];
+    const cNodeMinus3 = stack[index - 3 - 1];
+    const cNodeMinus4 = stack[index - 4 - 1];
+
+    if(cNodeMinus1) {
+
+      if(cNode._operator === 'Select Operator' || cNode._operator === 'HASHTABLEDUMMY' || cNode._operator === 'File Output Operator') {
+        // remove cNode from stack
+        stack.pop();
+        index--;
+        // recreate groups
+        cNodeMinus1._groups = [
+          ...(cNodeMinus1._groups || [doCloneAndOmit(cNodeMinus1, ['_groups'])]),
+          ...(cNode._groups || [doCloneAndOmit(cNode, ['_groups'])]),
+        ];
+        // move children
+        cNodeMinus1._children = cNode._children;
+
+        continue;
+      }
+      if(cNodeMinus1._operator === 'Select Operator' || cNodeMinus1._operator === 'HASHTABLEDUMMY' || cNodeMinus1._operator === 'File Output Operator') {
+        // remove cNode and cNodeMinus1 from stack
+        stack.pop();
+        index--;
+        stack.pop();
+        index--;
+
+        // recreate groups
+        cNode._groups = [
+          ...(cNodeMinus1._groups || [doCloneAndOmit(cNodeMinus1, ['_groups'])]),
+          ...(cNode._groups || [doCloneAndOmit(cNode, ['_groups'])]),
+        ];
+        // no need to move chldren
+        // reinsert cNode
+        stack.push(cNode);
+        index++;
+
+        continue;
+      }
+
+
+      if(cNode._operator === 'Map Join Operator' && cNodeMinus1._operator === 'Map Join Operator') {
+        // remove cNode from stack
+        stack.pop();
+        index--;
+        // recreate groups
+        cNodeMinus1._groups = [
+          ...(cNodeMinus1._groups || [doCloneAndOmit(cNodeMinus1, ['_groups'])]),
+          ...(cNode._groups || [doCloneAndOmit(cNode, ['_groups'])]),
+        ];
+        // move chldren
+        cNodeMinus1._children = cNode._children;
+
+        continue;
+      }
+
+      if(cNode._operator === 'Filter Operator' && cNodeMinus1._operator === 'TableScan') {
+        // remove cNode from stack
+        stack.pop();
+        index--;
+        // recreate groups
+        cNodeMinus1._groups = [
+          ...(cNodeMinus1._groups || [doCloneAndOmit(cNodeMinus1, ['_groups'])]),
+          ...(cNode._groups || [doCloneAndOmit(cNode, ['_groups'])]),
+        ];
+        // move children
+        cNodeMinus1._children = cNode._children;
+
+        continue;
+      }
+
+      if(cNodeMinus2 && cNodeMinus3) {
+        if(cNode._operator === 'Broadcast Pseudo-Edge' && cNodeMinus1._operator === 'Group By Operator' && cNodeMinus2._operator === 'Reduce Output Operator' && cNodeMinus3._operator === 'Group By Operator') {
+          // remove cNode from stack
+          stack.pop();
+          index--;
+          // remove cNodeMinus1 from stack
+          stack.pop();
+          index--;
+          // remove cNodeMinus2 from stack
+          stack.pop();
+          index--;
+          // remove cNodeMinus3 from stack
+          stack.pop();
+          index--;
+
+          // recreate groups
+          cNodeMinus1._groups = [
+            ...(cNodeMinus3._groups || [doCloneAndOmit(cNodeMinus3, ['_groups'])]),
+            ...(cNodeMinus2._groups || [doCloneAndOmit(cNodeMinus2, ['_groups'])]),
+            ...(cNodeMinus1._groups || [doCloneAndOmit(cNodeMinus1, ['_groups'])]),
+          ];
+          // move children if required, cNodeMinus1 as child of cNodeMinus4
+          if(cNodeMinus4) {
+            cNodeMinus4._children = cNodeMinus2._children;
+          }
+          // rename
+          cNodeMinus1._operator = 'Build Bloom Filter';
+          // add renamed node
+          stack.push(cNodeMinus1);
+          index++;
+          // add original broadcast edge node
+          stack.push(cNode);
+          index++;
+
+
+          continue;
+        }
+      }
+
+    }
+    index--;
+
+  }
+}
+
+function doCloneAndOmit(obj, keys) {
+  return Object
+    .keys(obj)
+    .filter(cObjKey => keys.indexOf(cObjKey) === -1)
+    .reduce((tObj, cObjKey) => Object.assign({}, tObj, {
+      [cObjKey]: obj[cObjKey]
+    }), {});
+}
---
+++ contrib/views/hive20/src/main/resources/ui/app/utils/hive-explainer/renderer.js
@@ -0,0 +1,327 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default function doRender(data, selector, onRequestDetail) {
+
+  const width = '100vw', height = '100vh';
+
+  d3.select(selector).select('*').remove();
+  const svg =
+    d3.select(selector)
+      .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+  const container = svg.append('g');
+  const zoom =
+    d3.behavior.zoom()
+      .scaleExtent([1 / 10, 4])
+      .on('zoom', () => {
+        container.attr('transform', `translate(${d3.event.translate}) scale(${d3.event.scale})`);
+      });
+
+    svg
+      .call(zoom);
+
+  const root =
+    container
+      .selectAll('g.vertex')
+        .data([data.tree])
+      .enter()
+        .append('g')
+      .attr('class', 'vertex')
+      .attr('data-vertex', d => d._vertex);
+
+  root
+    .call(recurseC, onRequestDetail);
+
+  root
+    .call(recurseV, onRequestDetail);
+
+  container.selectAll('path.edge')
+    .data(data.connections)
+    .enter()
+      .insert('path', ':first-child')
+    .attr('class', 'edge')
+    .attr('d', d => getConnectionPath(d, svg, container));
+
+  reset(zoom, svg, container);
+
+}
+
+function recurseV(vertices, onRequestDetail) {
+  vertices.each(function(cVertx) {
+    const vertex = d3.select(this);
+
+    const vertices =
+      vertex
+        .selectAll('g.vertex')
+          .data(d => d._vertices)
+        .enter()
+          .append('g')
+        .attr('class', 'vertex')
+        .attr('data-vertex', d => d._vertex)
+        .style('transform', d => `translate(${d._widthOfSelf * 200}px, ${d._offsetY * 100}px)`);
+
+      vertices
+        .call(recurseC, onRequestDetail);
+
+      vertices
+        .call(recurseV, onRequestDetail);
+  });
+}
+
+function recurseC(children, onRequestDetail) {
+  children.each(function(d) {
+    const child = d3.select(this);
+
+    const children =
+      child
+          .selectAll('g.child')
+        .data(d => d._children || []).enter()
+          .append('g')
+          .attr('class', 'child')
+          .style('transform', (d, index) => `translate(-${200}px, ${index * 100}px)`);
+
+      children
+          .append('rect')
+        .attr('id', d => d._uuid)
+        .attr('data-operator', d => d._operator)
+        .attr('class', d => `operator__box operator__box--${d._operator.toString().replace(/[ ]/g, '_')}`)
+        .attr('height', d => d._operator === 'Fetch Operator' ? 150 : 55)
+        .attr('width', 140)
+
+      children
+          .append('foreignObject')
+        .attr('data-uuid', d => d._uuid)
+        .attr('data-operator', d => d._operator)
+        .attr('class', d => `operator operator--${d._operator.toString().replace(/[ ]/g, '_')}`)
+        .attr('height', d => d._operator === 'Fetch Operator' ? 150 : 55)
+        .attr('width', 140)
+          .append('xhtml:body')
+        .style('margin', 0)
+          .html(d => getRenderer(d._operator)(d))
+        .on('click', d => onRequestDetail(doClean(d)));
+
+      children
+        .call(recurseC, onRequestDetail);
+    });
+}
+
+function getRenderer(type) {
+  if(type === 'Fetch Operator') {
+    return (d => {
+      return (`
+        <div style='display:flex;align-items: center;'>
+          <div class='operator-meta'>
+            <i class='fa ${getOperatorIcon(d._operator)}' aria-hidden='true'></i>
+          </div>
+          <div class='operator-body' style='margin-left: 10px;'>
+            <div>${getOperatorLabel(d)}</div>
+            ${d['limit:'] ? '<div><span style="font-weight: lighter;">Limit:</span> ' + d['limit:'] + ' </div>' : ''}
+          </div>
+        </div>
+      `);
+    });
+  }
+
+  return (d => {
+    const stats = d['Statistics:'] ?  `<div><span style='font-weight: lighter;'>Rows:</span> ${abbreviate(getNumberOfRows(d['Statistics:']))}</div>` : '';
+    return (`
+      <div style='display:flex;'>
+        <div class='operator-meta'>
+          <i class='fa ${getOperatorIcon(d._operator)}' aria-hidden='true'></i>
+        </div>
+        <div class='operator-body' style='margin-left: 10px;'>
+          <div>${getOperatorLabel(d)}</div>
+          ${stats}
+        </div>
+      </div>
+    `);
+  });
+
+}
+
+function getNumberOfRows(statistics) {
+  const match = statistics.match(/([^\?]*)\Num rows: (\d*)/);
+  return (match.length === 3 && Number.isNaN(Number(match[2])) === false) ? match[2] : 0;
+}
+function getOperatorLabel(d) {
+  const operator = d._operator;
+
+  if(operator === 'TableScan') {
+    return d['alias:'];
+  }
+
+  const operatorStr = operator.toString();
+  if(operatorStr.endsWith(' Operator')) {
+    return operatorStr.substring(0, operatorStr.length - ' Operator'.length);
+  }
+  if(operatorStr.endsWith(' Pseudo-Edge')) {
+    return operatorStr.substring(0, operatorStr.length - ' Pseudo-Edge'.length);
+  }
+  return operatorStr ? operatorStr : 'Unknown';
+}
+function getOperatorIcon(operator) {
+  switch(operator) {
+    case 'File Output Operator':
+      return 'fa-file-o';
+    case 'Partition/Sort Pseudo-Edge':
+    case 'Broadcast Pseudo-Edge':
+    case 'Partition Pseudo-Edge':
+    case 'Co-partition Pseudo-Edge':
+    case 'Cross-product Distribute Pseudo-Edge':
+    case 'Reduce Output Operator':
+      return 'fa-compress';
+    case 'Filter Operator':
+      return 'fa-filter';
+    case 'Dynamic Partitioning Event Operator':
+      return 'fa-columns'
+    case 'Map Join Operator':
+      return 'fa-code-fork'
+    case 'Limit':
+    case 'Group By Operator':
+    case 'Select Operator':
+    case 'TableScan':
+    case 'Fetch Operator':
+      return 'fa-table';
+    default:
+      return '';
+  }
+}
+function getIcon (type, subtype) {
+  switch(type) {
+    case 'join':
+      return 'fa-code-fork'
+    case 'vectorization':
+    case 'job':
+      return;
+    case 'broadcast':
+    case 'partition-sort':
+      return 'fa-compress';
+    case 'source':
+    case 'sink':
+    case 'group-by':
+    case 'select':
+      return 'fa-table';
+  }
+};
+function abbreviate(value) {
+  let newValue = value;
+  if (value >= 1000) {
+    const suffixes = ["", "k", "m", "b","t"];
+    const suffixNum = Math.floor(("" + value).length / 3);
+    let shortValue = '';
+    for (var precision = 2; precision >= 1; precision--) {
+      shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+      const dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+      if (dotLessShortValue.length <= 2) { break; }
+    }
+    if (shortValue % 1 != 0) {
+      const  shortNum = shortValue.toFixed(1);
+    }
+    newValue = shortValue+suffixes[suffixNum];
+  }
+  return newValue;
+}
+function reset(zoom, svg, container) {
+  const vertices = container.selectAll('g.vertex');
+  const bounds = [];
+  vertices.each(function(d) {
+    const cVertex = d3.select(this);
+    const box = cVertex.node().getBoundingClientRect();
+    bounds.push(box);
+  });
+  const PADDING_PERCENT = 0.95;
+  const svgRect = svg.node().getBoundingClientRect();
+  const fullWidth = svgRect.width;
+  const fullHeight = svgRect.height;
+  const offsetY = svgRect.top;
+  const top = Math.min(...bounds.map(cBound => cBound.top));
+  const left = Math.min(...bounds.map(cBound => cBound.left));
+  const width = Math.max(...bounds.map(cBound => cBound.right)) - left;
+  const height = Math.max(...bounds.map(cBound => cBound.bottom)) - top;
+  const midX = left + width / 2;
+  const midY = top + height / 2;
+  if (width == 0 || height == 0){
+    // nothing to fit
+    return;
+  }
+  const scale = PADDING_PERCENT / Math.max(width / fullWidth, height / fullHeight);
+  const translate = [fullWidth / 2 - scale * midX, fullHeight / 2 - scale * midY];
+
+  zoom.scale(scale).translate([translate[0], 50]);
+
+  svg
+    .transition()
+    .delay(750)
+    .call( zoom.event );
+}
+
+function getConnectionPath(connector, svg, container) {
+  const operators = container.selectAll('.operator');
+  const source = container.select(`#${connector._source._uuid}`);
+  const target = container.select(`#${connector._target._uuid}`);
+  const rSource = source.node().getBoundingClientRect();
+  const rTarget = target.node().getBoundingClientRect();
+  const pSource = {
+    x: (rSource.left + rSource.right) / 2,
+    y: (rSource.top + rSource.bottom) / 2,
+  };
+  const pTarget = {
+    x: (rTarget.left + rTarget.right) / 2,
+    y: (rTarget.top + rTarget.bottom) / 2,
+  };
+  const path = [
+    pTarget
+  ];
+  const junctionXMultiplier = (pTarget.x - pSource.x < 0) ? +1 : -1;
+  if(pSource.y !== pTarget.y) {
+    path.push({
+      x: pTarget.x + junctionXMultiplier * 90,
+      y: pTarget.y
+    }, {
+      x: pTarget.x + junctionXMultiplier * 90,
+      y: pSource.y
+    });
+  }
+  path.push(pSource);
+  const offsetY = svg.node().getBoundingClientRect().top;
+  return path.reduce((accumulator, cPoint, index) => {
+    if(index === 0) {
+      return accumulator + `M ${cPoint.x}, ${cPoint.y - offsetY}\n`
+    } else {
+      return accumulator + `L ${cPoint.x}, ${cPoint.y - offsetY}\n`
+    }
+  }, '');
+}
+
+function doClean(node) {
+  if(Array.isArray(node._groups)) {
+    return node._groups.map(cGroup => doClean(cGroup));
+  } else {
+    return (
+      Object.keys(node)
+        .filter(cNodeKey => cNodeKey !== '_children')
+        .reduce((accumulator, cNodeKey) => {
+          accumulator[cNodeKey] = node[cNodeKey];
+          return accumulator;
+        }, {})
+    );
+  }
+}
