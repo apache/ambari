d4839a9bc2942cb5aca7659b95f59d6bcb9ff95d
Jonathan Hurley <jhurley@hortonworks.com>
2017-03-09 16:29:52 -0500

    AMBARI-20387 - Upgrades on MySQL Should Also Drop Indexes With Foreign Key Names (jonathanhurley)

--- ambari-server/src/main/java/org/apache/ambari/server/orm/DBAccessor.java
+++ ambari-server/src/main/java/org/apache/ambari/server/orm/DBAccessor.java
@@ -352,9 +352,14 @@ public interface DBAccessor {
   void dropSequence(String sequenceName) throws SQLException;
 
   /**
-   * Drop a FK constraint from table
-   * @param tableName name of the table
-   * @param constraintName name of the constraint
+   * Drops a FK constraint from a table. In the case of
+   * {@link DatabaseType#MYSQL}, this will also ensure that any associated
+   * indexes are also dropped.
+   *
+   * @param tableName
+   *          name of the table
+   * @param constraintName
+   *          name of the constraint
    * @throws SQLException
    */
   void dropFKConstraint(String tableName, String constraintName) throws SQLException;
