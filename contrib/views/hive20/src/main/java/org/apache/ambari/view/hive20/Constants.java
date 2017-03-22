77d231b44d12507ce5998b93a8fae6d20dae2283
pallavkul <pallav.kul@gmail.com>
2017-03-22 22:47:05 +0530

    AMBARI-20517. make home directory check as optional in hive20 view (Nitiraj Rathore via pallavkul)

--- contrib/views/hive20/src/main/java/org/apache/ambari/view/hive20/Constants.java
+++ contrib/views/hive20/src/main/java/org/apache/ambari/view/hive20/Constants.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,4 +16,10 @@
  * limitations under the License.
  */
 
-export default { notStarted: 'NOT_STARTED', started: 'STARTED', completed: 'COMPLETED', errored: 'ERRORED', skipped : 'SKIPPED'};
+package org.apache.ambari.view.hive20;
+
+public interface Constants {
+  String VIEW_CONF_KEYVALUES = "view.conf.keyvalues";
+  String DEFAULT_FS = "fs.defaultFS";
+  String AMBARI_SKIP_HOME_DIRECTORY_CHECK_PROTOCOL_LIST = "views.skip.home-directory-check.file-system.list";
+}
