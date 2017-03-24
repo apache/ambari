52bc319d368cb337078161e7b2fdc4961c253415
pallavkul <pallav.kul@gmail.com>
2017-03-24 14:05:06 +0530

    AMBARI-20529. Make home directory check as optional in wfmanager view (Madhan Mohan Reddy via pallavkul)

--- contrib/views/wfmanager/src/main/resources/ui/app/routes/index.js
+++ contrib/views/wfmanager/src/main/resources/ui/app/routes/index.js
@@ -19,23 +19,45 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     afterModel(){
+      let workflowManagerConfigsPromise = this.getWorkflowManagerConfigs();
+      workflowManagerConfigsPromise.then(function(data){
+        this.invokeServiceChecksPromises(JSON.parse(data));
+      }.bind(this)).catch(function(errors){
+        this.controllerFor('index').set('errors', errors);
+      }.bind(this));
+    },
+    invokeServiceChecksPromises(workflowManagerConfigs) {
       let ooziePromise = this.checkOozie();
       let hdfsPromise = this.checkHdfs();
-      let homeDirPromise = this.checkUserHome();
-      let serviceChecks = this.controllerFor('index').get('serviceChecks');
       this.controllerFor('index').get('issues').clear();
+      let serviceChecks = this.controllerFor('index').get('serviceChecks');
       this.processServiceCheckPromise(ooziePromise, serviceChecks.findBy('name', 'oozie'));
       this.processServiceCheckPromise(hdfsPromise, serviceChecks.findBy('name', 'hdfs'));
-      this.processServiceCheckPromise(homeDirPromise, serviceChecks.findBy('name', 'homeDir'));
-      Ember.RSVP.Promise.all([ooziePromise, hdfsPromise, homeDirPromise]).then(()=>{
-        this.controllerFor('index').set('serviceChecksComplete', true);
-        Ember.run.later(()=>{
-          this.transitionTo('design');
-      }, 2000);
-      }).catch((errors)=>{
-        this.controllerFor('index').set('serviceChecksComplete', true);
-        this.controllerFor('index').set('errors', errors);
-      });
+
+      if (workflowManagerConfigs.checkHomeDir === 'true') {
+        serviceChecks.pushObject({'name':'homeDir', 'checkCompleted':false, isAvailable : true, 'displayName' : 'User Home Directory Test', 'errorMessage' : 'User home directory not found'})
+        let homeDirPromise = this.checkUserHome();
+        this.processServiceCheckPromise(homeDirPromise, serviceChecks.findBy('name', 'homeDir'));
+        Ember.RSVP.Promise.all([ooziePromise, hdfsPromise, homeDirPromise]).then(()=>{
+          this.controllerFor('index').set('serviceChecksComplete', true);
+          Ember.run.later(()=>{
+            this.transitionTo('design');
+        }, 2000);
+        }).catch((errors)=>{
+          this.controllerFor('index').set('serviceChecksComplete', true);
+          this.controllerFor('index').set('errors', errors);
+        });
+      } else {
+        Ember.RSVP.Promise.all([ooziePromise, hdfsPromise]).then(()=>{
+          this.controllerFor('index').set('serviceChecksComplete', true);
+          Ember.run.later(()=>{
+            this.transitionTo('design');
+        }, 2000);
+        }).catch((errors)=>{
+          this.controllerFor('index').set('serviceChecksComplete', true);
+          this.controllerFor('index').set('errors', errors);
+        });
+      }
     },
     processServiceCheckPromise(promise, serviceCheck){
       promise.then(()=>{
@@ -102,6 +124,26 @@ export default Ember.Route.extend({
           },
           success : function(response){
             resolve(true);
+          },
+          error : function(response){
+            reject(response);
+          }
+        });
+      });
+    },
+    getWorkflowManagerConfigs(){
+      return new Ember.RSVP.Promise((resolve, reject) => {
+        var url = Ember.ENV.API_URL + "/getWorkflowManagerConfigs";
+          Ember.$.ajax({
+          url: url,
+          method: "GET",
+          dataType: "text",
+          contentType: "text/plain;charset=utf-8",
+          beforeSend: function(request) {
+            request.setRequestHeader("X-Requested-By", "workflow-designer");
+          },
+          success : function(response){
+            resolve(response);
           },
           error : function(response){
             reject(response);
