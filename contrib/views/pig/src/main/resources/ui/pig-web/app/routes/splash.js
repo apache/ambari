87e6c2c066c308724704d81365a5928a5d15c063
pallavkul <pallav.kul@gmail.com>
2017-03-26 08:57:42 +0530

    AMBARI-20572. Issue while loading pig view (Venkata Sairam via pallavkul)

--- contrib/views/pig/src/main/resources/ui/pig-web/app/routes/splash.js
+++ contrib/views/pig/src/main/resources/ui/pig-web/app/routes/splash.js
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+"use strict";
 var App = require('app');
 
 App.SplashRoute = Em.Route.extend({
