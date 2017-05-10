f125f6059680572802f7a7dc9922e8379d0e5730
Swapan Shridhar <sshridhar@hortonworks.com>
2017-05-10 14:01:59 -0700

    AMBARI-20947. Use keytab file named 'hive.service.keytab' instead of 'hive.llap.zk.sm.keytab' in hive_server_interactive.py for copying to slider.

--- ambari-server/src/main/resources/common-services/HIVE/0.12.0.2.0/package/scripts/hive_server_interactive.py
+++ ambari-server/src/main/resources/common-services/HIVE/0.12.0.2.0/package/scripts/hive_server_interactive.py
@@ -366,9 +366,6 @@ class HiveServerInteractiveDefault(HiveServerInteractive):
       hive_interactive_kinit_cmd = format("{kinit_path_local} -kt {params.hive_server2_keytab} {params.hive_principal}; ")
       Execute(hive_interactive_kinit_cmd, user=params.hive_user)
 
-      llap_kinit_cmd = format("{kinit_path_local} -kt {params.hive_llap_keytab_file} {params.hive_llap_principal}; ")
-      Execute(llap_kinit_cmd, user=params.hive_user)
-
     """
     Get llap app status data for LLAP Tech Preview code base.
     """
