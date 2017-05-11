4e6babdf1569f83c188b817c0aff5c4535b4d015
oleewere <oleewere@gmail.com>
2017-05-11 15:03:31 +0200

    AMBARI-20989. Ambari infra manager: add batch support (oleewere)

--- ambari-infra/ambari-infra-manager/src/main/java/org/apache/ambari/infra/rest/JobResource.java
+++ ambari-infra/ambari-infra-manager/src/main/java/org/apache/ambari/infra/rest/JobResource.java
@@ -20,12 +20,24 @@ package org.apache.ambari.infra.rest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobInstanceAlreadyExistsException;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobException;
 import org.springframework.context.annotation.Scope;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import java.util.Date;
+import java.util.Set;
 
 @Api(value = "jobs", description = "Job operations")
 @Path("jobs")
@@ -33,11 +45,36 @@ import javax.ws.rs.Produces;
 @Scope("request")
 public class JobResource {
 
+  @Inject
+  private JobOperator jobOperator;
+
+  @Inject
+  private JobExplorer jobExplorer;
+
   @GET
   @Produces({"application/json"})
-  @ApiOperation("Get all jobs")
-  public String getAuditLogs() {
-    return "jobs..."; // TODO
+  @ApiOperation("Get all job names")
+  public Set<String> getAllJobNames() {
+    return jobOperator.getJobNames();
+  }
+
+  @GET
+  @Path("executions/{jobName}")
+  @Produces({"application/json"})
+  @ApiOperation("Get the id values of all the running job instances by job name")
+  public Set<Long> getExecutionIdsByJobName(
+    @PathParam("jobName") String jobName) throws NoSuchJobException {
+    return jobOperator.getRunningExecutions(jobName);
+  }
+
+  @POST
+  @Produces({"application/json"})
+  @Path("start/{jobName}")
+  public Long startJob(@PathParam("jobName") String jobName, @QueryParam("params") String params)
+    throws JobParametersInvalidException, JobInstanceAlreadyExistsException, NoSuchJobException {
+    JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+    jobParametersBuilder.addDate("date", new Date());
+    return jobOperator.start(jobName, jobParametersBuilder.toJobParameters() + "," + params);
   }
 
 }
