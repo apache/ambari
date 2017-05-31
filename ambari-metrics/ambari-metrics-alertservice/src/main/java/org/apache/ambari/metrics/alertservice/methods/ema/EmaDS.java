69339decf3e1ca7652ce1976aceac83414c4a8a7
Aravindan Vijayan <avijayan@hortonworks.com>
2017-05-31 11:31:57 -0700

    Fixing rat check failures and compilation issues. (avijayan)

--- ambari-metrics/ambari-metrics-alertservice/src/main/java/org/apache/ambari/metrics/alertservice/methods/ema/EmaDS.java
+++ ambari-metrics/ambari-metrics-alertservice/src/main/java/org/apache/ambari/metrics/alertservice/methods/ema/EmaDS.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.ambari.metrics.alertservice.methods.ema;
 
 import com.sun.org.apache.commons.logging.Log;
@@ -35,8 +52,6 @@ public class EmaDS implements Serializable {
 
         ema = weight * ema + (1 - weight) * metricValue;
         ems = Math.sqrt(weight * Math.pow(ems, 2.0) + (1 - weight) * Math.pow(metricValue - ema, 2.0));
-
-        System.out.println(ema + ", " + ems);
         LOG.info(ema + ", " + ems);
         return diff > 0 ? new EmaResult(diff) : null;
     }
@@ -44,7 +59,6 @@ public class EmaDS implements Serializable {
     public void update(double metricValue) {
         ema = weight * ema + (1 - weight) * metricValue;
         ems = Math.sqrt(weight * Math.pow(ems, 2.0) + (1 - weight) * Math.pow(metricValue - ema, 2.0));
-        System.out.println(ema + ", " + ems);
         LOG.info(ema + ", " + ems);
     }
 
