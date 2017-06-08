5ba0259e2daceffc6507f6f61d591f16b4bda3e1
Alex Antonenko <hiveww@gmail.com>
2017-06-08 17:19:00 +0300

    AMBARI-21184. When trying to Add Hiveserver2 service on a node, we just get a pop-up dialog box, and then a spinning wheel. Unable to click "Confirm Add" (alexantonenko)

--- ambari-web/app/utils/config.js
+++ ambari-web/app/utils/config.js
@@ -1268,7 +1268,7 @@ App.config = Em.Object.create({
   getTempletonHiveHosts: function (value) {
     var pattern = /thrift:\/\/.+:\d+/,
       patternMatch = value.match(pattern);
-    return patternMatch ? patternMatch[0].split('\\,') : value;
+    return patternMatch ? patternMatch[0].split('\\,') : [];
   },
 
   /**
