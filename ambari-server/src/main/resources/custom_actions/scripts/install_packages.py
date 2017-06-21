7cc6701bb03031779d322ddce14d4c02277ef9b0
Jayush Luniya <jluniya@hortonworks.com>
2017-06-21 12:10:07 -0700

    AMBARI-21135: Kafka service fails to start during EU from HDF 202 to 30 while resolving /etc/kafka/conf (jluniya)

--- ambari-server/src/main/resources/custom_actions/scripts/install_packages.py
+++ ambari-server/src/main/resources/custom_actions/scripts/install_packages.py
@@ -40,7 +40,7 @@ from resource_management.libraries.functions.version import format_stack_version
 from resource_management.libraries.functions.repo_version_history \
   import read_actual_version_from_history_file, write_actual_version_to_history_file, REPO_VERSION_HISTORY_FILE
 from resource_management.libraries.script.script import Script
-from resource_management.core.resources.system import Execute
+from resource_management.core.resources.system import Link
 from resource_management.libraries.functions.stack_features import check_stack_feature
 from resource_management.libraries.functions import StackFeature
 
@@ -204,6 +204,11 @@ class InstallPackages(Script):
     if not (target_stack_version and check_stack_feature(StackFeature.CONFIG_VERSIONING, target_stack_version)):
       Logger.info("Configuration symlinks are not needed for {0}".format(stack_version))
       return
+
+    # After upgrading hdf-select package from HDF-2.X to HDF-3.Y, we need to create this symlink
+    if self.stack_name.upper() == "HDF" \
+            and not os.path.exists("/usr/bin/conf-select") and os.path.exists("/usr/bin/hdfconf-select"):
+      Link("/usr/bin/conf-select", to = "/usr/bin/hdfconf-select")
 
     for package_name, directories in conf_select.get_package_dirs().iteritems():
       # if already on HDP 2.3, then we should skip making conf.backup folders
