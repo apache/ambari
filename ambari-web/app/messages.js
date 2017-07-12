e69b35f6bb1cadb2c7d7843f4cb341398a3f830c
Andrii Tkach <atkach@apache.org>
2017-07-12 13:23:49 +0300

    AMBARI-21437 Allow user to execute force_remove_packages custom action from UI, second patch. (atkach)

--- ambari-web/app/messages.js
+++ ambari-web/app/messages.js
@@ -1710,6 +1710,8 @@ Em.I18n.translations = {
   'admin.stackVersions.version.notInstalled': "Not Installed",
   'admin.stackVersions.version.hostsInfoTooltip': "There are {0} hosts that do not need packages installed:<li>{1} Maintenance Mode</li><li>{2} Not Required</li>",
   'admin.stackVersions.removeIopSelect': "Remove IOP select",
+  'admin.stackVersions.removeIopSelect.done': "IOP select successfully removed.",
+  'admin.stackVersions.removeIopSelect.fail': "IOP select remove failed",
   'admin.stackVersions.manageVersions': "Manage Versions",
   'admin.stackVersions.manageVersions.popup.body': 'You are about to leave the <b>Cluster Management</b> interface' +
     ' and go to the <b>Ambari Administration</b> interface. You can return to cluster management by using the' +
