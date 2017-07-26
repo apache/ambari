6d0708bed6bf5233b556dd7955f4e49c2544c4e1
Andrii Tkach <atkach@apache.org>
2017-07-26 13:15:06 +0300

    AMBARI-21428 Host Versions Page Doesn't Show Stacks From Different Vendors. (atkach)

--- ambari-web/app/controllers/global/cluster_controller.js
+++ ambari-web/app/controllers/global/cluster_controller.js
@@ -335,6 +335,7 @@ App.ClusterController = Em.Controller.extend(App.ReloadPopupMixin, {
       }
 
       upgradeController.loadStackVersionsToModel(true).done(function () {
+        upgradeController.loadCompatibleVersions();
         App.set('stackVersionsAvailable', App.StackVersion.find().content.length > 0);
       });
     });
