2d56dcb6c06cce42e5f50427a51c551f7dc667a1
Attila Magyar <amagyar@hortonworks.com>
2017-08-29 09:33:07 +0200

    AMBARI-21812. Installing mpack with .tgz extension fails with global name 'path' is not defined (amagyar)

--- ambari-common/src/main/python/resource_management/libraries/functions/tar_archive.py
+++ ambari-common/src/main/python/resource_management/libraries/functions/tar_archive.py
@@ -57,25 +57,21 @@ def untar_archive(archive, directory):
   )
 
 def extract_archive(archive, directory):
-  if archive.endswith('.tar.gz') or path.endswith('.tgz'):
-    mode = 'r:gz'
-  elif archive.endswith('.tar.bz2') or path.endswith('.tbz'):
-    mode = 'r:bz2'
-  else:
-    raise ValueError, "Could not extract `%s` as no appropriate extractor is found" % path
-  with closing(tarfile.open(archive, mode)) as tar:
+  with closing(tarfile.open(archive, mode(archive))) as tar:
     tar.extractall(directory)
 
 def get_archive_root_dir(archive):
-  if archive.endswith('.tar.gz') or path.endswith('.tgz'):
-    mode = 'r:gz'
-  elif archive.endswith('.tar.bz2') or path.endswith('.tbz'):
-    mode = 'r:bz2'
-  else:
-    raise ValueError, "Could not extract `%s` as no appropriate extractor is found" % path
   root_dir = None
-  with closing(tarfile.open(archive, mode)) as tar:
+  with closing(tarfile.open(archive, mode(archive))) as tar:
     names = tar.getnames()
     if names:
       root_dir = os.path.commonprefix(names)
   return root_dir
+
+def mode(archive):
+  if archive.endswith('.tar.gz') or archive.endswith('.tgz'):
+    return 'r:gz'
+  elif archive.endswith('.tar.bz2') or archive.endswith('.tbz'):
+    return 'r:bz2'
+  else:
+    raise ValueError("Could not extract `%s` as no appropriate extractor is found" % archive)
