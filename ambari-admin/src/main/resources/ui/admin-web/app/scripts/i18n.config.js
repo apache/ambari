2fc74535d79e4f677233f6caf12166bc7f837c2a
Alex Antonenko <aantonenko@hortonworks.com>
2017-08-31 11:56:35 +0300

    AMBARI-21851. Allow Hidden Repos To Become Visible So They Can Be Made Visible Permanently (alexantonenko)

--- ambari-admin/src/main/resources/ui/admin-web/app/scripts/i18n.config.js
+++ ambari-admin/src/main/resources/ui/admin-web/app/scripts/i18n.config.js
@@ -159,6 +159,7 @@ angular.module('ambariAdminConsole')
     'common.alerts.cannotUpdateAdminStatus': 'Cannot update User Admin status',
     'common.alerts.checkFailed': 'CHECK FAILED',
     'common.alerts.onlySimpleChars': 'Must contain only simple characters.',
+    'common.hidden' : 'Hidden',
 
     'main.title': 'Welcome to Apache Ambari',
     'main.noClusterDescription': 'Provision a cluster, manage who can access the cluster, and customize views for Ambari users.',
