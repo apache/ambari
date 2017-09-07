a5291abd9717aa0c97562c264effc1d330a4e129
Jonathan Hurley <jhurley@hortonworks.com>
2017-09-07 12:25:06 -0400

    Revert "AMBARI-21898. Property provider in-memory maps are refreshed too slowly after config updates. (swagle)"
    This reverts commit 8cb942393ce15efb5f6fbc9f594287c30971c296.

--- ambari-server/src/main/java/org/apache/ambari/server/state/Cluster.java
+++ ambari-server/src/main/java/org/apache/ambari/server/state/Cluster.java
@@ -119,11 +119,6 @@ public interface Cluster {
   List<ServiceComponentHost> getServiceComponentHosts(String serviceName, String componentName);
 
   /**
-   * Get all ServiceComponentHosts for this cluster.
-   */
-  List<ServiceComponentHost> getServiceComponentHosts();
-
-  /**
    * Get all hosts associated with this cluster.
    *
    * @return collection of hosts that are associated with this cluster
