7d277acbd87179d8721e3da5e50867fea11c2c4a
Alex Antonenko <aantonenko@hortonworks.com>
2017-09-14 22:13:05 +0300

    AMBARI-21957. UI should handle rejection of PATCH VDF (alexantonenko)

--- ambari-web/app/controllers/main/admin/stack_and_upgrade_controller.js
+++ ambari-web/app/controllers/main/admin/stack_and_upgrade_controller.js
@@ -1710,7 +1710,7 @@ App.MainAdminStackAndUpgradeController = Em.Controller.extend(App.LocalStorage, 
    * @param data
    * @method installStackVersionSuccess
    */
-  installRepoVersionError: function (data) {
+  installRepoVersionError: function (data, opt, params) {
     var header = Em.I18n.t('admin.stackVersions.upgrade.installPackage.fail.title');
     var body = "";
     if (data && data.responseText) {
@@ -1721,6 +1721,11 @@ App.MainAdminStackAndUpgradeController = Em.Controller.extend(App.LocalStorage, 
     }
     if (data && data.statusText === "timeout") {
       body = Em.I18n.t('admin.stackVersions.upgrade.installPackage.fail.timeout');
+    }
+    var version = App.RepositoryVersion.find(params.id);
+    version.set('defaultStatus', 'INSTALL_FAILED');
+    if (version.get('stackVersion')) {
+      version.set('stackVersion.state', 'INSTALL_FAILED');
     }
     App.showAlertPopup(header, body);
   },
@@ -1744,11 +1749,11 @@ App.MainAdminStackAndUpgradeController = Em.Controller.extend(App.LocalStorage, 
     var lastRepoVersionInstall = App.router.get('backgroundOperationsController.services').find(function(request) {
       return request.get('name').startsWith('Install version');
     });
-    if (!requestIds ||
-      (lastRepoVersionInstall && !requestIds.contains(lastRepoVersionInstall.get('id')))) {
+    if (lastRepoVersionInstall &&
+      (!requestIds || !requestIds.contains(lastRepoVersionInstall.get('id')))) {
       requestIds = [lastRepoVersionInstall.get('id')];
     }
-    return requestIds;
+    return requestIds || [];
   },
 
   /**
