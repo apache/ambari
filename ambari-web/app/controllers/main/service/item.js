2de51400d289519df15a2eee88af2059a510903c
Andrii Tkach <atkach@apache.org>
2017-09-15 14:33:49 +0300

    AMBARI-21965 Tez UI: DAG - All Task Attempts displays no records available. (atkach)

--- ambari-web/app/controllers/main/service/item.js
+++ ambari-web/app/controllers/main/service/item.js
@@ -1493,7 +1493,7 @@ App.MainServiceItemController = Em.Controller.extend(App.SupportClientConfigsDow
    * @return {Boolean}
    * @override
    */
-  allowUpdateProperty: function (parentProperties, name, fileName) {
+  allowUpdateProperty: function (parentProperties, name, fileName, configGroup, savedValue) {
     var stackProperty = App.configsCollection.getConfigByName(name, fileName);
     if (!stackProperty || (stackProperty.serviceName === this.get('content.serviceName'))) {
       /**
@@ -1506,12 +1506,12 @@ App.MainServiceItemController = Em.Controller.extend(App.SupportClientConfigsDow
       /**
        * update properties that depends on current service
        */
-      return !!stackProperty.propertyDependsOn.filter(function (p) {
+      return stackProperty.propertyDependsOn.some(function (p) {
         var service = App.config.get('serviceByConfigTypeMap')[p.type];
         return service && (this.get('content.serviceName') === service.get('serviceName'));
-      }, this).length;
+      }, this);
     }
-    return false;
+    return !Em.isNone(savedValue) && stackProperty.recommendedValue === savedValue;
   },
 
   /**
