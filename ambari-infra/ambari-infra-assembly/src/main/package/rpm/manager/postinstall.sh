6fb05cb0f089ef0fee7fcc462efda967b814b668
Oliver Szabo <oleewere@gmail.com>
2017-09-26 13:49:16 +0200

    AMBARI-22059. Use symlinks for Infra components (oleewere)

--- ambari-infra/ambari-infra-assembly/src/main/package/rpm/manager/postinstall.sh
+++ ambari-infra/ambari-infra-assembly/src/main/package/rpm/manager/postinstall.sh
@@ -12,17 +12,9 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the License
 
-: ${JAVA_HOME:?"Please set the JAVA_HOME variable!"}
+INFRA_MANAGER_LINK_NAME="/usr/bin/infra-manager"
+INFRA_MANAGER_SOURCE="/usr/lib/ambari-infra-manager/infraManager.sh"
 
-JVM="java"
-sdir="`dirname \"$0\"`"
-ldir="`dirname "$(readlink -f "$0")"`"
-
-DIR="$sdir"
-if [ "$sdir" != "$ldir" ]; then
-  DIR="$ldir"
-fi
-
-PATH=$JAVA_HOME/bin:$PATH nohup $JVM -classpath "/etc/ambari-infra-manager/conf:$DIR:$DIR/libs/*" $INFRA_MANAGER_OPTS org.apache.ambari.infra.InfraManager ${1+"$@"} &
+rm -f $INFRA_MANAGER_LINK_NAME ; ln -s $INFRA_MANAGER_SOURCE $INFRA_MANAGER_LINK_NAME
