91f1d523f9022bbfc5fee8b198e270f24b8674ea
Alex Antonenko <aantonenko@hortonworks.com>
2017-09-29 21:18:51 +0300

    AMBARI-22098. UI Change: Rename 'Rerun Checks' button to 'Rerun Pre-Upgrade Checks'

--- ambari-web/app/messages.js
+++ ambari-web/app/messages.js
@@ -1756,7 +1756,7 @@ Em.I18n.translations = {
   'admin.stackVersions.version.upgrade.upgradeOptions.HOU.title': "Host Ordered",
   'admin.stackVersions.version.upgrade.upgradeOptions.EU.description': "Services are stopped while the upgrade is performed. Incurs downtime, but faster upgrade.",
   'admin.stackVersions.version.upgrade.upgradeOptions.errors_bypassed': "Upgrade Checks Bypassed.",
-  'admin.stackVersions.version.upgrade.upgradeOptions.preCheck.rerun':'Rerun Checks',
+  'admin.stackVersions.version.upgrade.upgradeOptions.preCheck.rerun':'Rerun Pre-Upgrade Checks',
   'admin.stackVersions.version.upgrade.upgradeOptions.preCheck.msg.title':'Checks:',
   'admin.stackVersions.version.upgrade.upgradeOptions.preCheck.msg.checking': 'Checking...',
   'admin.stackVersions.version.upgrade.upgradeOptions.preCheck.msg.failed.title': 'Check failed',
