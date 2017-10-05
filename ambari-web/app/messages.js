3f00252534e2dd4b08bd5d308757070fe849cba2
Andrii Tkach <atkach@apache.org>
2017-10-05 17:56:44 +0300

    AMBARI-22148 Style changes for service summary. (atkach)

--- ambari-web/app/messages.js
+++ ambari-web/app/messages.js
@@ -357,6 +357,7 @@ Em.I18n.translations = {
   'common.repositoryType': 'Repository Type',
   'common.rolling.downgrade': 'Rolling Downgrade',
   'common.express.downgrade': 'Express Downgrade',
+  'common.views': 'Views',
 
   'models.alert_instance.tiggered.verbose': "Occurred on {0} <br> Checked on {1}",
   'models.alert_definition.triggered.verbose': "Occurred on {0}",
@@ -2961,8 +2962,9 @@ Em.I18n.translations = {
   'dashboard.services.hdfs.nodes.heapUsed':'{0} / {1}',
 
   'dashboard.services.hdfs.chart.label':'Capacity (Used/Total)',
-  'dashboard.services.hdfs.blockErrors':'{0} / {1} / {2}',
-  'dashboard.services.hdfs.blockErrorsDesc':'corrupt replica / missing / under replicated',
+  'dashboard.services.hdfs.blockErrors.corrupt': 'corrupt replica',
+  'dashboard.services.hdfs.blockErrors.replicated': 'under replicated',
+  'dashboard.services.hdfs.blockErrors.missing': 'missing',
   'dashboard.services.hdfs.datanode.status.tooltip.live': 'This is the number of DataNodes that are live as reported from ' +
     'the NameNode. Even if a DataNode process is up, NameNode might see the status as dead ' +
     'if the DataNode is not communicating with the NameNode as expected. This can be due situations ' +
