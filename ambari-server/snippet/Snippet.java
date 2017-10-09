f1c4626b9b4a6aafc48b71bd7d4e892362af1843
Jonathan Hurley <jhurley@hortonworks.com>
2017-10-09 12:22:35 -0400

    Revert "AMBARI-22160. hadooplzo package installation failed on devdeploys (aonishuk)"
    This reverts commit fc80a1837cc613160e3c60cc3290b7e517b5cd45.

--- ambari-server/snippet/Snippet.java
+++
@@ -1,8 +0,0 @@
-package snippet;
-
-public class Snippet {
-  public static void main(String[] args) {
-    /home/user/ambari/ambari-views/bin/.project
-  }
-}
-
--- ambari-server/src/test/python/stacks/utils/RMFTestCase.py
+++ ambari-server/src/test/python/stacks/utils/RMFTestCase.py
@@ -80,8 +80,7 @@ class RMFTestCase(TestCase):
                     mocks_dict={},
                     try_install=False,
                     command_args=[],
-                    log_out_files=False,
-                    available_packages_in_repos = []):
+                    log_out_files=False):
 
     norm_path = os.path.normpath(path)
 
@@ -126,7 +125,6 @@ class RMFTestCase(TestCase):
         Script.instance = None
         script_class_inst = RMFTestCase._get_attr(script_module, classname)()
         script_class_inst.log_out_files = log_out_files
-        script_class_inst.available_packages_in_repos = available_packages_in_repos
         method = RMFTestCase._get_attr(script_class_inst, command)
     except IOError, err:
       raise RuntimeError("Cannot load class %s from %s: %s" % (classname, norm_path, err.message))
