bfca1bd1296129ab27bf65751177aa0cc1b4b979
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2017-12-04 16:42:34 -0800

    Revert "AMBARI-22566. Upgrade Angular for Ambari Admin View (alexantonenko)"
    This reverts commit 41329e3b71b49ab9036228a8a9c794bff720cf7a.

--- ambari-admin/src/main/resources/ui/admin-web/app/scripts/app.js
+++ ambari-admin/src/main/resources/ui/admin-web/app/scripts/app.js
@@ -60,7 +60,7 @@ angular.module('ambariAdminConsole', [
     };
   }]);
 
-  $httpProvider.interceptors.push(['$rootScope', '$q', function (scope, $q) {
+  $httpProvider.responseInterceptors.push(['$rootScope', '$q', function (scope, $q) {
     function success(response) {
       return response;
     }
