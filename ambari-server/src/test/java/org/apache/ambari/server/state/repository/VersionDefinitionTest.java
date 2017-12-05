0fe2f8fa67595c27368acd0c122e04ed11870e4a
Jonathan Hurley <jhurley@hortonworks.com>
2017-12-05 13:18:39 -0500

    AMBARI-22590 - Messages for some services during PU package installation indicate circular dependency (part2) (jonathanhurley)

--- ambari-server/src/test/java/org/apache/ambari/server/state/repository/VersionDefinitionTest.java
+++ ambari-server/src/test/java/org/apache/ambari/server/state/repository/VersionDefinitionTest.java
@@ -46,11 +46,11 @@ import org.apache.ambari.server.state.stack.RepositoryXml;
 import org.apache.ambari.server.state.stack.RepositoryXml.Os;
 import org.apache.ambari.server.state.stack.RepositoryXml.Repo;
 import org.apache.commons.io.FileUtils;
-import org.apache.hadoop.metrics2.sink.relocated.google.common.collect.Sets;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 /**
  * Tests for repository definitions.
