be0e87bef52458955eddf1b824e6c5ddc6f31de6
Sandor Molnar <smolnar@hortonworks.com>
2017-12-18 23:38:29 -0500

    AMBARI-22571. Handle passwords/sensitive data in Ambari configuration properties (Sandor Molnar via rlevas)

--- ambari-server/src/main/java/org/apache/ambari/server/ldap/domain/AmbariLdapConfigurationKeys.java
+++ ambari-server/src/main/java/org/apache/ambari/server/ldap/domain/AmbariLdapConfigurationKeys.java
@@ -14,60 +14,71 @@
 
 package org.apache.ambari.server.ldap.domain;
 
+import static org.apache.ambari.server.configuration.ConfigurationPropertyType.PASSWORD;
+import static org.apache.ambari.server.configuration.ConfigurationPropertyType.PLAINTEXT;
+
+import org.apache.ambari.server.configuration.ConfigurationPropertyType;
+
 /**
  * Constants representing supported LDAP related property names
- * // todo extend this with validation information, description, defaults maybe
+ * // TODO: extend this with validation information, description, defaults maybe
  */
 public enum AmbariLdapConfigurationKeys {
 
-  LDAP_ENABLED("ambari.ldap.authentication.enabled"),
-  SERVER_HOST("ambari.ldap.connectivity.server.host"),
-  SERVER_PORT("ambari.ldap.connectivity.server.port"),
-  USE_SSL("ambari.ldap.connectivity.use_ssl"),
+  LDAP_ENABLED("ambari.ldap.authentication.enabled", PLAINTEXT),
+  SERVER_HOST("ambari.ldap.connectivity.server.host", PLAINTEXT),
+  SERVER_PORT("ambari.ldap.connectivity.server.port", PLAINTEXT),
+  USE_SSL("ambari.ldap.connectivity.use_ssl", PLAINTEXT),
 
-  TRUST_STORE("ambari.ldap.connectivity.trust_store"),
-  TRUST_STORE_TYPE("ambari.ldap.connectivity.trust_store.type"),
-  TRUST_STORE_PATH("ambari.ldap.connectivity.trust_store.path"),
-  TRUST_STORE_PASSWORD("ambari.ldap.connectivity.trust_store.password"),
-  ANONYMOUS_BIND("ambari.ldap.connectivity.anonymous_bind"),
+  TRUST_STORE("ambari.ldap.connectivity.trust_store", PLAINTEXT),
+  TRUST_STORE_TYPE("ambari.ldap.connectivity.trust_store.type", PLAINTEXT),
+  TRUST_STORE_PATH("ambari.ldap.connectivity.trust_store.path", PLAINTEXT),
+  TRUST_STORE_PASSWORD("ambari.ldap.connectivity.trust_store.password", PASSWORD),
+  ANONYMOUS_BIND("ambari.ldap.connectivity.anonymous_bind", PLAINTEXT),
 
-  BIND_DN("ambari.ldap.connectivity.bind_dn"),
-  BIND_PASSWORD("ambari.ldap.connectivity.bind_password"),
+  BIND_DN("ambari.ldap.connectivity.bind_dn", PLAINTEXT),
+  BIND_PASSWORD("ambari.ldap.connectivity.bind_password", PASSWORD),
 
-  ATTR_DETECTION("ambari.ldap.attributes.detection"), // manual | auto
+  ATTR_DETECTION("ambari.ldap.attributes.detection", PLAINTEXT), // manual | auto
 
-  DN_ATTRIBUTE("ambari.ldap.attributes.dn_attr"),
+  DN_ATTRIBUTE("ambari.ldap.attributes.dn_attr", PLAINTEXT),
 
-  USER_OBJECT_CLASS("ambari.ldap.attributes.user.object_class"),
-  USER_NAME_ATTRIBUTE("ambari.ldap.attributes.user.name_attr"),
-  USER_GROUP_MEMBER_ATTRIBUTE("ambari.ldap.attributes.user.group_member_attr"),
-  USER_SEARCH_BASE("ambari.ldap.attributes.user.search_base"),
+  USER_OBJECT_CLASS("ambari.ldap.attributes.user.object_class", PLAINTEXT),
+  USER_NAME_ATTRIBUTE("ambari.ldap.attributes.user.name_attr", PLAINTEXT),
+  USER_GROUP_MEMBER_ATTRIBUTE("ambari.ldap.attributes.user.group_member_attr", PLAINTEXT),
+  USER_SEARCH_BASE("ambari.ldap.attributes.user.search_base", PLAINTEXT),
 
-  GROUP_OBJECT_CLASS("ambari.ldap.attributes.group.object_class"),
-  GROUP_NAME_ATTRIBUTE("ambari.ldap.attributes.group.name_attr"),
-  GROUP_MEMBER_ATTRIBUTE("ambari.ldap.attributes.group.member_attr"),
-  GROUP_SEARCH_BASE("ambari.ldap.attributes.group.search_base"),
+  GROUP_OBJECT_CLASS("ambari.ldap.attributes.group.object_class", PLAINTEXT),
+  GROUP_NAME_ATTRIBUTE("ambari.ldap.attributes.group.name_attr", PLAINTEXT),
+  GROUP_MEMBER_ATTRIBUTE("ambari.ldap.attributes.group.member_attr", PLAINTEXT),
+  GROUP_SEARCH_BASE("ambari.ldap.attributes.group.search_base", PLAINTEXT),
 
-  USER_SEARCH_FILTER("ambari.ldap.advanced.user_search_filter"),
-  USER_MEMBER_REPLACE_PATTERN("ambari.ldap.advanced.user_member_replace_pattern"),
-  USER_MEMBER_FILTER("ambari.ldap.advanced.user_member_filter"),
+  USER_SEARCH_FILTER("ambari.ldap.advanced.user_search_filter", PLAINTEXT),
+  USER_MEMBER_REPLACE_PATTERN("ambari.ldap.advanced.user_member_replace_pattern", PLAINTEXT),
+  USER_MEMBER_FILTER("ambari.ldap.advanced.user_member_filter", PLAINTEXT),
 
-  GROUP_SEARCH_FILTER("ambari.ldap.advanced.group_search_filter"),
-  GROUP_MEMBER_REPLACE_PATTERN("ambari.ldap.advanced.group_member_replace_pattern"),
-  GROUP_MEMBER_FILTER("ambari.ldap.advanced.group_member_filter"),
+  GROUP_SEARCH_FILTER("ambari.ldap.advanced.group_search_filter", PLAINTEXT),
+  GROUP_MEMBER_REPLACE_PATTERN("ambari.ldap.advanced.group_member_replace_pattern", PLAINTEXT),
+  GROUP_MEMBER_FILTER("ambari.ldap.advanced.group_member_filter", PLAINTEXT),
 
-  FORCE_LOWERCASE_USERNAMES("ambari.ldap.advanced.force_lowercase_usernames"),
-  REFERRAL_HANDLING("ambari.ldap.advanced.referrals"), // folow
-  PAGINATION_ENABLED("ambari.ldap.advanced.pagination_enabled"); // true | false
+  FORCE_LOWERCASE_USERNAMES("ambari.ldap.advanced.force_lowercase_usernames", PLAINTEXT),
+  REFERRAL_HANDLING("ambari.ldap.advanced.referrals", PLAINTEXT), // folow
+  PAGINATION_ENABLED("ambari.ldap.advanced.pagination_enabled", PLAINTEXT); // true | false
 
   private String propertyName;
+  private ConfigurationPropertyType configurationPropertyType;
 
-  AmbariLdapConfigurationKeys(String propName) {
+  AmbariLdapConfigurationKeys(String propName, ConfigurationPropertyType configurationPropertyType) {
     this.propertyName = propName;
+    this.configurationPropertyType = configurationPropertyType;
   }
 
   public String key() {
     return this.propertyName;
+  }
+
+  public ConfigurationPropertyType getConfigurationPropertyType() {
+    return configurationPropertyType;
   }
 
   public static AmbariLdapConfigurationKeys fromKeyStr(String keyStr) {
@@ -77,7 +88,7 @@ public enum AmbariLdapConfigurationKeys {
       }
     }
 
-    throw new IllegalStateException("invalid konfiguration key found!");
+    throw new IllegalStateException("invalid configuration key found!");
 
   }
 }
