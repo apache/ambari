61495a03d58238a0f15051a422e5c85006ee0687
Jungtaek Lim <kabhwan@gmail.com>
2018-01-23 05:33:21 +0900

    AMBARI-22696 Whitelist execute latency from Storm Ambari metrics

--- ambari-server/src/test/python/stacks/2.5/common/test_stack_advisor.py
+++ ambari-server/src/test/python/stacks/2.5/common/test_stack_advisor.py
@@ -6040,6 +6040,7 @@ class TestHDP25StackAdvisor(TestCase):
     self.assertEquals(configurations['storm-site']['properties']['topology.metrics.consumer.register'], '[{"class": "org.apache.hadoop.metrics2.sink.storm.StormTimelineMetricsSink", '
                                                                                                       '"parallelism.hint": 1, '
                                                                                                       '"whitelist": ["kafkaOffset\\\..+/", "__complete-latency", "__process-latency", '
+                                                                                                      '"__execute-latency", '
                                                                                                       '"__receive\\\.population$", "__sendqueue\\\.population$", "__execute-count", "__emit-count", '
                                                                                                       '"__ack-count", "__fail-count", "memory/heap\\\.usedBytes$", "memory/nonHeap\\\.usedBytes$", '
                                                                                                       '"GC/.+\\\.count$", "GC/.+\\\.timeMs$"]}]')
