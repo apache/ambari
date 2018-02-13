b64ccb9de00331c84273dcb99589a4fbf34bf9e9
ababiichuk <ababiichuk@hortonworks.com>
2018-02-13 13:48:59 +0200

    AMBARI-22975 Add namespace-specific layout for service summary page. (ababiichuk)

--- ambari-web/app/models/host_component.js
+++ ambari-web/app/models/host_component.js
@@ -32,6 +32,7 @@ App.HostComponent = DS.Model.extend({
   publicHostName: DS.attr('string'),
   service: DS.belongsTo('App.Service'),
   adminState: DS.attr('string'),
+  haNameSpace: DS.attr('string'),
 
   serviceDisplayName: Em.computed.truncate('service.displayName', 14, 11),
 
