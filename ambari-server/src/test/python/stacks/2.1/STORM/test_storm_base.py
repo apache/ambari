f4a4dca591d18076c0afb26807d2961bc4ac1d46
Eugene Chekanskiy <echekanskiy@gmail.com>
2018-02-16 13:57:05 +0200

    AMBARI-22819. Issues with storm jaas files and recursive ch_(mod/own) calls (echekanskiy)

--- ambari-server/src/test/python/stacks/2.1/STORM/test_storm_base.py
+++ ambari-server/src/test/python/stacks/2.1/STORM/test_storm_base.py
@@ -28,7 +28,7 @@ class TestStormBase(RMFTestCase):
   COMMON_SERVICES_PACKAGE_DIR = "STORM/0.9.1/package"
   STACK_VERSION = "2.1"
 
-  def assert_configure_default(self, confDir="/etc/storm/conf"):
+  def assert_configure_default(self, confDir="/etc/storm/conf", has_metrics=False, legacy=True):
     import params
     self.assertResourceCalled('Directory', '/var/log/storm',
       owner = 'storm',
@@ -75,6 +75,35 @@ class TestStormBase(RMFTestCase):
     self.assertResourceCalled('File', confDir + '/storm-env.sh',
                               owner = 'storm',
                               content = InlineTemplate(self.getConfig()['configurations']['storm-env']['content'])
+                              )
+    if has_metrics:
+      self.assertResourceCalled('File', confDir + '/storm-metrics2.properties',
+                                content = Template('storm-metrics2.properties.j2'),
+                                owner = 'storm',
+                                group = 'hadoop',
+                                )
+      self.assertResourceCalled('Link', '/usr/lib/storm/lib//ambari-metrics-storm-sink.jar',
+                                action = ['delete'],
+                                )
+      self.assertResourceCalled('Link', '/usr/lib/storm/lib/ambari-metrics-storm-sink.jar',
+                                action = ['delete'],
+                                )
+      if legacy:
+        self.assertResourceCalled('Execute', 'ambari-sudo.sh ln -s /usr/lib/storm/lib/ambari-metrics-storm-sink-legacy-with-common-*.jar /usr/lib/storm/lib//ambari-metrics-storm-sink.jar',
+                                  not_if = 'ls /usr/lib/storm/lib//ambari-metrics-storm-sink.jar',
+                                  only_if = 'ls /usr/lib/storm/lib/ambari-metrics-storm-sink-legacy-with-common-*.jar',
+                                  )
+      else:
+        self.assertResourceCalled('Execute', 'ambari-sudo.sh ln -s /usr/lib/storm/lib/ambari-metrics-storm-sink-with-common-*.jar /usr/lib/storm/lib//ambari-metrics-storm-sink.jar',
+                                  not_if = 'ls /usr/lib/storm/lib//ambari-metrics-storm-sink.jar',
+                                  only_if = 'ls /usr/lib/storm/lib/ambari-metrics-storm-sink-with-common-*.jar',
+                                  )
+
+    self.assertResourceCalled('File', confDir + '/storm_jaas.conf',
+                              action=['delete'],
+                              )
+    self.assertResourceCalled('File', confDir + '/client_jaas.conf',
+                              action=['delete'],
                               )
     return storm_yarn_content
 
@@ -127,6 +156,7 @@ class TestStormBase(RMFTestCase):
                               )
     self.assertResourceCalled('TemplateConfig', confDir + '/storm_jaas.conf',
       owner = 'storm',
+      mode = 0644
     )
     return storm_yarn_content
 
