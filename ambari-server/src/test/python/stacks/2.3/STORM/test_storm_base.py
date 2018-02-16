45217961be73d90f09e41df795c99fe6a18095e2
Eugene Chekanskiy <echekanskiy@gmail.com>
2018-02-16 13:57:05 +0200

    AMBARI-22819. Issues with storm jaas files and recursive ch_(mod/own) calls (echekanskiy)

--- ambari-server/src/test/python/stacks/2.3/STORM/test_storm_base.py
+++ ambari-server/src/test/python/stacks/2.3/STORM/test_storm_base.py
@@ -108,6 +108,7 @@ class TestStormBase(RMFTestCase):
                               )
     self.assertResourceCalled('TemplateConfig', confDir + '/storm_jaas.conf',
       owner = 'storm',
+      mode = 0644,
     )
     return storm_yarn_content
 
