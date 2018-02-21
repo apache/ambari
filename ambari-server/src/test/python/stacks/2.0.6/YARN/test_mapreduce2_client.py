0c43023af860f96416660cb45e864b993107b86c
Sandor Molnar <smolnar@hortonworks.com>
2018-02-21 11:13:15 +0100

    AMBARI-22947. Reflecting container-executor changes in Python Unit tests

--- ambari-server/src/test/python/stacks/2.0.6/YARN/test_mapreduce2_client.py
+++ ambari-server/src/test/python/stacks/2.0.6/YARN/test_mapreduce2_client.py
@@ -153,7 +153,7 @@ class TestMapReduce2Client(RMFTestCase):
                               mode = 02050,
                               )
     self.assertResourceCalled('File', '/etc/hadoop/conf/container-executor.cfg',
-                              content = Template('container-executor.cfg.j2'),
+                              content = InlineTemplate(self.getConfig()['configurations']['container-executor']['content']),
                               group = 'hadoop',
                               mode = 0644,
                               )
@@ -317,7 +317,7 @@ class TestMapReduce2Client(RMFTestCase):
       mode = 06050,
     )
     self.assertResourceCalled('File', '/etc/hadoop/conf/container-executor.cfg',
-      content = Template('container-executor.cfg.j2'),
+      content = InlineTemplate(self.getConfig()['configurations']['container-executor']['content']),
       group = 'hadoop',
       mode = 0644,
     )
--- ambari-server/src/test/python/stacks/2.1/YARN/test_apptimelineserver.py
+++ ambari-server/src/test/python/stacks/2.1/YARN/test_apptimelineserver.py
@@ -222,7 +222,7 @@ class TestAppTimelineServer(RMFTestCase):
                               mode = 02050,
                               )
     self.assertResourceCalled('File', '/etc/hadoop/conf/container-executor.cfg',
-                              content = Template('container-executor.cfg.j2'),
+                              content = InlineTemplate(self.getConfig()['configurations']['container-executor']['content']),
                               group = 'hadoop',
                               mode = 0644,
                               )
