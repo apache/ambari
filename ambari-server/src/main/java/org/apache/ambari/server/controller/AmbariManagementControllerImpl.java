1ec645afbda1a0d5fdb4507c62aa26eb46be98c7
Myroslav Papirkovskyi <mpapirkovskyy@apache.org>
2018-03-06 15:16:26 +0200

    AMBARI-23055. GPL repo is not hidden when default version is chosen in UI. (mpapirkovskyy)

--- ambari-server/src/main/java/org/apache/ambari/server/controller/AmbariManagementControllerImpl.java
+++ ambari-server/src/main/java/org/apache/ambari/server/controller/AmbariManagementControllerImpl.java
@@ -4506,7 +4506,7 @@ public class AmbariManagementControllerImpl implements AmbariManagementControlle
         for (RepositoryXml.Repo repo : os.getRepos()) {
           RepositoryResponse resp = new RepositoryResponse(repo.getBaseUrl(), os.getFamily(),
               repo.getRepoId(), repo.getRepoName(), repo.getDistribution(), repo.getComponents(), repo.getMirrorsList(),
-              repo.getBaseUrl(), repo.getLatestUri(), Collections.<String>emptyList(), Collections.<RepoTag>emptySet());
+              repo.getBaseUrl(), repo.getLatestUri(), Collections.<String>emptyList(), repo.getTags());
 
           resp.setVersionDefinitionId(versionDefinitionId);
           resp.setStackName(stackId.getStackName());
