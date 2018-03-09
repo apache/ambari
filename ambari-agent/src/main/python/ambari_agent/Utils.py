09985adf941c915703cecf37a376ad761c195e2c
Andrew Onishuk <aonishuk@hortonworks.com>
2018-03-09 12:26:32 +0200

    AMBARI-23184. Metrics Collector Install failed on HA cluster (aonishuk)

--- ambari-agent/src/main/python/ambari_agent/Utils.py
+++ ambari-agent/src/main/python/ambari_agent/Utils.py
@@ -18,6 +18,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import os
+import time
 import threading
 import collections
 from functools import wraps
@@ -215,3 +216,12 @@ def lazy_property(undecorated):
       return v
 
   return decorated
+
+def execute_with_retries(tries, try_sleep, rety_exception_class, func, *args, **kwargs):
+  for i in range(tries):
+    try:
+      func(*args, **kwargs)
+    except rety_exception_class:
+      if i==tries-1:
+        raise
+      time.sleep(try_sleep)
