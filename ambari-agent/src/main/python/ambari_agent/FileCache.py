d72d0ecb8decc9d61d6e8d202f27bb17d631d7bf
Akhil S Naik <asnaik@hortonworks.com>
2018-03-23 01:00:07 +0530

    AMBARI-23256 Ambari-agent setting permission of .hash files to 666  (#681)
    (cherry picked from commit 71915a45bde4b4f91654f692dc855c7885fac740)

--- ambari-agent/src/main/python/ambari_agent/FileCache.py
+++ ambari-agent/src/main/python/ambari_agent/FileCache.py
@@ -246,7 +246,7 @@ class FileCache():
     try:
       with open(hash_file, "w") as fh:
         fh.write(new_hash)
-      os.chmod(hash_file, 0o666)
+      os.chmod(hash_file, 0o644)
     except Exception, err:
       raise CachingException("Can not write to file {0} : {1}".format(hash_file,
                                                                  str(err)))
