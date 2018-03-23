3241937137a27e741a107d322754804ead0e1aeb
Jonathan Hurley <jhurley@hortonworks.com>
2018-03-23 13:59:43 -0400

    AMBARI-23314 - Prevent Multiple OS Entries For a Single Mpack

--- ambari-server/src/main/resources/Ambari-DDL-MySQL-CREATE.sql
+++ ambari-server/src/main/resources/Ambari-DDL-MySQL-CREATE.sql
@@ -279,7 +279,8 @@ CREATE TABLE repo_os (
   ambari_managed TINYINT(1) DEFAULT 1,
   CONSTRAINT PK_repo_os_id PRIMARY KEY (id),
   CONSTRAINT FK_repo_os_id_repo_version_id FOREIGN KEY (repo_version_id) REFERENCES repo_version (repo_version_id),
-  CONSTRAINT FK_repo_os_mpack_id FOREIGN KEY (mpack_id) REFERENCES mpacks (id));
+  CONSTRAINT FK_repo_os_mpack_id FOREIGN KEY (mpack_id) REFERENCES mpacks (id),
+  CONSTRAINT UQ_repo_os_family_mpack_id UNIQUE (mpack_id, family));
 
 CREATE TABLE repo_definition (
   id BIGINT NOT NULL,
@@ -292,7 +293,8 @@ CREATE TABLE repo_definition (
   unique_repo TINYINT(1) DEFAULT 1,
   mirrors MEDIUMTEXT,
   CONSTRAINT PK_repo_definition_id PRIMARY KEY (id),
-  CONSTRAINT FK_repo_definition_repo_os_id FOREIGN KEY (repo_os_id) REFERENCES repo_os (id));
+  CONSTRAINT FK_repo_definition_repo_os_id FOREIGN KEY (repo_os_id) REFERENCES repo_os (id),
+  CONSTRAINT UQ_repo_def_os_id_repo_id UNIQUE (repo_os_id, repo_id));
 
 CREATE TABLE repo_tags (
   repo_definition_id BIGINT NOT NULL,
@@ -1068,8 +1070,8 @@ CREATE TABLE upgrade_plan (
   cluster_id BIGINT NOT NULL,
   upgrade_type VARCHAR(255) DEFAULT 'ROLLING' NOT NULL,
   direction VARCHAR(255) DEFAULT 'UPGRADE' NOT NULL,
-  skip_failures SMALLINT DEFAULT 0 NOT NULL, 
-  skip_sc_failures SMALLINT DEFAULT 0 NOT NULL, 
+  skip_failures SMALLINT DEFAULT 0 NOT NULL,
+  skip_sc_failures SMALLINT DEFAULT 0 NOT NULL,
   skip_prechecks SMALLINT DEFAULT 0 NOT NULL,
   fail_on_precheck_warnings SMALLINT DEFAULT 0 NOT NULL,
   skip_service_checks SMALLINT DEFAULT 0 NOT NULL,
