42190f3ff6b3d29215d1480ca6e5909214a9faba
Sandor Molnar <smolnar@hortonworks.com>
2018-03-23 09:11:37 +0100

    AMBARI-23311. Use Ambari CLI to specify which services should be setup for SSO integration

--- ambari-server/src/main/python/ambari-server.py
+++ ambari-server/src/main/python/ambari-server.py
@@ -574,6 +574,7 @@ def init_ldap_setup_parser_options(parser):
 @OsFamilyFuncImpl(OsFamilyImpl.DEFAULT)
 def init_setup_sso_options(parser):
   parser.add_option('--sso-enabled', default=None, help="Indicates whether to enable/disable SSO", dest="sso_enabled")
+  parser.add_option('--sso-enabled-services', default=None, help="A comma separated list of services that are expected to be configured for SSO (you are allowed to use '*' to indicate ALL services)", dest='sso_enabled_services')
   parser.add_option('--sso-provider-url', default=None, help="The URL of SSO provider; this must be provided when --sso-enabled is set to 'true'", dest="sso_provider_url")
   parser.add_option('--sso-public-cert-file', default=None, help="The path where the public certificate PEM is located; this must be provided when --sso-enabled is set to 'true'", dest="sso_public_cert_file")
   parser.add_option('--sso-jwt-cookie-name', default=None, help="The name of the JWT cookie", dest="sso_jwt_cookie_name")
--- ambari-server/src/main/python/ambari_server/setupSso.py
+++ ambari-server/src/main/python/ambari_server/setupSso.py
@@ -17,16 +17,22 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
+import ambari_simplejson as json
+import base64
 import logging
 import re
+import sys
+import urllib2
 
 from ambari_commons.os_utils import is_root, run_os_command, copy_file, set_file_permissions, remove_file
 from ambari_commons.exceptions import FatalException, NonFatalException
 from ambari_commons.logging_utils import get_silent, print_warning_msg, print_error_msg
 from ambari_server.userInput import get_validated_string_input, get_YN_input, get_multi_line_input
+from ambari_server.serverUtils import is_server_runing, get_ambari_server_api_base, get_ambari_admin_username_password_pair, get_cluster_name, perform_changes_via_rest_api
 from ambari_server.setupSecurity import REGEX_HOSTNAME_PORT, REGEX_TRUE_FALSE
 from ambari_server.serverConfiguration import get_ambari_properties, get_value_from_properties, update_properties, \
   store_password_file
+from contextlib import closing
 
 logger = logging.getLogger(__name__)
 
@@ -47,8 +53,14 @@ JWT_PUBLIC_KEY_FILENAME = "jwt-cert.pem"
 JWT_PUBLIC_KEY_HEADER = "-----BEGIN CERTIFICATE-----\n"
 JWT_PUBLIC_KEY_FOOTER = "\n-----END CERTIFICATE-----\n"
 
+SSO_ENABLED_SERVICES = "ambari.sso.enabled_services"
+WILDCARD_FOR_ALL_SERVICES = "*"
 
-def validateOptions(options):
+URL_TO_FETCH_SERVICES_ELIGIBLE_FOR_SSO = "clusters/:CLUSTER_NAME/services?ServiceInfo/sso_integration_supported=true" ## :CLUSTER_NAME should be replaced
+SETUP_SSO_CONFIG_URL = 'services/AMBARI/components/AMBARI_SERVER/configurations/sso-configuration'
+
+
+def validate_options(options):
   errors = []
   if options.sso_enabled and not re.search(REGEX_TRUE_FALSE, options.sso_enabled):
     errors.append("--sso-enabled should be to either 'true' or 'false'")
@@ -66,7 +78,7 @@ def validateOptions(options):
     raise FatalException(1, error_msg.format(str(errors)))
 
 
-def populateSsoProviderUrl(options, properties):
+def populate_sso_provide_url(options, properties):
   if not options.sso_provider_url:
       provider_url = get_value_from_properties(properties, JWT_AUTH_PROVIDER_URL, JWT_AUTH_PROVIDER_URL_DEFAULT)
       provider_url = get_validated_string_input("Provider URL [URL] ({0}):".format(provider_url), provider_url, REGEX_HOSTNAME_PORT,
@@ -77,7 +89,7 @@ def populateSsoProviderUrl(options, properties):
   properties.process_pair(JWT_AUTH_PROVIDER_URL, provider_url)
 
 
-def populateSsoPublicCert(options, properties):
+def populate_sso_public_cert(options, properties):
   if not options.sso_public_cert_file:
     cert_path = properties.get_property(JWT_PUBLIC_KEY)
     cert_string = get_multi_line_input("Public Certificate pem ({0})".format('stored' if cert_path else 'empty'))
@@ -93,7 +105,7 @@ def populateSsoPublicCert(options, properties):
   properties.process_pair(JWT_PUBLIC_KEY, cert_path)
 
 
-def populateJwtCookieName(options, properties):
+def populate_jwt_cookie_name(options, properties):
   if not options.sso_jwt_cookie_name:
     cookie_name = get_value_from_properties(properties, JWT_COOKIE_NAME, JWT_COOKIE_NAME_DEFAULT)
     cookie_name = get_validated_string_input("JWT Cookie name ({0}):".format(cookie_name), cookie_name, REGEX_ANYTHING,
@@ -104,7 +116,7 @@ def populateJwtCookieName(options, properties):
   properties.process_pair(JWT_COOKIE_NAME, cookie_name)
 
 
-def populateJwtAudiences(options, properties):
+def populate_jwt_audiences(options, properties):
   if not options.sso_jwt_audience_list:
     audiences = properties.get_property(JWT_AUDIENCES)
     audiences = get_validated_string_input("JWT audiences list (comma-separated), empty for any ({0}):".format(audiences), audiences,
@@ -113,6 +125,87 @@ def populateJwtAudiences(options, properties):
     audiences = options.sso_jwt_audience_list
 
   properties.process_pair(JWT_AUDIENCES, audiences)
+  
+def get_eligible_services(properties, admin_login, admin_password, cluster_name):
+  url = get_ambari_server_api_base(properties) + URL_TO_FETCH_SERVICES_ELIGIBLE_FOR_SSO.replace(":CLUSTER_NAME", cluster_name)
+  admin_auth = base64.encodestring('%s:%s' % (admin_login, admin_password)).replace('\n', '')
+  request = urllib2.Request(url)
+  request.add_header('Authorization', 'Basic %s' % admin_auth)
+  request.add_header('X-Requested-By', 'ambari')
+  request.get_method = lambda: 'GET'
+
+  services = []
+  sys.stdout.write('\nFetching SSO enabled services')
+  numOfTries = 0
+  request_in_progress = True
+  while request_in_progress:
+    numOfTries += 1
+    if (numOfTries == 60):
+      raise FatalException(1, "Could not fetch eligible services within a minute; giving up!")
+    sys.stdout.write('.')
+    sys.stdout.flush()
+
+    try:
+      with closing(urllib2.urlopen(request)) as response:
+        response_status_code = response.getcode()
+        if response_status_code != 200:
+          request_in_progress = False
+          err = 'Error while fetching eligible services. Http status code - ' + str(response_status_code)
+          raise FatalException(1, err)
+        else:
+            response_body = json.loads(response.read())
+            items = response_body['items']
+            if len(items) > 0:
+              for item in items:
+                services.append(item['ServiceInfo']['service_name'])
+            if not items:
+              time.sleep(1)
+            else:
+              request_in_progress = False
+
+    except Exception as e:
+      request_in_progress = False
+      err = 'Error while fetching eligible services. Error details: %s' % e
+      raise FatalException(1, err)
+  if (len(services) == 0):
+    sys.stdout.write('\nThere is no SSO enabled services found\n')
+  else:
+    sys.stdout.write('\nFound SSO enabled services: {0}\n'.format(', '.join(str(s) for s in services)))
+  return services
+
+def get_services_requires_sso(options, properties, admin_login, admin_password):
+  if not options.sso_enabled_services:
+    configure_for_all_services = get_YN_input("Use SSO for all services [y/n] (n): ", False)
+    if configure_for_all_services:
+      services = WILDCARD_FOR_ALL_SERVICES
+    else:
+      cluster_name = get_cluster_name(properties, admin_login, admin_password)
+      eligible_services = get_eligible_services(properties, admin_login, admin_password, cluster_name)
+      services = ''
+      for service in eligible_services:
+        question = "Use SSO for {0} [y/n] (y): ".format(service)
+        if get_YN_input(question, True):
+          if len(services) > 0:
+            services = services + ", "
+          services = services + service
+  else:
+    services = options.sso_enabled_services
+
+  return services
+
+
+def update_sso_conf(properties, services, admin_login, admin_password):
+  sso_configuration_properties = {}
+  sso_configuration_properties[SSO_ENABLED_SERVICES] = services
+  request_data = {
+    "Configuration": {
+      "category": "sso-configuration",
+      "properties": {
+      }
+    }
+  }
+  request_data['Configuration']['properties'] = sso_configuration_properties
+  perform_changes_via_rest_api(properties, admin_login, admin_password, SETUP_SSO_CONFIG_URL, 'PUT', request_data)
 
 
 def setup_sso(options):
@@ -120,8 +213,13 @@ def setup_sso(options):
   if not is_root():
     raise FatalException(4, 'ambari-server setup-sso should be run with root-level privileges')
 
+  server_status, pid = is_server_runing()
+  if not server_status:
+    err = 'Ambari Server is not running.'
+    raise FatalException(1, err)
+
   if not get_silent():
-    validateOptions(options)
+    validate_options(options)
 
     properties = get_ambari_properties()
 
@@ -142,10 +240,14 @@ def setup_sso(options):
       must_setup_params = options.sso_enabled == 'true'
 
     if must_setup_params:
-      populateSsoProviderUrl(options, properties)
-      populateSsoPublicCert(options, properties)
-      populateJwtCookieName(options, properties)
-      populateJwtAudiences(options, properties)
+      populate_sso_provide_url(options, properties)
+      populate_sso_public_cert(options, properties)
+      populate_jwt_cookie_name(options, properties)
+      populate_jwt_audiences(options, properties)
+
+      admin_login, admin_password = get_ambari_admin_username_password_pair(options)
+      services = get_services_requires_sso(options, properties, admin_login, admin_password)
+      update_sso_conf(properties, services, admin_login, admin_password)
 
     update_properties(properties)
 
