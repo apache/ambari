768ee9a2035d8ba6061e16c44b371afbf9209276
Robert Levas <rlevas@hortonworks.com>
2018-04-03 16:15:44 -0400

    [AMBARI-23433] Remove unnecessary properties from the Ranger SSO configuration updates via the stack advisor

--- ambari-server/src/main/resources/stacks/HDP/2.6/services/stack_advisor.py
+++ ambari-server/src/main/resources/stacks/HDP/2.6/services/stack_advisor.py
@@ -576,7 +576,6 @@ class HDP26StackAdvisor(HDP25StackAdvisor):
         if ambari_sso_details:
           putRangerAdminSiteProperty('ranger.sso.providerurl', ambari_sso_details.get_jwt_provider_url())
           putRangerAdminSiteProperty('ranger.sso.publicKey', ambari_sso_details.get_jwt_public_key(False, True))
-          putRangerAdminSiteProperty('ranger.sso.cookiename', ambari_sso_details.get_jwt_cookie_name())
           putRangerAdminSiteProperty('ranger.sso.browser.useragent', 'Mozilla,chrome')
 
       # If SSO should be disabled for this service
