c9b77895b2b171df8ee34480a2a5e29deaaa2bf9
Oliver Szabo <oleewere@gmail.com>
2018-04-03 15:39:40 +0200

    AMBARI-23080. Log Search: use comositeId instead of implicit routing by default for collections.

--- ambari-logsearch/ambari-logsearch-logfeeder/src/main/java/org/apache/ambari/logfeeder/output/OutputSolr.java
+++ ambari-logsearch/ambari-logsearch-logfeeder/src/main/java/org/apache/ambari/logfeeder/output/OutputSolr.java
@@ -81,7 +81,7 @@ public class OutputSolr extends Output<LogFeederProps, InputMarker> implements C
   private int maxIntervalMS;
   private int workers;
   private int maxBufferSize;
-  private boolean isComputeCurrentCollection = false;
+  private boolean implicitRouting = false;
   private int lastSlotByMin = -1;
   private boolean skipLogtime = false;
 
@@ -115,14 +115,14 @@ public class OutputSolr extends Output<LogFeederProps, InputMarker> implements C
   @Override
   public void init(LogFeederProps logFeederProps) throws Exception {
     this.logFeederProps = logFeederProps;
-    initParams();
+    initParams(logFeederProps);
     setupSecurity();
     createOutgoingBuffer();
     createSolrStateWatcher();
     createSolrWorkers();
   }
 
-  private void initParams() throws Exception {
+  private void initParams(LogFeederProps logFeederProps) throws Exception {
     type = getStringValue("type");
     while (true) {
       OutputSolrProperties outputSolrProperties = getLogSearchConfig().getOutputSolrProperties(type);
@@ -153,6 +153,13 @@ public class OutputSolr extends Output<LogFeederProps, InputMarker> implements C
 
     LOG.info(String.format("Config: Number of workers=%d, splitMode=%s, splitInterval=%d."
         + getShortDescription(), workers, splitMode, splitInterval));
+
+    implicitRouting = logFeederProps.isSolrImplicitRouting(); // TODO: in the future, load it from output config (can be a use case to use different routing for audit/service logs)
+    if (logFeederProps.isSolrImplicitRouting()) {
+      LOG.info("Config: Use implicit routing globally for adding docs to Solr.");
+    } else {
+      LOG.info("Config: Use compositeId globally for adding docs to Solr.");
+    }
   }
 
   @Override
@@ -166,7 +173,6 @@ public class OutputSolr extends Output<LogFeederProps, InputMarker> implements C
       if (!splitMode.equalsIgnoreCase("none")) {
         splitInterval = Integer.parseInt(splitMode);
       }
-      isComputeCurrentCollection = !splitMode.equalsIgnoreCase("none");
 
       // collection can not be overwritten after initialization
       if (init) {
@@ -424,7 +430,7 @@ public class OutputSolr extends Output<LogFeederProps, InputMarker> implements C
       while (!isDrain()) {
         try {
           synchronized (propertiesLock) {
-            if (isComputeCurrentCollection) {
+            if (implicitRouting) {
               // Compute the current router value
               addRouterField();
             }
