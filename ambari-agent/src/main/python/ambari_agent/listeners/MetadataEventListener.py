bdb566b9bd0491c1ddfb5a344f0c6b828610f8f7
Myroslav Papirkovskyi <mpapirkovskyy@apache.org>
2018-05-14 18:25:23 +0300

    AMBARI-23791. Agent is not notified about cluster delete in runtime. (aonishuk)

--- ambari-agent/src/main/python/ambari_agent/listeners/MetadataEventListener.py
+++ ambari-agent/src/main/python/ambari_agent/listeners/MetadataEventListener.py
@@ -47,7 +47,16 @@ class MetadataEventListener(EventListener):
     if message == {}:
       return
 
-    self.metadata_cache.cache_update(message['clusters'], message['hash'])
+    event_type = message['eventType']
+
+    if event_type == 'CREATE':
+      self.metadata_cache.rewrite_cache(message['clusters'], message['hash'])
+    elif event_type == 'UPDATE':
+      self.metadata_cache.cache_update(message['clusters'], message['hash'])
+    elif event_type == 'DELETE':
+      self.metadata_cache.cache_delete(message['clusters'], message['hash'])
+    else:
+      logger.error("Unknown event type '{0}' for metadata event")
 
     try:
       self.config.update_configuration_from_metadata(message['clusters']['-1']['agentConfigs'])
