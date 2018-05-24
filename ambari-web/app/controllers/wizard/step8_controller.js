a7ac282e4c6b26cad34eae7ccff4cf67260e407e
Ishan Bhatt <ishanbhatt.1989@gmail.com>
2018-05-24 10:47:45 -0700

    [AMBARI-23942] Invalid repo url displayed on Ambari UI during service… (#1364)
    * [AMBARI-23942] Invalid repo url displayed on Ambari UI during service installation
    
    * remove commented out code

--- ambari-web/app/controllers/wizard/step8_controller.js
+++ ambari-web/app/controllers/wizard/step8_controller.js
@@ -331,9 +331,17 @@ App.WizardStep8Controller = Em.Controller.extend(App.AddSecurityConfigs, App.wiz
    */
   loadRepoInfo: function () {
     var stackName = App.get('currentStackName');
-    var currentStackVersionNumber = App.get('currentStackVersionNumber');
-    var currentStackVersion = App.StackVersion.find().filterProperty('stack', stackName).findProperty('version', currentStackVersionNumber);
-    var currentRepoVersion = currentStackVersion.get('repositoryVersion.repositoryVersion');
+
+    var currentRepoVersion;
+    App.RepositoryVersion.find().forEach(function (repoVersion) {
+      if (repoVersion.get('stackVersionType') === stackName && repoVersion.get('isCurrent') && repoVersion.get('isStandard')) {
+        currentRepoVersion = repoVersion.get('repositoryVersion');
+      }
+    });
+
+    if (!currentRepoVersion) {
+      console.error('Error while getting current stack repository version');
+    }
 
     return App.ajax.send({
       name: 'cluster.load_repo_version',
