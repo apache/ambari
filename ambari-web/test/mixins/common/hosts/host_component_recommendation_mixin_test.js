a2c052509029ef90240a36009fce38cd65186130
Jason Golieb <jgolieb@hortonworks.com>
2018-06-22 14:57:49 -0400

    Fixed unit test.

--- ambari-web/test/mixins/common/hosts/host_component_recommendation_mixin_test.js
+++ ambari-web/test/mixins/common/hosts/host_component_recommendation_mixin_test.js
@@ -163,8 +163,7 @@ describe('App.HostComponentRecommendationMixin', function() {
                 ]
               }
             }
-          },
-          stackVersionUrl: '/stack/url'
+          }
         },
         m: 'when blueprint not passed it should be generated from components list'
       },
