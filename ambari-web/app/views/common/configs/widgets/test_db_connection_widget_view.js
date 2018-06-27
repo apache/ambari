4671354a343025bad72617ea00ac48b0a0e1246a
Ishan Bhatt <ishanbhatt.1989@gmail.com>
2018-06-26 19:18:00 -0700

    [AMBARI-24198] Ranger KMS Database Connection Test hangs with JS error.

--- ambari-web/app/views/common/configs/widgets/test_db_connection_widget_view.js
+++ ambari-web/app/views/common/configs/widgets/test_db_connection_widget_view.js
@@ -295,7 +295,8 @@ App.TestDbConnectionWidgetView = App.ConfigWidgetView.extend({
       KERBEROS: ['kdc_hosts'],
       RANGER: ranger && ranger.compareCurrentVersion('0.5') > -1 ?
         ['db_user', 'db_password', 'db_name', 'ranger.jpa.jdbc.url', 'ranger.jpa.jdbc.driver'] :
-        ['db_user', 'db_password', 'db_name', 'ranger_jdbc_connection_url', 'ranger_jdbc_driver']
+        ['db_user', 'db_password', 'db_name', 'ranger_jdbc_connection_url', 'ranger_jdbc_driver'],
+      RANGER_KMS: ['db_user', 'db_password', 'ranger.ks.jpa.jdbc.url', 'ranger.ks.jpa.jdbc.driver']
     };
     return propertiesMap[this.get('parentView.content.serviceName')];
   }.property(),
