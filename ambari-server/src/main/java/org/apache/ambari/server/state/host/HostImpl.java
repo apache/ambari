d9d7cf590722d56c9843525cd7a31ffc01e10409
Doroszlai, Attila <adoroszlai@apache.org>
2018-07-09 09:05:52 +0200

    AMBARI-24245. FindBugs: comparison of String using !=

--- ambari-server/src/main/java/org/apache/ambari/server/state/host/HostImpl.java
+++ ambari-server/src/main/java/org/apache/ambari/server/state/host/HostImpl.java
@@ -932,7 +932,7 @@ public class HostImpl implements Host {
 
   @Override
   public void setStatus(String status) {
-    if (this.status != status) {
+    if (!Objects.equals(this.status, status)) {
       ambariEventPublisher.publish(new HostStatusUpdateEvent(getHostName(), status));
     }
     this.status = status;
