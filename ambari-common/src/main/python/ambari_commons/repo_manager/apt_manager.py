177d3f1436bb2948f90e7454048875908f5a3a3a
jkaur <jkaur@hortonworks.com>
2018-07-25 13:01:04 +0530

    AMBARI-24344: Upgrade Infra Solr client operation fails for Ubuntu while running migration script for solr upgrade

--- ambari-common/src/main/python/ambari_commons/repo_manager/apt_manager.py
+++ ambari-common/src/main/python/ambari_commons/repo_manager/apt_manager.py
@@ -29,10 +29,10 @@ from resource_management.core.logger import Logger
 
 
 def replace_underscores(function_to_decorate):
-  def wrapper(*args):
+  def wrapper(*args, **kwargs):
     self = args[0]
     name = args[1].replace("_", "-")
-    return function_to_decorate(self, name, *args[2:])
+    return function_to_decorate(self, name, *args[2:], **kwargs)
   return wrapper
 
 
