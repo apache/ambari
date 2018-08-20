9825e6867941fb112fe267f2c80a39fe96736120
Oliver Szabo <oleewere@gmail.com>
2018-08-20 21:43:58 +0200

    AMBARI-24506. Upgrade: Infra Solr service is not renamed in Upgrade History table.

--- ambari-server/src/test/java/org/apache/ambari/server/upgrade/UpgradeCatalog271Test.java
+++ ambari-server/src/test/java/org/apache/ambari/server/upgrade/UpgradeCatalog271Test.java
@@ -92,7 +92,7 @@ public class UpgradeCatalog271Test {
     Method addNewConfigurationsFromXml = AbstractUpgradeCatalog.class.getDeclaredMethod("addNewConfigurationsFromXml");
     Method updateRangerLogDirConfigs = UpgradeCatalog271.class.getDeclaredMethod("updateRangerLogDirConfigs");
     Method updateRangerKmsDbUrl = UpgradeCatalog271.class.getDeclaredMethod("updateRangerKmsDbUrl");
-    Method renameAmbariInfraInConfigGroups = UpgradeCatalog271.class.getDeclaredMethod("renameAmbariInfraInConfigGroups");
+    Method renameAmbariInfraInConfigGroups = UpgradeCatalog271.class.getDeclaredMethod("renameAmbariInfraInConfigGroupsAndUpgradeHistory");
     Method removeLogSearchPatternConfigs = UpgradeCatalog271.class.getDeclaredMethod("removeLogSearchPatternConfigs");
     Method updateSolrConfigurations = UpgradeCatalog271.class.getDeclaredMethod("updateSolrConfigurations");
 
@@ -114,7 +114,7 @@ public class UpgradeCatalog271Test {
     upgradeCatalog271.updateRangerKmsDbUrl();
     expectLastCall().once();
 
-    upgradeCatalog271.renameAmbariInfraInConfigGroups();
+    upgradeCatalog271.renameAmbariInfraInConfigGroupsAndUpgradeHistory();
     expectLastCall().once();
 
     upgradeCatalog271.removeLogSearchPatternConfigs();
