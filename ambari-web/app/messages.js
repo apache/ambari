2b52c9a93c1ad6c7654736faf7eb3559075d3ccf
Aleksandr Kovalenko <akovalenko@apache.org>
2018-09-03 19:57:45 +0300

    AMBARI-24586. User get stuck between steps if API returns empty version_definitions array (akovalenko)

--- ambari-web/app/messages.js
+++ ambari-web/app/messages.js
@@ -682,6 +682,7 @@ Em.I18n.translations = {
   'installer.step1.invalidURLAttention': '<b>Attention:</b> Please make sure all repository URLs are valid before proceeding.',
   'installer.step1.checkAtLeastOneAttention': '<b>Attention:</b> Please check at least one repository.',
   'installer.step1.retryRepoUrls': 'Click <b>here</b> to retry.',
+  'installer.step1.noVersionDefinitions': 'Unable to load versions data from server.',
 
   'installer.step2.header':'Install Options',
   'installer.step2.body':'Enter the list of hosts to be included in the cluster and provide your SSH key.',
