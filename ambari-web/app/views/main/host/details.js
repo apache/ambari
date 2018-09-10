c508960c85b2405c09fb20650b9dd65a3d3d8c91
Andrii Tkach <atkach@apache.org>
2018-09-10 14:20:40 +0300

    AMBARI-24612 Host Level Maintenance mode is not working through Ambari UI

--- ambari-web/app/views/main/host/details.js
+++ ambari-web/app/views/main/host/details.js
@@ -39,7 +39,6 @@ App.MainHostDetailsView = Em.View.extend({
   hasManyClientsWithConfigs: Em.computed.gt('clientsWithConfigs.length', 1),
 
   maintenance: function () {
-    var onOff = this.get('controller.content.isActive') ? "On" : "Off";
     var result = [];
     if (App.isAuthorized("SERVICE.START_STOP")) {
       result = result.concat([
@@ -74,8 +73,8 @@ App.MainHostDetailsView = Em.View.extend({
         action: 'onOffPassiveModeForHost',
         liClass: '',
         cssClass: 'icon-medkit',
-        active: this.get('isActive'),
-        label: this.t('passiveState.turn' + onOff)
+        active: this.get('controller.content.isActive'),
+        label: this.t('passiveState.turn' + (this.get('controller.content.isActive') ? "On" : "Off"))
       });
     }
     if (App.get('isKerberosEnabled') && App.get('supports.regenerateKeytabsOnSingleHost')){
