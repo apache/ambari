6231892fdc052016eba183935d69277b56a73917
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2018-12-11 18:04:53 +0100

    AMBARI-25030. Improve component-host association syntax in Add Service request (#2711)

--- ambari-server/src/test/java/org/apache/ambari/server/controller/AddServiceRequestTest.java
+++ ambari-server/src/test/java/org/apache/ambari/server/controller/AddServiceRequestTest.java
@@ -18,22 +18,14 @@
 
 package org.apache.ambari.server.controller;
 
-import static org.apache.ambari.server.controller.AddServiceRequest.COMPONENTS;
 import static org.apache.ambari.server.controller.AddServiceRequest.Component;
 import static org.apache.ambari.server.controller.AddServiceRequest.OperationType.ADD_SERVICE;
-import static org.apache.ambari.server.controller.AddServiceRequest.SERVICES;
-import static org.apache.ambari.server.controller.AddServiceRequest.STACK_NAME;
-import static org.apache.ambari.server.controller.AddServiceRequest.STACK_VERSION;
 import static org.apache.ambari.server.controller.AddServiceRequest.Service;
-import static org.apache.ambari.server.controller.internal.BaseClusterRequest.PROVISION_ACTION_PROPERTY;
 import static org.apache.ambari.server.controller.internal.ProvisionAction.INSTALL_AND_START;
 import static org.apache.ambari.server.controller.internal.ProvisionAction.INSTALL_ONLY;
-import static org.apache.ambari.server.controller.internal.ProvisionClusterRequest.CONFIG_RECOMMENDATION_STRATEGY;
-import static org.apache.ambari.server.controller.internal.ServiceResourceProvider.OPERATION_TYPE;
 import static org.apache.ambari.server.topology.ConfigRecommendationStrategy.ALWAYS_APPLY;
 import static org.apache.ambari.server.topology.ConfigRecommendationStrategy.NEVER_APPLY;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -44,21 +36,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import org.apache.ambari.server.controller.internal.ClusterResourceProvider;
-import org.apache.ambari.server.controller.internal.ProvisionAction;
 import org.apache.ambari.server.security.encryption.CredentialStoreType;
 import org.apache.ambari.server.state.SecurityType;
-import org.apache.ambari.server.topology.ConfigRecommendationStrategy;
-import org.apache.ambari.server.topology.Configurable;
 import org.apache.ambari.server.topology.Configuration;
 import org.apache.ambari.server.topology.Credential;
 import org.apache.ambari.server.topology.SecurityConfiguration;
-import org.apache.ambari.server.topology.SecurityConfigurationFactory;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -112,7 +98,7 @@ public class AddServiceRequestTest {
       configuration.getProperties());
 
     assertEquals(
-      ImmutableSet.of(Component.of("NIMBUS", "c7401.ambari.apache.org"), Component.of("BEACON_SERVER", "c7402.ambari.apache.org")),
+      ImmutableSet.of(Component.of("NIMBUS", "c7401.ambari.apache.org", "c7402.ambari.apache.org"), Component.of("BEACON_SERVER", "c7402.ambari.apache.org", "c7403.ambari.apache.org")),
       request.getComponents());
 
     assertEquals(
@@ -137,7 +123,7 @@ public class AddServiceRequestTest {
 
     // filled-out values
     assertEquals(
-      ImmutableSet.of(Component.of("NIMBUS", "c7401.ambari.apache.org"), Component.of("BEACON_SERVER", "c7402.ambari.apache.org")),
+      ImmutableSet.of(Component.of("NIMBUS", "c7401.ambari.apache.org", "c7402.ambari.apache.org"), Component.of("BEACON_SERVER", "c7402.ambari.apache.org", "c7403.ambari.apache.org")),
       request.getComponents());
 
     assertEquals(
@@ -187,7 +173,7 @@ public class AddServiceRequestTest {
 
     // filled-out values
     assertEquals(
-      ImmutableSet.of(Component.of("NIMBUS", "c7401.ambari.apache.org"), Component.of("BEACON_SERVER", "c7402.ambari.apache.org")),
+      ImmutableSet.of(Component.of("NIMBUS", "c7401.ambari.apache.org", "c7402.ambari.apache.org"), Component.of("BEACON_SERVER", "c7402.ambari.apache.org", "c7403.ambari.apache.org")),
       request.getComponents());
 
     // default / empty values
@@ -223,78 +209,27 @@ public class AddServiceRequestTest {
   @Test
   public void testSerialize_basic() throws Exception {
     AddServiceRequest request = mapper.readValue(REQUEST_ALL_FIELDS_SET, AddServiceRequest.class);
-
-    Map<String, ?> serialized = serialize(request);
-
-    assertEquals(AddServiceRequest.OperationType.ADD_SERVICE.name(), serialized.get(OPERATION_TYPE));
-    assertEquals(ConfigRecommendationStrategy.ALWAYS_APPLY.name(), serialized.get(CONFIG_RECOMMENDATION_STRATEGY));
-    assertEquals(ProvisionAction.INSTALL_ONLY.name(), serialized.get(PROVISION_ACTION_PROPERTY));
-    assertEquals("HDP", serialized.get(STACK_NAME));
-    assertEquals("3.0", serialized.get(STACK_VERSION));
-
-    assertEquals(
-      ImmutableSet.of(ImmutableMap.of(Service.NAME, "BEACON"), ImmutableMap.of(Service.NAME, "STORM")),
-      ImmutableSet.copyOf((List<String>) serialized.get(SERVICES)) );
-
-    assertEquals(
-      ImmutableSet.of(
-        ImmutableMap.of(Component.COMPONENT_NAME, "NIMBUS", Component.FQDN, "c7401.ambari.apache.org"),
-        ImmutableMap.of(Component.COMPONENT_NAME, "BEACON_SERVER", Component.FQDN, "c7402.ambari.apache.org")),
-      ImmutableSet.copyOf((List<String>) serialized.get(COMPONENTS)) );
-
-    assertEquals(
-      ImmutableList.of(
-        ImmutableMap.of(
-          "storm-site",
-          ImmutableMap.of(
-            "properties", ImmutableMap.of("ipc.client.connect.max.retries", "50"),
-            "properties_attributes", ImmutableMap.of("final", ImmutableMap.of("fs.defaultFS", "true"))
-          )
-        )
-      ),
-      serialized.get(Configurable.CONFIGURATIONS)
-    );
-
-    assertEquals(
-      ImmutableMap.of(
-        SecurityConfigurationFactory.TYPE_PROPERTY_ID, SecurityType.KERBEROS.name(),
-        SecurityConfigurationFactory.KERBEROS_DESCRIPTOR_PROPERTY_ID, KERBEROS_DESCRIPTOR1,
-        SecurityConfigurationFactory.KERBEROS_DESCRIPTOR_REFERENCE_PROPERTY_ID, "ref_to_kerb_desc"
-      ),
-      serialized.get(ClusterResourceProvider.SECURITY)
-    );
-
-    assertNull(serialized.get(ClusterResourceProvider.CREDENTIALS));
+    AddServiceRequest serialized = serialize(request);
+    assertEquals(request, serialized);
+    assertEquals(ImmutableMap.of(), serialized.getCredentials());
   }
 
   @Test
   public void testSerialize_EmptyOmitted() throws Exception {
     AddServiceRequest request = mapper.readValue(REQUEST_MINIMAL_SERVICES_ONLY, AddServiceRequest.class);
-    Map<String, ?> serialized = serialize(request);
-
-    assertEquals(AddServiceRequest.OperationType.ADD_SERVICE.name(), serialized.get(OPERATION_TYPE));
-    assertEquals(ProvisionAction.INSTALL_AND_START.name(), serialized.get(PROVISION_ACTION_PROPERTY));
-    assertEquals(
-      ImmutableSet.of(ImmutableMap.of(Service.NAME, "BEACON"), ImmutableMap.of(Service.NAME, "STORM")),
-      ImmutableSet.copyOf((List<String>) serialized.get(SERVICES)) );
-
-    assertFalse(serialized.containsKey(STACK_NAME));
-    assertFalse(serialized.containsKey(STACK_VERSION));
-    assertFalse(serialized.containsKey(Configurable.CONFIGURATIONS));
-    assertFalse(serialized.containsKey(COMPONENTS));
-
+    AddServiceRequest serialized = serialize(request);
+    assertEquals(request, serialized);
   }
 
-  private Map<String, ?> serialize(AddServiceRequest request) throws IOException {
+  private AddServiceRequest serialize(AddServiceRequest request) throws IOException {
     String serialized = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(request);
-    return mapper.readValue(serialized, new TypeReference<Map<String, ?>>() {});
+    return mapper.readValue(serialized, AddServiceRequest.class);
   }
 
   private static String read(String resourceName) {
     try {
       return Resources.toString(Resources.getResource(resourceName), StandardCharsets.UTF_8);
-    }
-    catch (IOException e) {
+    } catch (IOException e) {
       throw new UncheckedIOException(e);
     }
   }
