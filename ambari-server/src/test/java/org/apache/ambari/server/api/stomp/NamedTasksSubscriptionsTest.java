742d92472cfb331dee8f40c0163eaee3c369195e
Myroslav Papirkovskyi <mpapirkovskyy@apache.org>
2018-12-28 16:59:27 +0200

    AMBARI-24974. Sometimes Task Log is not refreshed in UI after operation completes. (mpapirkovskyy)

--- ambari-server/src/test/java/org/apache/ambari/server/api/stomp/NamedTasksSubscriptionsTest.java
+++ ambari-server/src/test/java/org/apache/ambari/server/api/stomp/NamedTasksSubscriptionsTest.java
@@ -22,7 +22,6 @@ import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
