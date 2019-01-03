894381cb259434207a804a65a50c0bd99c1bf699
Andrii Tkach <atkach@apache.org>
2019-01-03 13:41:47 +0200

    AMBARI-25082 Cover HA wizard controller with unit tests

--- ambari-web/app/assets/test/tests.js
+++ ambari-web/app/assets/test/tests.js
@@ -91,6 +91,7 @@ var files = [
   'test/controllers/main/admin/highAvailability/resourceManager/wizard_controller_test',
   'test/controllers/main/admin/highAvailability/hawq/addStandby/step3_controller_test',
   'test/controllers/main/admin/highAvailability/hawq/addStandby/step4_controller_test',
+  'test/controllers/main/admin/highAvailability/hawq/addStandby/wizard_controller_test',
   'test/controllers/main/admin/highAvailability/hawq/removeStandby/step2_controller_test',
   'test/controllers/main/admin/highAvailability/hawq/activateStandby/step2_controller_test',
   'test/controllers/main/admin/highAvailability/hawq/activateStandby/step3_controller_test',
