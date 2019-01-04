ecce21178c9f3612dca8354b9874e16ec7702078
Attila Magyar <m.magyar3@gmail.com>
2019-01-04 09:58:28 +0100

    AMBARI-25084. Delete identities fails when removing service in reverse order (amagyar) (#2751)

--- ambari-server/src/main/java/org/apache/ambari/server/orm/dao/ClusterDAO.java
+++ ambari-server/src/main/java/org/apache/ambari/server/orm/dao/ClusterDAO.java
@@ -19,6 +19,7 @@
 package org.apache.ambari.server.orm.dao;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -223,6 +224,9 @@ public class ClusterDAO {
   @RequiresSession
   public List<ClusterConfigEntity> getLatestConfigurationsWithTypes(long clusterId, StackId stackId, Collection<String> configTypes) {
     StackEntity stackEntity = stackDAO.find(stackId.getStackName(), stackId.getStackVersion());
+    if (configTypes.isEmpty()) {
+      return Collections.emptyList();
+    }
     return daoUtils.selectList(
       entityManagerProvider.get()
       .createNamedQuery("ClusterConfigEntity.findLatestConfigsByStackWithTypes", ClusterConfigEntity.class)
