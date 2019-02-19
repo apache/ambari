8d796f8d7a72aa97be46a5cd123f5845d8c20723
Alex Antonenko <aantonenko@hortonworks.com>
2019-02-19 16:20:40 +0300

    AMBARI-25157. Cover Overridden Row View

--- ambari-web/test/views/common/configs/overriddenProperty_view_test.js
+++ ambari-web/test/views/common/configs/overriddenProperty_view_test.js
@@ -21,7 +21,10 @@ require('views/common/configs/overriddenProperty_view');
 
 describe('App.ServiceConfigView.SCPOverriddenRowsView', function () {
 
-  var view = App.ServiceConfigView.SCPOverriddenRowsView.create();
+  var view;
+  beforeEach(function () {
+    view = App.ServiceConfigView.SCPOverriddenRowsView.create();
+  });
 
   describe('#didInsertElement', function () {
 
@@ -36,6 +39,121 @@ describe('App.ServiceConfigView.SCPOverriddenRowsView', function () {
     it('setSwitchLinks method should be executed', function () {
       view.didInsertElement();
     });
+  });
 
+  describe('#toggleFinalFlag', function () {
+    it('should not change anyting if isNotEditable', function () {
+      var e = {
+        contexts: [Em.Object.create({isNotEditable: true, isFinal: false})]
+      };
+      view.toggleFinalFlag(e);
+      expect(e.contexts[0].get('isFinal')).to.be.false;
+    });
+
+    it('should change isFinal if isNotEditable is false', function () {
+      var e = {
+        contexts: [Em.Object.create({isNotEditable: false, isFinal: false})]
+      };
+      view.toggleFinalFlag(e);
+      expect(e.contexts[0].get('isFinal')).to.be.true;
+    });
+  });
+
+  describe('#removeOverride', function () {
+    it('should call removeObject if controller name is wizardStep7Controller', function () {
+      var e = {
+        contexts: [Em.Object.create({isNotEditable: true, isFinal: false})]
+      };
+      var properties = {
+        removeObject: function () {}
+      };
+      sinon.stub(properties, 'removeObject');
+      var controller = Em.Object.create({
+        name: 'wizardStep7Controller',
+        selectedConfigGroup: {name: 'test'},
+        selectedService: {configGroups: [Em.Object.create({name: 'test', properties: properties})]}
+      });
+      view.set('controller', controller);
+      view.set('serviceConfigProperty', Em.Object.create({overrides: null}));
+      view.removeOverride(e);
+      expect(properties.removeObject.calledOnce).to.be.true;
+      properties.removeObject.restore();
+    });
+
+    it('should not call removeObject if controller name is not wizardStep7Controller', function () {
+      var e = {
+        contexts: [Em.Object.create({isNotEditable: true, isFinal: false})]
+      };
+      var properties = {
+        removeObject: function () {}
+      };
+      sinon.stub(properties, 'removeObject');
+      var controller = Em.Object.create({
+        name: 'test',
+        selectedConfigGroup: {name: 'test'},
+        selectedService: {configGroups: [Em.Object.create({name: 'test', properties: properties})]}
+      });
+      view.set('controller', controller);
+      view.set('serviceConfigProperty', Em.Object.create({overrides: null}));
+      view.removeOverride(e);
+      expect(properties.removeObject.calledOnce).to.be.false;
+      properties.removeObject.restore();
+    });
+
+    it('should reset overrides property if overrides is defined', function () {
+      var override = Em.Object.create({isNotEditable: true, isFinal: false});
+      var e = {
+        contexts: [override]
+      };
+      var properties = {
+        removeObject: function () {}
+      };
+      var controller = Em.Object.create({
+        name: 'test',
+        selectedConfigGroup: {name: 'test'},
+        selectedService: {configGroups: [Em.Object.create({name: 'test', properties: properties})]}
+      });
+      view.set('controller', controller);
+      view.set('serviceConfigProperty', Em.Object.create({overrides: [override, Em.Object.create({})]}));
+      view.removeOverride(e);
+      expect(view.get('serviceConfigProperty.overrides').length).to.be.equal(1);
+    });
+
+    it('should reset overrides property if overrides is defined', function () {
+      var serviceConfigs = {
+        removeObject: function () {}
+      };
+      var categoryConfigsAll = {
+        removeObject: function () {}
+      };
+      sinon.stub(serviceConfigs, 'removeObject');
+      sinon.stub(categoryConfigsAll, 'removeObject');
+      var parentView = Em.Object.create({
+        serviceConfigs: serviceConfigs,
+        categoryConfigsAll: categoryConfigsAll
+      });
+      var view = App.ServiceConfigView.SCPOverriddenRowsView.create({parentView: parentView});
+      var override = Em.Object.create({isNotEditable: true, isFinal: false});
+      var e = {
+        contexts: [override]
+      };
+      var properties = {
+        removeObject: function () {}
+      };
+      var controller = Em.Object.create({
+        name: 'test',
+        selectedConfigGroup: {name: 'test'},
+        selectedService: {configGroups: [Em.Object.create({name: 'test', properties: properties})]}
+      });
+      view.set('controller', controller);
+      view.set('serviceConfigProperty', Em.Object.create({overrides: null, isUserProperty: true}));
+
+      view.set('parentView', parentView);
+      view.removeOverride(e);
+      expect(categoryConfigsAll.removeObject.calledOnce).to.be.true;
+      expect(serviceConfigs.removeObject.calledOnce).to.be.true;
+      categoryConfigsAll.removeObject.restore();
+      serviceConfigs.removeObject.restore();
+    });
   });
 });
