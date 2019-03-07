7ba01ea442b93f871c48eade314c693cb7d230ca
Alex Antonenko <aantonenko@hortonworks.com>
2019-03-07 14:24:56 +0300

    AMBARI-25192. Add 'hadoop.kms.authentication.zk-dt-secret-manager.enable' property during multiple Ranger KMS

--- ambari-web/test/controllers/main/host/details_test.js
+++ ambari-web/test/controllers/main/host/details_test.js
@@ -3445,6 +3445,7 @@ describe('App.MainHostDetailsController', function () {
             properties: {
               'kms-site': {
                 'hadoop.kms.cache.enable': 'true',
+                'hadoop.kms.authentication.zk-dt-secret-manager.enable': 'false',
                 'hadoop.kms.cache.timeout.ms': '600000',
                 'hadoop.kms.current.key.cache.timeout.ms': '30000',
                 'hadoop.kms.authentication.signer.secret.provider': 'random',
@@ -3478,6 +3479,7 @@ describe('App.MainHostDetailsController', function () {
             properties: {
               'kms-site': {
                 'hadoop.kms.cache.enable': 'false',
+                'hadoop.kms.authentication.zk-dt-secret-manager.enable': 'true',
                 'hadoop.kms.cache.timeout.ms': '0',
                 'hadoop.kms.current.key.cache.timeout.ms': '0',
                 'hadoop.kms.authentication.signer.secret.provider': 'zookeeper',
