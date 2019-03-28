ba58df239c1ee196661c67f309e294d43f93506b
AMARNATH REDDY <apappu@hortonworks.com>
2019-03-28 14:48:26 -0700

    [AMBARI-25124] ambari-audit.log entries span multiple lines (apappu)

--- ambari-server/src/main/java/org/apache/ambari/server/audit/event/request/ClusterPrivilegeChangeRequestAuditEvent.java
+++ ambari-server/src/main/java/org/apache/ambari/server/audit/event/request/ClusterPrivilegeChangeRequestAuditEvent.java
@@ -80,9 +80,7 @@ public class ClusterPrivilegeChangeRequestAuditEvent extends RequestAuditEvent {
       roleSet.addAll(roles.keySet());
 
       builder.append(", Roles(");
-      if (!users.isEmpty() || !groups.isEmpty()|| !roles.isEmpty()) {
-        builder.append(System.lineSeparator());
-      }
+
 
       List<String> lines = new LinkedList<>();
 
@@ -99,7 +97,7 @@ public class ClusterPrivilegeChangeRequestAuditEvent extends RequestAuditEvent {
         }
       }
 
-      builder.append(StringUtils.join(lines, System.lineSeparator()));
+      builder.append(StringUtils.join(lines, " , "));
 
       builder.append(")");
     }
