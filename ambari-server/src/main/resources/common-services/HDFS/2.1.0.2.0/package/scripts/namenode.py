ba5349a0db9ea96bec22b24bd35f39269f1b7c14
Viraj Jasani <vjasani@salesforce.com>
2019-05-12 22:31:03 +0530

    [AMBARI-25220] : NameNode to provide rack topology

--- ambari-server/src/main/resources/common-services/HDFS/2.1.0.2.0/package/scripts/namenode.py
+++ ambari-server/src/main/resources/common-services/HDFS/2.1.0.2.0/package/scripts/namenode.py
@@ -169,6 +169,16 @@ class NameNode(Script):
     hdfs_binary = self.get_hdfs_binary()
     namenode(action="decommission", hdfs_binary=hdfs_binary)
 
+  def print_topology(self, env):
+    import params
+    env.set_params(params)
+    Execute(format("hdfs dfsadmin -printTopology"),
+            user=params.hdfs_user,
+            path=[params.hadoop_bin_dir],
+            logoutput=True
+            )
+
+
 @OsFamilyImpl(os_family=OsFamilyImpl.DEFAULT)
 class NameNodeDefault(NameNode):
 
