616d19716a999312d6d4a47bcfc9935f4e9e6efe
Viraj Jasani <vjasani@salesforce.com>
2019-07-16 02:37:26 +0530

    AMBARI-25336 : Perf improvement with Collections usages over iterations (#3048)

--- ambari-server/src/main/java/org/apache/ambari/server/actionmanager/ActionDBAccessorImpl.java
+++ ambari-server/src/main/java/org/apache/ambari/server/actionmanager/ActionDBAccessorImpl.java
@@ -20,7 +20,6 @@ package org.apache.ambari.server.actionmanager;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -736,14 +735,13 @@ public class ActionDBAccessorImpl implements ActionDBAccessor {
       return Collections.emptyList();
     }
 
-    List<HostRoleCommand> commands = new ArrayList<>();
+    List<HostRoleCommand> commands;
     try {
       hrcOperationsLock.readLock().lock();
       Map<Long, HostRoleCommand> cached = hostRoleCommandCache.getAllPresent(taskIds);
-      commands.addAll(cached.values());
+      commands = new ArrayList<>(cached.values());
 
-      List<Long> absent = new ArrayList<>();
-      absent.addAll(taskIds);
+      List<Long> absent = new ArrayList<>(taskIds);
       absent.removeAll(cached.keySet());
 
       if (!absent.isEmpty()) {
@@ -753,12 +751,7 @@ public class ActionDBAccessorImpl implements ActionDBAccessor {
           cacheHostRoleCommand(hostRoleCommand);
         }
       }
-      Collections.sort(commands, new Comparator<HostRoleCommand>() {
-        @Override
-        public int compare(HostRoleCommand o1, HostRoleCommand o2) {
-          return (int) (o1.getTaskId()-o2.getTaskId());
-        }
-      });
+      commands.sort((o1, o2) -> (int) (o1.getTaskId() - o2.getTaskId()));
     } finally {
       hrcOperationsLock.readLock().unlock();
     }
