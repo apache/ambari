5ea6ec295d040a4b7db19e3f889b073329dcb3fa
Szilárd Antal <santal@cloudera.com>
2020-08-10 05:25:54 +0200

    AMBARI-25353 Seeing an error stack when running an API call against Ambari server (santal via dgrinenko) (#3208)

--- ambari-server/src/main/java/org/apache/ambari/server/configuration/Configuration.java
+++ ambari-server/src/main/java/org/apache/ambari/server/configuration/Configuration.java
@@ -2644,6 +2644,13 @@ public class Configuration {
   public static final ConfigurationProperty<Integer> KERBEROS_SERVER_ACTION_THREADPOOL_SIZE = new ConfigurationProperty<>(
     "server.kerberos.action.threadpool.size", 1);
 
+  /**
+   * A flag to determine whether error stacks appear on the error page
+   */
+  @Markdown(description = "Show or hide the error stacks on the error page")
+  public static final ConfigurationProperty<String> SERVER_SHOW_ERROR_STACKS = new ConfigurationProperty<>(
+    "server.show.error.stacks", "false");
+
   private static final Logger LOG = LoggerFactory.getLogger(
     Configuration.class);
 
@@ -6151,5 +6158,14 @@ public class Configuration {
 
   public int getAlertServiceCorePoolSize() {
     return Integer.parseInt(getProperty(SERVER_SIDE_ALERTS_CORE_POOL_SIZE));
+  }
+
+  /**
+   * Determines whether error stacks appear on the error page
+   *
+   * @return true if error stacks appear on the error page (defaults to {@code false})
+   */
+  public boolean isServerShowErrorStacks() {
+    return Boolean.parseBoolean(getProperty(SERVER_SHOW_ERROR_STACKS));
   }
 }
