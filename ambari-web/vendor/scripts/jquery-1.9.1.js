b3cdb14b0078f7271d411d2ee631a60dafaea4a4
pidhaietsdp <62650175+pidhaietsdp@users.noreply.github.com>
2020-11-10 14:03:31 +0200

    AMBARI-25581. Prototype pollution issue in JQuery (dpidhaiets via dgrinenko) (#3258)

--- ambari-web/vendor/scripts/jquery-1.9.1.js
+++ ambari-web/vendor/scripts/jquery-1.9.1.js
@@ -350,7 +350,7 @@
           copy = options[ name ];
 
           // Prevent never-ending loop
-          if ( target === copy ) {
+          if ( name === "__proto__" || target === copy ) {
             continue;
           }
 
