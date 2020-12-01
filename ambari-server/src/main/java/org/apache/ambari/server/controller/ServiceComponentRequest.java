1aaa621bf190e0a17d143e6e550356edf83e21b2
taoyuxin <taoyuxin@inspur.com>
2020-12-01 15:29:28 +0800

    Fix a parameter name error

--- ambari-server/src/main/java/org/apache/ambari/server/controller/ServiceComponentRequest.java
+++ ambari-server/src/main/java/org/apache/ambari/server/controller/ServiceComponentRequest.java
@@ -137,6 +137,6 @@ public class ServiceComponentRequest {
   @Override
   public String toString() {
     return String.format("[clusterName=%s, serviceName=%s, componentName=%s, desiredState=%s, recoveryEnabled=%s, componentCategory=%s]",
-        clusterName, serviceName, clusterName, desiredState, recoveryEnabled, componentCategory);
+        clusterName, serviceName, componentName, desiredState, recoveryEnabled, componentCategory);
   }
 }
