1aa4a456577e4c6bd9fb613aa60bd5b19784a1f5
taoyuxin <taoyuxin@inspur.com>
2020-12-02 08:58:03 +0800

    AMBARI-25598. Unit Tests(#3269)(THOMAS TAO)

--- ambari-server/src/main/java/org/apache/ambari/server/controller/ServiceComponentRequest.java
+++ ambari-server/src/main/java/org/apache/ambari/server/controller/ServiceComponentRequest.java
@@ -21,122 +21,122 @@ package org.apache.ambari.server.controller;
 
 public class ServiceComponentRequest {
 
-  private String clusterName; // REF
+    private String clusterName; // REF
 
-  private String serviceName; // GET/CREATE/UPDATE/DELETE
+    private String serviceName; // GET/CREATE/UPDATE/DELETE
 
-  private String componentName; // GET/CREATE/UPDATE/DELETE
+    private String componentName; // GET/CREATE/UPDATE/DELETE
 
-  private String desiredState; // CREATE/UPDATE
+    private String desiredState; // CREATE/UPDATE
 
-  private String componentCategory;
+    private String componentCategory;
 
-  private String recoveryEnabled; // CREATE/UPDATE
+    private String recoveryEnabled; // CREATE/UPDATE
 
-  public ServiceComponentRequest(String clusterName, String serviceName,
-                                 String componentName, String desiredState) {
-    this(clusterName, serviceName, componentName, desiredState, null, null);
-  }
+    public ServiceComponentRequest(String clusterName, String serviceName,
+                                   String componentName, String desiredState) {
+        this(clusterName, serviceName, componentName, desiredState, null, null);
+    }
 
-  public ServiceComponentRequest(String clusterName, String serviceName,
-                                 String componentName, String desiredState,
-                                 String recoveryEnabled) {
-    this(clusterName, serviceName, componentName, desiredState, recoveryEnabled, null);
-  }
+    public ServiceComponentRequest(String clusterName, String serviceName,
+                                   String componentName, String desiredState,
+                                   String recoveryEnabled) {
+        this(clusterName, serviceName, componentName, desiredState, recoveryEnabled, null);
+    }
 
-  public ServiceComponentRequest(String clusterName,
-                                 String serviceName, String componentName,
-                                 String desiredState, String recoveryEnabled,
-                                 String componentCategory) {
-    this.clusterName = clusterName;
-    this.serviceName = serviceName;
-    this.componentName = componentName;
-    this.desiredState = desiredState;
-    this.recoveryEnabled = recoveryEnabled;
-    this.componentCategory = componentCategory;
-  }
+    public ServiceComponentRequest(String clusterName,
+                                   String serviceName, String componentName,
+                                   String desiredState, String recoveryEnabled,
+                                   String componentCategory) {
+        this.clusterName = clusterName;
+        this.serviceName = serviceName;
+        this.componentName = componentName;
+        this.desiredState = desiredState;
+        this.recoveryEnabled = recoveryEnabled;
+        this.componentCategory = componentCategory;
+    }
 
-  /**
-   * @return the serviceName
-   */
-  public String getServiceName() {
-    return serviceName;
-  }
+    /**
+     * @return the serviceName
+     */
+    public String getServiceName() {
+        return serviceName;
+    }
 
-  /**
-   * @param serviceName the serviceName to set
-   */
-  public void setServiceName(String serviceName) {
-    this.serviceName = serviceName;
-  }
+    /**
+     * @param serviceName the serviceName to set
+     */
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
 
-  /**
-   * @return the componentName
-   */
-  public String getComponentName() {
-    return componentName;
-  }
+    /**
+     * @return the componentName
+     */
+    public String getComponentName() {
+        return componentName;
+    }
 
-  /**
-   * @param componentName the componentName to set
-   */
-  public void setComponentName(String componentName) {
-    this.componentName = componentName;
-  }
+    /**
+     * @param componentName the componentName to set
+     */
+    public void setComponentName(String componentName) {
+        this.componentName = componentName;
+    }
 
-  /**
-   * @return the desiredState
-   */
-  public String getDesiredState() {
-    return desiredState;
-  }
+    /**
+     * @return the desiredState
+     */
+    public String getDesiredState() {
+        return desiredState;
+    }
 
-  /**
-   * @param desiredState the desiredState to set
-   */
-  public void setDesiredState(String desiredState) {
-    this.desiredState = desiredState;
-  }
+    /**
+     * @param desiredState the desiredState to set
+     */
+    public void setDesiredState(String desiredState) {
+        this.desiredState = desiredState;
+    }
 
-  /**
-   * @return the clusterName
-   */
-  public String getClusterName() {
-    return clusterName;
-  }
+    /**
+     * @return the clusterName
+     */
+    public String getClusterName() {
+        return clusterName;
+    }
 
-  /**
-   * @param clusterName the clusterName to set
-   */
-  public void setClusterName(String clusterName) {
-    this.clusterName = clusterName;
-  }
+    /**
+     * @param clusterName the clusterName to set
+     */
+    public void setClusterName(String clusterName) {
+        this.clusterName = clusterName;
+    }
 
-  /**
-   * @return recoveryEnabled
-   */
-  public String getRecoveryEnabled() {
-    return recoveryEnabled;
-  }
+    /**
+     * @return recoveryEnabled
+     */
+    public String getRecoveryEnabled() {
+        return recoveryEnabled;
+    }
 
-  /**
-   * @param recoveryEnabled the recoveryEnabled value to set.
-   */
-  public void setRecoveryEnabled(String recoveryEnabled) {
-    this.recoveryEnabled = recoveryEnabled;
-  }
+    /**
+     * @param recoveryEnabled the recoveryEnabled value to set.
+     */
+    public void setRecoveryEnabled(String recoveryEnabled) {
+        this.recoveryEnabled = recoveryEnabled;
+    }
 
-  public String getComponentCategory() {
-    return componentCategory;
-  }
+    public String getComponentCategory() {
+        return componentCategory;
+    }
 
-  public void setComponentCategory(String componentCategory) {
-    this.componentCategory = componentCategory;
-  }
+    public void setComponentCategory(String componentCategory) {
+        this.componentCategory = componentCategory;
+    }
 
-  @Override
-  public String toString() {
-    return String.format("[clusterName=%s, serviceName=%s, componentName=%s, desiredState=%s, recoveryEnabled=%s, componentCategory=%s]",
-        clusterName, serviceName, componentName, desiredState, recoveryEnabled, componentCategory);
-  }
+    @Override
+    public String toString() {
+        return String.format("[clusterName=%s, serviceName=%s, componentName=%s, desiredState=%s, recoveryEnabled=%s, componentCategory=%s]",
+                clusterName, serviceName, componentName, desiredState, recoveryEnabled, componentCategory);
+    }
 }
