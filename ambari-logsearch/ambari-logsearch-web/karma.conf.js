850128dce1f5ab8d86e405a0ec58397116dcac51
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2021-05-25 10:30:25 +0300

    AMBARI-25607 Update node.js and yarn

--- ambari-logsearch/ambari-logsearch-web/karma.conf.js
+++ ambari-logsearch/ambari-logsearch-web/karma.conf.js
@@ -19,8 +19,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-process.env.CHROME_BIN = require('puppeteer').executablePath()
-
 module.exports = function (config) {
   config.set({
     basePath: '',
