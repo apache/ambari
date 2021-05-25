e1ef44218689da48217f0b4df87016a795fb7c29
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2021-05-25 10:22:34 +0300

    AMBARI-25607 Replace PhantomJS with headless Chromium in logsearch

--- ambari-logsearch/ambari-logsearch-web/karma.conf.js
+++ ambari-logsearch/ambari-logsearch-web/karma.conf.js
@@ -19,13 +19,15 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -56,7 +58,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['PhantomJS'],
+    browsers: ['ChromiumHeadless'],
     singleRun: true
   });
 };
