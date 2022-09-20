1c5835016f1835f2b9042887f66c6573bd8ac2bd
Zhiguo Wu <wuzhiguo@apache.org>
2022-09-20 10:39:27 +0800

    update scripts

--- dev-support/docker/centos7/distribute-scripts.sh
+++ dev-support/docker/centos7/distribute-scripts.sh
@@ -17,19 +17,19 @@
 
 echo -e "\033[32mSynchronizing script to ambari-server\033[0m"
 docker exec ambari-server bash -c "mkdir -p /var/lib/ambari-agent/cache/stacks/BIGTOP/3.2.0/services/"
-docker cp ../../../ambari-server/src/main/resources/stacks/BIGTOP/3.2.0/services/ ambari-server:/var/lib/ambari-agent/cache/stacks/BIGTOP/3.2.0/
-docker cp ../../../ambari-server/src/main/resources/stacks/BIGTOP/3.2.0/services/ ambari-server:/var/lib/ambari-server/resources/stacks/BIGTOP/3.2.0/
+docker cp ../../../ambari-server/src/main/resources/stacks/ ambari-server:/var/lib/ambari-agent/cache/
+docker cp ../../../ambari-server/src/main/resources/stacks/ ambari-server:/var/lib/ambari-server/resources/
 docker exec ambari-server bash -c "ambari-server restart --debug"
 docker exec ambari-server bash -c "ambari-agent restart"
 
 echo -e "\033[32mSynchronizing script to ambari-agent-01\033[0m"
 docker exec ambari-agent-01 bash -c "mkdir -p /var/lib/ambari-agent/cache/stacks/BIGTOP/3.2.0/services/"
-docker cp ../../../ambari-server/src/main/resources/stacks/BIGTOP/3.2.0/services/ ambari-agent-01:/var/lib/ambari-agent/cache/stacks/BIGTOP/3.2.0/
+docker cp ../../../ambari-server/src/main/resources/stacks/ ambari-agent-01:/var/lib/ambari-agent/cache/
 docker exec ambari-agent-01 bash -c "ambari-agent restart"
 
 echo -e "\033[32mSynchronizing script to ambari-agent-02\033[0m"
 docker exec ambari-agent-02 bash -c "mkdir -p /var/lib/ambari-agent/cache/stacks/BIGTOP/3.2.0/services/"
-docker cp ../../../ambari-server/src/main/resources/stacks/BIGTOP/3.2.0/services/ ambari-agent-02:/var/lib/ambari-agent/cache/stacks/BIGTOP/3.2.0/
+docker cp ../../../ambari-server/src/main/resources/stacks/ ambari-agent-02:/var/lib/ambari-agent/cache/
 docker exec ambari-agent-02 bash -c "ambari-agent restart"
 
 echo -e "\033[32mDone!\033[0m"
