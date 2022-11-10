f63a80e062c6afb8ed20ba183c9b3c67b914541d
houyu <524860213@qq.com>
2022-11-11 01:00:19 +0800

    fix execute ntp check failed

--- ambari-agent/src/main/python/ambari_agent/HostInfo.py
+++ ambari-agent/src/main/python/ambari_agent/HostInfo.py
@@ -371,10 +371,12 @@ class HostInfoLinux(HostInfo):
     pass
 
   def getServiceStatus(self, service_name):
-    service_check_live = list(self.SERVICE_STATUS_CMD_LIST)
     if OSCheck.is_redhat_family() and int(OSCheck.get_os_major_version()) >= 7:
       service_check_live = list(self.REDHAT7_SERVICE_STATUS_CMD_LIST)
-    service_check_live[1] = service_name
+      service_check_live[2] = service_name
+    else:
+      service_check_live = list(self.SERVICE_STATUS_CMD_LIST)
+      service_check_live[1] = service_name
     try:
       code, out, err = shell.call(service_check_live, stdout = subprocess32.PIPE, stderr = subprocess32.PIPE, timeout = 5, quiet = True)
       return out, err, code
