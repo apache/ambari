7cf0881f319986dbd3eb5002c441e370647a6af5
Zhiguo Wu <wuzhiguo@apache.org>
2022-11-26 22:04:24 +0800

    AMBARI-25363: Daily Namenode Heap Usage alert does not work

--- ambari-server/src/main/java/org/apache/ambari/server/upgrade/UpgradeCatalog274.java
+++ ambari-server/src/main/java/org/apache/ambari/server/upgrade/UpgradeCatalog274.java
@@ -18,12 +18,25 @@
 package org.apache.ambari.server.upgrade;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 import javax.persistence.Table;
 
 import org.apache.ambari.server.AmbariException;
+import org.apache.ambari.server.controller.AmbariManagementController;
 import org.apache.ambari.server.orm.DBAccessor;
+import org.apache.ambari.server.orm.dao.AlertDefinitionDAO;
+import org.apache.ambari.server.orm.entities.AlertDefinitionEntity;
 import org.apache.ambari.server.orm.entities.AmbariConfigurationEntity;
+import org.apache.ambari.server.state.Cluster;
+import org.apache.ambari.server.state.Clusters;
+import org.apache.ambari.server.state.alert.AlertDefinition;
+import org.apache.ambari.server.state.alert.AlertDefinitionFactory;
+import org.apache.ambari.server.state.alert.ParameterizedSource;
+import org.apache.ambari.server.state.alert.ScriptSource;
+import org.apache.ambari.server.state.alert.SourceType;
+import org.apache.commons.collections.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -39,6 +52,11 @@ public class UpgradeCatalog274 extends AbstractUpgradeCatalog {
   static final String AMBARI_CONFIGURATION_TABLE = AmbariConfigurationEntity.class.getAnnotation(Table.class).name();
   static final String AMBARI_CONFIGURATION_PROPERTY_VALUE_COLUMN = UpgradeCatalog270.AMBARI_CONFIGURATION_PROPERTY_VALUE_COLUMN;
   static final Integer AMBARI_CONFIGURATION_PROPERTY_VALUE_COLUMN_LEN = 4000;
+
+  static final String HDFS_SERVICE_NAME = "HDFS";
+  static final String NAMENODE_COMPONENT_NAME = "NAMENODE";
+  static final String APPID_PROPERTY_NAME = "appId";
+  static final String NAMENODE_APP_ID = NAMENODE_COMPONENT_NAME.toLowerCase();
 
 
   @Inject
@@ -87,6 +105,42 @@ public class UpgradeCatalog274 extends AbstractUpgradeCatalog {
   @Override
   protected void executeDMLUpdates() throws AmbariException, SQLException {
     addNewConfigurationsFromXml();
+    updateNameNodeAlertsAppId();
+  }
+
+  protected void updateNameNodeAlertsAppId() {
+    AlertDefinitionDAO alertDefinitionDAO = injector.getInstance(AlertDefinitionDAO.class);
+    AmbariManagementController ambariManagementController = injector.getInstance(AmbariManagementController.class);
+    AlertDefinitionFactory alertDefinitionFactory = injector.getInstance(AlertDefinitionFactory.class);
+
+    Clusters clusters = ambariManagementController.getClusters();
+    if (clusters != null) {
+      Map<String, Cluster> clusterMap = clusters.getClusters();
+      if (MapUtils.isNotEmpty(clusterMap)) {
+        for (final Cluster cluster : clusterMap.values()) {
+          if (cluster.getServices().containsKey(HDFS_SERVICE_NAME)) {
+            List<AlertDefinitionEntity> alertDefinitions =
+                alertDefinitionDAO.findByServiceComponent(cluster.getClusterId(), HDFS_SERVICE_NAME, NAMENODE_COMPONENT_NAME);
+            for (AlertDefinitionEntity alertDefinitionEntity : alertDefinitions) {
+              if (SourceType.SCRIPT.equals(alertDefinitionEntity.getSourceType())) {
+                AlertDefinition databaseDefinition = alertDefinitionFactory.coerce(alertDefinitionEntity);
+                ScriptSource scriptSource = (ScriptSource) databaseDefinition.getSource();
+                for (ParameterizedSource.AlertParameter scriptParameter : scriptSource.getParameters()) {
+                  if (APPID_PROPERTY_NAME.equals(scriptParameter.getName())) {
+                    String value = (String) scriptParameter.getValue();
+                    if (value.equalsIgnoreCase(NAMENODE_APP_ID) && !value.equals(NAMENODE_APP_ID)) {
+                      scriptParameter.setValue(NAMENODE_APP_ID);
+                    }
+                  }
+                }
+                alertDefinitionEntity = alertDefinitionFactory.mergeSource(scriptSource, alertDefinitionEntity);
+                alertDefinitionDAO.merge(alertDefinitionEntity);
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
 
