521cbbdb93c7d5f0dd93ef1f5ab357df8d12bb71
Mohammad Arshad <arshad@apache.org>
2024-01-11 20:05:10 +0530

    AMBARI-26032: WidgetResourceProvider wrong import order causing compilation failure (#3765)
    Signed-off-by: Brahma Reddy Battula <brahma@apache.org>

--- ambari-server/src/main/java/org/apache/ambari/server/controller/internal/WidgetResourceProvider.java
+++ ambari-server/src/main/java/org/apache/ambari/server/controller/internal/WidgetResourceProvider.java
@@ -54,10 +54,10 @@ import org.springframework.security.access.AccessDeniedException;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSerializer;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import com.google.inject.Inject;
 
 /**
