eb8cc76c77a7e5ab21f9a386951408de9eac3441
jialiang <jialiangcaimd@gmail.com>
2024-02-18 10:22:26 +0800

    AMBARI-26049: Fix type error in ambari port alert

--- ambari-agent/src/main/python/ambari_agent/alerts/port_alert.py
+++ ambari-agent/src/main/python/ambari_agent/alerts/port_alert.py
@@ -154,8 +154,8 @@ class PortAlert(BaseAlert):
         start_time = time.time()
         s.connect((host, port))
         if self.socket_command is not None:
-          s.sendall(self.socket_command)
-          data = s.recv(1024)
+          s.sendall(self.socket_command.encode())
+          data = s.recv(1024).decode()
           if self.socket_command_response is not None and data != self.socket_command_response:
             raise Exception("Expected response {0}, Actual response {1}".format(
               self.socket_command_response, data))
