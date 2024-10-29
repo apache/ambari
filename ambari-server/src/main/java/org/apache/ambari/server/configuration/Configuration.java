f51f4d2059a09584cbbd092dac32f6c2cc1f08e5
coldless177 <baishangxia707@gmail.com>
2024-10-29 14:52:59 +0800

    AMBARI-26212: Fix checkstyle error (#3862)

--- ambari-server/src/main/java/org/apache/ambari/server/configuration/Configuration.java
+++ ambari-server/src/main/java/org/apache/ambari/server/configuration/Configuration.java
@@ -53,8 +53,6 @@ import org.apache.ambari.annotations.ConfigurationMarkdown;
 import org.apache.ambari.annotations.Experimental;
 import org.apache.ambari.annotations.ExperimentalFeature;
 import org.apache.ambari.annotations.Markdown;
-import org.apache.ambari.annotations.ConfigurationMarkdown;
-import org.apache.ambari.annotations.ClusterScale;
 import org.apache.ambari.server.AmbariException;
 import org.apache.ambari.server.actionmanager.CommandExecutionType;
 import org.apache.ambari.server.actionmanager.HostRoleCommand;
