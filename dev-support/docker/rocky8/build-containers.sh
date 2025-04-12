18087bc806e7edf737bd50f4bfc957a22b8d2a1e
Wei-Chiu Chuang <weichiu@apache.org>
2025-04-11 19:28:10 -0700

    AMBARI-26439: Add Rocky 8 Dockerfile for development.
    Update CentOS 7
    
    Remove temp files.
    
    Restore centos7 change.
    
    Workaround: mariadb can't be started by systemctl in docker.
    Use another docker container to execute mariadb.
    
    Update other scripts
    
    Fix ambari-agent permission
    
    Open up Ambari Server port 8440 to allow connection from Ambari agent.
    
    Generate ssh private/public key in the old PEM format.
    
    Starting from CentOS 7.8, ssh-keygen defaults to the new "OpenSSL Private Key" format, which is not compatible with the old PEM format.
    
    Ambari requires the old PEM format.
    
    1. enable PowerTools for Rocky Linux 8 (for snappy-devel, required to install HDFS DataNode)
    2. install jdk8 and use jdk8 for BigTop stack components
    
    install powertools without prompt
    
    Dockerfile initializes host ssh keys.
    
    Also, start ambari-agent process in ambari-server, ambari-agent-01, ambari-agent-02 containers.
    
    1. Make JDK17 the default so that Ambari build passes.
    2. Update per review comments.
    
    Open up Ambari server port 8440, 8441, 8670 for agents to connect to.

--- dev-support/docker/rocky8/build-containers.sh
+++ dev-support/docker/rocky8/build-containers.sh
@@ -20,7 +20,7 @@ if [[ -z $(docker ps -a --format "table {{.Names}}" | grep "ambari-rpm-build") ]
   docker run -it -d --name ambari-rpm-build --privileged=true -e "container=docker" \
     -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v $PWD/../../../:/opt/ambari/ \
     -w /opt/ambari \
-    ambari/develop:trunk-centos-7
+    ambari/develop:trunk-rocky-8
 else
   docker start ambari-rpm-build
 fi
@@ -33,52 +33,65 @@ echo -e "\033[32mCreating network ambari\033[0m"
 docker network create --driver bridge ambari
 
 echo -e "\033[32mCreating container ambari-server\033[0m"
-docker run -d -p 3306:3306 -p 5005:5005 -p 8080:8080 -p 8440:8440 --name ambari-server --hostname ambari-server --network ambari --privileged -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup:ro ambari/develop:trunk-centos-7 /usr/sbin/init
+docker run -d -p 5005:5005 -p 8080:8080 -p 8440:8440 -p 8441:8441 -p 8670:8670 --name ambari-server --hostname ambari-server --network ambari --privileged -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup:ro ambari/develop:trunk-rocky-8 /usr/sbin/init
 docker cp ../../../ambari-server/target/rpm/ambari-server/RPMS/x86_64/ambari-server*.rpm ambari-server:/root/ambari-server.rpm
 docker cp ../../../ambari-agent/target/rpm/ambari-agent/RPMS/x86_64/ambari-agent*.rpm ambari-server:/root/ambari-agent.rpm
 SERVER_PUB_KEY=`docker exec ambari-server /bin/cat /root/.ssh/id_rsa.pub`
 docker exec ambari-server bash -c "yum -y install /root/ambari-server.rpm"
 docker exec ambari-server bash -c "yum -y install /root/ambari-agent.rpm"
+docker exec ambari-server bash -c "chmod a+x /etc/init.d/ambari-agent"
+docker exec ambari-server bash -c "ambari-agent start"
 docker exec ambari-server bash -c "echo '$SERVER_PUB_KEY' > /root/.ssh/authorized_keys"
-docker exec ambari-server /bin/systemctl enable sshd
-docker exec ambari-server /bin/systemctl start sshd
+# Install a wrapper to workaround systemctl on docker
+docker exec ambari-server bash -c "wget https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py -O /usr/local/bin/systemctl"
+docker exec ambari-server bash -c "chmod a+x /usr/local/bin/systemctl"
+docker exec ambari-server systemctl enable sshd
+docker exec ambari-server systemctl start sshd
 
 echo -e "\033[32mSetting up mariadb-server\033[0m"
-docker exec ambari-server /bin/systemctl enable mariadb
-docker exec ambari-server /bin/systemctl start mariadb
-docker exec ambari-server bash -c "mysql -e \"UPDATE mysql.user SET Password = PASSWORD('root') WHERE User = 'root'\""
-docker exec ambari-server bash -c "mysql -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION\""
-docker exec ambari-server bash -c "mysql -e \"DROP USER ''@'localhost'\""
-docker exec ambari-server bash -c "mysql -e \"DROP USER ''@'ambari-server'\""
-docker exec ambari-server bash -c "mysql -e \"DROP DATABASE test\""
-docker exec ambari-server bash -c "mysql -e \"CREATE DATABASE ambari\""
-docker exec ambari-server bash -c "mysql --database=ambari -e  \"source /var/lib/ambari-server/resources/Ambari-DDL-MySQL-CREATE.sql\""
+docker run --name ambari-mariadb --network ambari -e  MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d docker.io/library/mariadb:10.4
+sleep 10
+docker exec ambari-mariadb bash -c "mysql -proot -e \"UPDATE mysql.user SET Password = PASSWORD('root') WHERE User = 'root'\""
+docker exec ambari-mariadb bash -c "mysql -proot -e \"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION\""
+docker exec ambari-mariadb bash -c "mysql -proot -e \"DROP USER ''@'localhost'\""
+docker exec ambari-mariadb bash -c "mysql -proot -e \"DROP USER ''@'ambari-server'\""
+docker exec ambari-mariadb bash -c "mysql -proot -e \"DROP DATABASE test\""
+docker exec ambari-mariadb bash -c "mysql -proot -e \"CREATE DATABASE ambari\""
+docker exec ambari-server bash -c "mysql -proot -h ambari-mariadb --database=ambari -e  \"source /var/lib/ambari-server/resources/Ambari-DDL-MySQL-CREATE.sql\""
 
-docker exec ambari-server bash -c "mysql -e \"CREATE USER 'hive'@'%' IDENTIFIED BY 'hive'\""
-docker exec ambari-server bash -c "mysql  -e \"GRANT ALL PRIVILEGES ON *.* TO 'hive'@'%' IDENTIFIED BY 'hive'\""
-docker exec ambari-server bash -c "mysql -e \"CREATE DATABASE hive\""
+docker exec ambari-mariadb bash -c "mysql -proot -e \"CREATE USER 'hive'@'%' IDENTIFIED BY 'hive'\""
+docker exec ambari-mariadb bash -c "mysql -proot -e \"GRANT ALL PRIVILEGES ON *.* TO 'hive'@'%' IDENTIFIED BY 'hive'\""
+docker exec ambari-mariadb bash -c "mysql -proot -e \"CREATE DATABASE hive\""
 
-docker exec ambari-server bash -c "mysql -e \"FLUSH PRIVILEGES\""
+docker exec ambari-mariadb bash -c "mysql -proot -e \"FLUSH PRIVILEGES\""
 
 echo -e "\033[32mSetting up ambari-server\033[0m"
 docker exec ambari-server bash -c "ambari-server setup --jdbc-db=mysql --jdbc-driver=/usr/share/java/mysql-connector-java.jar"
-docker exec ambari-server bash -c "ambari-server setup --java-home=/usr/lib/jvm/java --database=mysql --databasehost=localhost --databaseport=3306 --databasename=ambari --databaseusername=root --databasepassword=root -s"
+docker exec ambari-server bash -c "ambari-server setup --java-home=/usr/lib/jvm/java-1.8.0 --ambari-java-home=/usr/lib/jvm/java-17 --database=mysql --databasehost=ambari-mariadb --databaseport=3306 --databasename=ambari --databaseusername=root --databasepassword=root -s"
 
 echo -e "\033[32mCreating container ambari-agent-01\033[0m"
-docker run -d -p 9995:9995 --name ambari-agent-01 --hostname ambari-agent-01 --network ambari --privileged -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup:ro ambari/develop:trunk-centos-7 /usr/sbin/init
+docker run -d -p 9995:9995 --name ambari-agent-01 --hostname ambari-agent-01 --network ambari --privileged -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup:ro ambari/develop:trunk-rocky-8 /usr/sbin/init
 docker cp ../../../ambari-agent/target/rpm/ambari-agent/RPMS/x86_64/ambari-agent*.rpm ambari-agent-01:/root/ambari-agent.rpm
 docker exec ambari-agent-01 bash -c "yum -y install /root/ambari-agent.rpm"
+docker exec ambari-agent-01 bash -c "chmod a+x /etc/init.d/ambari-agent"
+docker exec ambari-agent-01 bash -c "ambari-agent start"
 docker exec ambari-agent-01 bash -c "echo '$SERVER_PUB_KEY' > /root/.ssh/authorized_keys"
-docker exec ambari-agent-01 /bin/systemctl enable sshd
-docker exec ambari-agent-01 /bin/systemctl start sshd
+docker exec ambari-agent-01 bash -c "wget https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py -O /usr/local/bin/systemctl"
+docker exec ambari-agent-01 bash -c "chmod a+x /usr/local/bin/systemctl"
+docker exec ambari-agent-01 systemctl enable sshd
+docker exec ambari-agent-01 systemctl start sshd
 
 echo -e "\033[32mCreating container ambari-agent-02\033[0m"
-docker run -d -p 8088:8088 --name ambari-agent-02 --hostname ambari-agent-02 --network ambari --privileged -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup:ro ambari/develop:trunk-centos-7 /usr/sbin/init
+docker run -d -p 8088:8088 --name ambari-agent-02 --hostname ambari-agent-02 --network ambari --privileged -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup:ro ambari/develop:trunk-rocky-8 /usr/sbin/init
 docker cp ../../../ambari-agent/target/rpm/ambari-agent/RPMS/x86_64/ambari-agent*.rpm ambari-agent-02:/root/ambari-agent.rpm
 docker exec ambari-agent-02 bash -c "yum -y install /root/ambari-agent.rpm"
+docker exec ambari-agent-02 bash -c "chmod a+x /etc/init.d/ambari-agent"
+docker exec ambari-agent-02 bash -c "ambari-agent start"
 docker exec ambari-agent-02 bash -c "echo '$SERVER_PUB_KEY' > /root/.ssh/authorized_keys"
-docker exec ambari-agent-02 /bin/systemctl enable sshd
-docker exec ambari-agent-02 /bin/systemctl start sshd
+docker exec ambari-agent-02 bash -c "wget https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py -O /usr/local/bin/systemctl"
+docker exec ambari-agent-02 bash -c "chmod a+x /usr/local/bin/systemctl"
+docker exec ambari-agent-02 systemctl enable sshd
+docker exec ambari-agent-02 systemctl start sshd
 
 echo -e "\033[32mConfiguring hosts file\033[0m"
 AMBARI_SERVER_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ambari-server`
@@ -98,15 +111,15 @@ docker cp ./krb5.conf ambari-agent-01:/etc/krb5.conf
 docker cp ./krb5.conf ambari-agent-02:/etc/krb5.conf
 docker exec ambari-server bash -c "echo -e \"admin\nadmin\" | kdb5_util create -s -r EXAMPLE.COM"
 docker exec ambari-server bash -c "echo -e \"admin\nadmin\" | kadmin.local -q \"addprinc admin/admin\""
-docker exec ambari-server bash -c "systemctl start krb5kdc"
-docker exec ambari-server bash -c "systemctl enable krb5kdc"
-docker exec ambari-server bash -c "systemctl start kadmin"
-docker exec ambari-server bash -c "systemctl enable kadmin"
+docker exec ambari-server bash -c "/usr/local/bin/systemctl start krb5kdc"
+docker exec ambari-server bash -c "/usr/local/bin/systemctl enable krb5kdc"
+docker exec ambari-server bash -c "/usr/local/bin/systemctl start kadmin"
+docker exec ambari-server bash -c "/usr/local/bin/systemctl enable kadmin"
 
 echo -e "\033[32mSynchronize Chrony\033[0m"
-docker exec ambari-server bash -c "systemctl enable chronyd; systemctl start chronyd; chronyc tracking"
-docker exec ambari-agent-01 bash -c "systemctl enable chronyd; systemctl start chronyd; chronyc tracking"
-docker exec ambari-agent-02 bash -c "systemctl enable chronyd; systemctl start chronyd; chronyc tracking"
+docker exec ambari-server bash -c "/usr/local/bin/systemctl enable chronyd; /usr/local/bin/systemctl start chronyd; chronyc tracking"
+docker exec ambari-agent-01 bash -c "/usr/local/bin/systemctl enable chronyd; /usr/local/bin/systemctl start chronyd; chronyc tracking"
+docker exec ambari-agent-02 bash -c "/usr/local/bin/systemctl enable chronyd; /usr/local/bin/systemctl start chronyd; chronyc tracking"
 
 docker exec ambari-server bash -c "ambari-server restart --debug"
 
