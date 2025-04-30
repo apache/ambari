469fb4dd0c32b5e17b532f04725f6fc2ebcb66e6
Peng Lu <lupeng_nwpu@qq.com>
2025-04-30 12:28:32 +0800

    AMBARI-26494: Ambari server failed to start due to database check failure (#4011)

--- ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
+++ ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
@@ -1512,8 +1512,8 @@ INSERT INTO permission_roleauthorization(permission_id, authorization_id)
 INSERT INTO adminprivilege (privilege_id, permission_id, resource_id, principal_id) VALUES
   (1, 1, 1, 1);
 
-INSERT INTO metainfo(metainfo_key, metainfo_value) VALUES
-('version','3.0.0');
+INSERT INTO metainfo (metainfo_key, metainfo_value) VALUES
+('version','${ambariSchemaVersion}');
 COMMIT;
 
 -- Quartz tables
