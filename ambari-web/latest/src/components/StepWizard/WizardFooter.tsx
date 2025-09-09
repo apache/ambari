ab5844a717d34a69639e2955f0d5456f363b5ca6
Himanshu Maurya <54660538+himanshumaurya09876@users.noreply.github.com>
2025-09-09 10:40:26 +0530

    AMBARI-26361 : Ambari Web React: Cluster Installation Wizard - Step2 (#4054)

---
+++ ambari-web/latest/src/components/StepWizard/WizardFooter.tsx
@@ -0,0 +1,99 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Button, Stack } from "react-bootstrap";
+import { ArrowLeft, ArrowRight } from "react-bootstrap-icons";
+import { Step } from "../../types/StepWizard";
+import Modal from "../Modal";
+import { useState } from "react";
+import "./styles.scss";
+
+interface PropTypes {
+  onBack: Function;
+  step: Step;
+  onNext: Function;
+  isNextEnabled: boolean;
+  isBackEnabled?: boolean;
+  onCancel?: () => void;
+}
+
+function WizardFooter({
+  onBack,
+  step,
+  onNext,
+  isNextEnabled,
+  onCancel = () => {},
+  isBackEnabled = true,
+}: PropTypes) {
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  return (
+    <div className="step-wizard-footer d-flex justify-content-between bg-white p-2">
+      <Modal
+        isOpen={showConfirmationModal}
+        onClose={() => {
+          setShowConfirmationModal(false);
+        }}
+        options={{}}
+        modalTitle="Confirmation"
+        modalBody="Are you sure?"
+        successCallback={() => {
+          onCancel();
+        }}
+      />
+      <Stack direction="horizontal">
+        {step.canGoBack ? (
+          <Button
+            variant="outline-secondary"
+            className="d-flex align-items-center ms-3 h-100"
+            onClick={() => {
+              onBack();
+            }}
+            disabled={!isBackEnabled}
+          >
+            <ArrowLeft />
+            <span className="ms-1">BACK</span>
+          </Button>
+        ) : null}
+        <Button
+          variant="outline-secondary"
+          className="d-flex align-items-center ms-3 h-100"
+          onClick={() => {
+            if (!onCancel) setShowConfirmationModal(true);
+            else {
+              onCancel();
+            }
+          }}
+        >
+          <span className="ms-1">CANCEL</span>
+        </Button>
+      </Stack>
+      <Button
+        variant="success"
+        className="me-3"
+        onClick={() => {
+          onNext();
+        }}
+        disabled={!isNextEnabled}
+      >
+        <span className="me-1">{step.nextLabel || "NEXT"}</span>
+        <ArrowRight />
+      </Button>
+    </div>
+  );
+}
+export default WizardFooter;
---
+++ ambari-web/latest/src/screens/ClusterWizard/index.tsx
@@ -0,0 +1,61 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createContext } from "react";
+import StepWizard from "../../components/StepWizard";
+import useStepWizard from "../../hooks/useStepWizard";
+// import { redirectToAdminView } from "../../Utils/adminViewRedirect";
+
+type PropTypes = {
+  Context: any;
+  Provider: any;
+  wizardSteps: Partial<{
+    label: string;
+    completed: boolean;
+    Component: any;
+    canGoBack: boolean;
+    isNextEnabled: boolean;
+    name: string;
+  }>[];
+  initialActiveStep?: number;
+};
+
+export const ContextWrapper = createContext<{ Context: any }>({ Context: {} });
+
+const ClusterCreationWizard = ({
+  Context,
+  Provider,
+  wizardSteps,
+  initialActiveStep = 0,
+}: PropTypes) => {
+  const stepWizardUtilities = useStepWizard(
+    wizardSteps,
+    initialActiveStep
+    // redirectToAdminView
+  );
+
+  return (
+    <ContextWrapper.Provider value={{ Context }}>
+      <Provider stepWizardUtilities={stepWizardUtilities}>
+        <StepWizard wizardUtilities={stepWizardUtilities} Context={Context} />
+      </Provider>
+    </ContextWrapper.Provider>
+  );
+};
+
+export default ClusterCreationWizard;
