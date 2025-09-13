8d2d1bafee2320ca06b555956f5a43295422b16c
Sandeep  Kumar <skumar34@visa.com>
2025-09-13 22:44:54 +0530

    AMBARI-26384: Ambari Web React: Disable kerberos (#4064)

---
+++ ambari-web/latest/src/screens/Kerberos/DisableKerberos.tsx
@@ -0,0 +1,188 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../store/context";
+import { map } from "lodash";
+import { translate } from "../../Utils/Utility";
+import { RequestApi } from "../../api/requestApi";
+import OperationsProgress from "../../components/OperationProgress";
+
+type disableKerberosProps = {
+  setDisableKerberosInProgress: any;
+};
+
+export default function disableKerberos({
+  setDisableKerberosInProgress,
+}: disableKerberosProps) {
+  const [completionStatus, setCompletionStatus] = useState(false);
+  const { clusterName, services } = useContext(AppContext);
+  // TODO: use useKDCSessionState whenever available
+
+  useEffect(() => {
+    setDisableKerberosInProgress(!completionStatus);
+  }, [completionStatus]);
+
+  const operations = [
+    {
+      id: "1",
+      label: "Start Zookeeper",
+      skippable: false,
+      context: "Start required services",
+      callback: async () => {
+        const zookeeperPayload = {
+          RequestInfo: {
+            context: "Start required services",
+            operation_level: {
+              level: "CLUSTER",
+              cluster_name: `${clusterName}`,
+            },
+          },
+          Body: {
+            ServiceInfo: {
+              state: "STARTED",
+            },
+          },
+        };
+        const params = "ServiceInfo/service_name.in(ZOOKEEPER)";
+        const requestData = await RequestApi.getServicesWithStatus(
+          clusterName,
+          zookeeperPayload,
+          params
+        );
+        return requestData;
+      },
+    },
+    {
+      id: "2",
+      label: "Stop Required Services",
+      skippable: false,
+      context: "Stop required services",
+      callback: async () => {
+        const servicesPayload = {
+          RequestInfo: {
+            context: "Stop required services",
+            operation_level: {
+              level: "CLUSTER",
+              cluster_name: `${clusterName}`,
+            },
+          },
+          Body: {
+            ServiceInfo: {
+              state: "INSTALLED",
+            },
+          },
+        };
+        const serviceNames = map(
+          services.filter(
+            (service) => service.ServiceInfo.service_name !== "ZOOKEEPER"
+          ),
+          "ServiceInfo.service_name"
+        ).join(",");
+
+        const params = `ServiceInfo/service_name.in(${serviceNames})`;
+        const requestData = await RequestApi.getServicesWithStatus(
+          clusterName,
+          servicesPayload,
+          params
+        );
+        return requestData;
+      },
+    },
+    {
+      id: "3",
+      label: "Unkerberize Cluster",
+      skippable: false,
+      context: "Unkerbize cluster",
+      callback: async () => {
+        const payload = {
+          Clusters: {
+            security_type: "NONE",
+          },
+        };
+        const requestData = await RequestApi.preparingOperations(
+          clusterName,
+          payload
+        );
+        return requestData;
+      },
+    },
+    {
+      id: "4",
+      label: "Remove Kerberos",
+      skippable: false,
+      context: "remove kerberos",
+      callback: async () => {
+        const payload = {
+          Clusters: {
+            security_type: "NONE",
+          },
+        };
+        const params = "manage_kerberos_identities=false";
+        const requestData = await RequestApi.preparingOperations(
+          clusterName,
+          payload,
+          params
+        );
+        return requestData;
+      },
+    },
+    {
+      id: "5",
+      label: "Start Services",
+      skippable: false,
+      context: "Start services",
+      callback: async () => {
+        const startAndTestServicesPayload = {
+          RequestInfo: {
+            context: "Start services",
+            operation_level: {
+              level: "CLUSTER",
+              cluster_name: `${clusterName}`,
+            },
+          },
+          Body: {
+            ServiceInfo: {
+              state: "STARTED",
+            },
+          },
+        };
+        const requestData = await RequestApi.startServices(
+          clusterName,
+          startAndTestServicesPayload,
+          ""
+        );
+        return requestData;
+      },
+    },
+  ];
+  return (
+    <>
+      {completionStatus && (
+        <div className="alert alert-success">
+          {translate("admin.security.disable.body.success.header")}
+        </div>
+      )}
+      <OperationsProgress
+        operations={operations as any}
+        title="Disable kerberos"
+        description="Disable Kerberos"
+        setCompletionStatus={setCompletionStatus}
+      />
+    </>
+  );
+}
