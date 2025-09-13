fd5000733647a7576b3446a430f8d96beb09888a
Himanshu Maurya <54660538+himanshumaurya09876@users.noreply.github.com>
2025-09-13 22:41:53 +0530

    AMBARI-26547 : Ambari Web React: Implement Rolling Restart Modal (#4063)

--- ambari-web/latest/src/screens/Hosts/utils.tsx
+++ ambari-web/latest/src/screens/Hosts/utils.tsx
@@ -18,6 +18,7 @@
 
 import { get } from "lodash";
 import { ComponentType } from "./enums";
+import { translate, translateWithVariables } from "../../Utils/Utility";
 
 export const sortBasedOnMasterSlave = (data: any, key: string) => {
   const masterComponents = data.filter(
@@ -30,4 +31,40 @@ export const sortBasedOnMasterSlave = (data: any, key: string) => {
     (component: any) => get(component, key, "") === ComponentType.CLIENT
   );
   return masterComponents.concat(slaveComponents).concat(clientComponents);
+};
+
+export const pluralize = (name: string) => {
+  return name + "s";
+};
+
+export const validateInteger = (
+  str: string | number,
+  min?: number,
+  max?: number
+): string => {
+  if (typeof str === "number") {
+    str = str.toString();
+  }
+  if (str === "" || str.trim().length < 1) {
+    return translate("number.validate.empty") as string;
+  }
+  str = str.trim();
+  const number = parseInt(str);
+  if (isNaN(number)) {
+    return translate("number.validate.notValidNumber") as string;
+  }
+  if (str.length !== number.toString().length) {
+    return translate("number.validate.notValidNumber") as string;
+  }
+  if (min && number < min) {
+    return translateWithVariables("number.validate.lessThanMinimum", {
+      "0": min.toString(),
+    }) as string;
+  }
+  if (max && number > max) {
+    return translateWithVariables("number.validate.moreThanMaximum", {
+      "0": max.toString(),
+    }) as string;
+  }
+  return "";
 };
