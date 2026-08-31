7b171d19b9ae680a2bb61d2bb17dd86ea388fcaf
jialiang <jialiangcaimd@gmail.com>
2026-08-31 10:59:26 +0800

    AMBARI-26641: Add BIGTOP component JDK overrides

--- ambari-agent/src/test/python/ambari_agent/TestConfigurationBuilder.py
+++ ambari-agent/src/test/python/ambari_agent/TestConfigurationBuilder.py
@@ -35,3 +35,90 @@ class TestConfigurationBuilder(TestCase):
 
     config_builder = ConfigurationBuilder(initializer_module)
     self.assertEqual("c6401.ambari.apache.org", config_builder.public_fqdn)
+
+  @patch("ambari_agent.ConfigurationBuilder.os.path.isfile", return_value=True)
+  def test_component_java_home_override_takes_precedence(self, isfile_mock):
+    command = self._command_with_overrides(
+      '{"SOLR": {"home": "/java/service", "version": 11}, '
+      '"SOLR_SERVER": {"home": "/java/component", "version": 17}}'
+    )
+
+    ConfigurationBuilder._apply_java_home_override(
+      command, "SOLR", "SOLR_SERVER"
+    )
+
+    self.assertEqual("/java/component", command["ambariLevelParams"]["java_home"])
+    self.assertEqual("17", command["ambariLevelParams"]["java_version"])
+    self.assertEqual(
+      "/java/ambari", command["ambariLevelParams"]["ambari_java_home"]
+    )
+    self.assertIsNone(command["ambariLevelParams"]["jdk_name"])
+    isfile_mock.assert_called_once_with("/java/component/bin/java")
+
+  @patch("ambari_agent.ConfigurationBuilder.os.path.isfile", return_value=True)
+  def test_service_java_home_override_is_used_as_fallback(self, isfile_mock):
+    command = self._command_with_overrides(
+      '{"solr": {"home": "/java/service", "version": "11"}}'
+    )
+
+    ConfigurationBuilder._apply_java_home_override(
+      command, "SOLR", "SOLR_SERVER"
+    )
+
+    self.assertEqual("/java/service", command["ambariLevelParams"]["java_home"])
+    self.assertEqual("11", command["ambariLevelParams"]["java_version"])
+    isfile_mock.assert_called_once_with("/java/service/bin/java")
+
+  def test_invalid_java_home_override_keeps_stack_default(self):
+    command = self._command_with_overrides(
+      '{"SOLR": {"home": "/java/service"}}'
+    )
+
+    ConfigurationBuilder._apply_java_home_override(
+      command, "SOLR", "SOLR_SERVER"
+    )
+
+    self.assertEqual("/java/stack", command["ambariLevelParams"]["java_home"])
+    self.assertEqual("11", command["ambariLevelParams"]["java_version"])
+
+  def test_invalid_java_home_override_version_type_keeps_stack_default(self):
+    command = self._command_with_overrides(
+      '{"SOLR": {"home": "/java/service", "version": true}}'
+    )
+
+    ConfigurationBuilder._apply_java_home_override(
+      command, "SOLR", "SOLR_SERVER"
+    )
+
+    self.assertEqual("/java/stack", command["ambariLevelParams"]["java_home"])
+    self.assertEqual("11", command["ambariLevelParams"]["java_version"])
+
+  @patch("ambari_agent.ConfigurationBuilder.os.path.isfile", return_value=False)
+  def test_missing_java_home_override_keeps_stack_default(self, isfile_mock):
+    command = self._command_with_overrides(
+      '{"SOLR": {"home": "/java/missing", "version": 17}}'
+    )
+
+    ConfigurationBuilder._apply_java_home_override(
+      command, "SOLR", "SOLR_SERVER"
+    )
+
+    self.assertEqual("/java/stack", command["ambariLevelParams"]["java_home"])
+    self.assertEqual("11", command["ambariLevelParams"]["java_version"])
+    isfile_mock.assert_called_once_with("/java/missing/bin/java")
+
+  @staticmethod
+  def _command_with_overrides(overrides):
+    return {
+      "ambariLevelParams": {
+        "ambari_java_home": "/java/ambari",
+        "ambari_java_version": "17",
+        "java_home": "/java/stack",
+        "java_version": "11",
+        "jdk_name": "stack-jdk.tar.gz",
+        "jce_name": "stack-jce.zip",
+      },
+      "configurations": {
+        "cluster-env": {"java_home_overrides": overrides},
+      },
+    }
